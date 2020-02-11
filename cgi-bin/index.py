0349e72df104506815b80d3c738624ecd5cf1021
customfolk <custom1485@gmail.com>
2020-02-11 13:34:25 +0900

    page79 (string match)

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python
 # coding: UTF-8
 # math関数で色々な数式を扱える
-string = 'spam'
-print string[0]
-print string[0:3] # 一個めから３つ抽出
-print string[0:-1] # 一個めから最後から１つ前までを抽出
-# ポリモーフィズム（オブジェクトの型によって演算子が加算だったり連結だったりに変化する　
-print string * 8
+import re
+match = re.match('Hello[ ]*(.*)world', 'Hello python world')
+print match.group(1)
