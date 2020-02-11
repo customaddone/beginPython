1c6f6e5816d282f1077392e373e0436dc1589145
customfolk <custom1485@gmail.com>
2020-02-11 12:32:53 +0900

    page 71(object type)

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 # coding: UTF-8
-import myfile
-# インポートできる
-print (myfile.title)
+# pythonもruby同様「全てがオブジェクト」
+print (1234, 3.1415) # 数値
+print ('spam', 'guard') # 文字列
+print ([1, 4, 'spam']) # リスト
+print ({'food': 'spam'}) # ディクショナリ
+print ((1, 'spam', 4)) # タプル
