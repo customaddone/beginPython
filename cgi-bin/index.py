7671ed3b5da087778645a14cfe025efa815a6864
customfolk <custom1485@gmail.com>
2020-02-11 18:27:39 +0900

    page 87 sort

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -2,16 +2,11 @@
 # coding: UTF-8
 # math関数で色々な数式を扱える
 
-D = { 'food': 'Spam', 'quantity': 4, 'color': 'pink' }
+D = { 'a': 1, 'b': 2, 'c': 3}
 
-print D['food']
+Ks = D.keys()
+print Ks
+Ks.sort() #sortは破壊的メソッド　戻り値はなし
+print Ks
 
-# quantityに対応する要素に１を加える
-print D['quantity'] + 1
-
-# 現在存在しないキーを指定して入れることもできる
-M = {}
-M['name'] = 'bob'
-M['job'] = 'dev'
-M['age'] = 40
-print M
+for key in Ks: print key, '=>', D[key]
