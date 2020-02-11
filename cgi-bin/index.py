36d1ae4549eb05e89d100a32da93f533434ac230
customfolk <custom1485@gmail.com>
2020-02-11 12:51:14 +0900

    page 73(math function)

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
 # coding: UTF-8
-# pythonもruby同様「全てがオブジェクト」
-print (1234, 3.1415) # 数値
-print ('spam', 'guard') # 文字列
-print ([1, 4, 'spam']) # リスト
-print ({'food': 'spam'}) # ディクショナリ
-print ((1, 'spam', 4)) # タプル
+# math関数で色々な数式を扱える
+import math
+print math.pi
+print math.sqrt(85)
