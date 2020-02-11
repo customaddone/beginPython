504ad891018c5985246b317163e4d9fe1085def0
customfolk <custom1485@gmail.com>
2020-02-11 19:45:32 +0900

    check key exist

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -2,11 +2,8 @@
 # coding: UTF-8
 # math関数で色々な数式を扱える
 
-D = { 'a': 1, 'b': 2, 'c': 3}
+D = { 'a': 1, 'b': 2, 'c': 3 }
 
-# さっきのがこれで一発でできる
-for key in sorted(D): print key, '=>', D[key]
-
-# 文字列もソートできる
-string = 'spam'
-for case in sorted(string): print case.upper()
+print D.has_key('f')
+if not D.has_key('f'):
+    print 'missing'
