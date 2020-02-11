d009dbfb4f367a2ff6fbfbf1d1b0684aa1f5158f
customfolk <custom1485@gmail.com>
2020-02-11 13:11:31 +0900

    page 75(string)

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 # coding: UTF-8
 # math関数で色々な数式を扱える
-import math
-print math.pi
-print math.sqrt(85)
+string = 'spam'
+print string[0]
+print string[0:3] # 一個めから３つ抽出
+print string[0:-1] # 一個めから最後から１つ前までを抽出
+# ポリモーフィズム（オブジェクトの型によって演算子が加算だったり連結だったりに変化する　
+print string * 8
