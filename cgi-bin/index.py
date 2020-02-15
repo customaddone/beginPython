31619cd508312f40a308ff68930a846bc8cb26de
customaddone <custom1485@docomo.ne.jp>
2020-02-15 23:49:03 +0900

    function

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -3,7 +3,13 @@
 # math関数で色々な数式を扱える
 
 # 0はfalseになる
-x = 10
-for i in range(10):
-    if (10 - i) % 2 == 0: continue
-    print(10 - i)
+x = 9
+y = 5
+if x < 10:
+    def times(x, y):
+        return x * y
+else:
+    def times(x, y):
+        return x + y
+
+print(times(x, y))
