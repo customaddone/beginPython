71200f3ea88081618577400be0c2354bd349ed2f
customaddone <custom1485@docomo.ne.jp>
2020-02-15 18:45:43 +0900

    page 153 replace

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -2,5 +2,11 @@
 # coding: UTF-8
 # math関数で色々な数式を扱える
 
-M = [1, 2, 3, 4, 5]
-print M[1:3]
+S = "spamy"
+S = S[:3] + 'xx' + S[3:]
+print(S)
+
+# replaceは使用頻度多いよ
+S = "spamy"
+S = S.replace('m', 'xx')
+print(S)
