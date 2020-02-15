5bde13b5e99d7a4fa25c796f03540d7393f4dd99
customaddone <custom1485@docomo.ne.jp>
2020-02-15 22:01:37 +0900

    multi nest

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -4,5 +4,14 @@
 
 while True:
     reply = input('Enter text:')
-    if reply == 'stop': break
-    print(reply)
+    if reply == 'stop':
+        break
+    elif not reply.isdigit():
+        print('Bad'* 8)
+    else:
+        num = int(reply)
+        if num < 20:
+            print('row')
+        else:
+            print(num ** 2)
+print('Bye')
