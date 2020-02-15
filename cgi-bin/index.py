408ec0759b8d6a2969eec5038f955da0b2b3a73a
customaddone <custom1485@docomo.ne.jp>
2020-02-15 23:52:38 +0900

    intersect

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -3,13 +3,14 @@
 # math関数で色々な数式を扱える
 
 # 0はfalseになる
-x = 9
-y = 5
-if x < 10:
-    def times(x, y):
-        return x * y
-else:
-    def times(x, y):
-        return x + y
 
-print(times(x, y))
+def intersect(sec1, sec2):
+    res = []
+    for x in sec1:
+        if x in sec2:
+            res.append(x)
+    return res
+
+sec1 = 'apple'
+sec2 = 'melon'
+print(intersect(sec1, sec2))
