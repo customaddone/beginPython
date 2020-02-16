275328127ceca22bc083fba5e4578d092b5c312d
customaddone <custom1485@docomo.ne.jp>
2020-02-16 17:22:49 +0900

    print split

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 # coding: UTF-8
 
-# https://atcoder.jp/contests/abc042/tasks/abc042_b
-# いろはちゃんは 長さLの文字列を個持っており、それぞれS1,S2,Nです。
-# それらの文字列を好きな順番で全て結合してできる文字列のうち、もっとも辞書順で小さいものを求めてください。
-a, b = map(int,input().split())
-s = sorted([input() for i in range(b)])
-# joinで配列の中に入っている文字列を全て結合
-print("".join(s))
+a = 1
+b = 2
+c = 3
+# 横並び　スペースで区切られる
+print(a, b, c)
+# sepの中の文字（改行）で区切られる
+print(a, b ,c, sep="\n")
