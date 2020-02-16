acbe3c73573783d8aa17ee56d1c85a014073d145
customaddone <custom1485@docomo.ne.jp>
2020-02-16 17:19:34 +0900

    ABC042

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 # coding: UTF-8
 
-list = [[1,2,3],[4,5,6]]
-for row in list:
-    for col in row:
-        print(col)
+# https://atcoder.jp/contests/abc042/tasks/abc042_b
+# いろはちゃんは 長さLの文字列を個持っており、それぞれS1,S2,Nです。
+# それらの文字列を好きな順番で全て結合してできる文字列のうち、もっとも辞書順で小さいものを求めてください。
+a, b = map(int,input().split())
+s = sorted([input() for i in range(b)])
+# joinで配列の中に入っている文字列を全て結合
+print("".join(s))
