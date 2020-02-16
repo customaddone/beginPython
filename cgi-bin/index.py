74d1a3b9e2be88c003c72636620b0e1b2083094d
customaddone <custom1485@docomo.ne.jp>
2020-02-16 16:46:25 +0900

    multi dimention

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python
 # coding: UTF-8
 
-# すぬけ君は、AtCoder s Contestという名前のコンテストを開こうとしています。ここで、sは長さ
-# １以上の文字列であり、１文字目は英大文字、２文字目以降は英語小文字です
-# すぬけ君は、このコンテストの略称を A x C に決めました。 ここで、x  は s  の先頭の英大文字です。
-# コンテストの名前が与えられるので、コンテストの略称を出力してください。
-
-list = input().split()
-abbr = ""
-for str in list:
-    abbr += str[0].upper()
-print(abbr)
+list = [[1,2,3],[4,5,6]]
+for row in list:
+    for col in row:
+        print(col)
