b3b1895b4f76a13796ad62f99e9b56781413e0e9
customaddone <custom1485@docomo.ne.jp>
2020-02-17 19:40:14 +0900

    List comprehension plus if

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python
 # coding: UTF-8
-from functools import reduce
-
-# https://atcoder.jp/contests/tenka1-2019-beginner/tasks/tenka1_2019_b
-# 英小文字からなる長さNの文字列Sと整数Kが与えられます。
-# SのK番目の文字と異なる文字全てを * で置き換えてできる文字列を出力してください。
-
-input()
-S = input()
-k = int(input())
-# endのおかげで横並びになる
-for s in S:print(s if s == S[k - 1] else "*", end="")
+col = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# ifを後ろに繋げることでfilterできる
+col2 = [row[0] for row in col if row[0] % 2 == 0]
+print(col2)
