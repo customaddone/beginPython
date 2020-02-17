c9f792c7108cb325e6ce43e0782a84444cef4aeb
customaddone <custom1485@docomo.ne.jp>
2020-02-17 21:25:07 +0900

    prime #2

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -4,8 +4,10 @@
 import math
 
 x = int(input())
-for i in range(2, math.floor(math.sqrt(x)) + 1):
-    if x % i == 0:
-        print(x, 'has factor', i)
-        break
-    print(x, 'is prime')
+def prime(num):
+    for i in range(2, math.floor(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+print(prime(x))
