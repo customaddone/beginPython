6cb3cd9264751b04dd3a81fd6357fb120c8f9d66
customaddone <custom1485@docomo.ne.jp>
2020-02-17 23:34:44 +0900

    ABC085B

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # coding: UTF-8
 # これ使ってswitchみたいな条件分岐
-# https://qiita.com/KoyanagiHitoshi/items/32dc42d8c5ee75339e54
+# https://atcoder.jp/contests/abc085/tasks/abc085_b
 
-# iは使わなくていい（ただ繰り返しをするためのi）()
-n, k, x, y = [int(input()) for i in range(4)]
-# ここのmaxの使い方覚える
-print(n * x + max(n - k, 0) * y)
+n = int(input())
+list = [int(input()) for i in range(n)]
+# setで重複したものを消す
+print(len(set(list)))
