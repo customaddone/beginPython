4da9b0540518a638b637203a185f844d7a1107ff
customaddone <custom1485@docomo.ne.jp>
2020-02-17 23:01:21 +0900

    ABC103A

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
 # coding: UTF-8
 # これ使ってswitchみたいな条件分岐
-# https://atcoder.jp/contests/abc069/tasks/abc069_b
-s = input()
-print(s[0] + str(len(s[1:-1])) + s[-1])
-
-a, *b, c = input()
-print(a + str(len(b)) + c)
+# https://atcoder.jp/contests/abc103/tasks/abc103_a
+*a, = map(int, input().split())
+print(a)
+# max()で配列の中の最大の要素を、minで最小の要素をとる
+print(max(a) - min(a))
