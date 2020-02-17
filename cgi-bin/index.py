993935c938b68d1b969a2988bb4f4cda2123fe66
customaddone <custom1485@docomo.ne.jp>
2020-02-17 20:46:30 +0900

    infinite loop of continue

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 # coding: UTF-8
 # これ使ってswitchみたいな条件分岐
-f = {'spam': 1.25,
-     'ham': 1.99,
-     'eggs': 0.99}
-
-print(f.get('spam', 'Bad choice!!'))
+x = 10
+while x:
+    # continueの前にxの増減を書かないと無限ループ
+    x -= 1
+    if x % 2 == 0:
+        continue
+    print(x)
