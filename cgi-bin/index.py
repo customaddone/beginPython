d4f246c09fbef4483f79cd0e82977efe5504f723
customaddone <custom1485@docomo.ne.jp>
2020-02-20 00:06:00 +0900

    index in array

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -2,10 +2,13 @@
 # coding: UTF-8
 # これ使ってswitchみたいな条件分岐
 
-N, T = map(int, input().split())
-m = 1001
+n = int(input())
+town = []
+people = []
 for i in range(n):
-    c, t = map(int, input().split())
-    # 順繰りにこれまでで一番小さかったmと比べる
-    if t <= T:m=min(m, c)
-print("TLE" if m==1001 else m)
+    t, p = input().split()
+    town.append(t)
+    people.append(int(p))
+# indexでpeopleの中での最大の数値のインデックスが取れる
+if max(people)>sum(people)/2:print(town[people.index(max(people))])
+else: print("atcoder")
