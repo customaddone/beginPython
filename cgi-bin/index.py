5b802de316bdd0c8434e93ca02aa319e66fda185
customaddone <custom1485@docomo.ne.jp>
2020-02-19 22:49:12 +0900

    input row

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -2,6 +2,6 @@
 # coding: UTF-8
 # これ使ってswitchみたいな条件分岐
 
-# sortedでもう配列にできる
-num = sorted(map(int, input().split()))
-print(num)
+# 下のinputの仕方覚える
+n, k, x, y = (int(input()) for i in range(4))
+print(k * x + max(n - k, 0) * y)
