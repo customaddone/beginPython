baf3f314410fb11d3f4e792130ed10c3c6e93f33
customaddone <custom1485@docomo.ne.jp>
2020-02-19 23:14:36 +0900

    array sum

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -2,6 +2,7 @@
 # coding: UTF-8
 # これ使ってswitchみたいな条件分岐
 
-# 下のinputの仕方覚える
-n, k, x, y = (int(input()) for i in range(4))
-print(k * x + max(n - k, 0) * y)
+n = [int(input()) for _ in range(int(input()))]
+# sumで配列内の数字を全て足す
+# intで整数にしよう
+print(sum(n) - int(max(n)/2))
