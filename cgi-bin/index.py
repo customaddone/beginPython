dedcfff359f05467ded1521f0881d01a0860715e
customaddone <custom1485@docomo.ne.jp>
2020-02-19 22:14:30 +0900

    sorted int

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 # coding: UTF-8
 # これ使ってswitchみたいな条件分岐
-# https://atcoder.jp/contests/abc085/tasks/abc085_b
 
-n = int(input())
-list = [int(input()) for i in range(n)]
-# setで重複したものを消す
-print(len(set(list)))
+# sortedでもう配列にできる
+num = sorted(map(int, input().split()))
+print(num)
