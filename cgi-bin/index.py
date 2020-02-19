c60004c16c288963949143662b998e23666d926c
customaddone <custom1485@docomo.ne.jp>
2020-02-19 23:45:17 +0900

    min in row

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -2,8 +2,10 @@
 # coding: UTF-8
 # これ使ってswitchみたいな条件分岐
 
-# 「以上」の書き方を覚えよう
-a, b = map(int, input().split())
-n = [int(input()) for i in range(int(input()))]
-for i in n:
-    print(a - i if i < a else 0 if a <= i <= b else -1)
+N, T = map(int, input().split())
+m = 1001
+for i in range(n):
+    c, t = map(int, input().split())
+    # 順繰りにこれまでで一番小さかったmと比べる
+    if t <= T:m=min(m, c)
+print("TLE" if m==1001 else m)
