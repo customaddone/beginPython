a6c9d6d095b1b656f09b0faa31da7a5149d7a42d
customaddone <custom1485@docomo.ne.jp>
2020-03-21 20:11:21 +0900

    for use of index

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,6 +1,3 @@
-n, t = map(int, input().split())
-lowest = 1001
-for i in range(n):
-    c1, t1 = map(int, input().split())
-    if t1<=t:lowest=min(lowest,c1)
-print("TLE" if lowest==1001 else lowest)
+list = [1, 2, 3, 4, 5]
+print(max(list))
+print(list.index(max(list)))
