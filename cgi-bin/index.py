9d2b2c3eb8642daa7095e0976092f687c07cc565
customaddone <custom1485@docomo.ne.jp>
2020-03-22 06:44:58 +0900

    ABC138 C

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,3 +1,20 @@
-# cが√cの小数点以下を切り捨てた数の二乗と一致するか
-c = int(input().replace(" ",""))
-print("Yes" if c==int(C**.5)**2 else "No")
+# 1 pop(0)が処理される
+# 2 pop(0)が処理される（一番最初から見て２つめが処理される）
+# appendが処理される
+n = int(input())
+*a, = map(int, input().split())
+for _ in range(n - 1):
+    a = sorted(a)
+    x = a.pop(0)
+    y = a.pop(0)
+    a.append((x + y)/2)
+print(a[0])
+
+# 模範解答
+N=int(input())
+v=sorted(map(int,input().split()))
+# vの最小値
+value=v[0]
+for i in range(1,N):
+    value=(value+v[i])/2
+print(value)
