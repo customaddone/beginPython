2aa176fb3f379f3e3ba975fe6a618ecd6b314a7d
customaddone <custom1485@docomo.ne.jp>
2020-03-21 19:56:42 +0900

    ABC121 B

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,16 +1,8 @@
-#nが人数、mが商品の種類
-n, m = map(int, input().split())
-counter = [0] * m
+n, m, c = map(int, input().split())
+*b, = map(int, input().split())
+counter = 0
 for i in range(n):
-    a, *b, = map(int, input().split())
-    for i in b:
-        counter[i - 1] += 1
-print(sum([i == n for i in counter]))
-
-#模範解答
-n,m=map(int,input().split())
-S=set(range(1,m+1))
-for i in range(n):
-    K,*A=map(int,input().split())
-    S&=set(A)
-print(len(S))
+    *a, = map(int, input().split())
+    value = sum([a * b for a, b in zip(a,b)]) + c
+    counter += (value > 0)
+print(counter)
