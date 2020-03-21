874ff79d4c9982974e7940dee31eefbecc023274
customaddone <custom1485@docomo.ne.jp>
2020-03-21 16:46:26 +0900

    Otoshidama

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python
 # coding: UTF-8
 # これ使ってswitchみたいな条件分岐
+N, Y = map(int, input().split())
+answer = -1, -1, -1
 
-n = int(input())
-#パターン１
-#list = [[list(map(int, input().split()))] for i in range(n)]
-
-
-#パターン２
-list = [[int(j) for j in input().split()] for i in range(n)]
-
-#パターン３
-#list = []
-#for i in range(n):
-#    list.append([int(i) for i in input().split()])
-
-print(list)
+for x in range(N + 1):
+    for y in range(N - x + 1):
+        z = N - x - y
+        if 0 <= z <= 2000 and 10000 * x + 5000 * y + 1000 * z == Y:
+            answer = x, y, z
+            break
+    else:
+        continue
+    break
+print(*answer)
