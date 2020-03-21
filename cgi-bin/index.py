33d88ad04d76e0a3a70a66d8bc5f1228dc860838
customaddone <custom1485@docomo.ne.jp>
2020-03-22 07:23:48 +0900

    ABC095 B

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,14 +1,3 @@
-n = int(input())
-*a, = map(int, input().split())
-fraction = []
-denominator = 1
-for i in range(n):
-    denominator *= a[i]
-for i in range(n):
-    fraction.append(denominator / a[i])
-print(1/(sum(fraction)/denominator))
-
-# 模範解答
-input()
-A = map(int, input().split())
-print(1/sum(1/a for a in A))
+n, x = map(int, input().split())
+donuts = [int(input()) for _ in range(n)]
+print((x - sum(donuts)) // min(donuts) + len(donuts))
