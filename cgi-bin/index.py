05cb5a2faa94c85a60efd2caa2b333dc46de95a8
customaddone <custom1485@docomo.ne.jp>
2020-03-21 19:04:46 +0900

    ABC094 B

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python
 # coding: UTF-8
-# abs 絶対値
+# int(i)<xを満たすものの個数
 
-n, q = map(int, input().split())
-a = [0] * n
-for i in range(q):
-    l, r, t = map(int, input().split())
-    for i in range(l, r+1):a[i-1]=t
-print(*a, sep="\n")
+n, m, x = map(int, input().split())
+s = sum(int(i)<x for i in input().split())
+print(s)
