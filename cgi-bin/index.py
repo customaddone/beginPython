fb45015e7df412f3ae649f3e9498fb5fc1932ac8
customaddone <custom1485@docomo.ne.jp>
2020-03-21 18:06:52 +0900

    ABC123A

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python
 # coding: UTF-8
-# elseブロックの中にcontinueが置かれると、continue以降の外側の処理が全てスキップされます
-# 内側ループでbreakに該当した場合はcontinue効かない(breakされる)
-N, Y = map(int, input().split())
-answer = -1, -1, -1
+# abs 絶対値
 
-for x in range(N + 1):
-    for y in range(N - x + 1):
-        z = N - x - y
-        if 0 <= z <= 2000 and 10000 * x + 5000 * y + 1000 * z == Y:
-            answer = x, y, z
-            break
-    else:
-        continue
-    break
-print(*answer)
+a, b, c, d, e, k = [int(input()) for i in range(6)]
+print("Yay!" if abs(a-e)<=k else ":(")
