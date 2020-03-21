b7078876f642768c93e6b33b9f154e89c64c9073
customaddone <custom1485@docomo.ne.jp>
2020-03-22 07:06:46 +0900

    ABC138 B

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,20 +1,14 @@
-# 1 pop(0)が処理される
-# 2 pop(0)が処理される（一番最初から見て２つめが処理される）
-# appendが処理される
 n = int(input())
 *a, = map(int, input().split())
-for _ in range(n - 1):
-    a = sorted(a)
-    x = a.pop(0)
-    y = a.pop(0)
-    a.append((x + y)/2)
-print(a[0])
+fraction = []
+denominator = 1
+for i in range(n):
+    denominator *= a[i]
+for i in range(n):
+    fraction.append(denominator / a[i])
+print(1/(sum(fraction)/denominator))
 
 # 模範解答
-N=int(input())
-v=sorted(map(int,input().split()))
-# vの最小値
-value=v[0]
-for i in range(1,N):
-    value=(value+v[i])/2
-print(value)
+input()
+A = map(int, input().split())
+print(1/sum(1/a for a in A))
