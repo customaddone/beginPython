dd5f1096bb731a7a0ebff0f72f259a8e0153bb17
customaddone <custom1485@docomo.ne.jp>
2020-03-21 19:37:14 +0900

    ABC118 B

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,7 +1,16 @@
-#!/usr/bin/python
-# coding: UTF-8
-# int(i)<xを満たすものの個数
+#nが人数、mが商品の種類
+n, m = map(int, input().split())
+counter = [0] * m
+for i in range(n):
+    a, *b, = map(int, input().split())
+    for i in b:
+        counter[i - 1] += 1
+print(sum([i == n for i in counter]))
 
-n, m, x = map(int, input().split())
-s = sum(int(i)<x for i in input().split())
-print(s)
+#模範解答
+n,m=map(int,input().split())
+S=set(range(1,m+1))
+for i in range(n):
+    K,*A=map(int,input().split())
+    S&=set(A)
+print(len(S))
