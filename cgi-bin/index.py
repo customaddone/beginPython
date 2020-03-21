ea57a3710aaf5a7e307974d017981c257ea37741
customaddone <custom1485@docomo.ne.jp>
2020-03-21 21:35:28 +0900

    math factorial

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,11 +1,2 @@
-#「P内」のPの最大値のインデックス
-n = int(input())
-S = []
-P = []
-name = 'atcoder'
-for i in range(n):
-    s, p = input().split()
-    S.append(s)
-    P.append(int(p))
-if max(P)>(sum(P)/2):name=S[P.index(max(P))]
-print(name)
+import math
+print(math.factorial(10))
