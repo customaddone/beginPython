278295433d418c667d200939358ff542c846e0be
customaddone <custom1485@docomo.ne.jp>
2020-03-21 21:05:57 +0900

    ABC033 B

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,3 +1,11 @@
-list = [1, 2, 3, 4, 5]
-print(max(list))
-print(list.index(max(list)))
+#「P内」のPの最大値のインデックス
+n = int(input())
+S = []
+P = []
+name = 'atcoder'
+for i in range(n):
+    s, p = input().split()
+    S.append(s)
+    P.append(int(p))
+if max(P)>(sum(P)/2):name=S[P.index(max(P))]
+print(name)
