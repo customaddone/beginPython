bc13dec012e0ef9b398387d53752150beb9e09c8
customaddone <custom1485@docomo.ne.jp>
2020-03-22 08:57:36 +0900

    tenary

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,3 +1,16 @@
-n, x = map(int, input().split())
-donuts = [int(input()) for _ in range(n)]
-print((x - sum(donuts)) // min(donuts) + len(donuts))
+# 3進数に変換
+n = int(input())
+result = ''
+
+def tenary(n):
+    if n == 0:
+        return '3'
+    elif n == 1:
+        return '5'
+    else:
+        return '7'
+while n >= 3:
+    # １桁目、２桁目...
+    result = str(n % 3) + result
+    n = n // 3
+result = str(n) +result
