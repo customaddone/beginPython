6e203c830b9899c2f55d1e67363fb2b1b8337b2e
customaddone <custom1485@docomo.ne.jp>
2020-03-22 11:33:13 +0900

    ABC099 C

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,5 +1,19 @@
-# count
-import math
-n = int(input())
-bug = list(map(int, input().split()))
-print(math.ceil(sum(bug)/(n - bug.count(0))))
+#　割る9の取り分、割る6の取り分をi, N - iで決めて1 <= i <= N + 1の範囲で動かす
+N = int(input())
+ans = N
+# 0, 1, 2, 3... 127
+for i in range(N + 1):
+    cnt = 0
+    # t = 1
+    t = i
+    while t > 0:
+        # tを6で割った余りをcntに足す
+        cnt += t % 6
+        # tを６で割ったときの商を求める→余りを足す...
+        t //= 6
+    j = N - i
+    while j>0:
+        cnt+=j%9
+        j//=9
+    ans = min(ans,cnt)
+print(ans)
