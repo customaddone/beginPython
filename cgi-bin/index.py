bb248b3c42f01b81ab3851bc43464a9e428db7a8
customaddone <custom1485@docomo.ne.jp>
2020-03-22 09:36:32 +0900

    ABC015 B

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,17 +1,5 @@
-# 全員をスタート地点から都市１に移動させるのにn // time[0] + 1分かかる
-# 全員をスタート地点から都市２に移動させるのに上記 + (n % time[i]) // time[1]分かかる
-# ...
-n = int(input())
-time = [int(input()) for _ in range(5)]
-result = 0
-for i in range(5):
-    result += (n // time[i])
-    result += 1
-    n = n % time[i]
-print(result)
-
-# 模範解答 最初っからminで
+# count
 import math
-n=int(input())
-l=[int(input()) for i in range(5)]
-print(math.ceil(n/min(l))+4)
+n = int(input())
+bug = list(map(int, input().split()))
+print(math.ceil(sum(bug)/(n - bug.count(0))))
