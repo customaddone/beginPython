1c6caffe322c13d1c2b76e4a52946df941d5e1ac
customaddone <custom1485@docomo.ne.jp>
2020-03-22 20:26:31 +0900

    chr & ord

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,3 +1,7 @@
-# any いずれかの要素がtrueであればtrueを返す
-a, b, c = map(int, input().split())
-print("Yes" if any((a * i) % b == c for i in range(1, b + 1)) else "No")
+N=int(input())
+S=input()
+# ord 文字 →　アスキーコード
+# chr アスキーコード → 文字
+# 大文字のAは６５番、小文字のaは９７番
+# Z → Aへとループさせるために%26する
+print("".join(chr(65+(ord(s)-65+N)%26) for s in S))
