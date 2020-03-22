1818daa55944b4cff6401450886286ce98729f37
customaddone <custom1485@docomo.ne.jp>
2020-03-22 17:14:51 +0900

    ABC133 C

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,19 +1,8 @@
-#　割る9の取り分、割る6の取り分をi, N - iで決めて1 <= i <= N + 1の範囲で動かす
-N = int(input())
-ans = N
-# 0, 1, 2, 3... 127
-for i in range(N + 1):
-    cnt = 0
-    # t = 1
-    t = i
-    while t > 0:
-        # tを6で割った余りをcntに足す
-        cnt += t % 6
-        # tを６で割ったときの商を求める→余りを足す...
-        t //= 6
-    j = N - i
-    while j>0:
-        cnt+=j%9
-        j//=9
-    ans = min(ans,cnt)
+l, r = map(int, input().split())
+ans = 0
+min2019 = 2019 * (l // 2019)
+max2019 = 2019 * ((r // 2019) + 1)
+if r < (min2019 + 2019):
+    absvalue = min((l - min2019), (max2019 - r))
+    ans = absvalue * (absvalue + 1)
 print(ans)
