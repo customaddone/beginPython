a93295ccd011db4a2be27de7ca393fd0b5a8ba80
customaddone <custom1485@docomo.ne.jp>
2020-03-22 20:19:00 +0900

    any

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,8 +1,3 @@
-l, r = map(int, input().split())
-ans = 0
-min2019 = 2019 * (l // 2019)
-max2019 = 2019 * ((r // 2019) + 1)
-if r < (min2019 + 2019):
-    absvalue = min((l - min2019), (max2019 - r))
-    ans = absvalue * (absvalue + 1)
-print(ans)
+# any いずれかの要素がtrueであればtrueを返す
+a, b, c = map(int, input().split())
+print("Yes" if any((a * i) % b == c for i in range(1, b + 1)) else "No")
