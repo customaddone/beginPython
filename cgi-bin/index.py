956759bf36f513ac46ba4adf662cefa9d5391660
customaddone <custom1485@docomo.ne.jp>
2020-03-25 20:34:35 +0900

    ABC121 B

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,15 +1,7 @@
-n = int(input())
-an = 0
-a3, a2, a1 = 0, 0, 1
-
-for _ in range(n - 3):
-    an = a3 + a2 + a1
-    a3, a2, a1 = a2, a1, an
-print(an % 10007)
-
-# 模範解答 for文の中で10007の余りを求めとく
-n=int(input())
-a,b,c=0,0,1
-for i in range(n-1):
-    a,b,c=b,c,(a+b+c)%10007
-print(a)
+n, m, c = map(int, input().split())
+b = list(map(int, input().split()))
+counter = 0
+for i in range(n):
+    a = list(map(int, input().split()))
+    counter += ((sum([x * y for x, y in zip(a, b)]) + c) > 0)
+print(counter)
