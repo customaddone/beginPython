15ef7d2d1523c88ca7984efff958b2b05aef83bf
customaddone <custom1485@docomo.ne.jp>
2020-03-25 22:38:05 +0900

    ABC122 B - ATCoder

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,12 +1,11 @@
-s = list(input())
-t = input()
-ans = 'No'
-for _ in range(len(s)):
-    s.insert(0, s.pop())
-    if ''.join(s) == t:
-        ans = 'Yes'
-print(ans)
-
-# 模範解答
-# 文字列を二つ繋げてその中に目的の文字列があるか
-print("Yes" if input() in input()*2 else "No")
+S = input()
+m = 0
+count = 0
+for s in S:
+    if s in "A": count += 1
+    elif s in "C":count += 1
+    elif s in "G":count += 1
+    elif s in "T":count += 1
+    else:count=0
+    m = max(m, count)
+print(m)
