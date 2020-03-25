4eacaf8591fef217d22691b685f7cd3af95294ee
customaddone <custom1485@docomo.ne.jp>
2020-03-25 22:53:14 +0900

    ARC0ARC010 A 名刺交換

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,11 +1,11 @@
-S = input()
-m = 0
-count = 0
-for s in S:
-    if s in "A": count += 1
-    elif s in "C":count += 1
-    elif s in "G":count += 1
-    elif s in "T":count += 1
-    else:count=0
-    m = max(m, count)
-print(m)
+n, m, a, b = map(int, input().split())
+ans = 'complete'
+for i in range(m):
+    if n <= a:
+        n += b
+    n -= int(input())
+    if n < 0:
+        print(i + 1)
+        break
+else:
+    print("complete")
