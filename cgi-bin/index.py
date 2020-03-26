b336019e4450bd0a7ff4111bbebb8b88da11a48c
customaddone <custom1485@docomo.ne.jp>
2020-03-26 22:46:40 +0900

    ABC087B - Coins

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,10 +1,12 @@
-input()
-list = list(map(int, input().split()))
+a = int(input())
+b = int(input())
+c = int(input())
+x = int(input())
 counter = 0
-while 1:
-    if all([i % 2 == 0 for i in list]):
-        list = [int(i / 2) for i in list]
-        counter += 1
-    else:
-        break
+for l in range(a + 1):
+    for m in range(b + 1):
+        if 500 * l + 100 * m > x:
+            break
+        if 500 * l + 100 * m + 50 * c >= x:
+            counter += 1
 print(counter)
