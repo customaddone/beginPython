09a37a3eb9a1253242fe623851c73fbe245f22ef
customaddone <custom1485@docomo.ne.jp>
2020-03-27 19:07:53 +0900

    ABC085C - Otoshidama

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,6 +1,18 @@
-n, a, b = map(int, input().split())
-ans = 0
-for n in range(1, n + 1):
-    if a <= sum([int(i) for i in list(str(n))]) <= b:
-        ans += n
-print(ans)
+k10, k5, k1 = 0, 0, 0
+n, y = map(int, input().split())
+for l in range((y // 10000) + 1):
+    if y - l * 10000 > (n - l) * 5000:
+        continue
+    for m in range(((y - l * 10000) // 5000) + 1):
+        if 5000 * m + (n - l - m) * 1000 == y - l * 10000:
+            k10, k5, k1 = l, m, (n - l - m)
+            break
+    else:
+        continue
+    break
+if [k10, k5, k1] == [0, 0, 0]:
+    print('-1 -1 -1')
+else:
+    print(k10, end=' ')
+    print(k5, end=' ')
+    print(k1)
