fd74503c09d4ade5510753c9ae2f0cc40079a99b
customaddone <custom1485@docomo.ne.jp>
2020-03-27 21:47:54 +0900

    ABC068 B - Break Number

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,3 +1,7 @@
-# print(*[])で配列内の文字全てをプリント
-k, x = map(int, input().split())
-print(*[x for x in range(x - k + 1, x + k - 1)])
+#2の累乗でもっとも大きいものをansに
+#2,4,8と試していく
+n = int(input())
+ans = 1
+for i in range(7):
+    if 2**i<=n:ans=a**i
+print(ans)
