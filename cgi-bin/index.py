ab653ae70e85b3fb51949c87d0f587c22f859a88
customaddone <custom1485@docomo.ne.jp>
2020-03-27 20:28:50 +0900

    ABC086C - Traveling

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,18 +1,18 @@
-k10, k5, k1 = 0, 0, 0
-n, y = map(int, input().split())
-for l in range((y // 10000) + 1):
-    if y - l * 10000 > (n - l) * 5000:
-        continue
-    for m in range(((y - l * 10000) // 5000) + 1):
-        if 5000 * m + (n - l - m) * 1000 == y - l * 10000:
-            k10, k5, k1 = l, m, (n - l - m)
-            break
-    else:
-        continue
-    break
-if [k10, k5, k1] == [0, 0, 0]:
-    print('-1 -1 -1')
-else:
-    print(k10, end=' ')
-    print(k5, end=' ')
-    print(k1)
+n = int(input())
+time = []
+x_axis = []
+y_axis = []
+ans = 'Yes'
+for _ in range(n):
+    t, x, y = map(int, input().split())
+    time.append(t)
+    x_axis.append(x)
+    y_axis.append(y)
+if (x_axis[0] + y_axis[0] - time[0]) % 2 != 0 or time[0] < x_axis[0] + y_axis[0]:
+    ans = 'No'
+for i in range(n - 1):
+    fortime = abs(time[i + 1] - time[i])
+    distance = abs(x_axis[i + 1] - x_axis[i]) + abs(y_axis[i + 1] - y_axis[i])
+    if (fortime - distance) % 2 != 0 or fortime < distance:
+        ans = 'No'
+print(ans)
