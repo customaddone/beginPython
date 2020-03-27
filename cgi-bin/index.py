005d7ab5c8d43659b0b43a8ff96e61d956166d9b
customaddone <custom1485@docomo.ne.jp>
2020-03-27 20:49:18 +0900

    * [ for i in ~]

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,18 +1,3 @@
-n = int(input())
-time = []
-x_axis = []
-y_axis = []
-ans = 'Yes'
-for _ in range(n):
-    t, x, y = map(int, input().split())
-    time.append(t)
-    x_axis.append(x)
-    y_axis.append(y)
-if (x_axis[0] + y_axis[0] - time[0]) % 2 != 0 or time[0] < x_axis[0] + y_axis[0]:
-    ans = 'No'
-for i in range(n - 1):
-    fortime = abs(time[i + 1] - time[i])
-    distance = abs(x_axis[i + 1] - x_axis[i]) + abs(y_axis[i + 1] - y_axis[i])
-    if (fortime - distance) % 2 != 0 or fortime < distance:
-        ans = 'No'
-print(ans)
+# print(*[])で配列内の文字全てをプリント
+k, x = map(int, input().split())
+print(*[x for x in range(x - k + 1, x + k - 1)])
