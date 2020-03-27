c08d1de6a0c5fac1533e694151617e8ba0cc4c8c
customaddone <custom1485@docomo.ne.jp>
2020-03-28 07:39:36 +0900

    while not

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,8 +1,9 @@
-# if文の条件ををwhileの後ろにつける
-input()
-a = list(map(int, input().split()))
-counter = 0
-while all([(i % 2 == 0) for i in a]):
-    a = [int(i / 2) for i in a]
-    counter += 1
-print(counter)
+s = int(input())
+list = []
+while not(s in list):
+    list.append(s)
+    if s % 2 == 0:
+        s /= 2
+    else:
+        s = 3 * s + 1
+print(len(list) + 1)
