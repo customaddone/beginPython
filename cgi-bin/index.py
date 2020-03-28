8494f8b00b1323d028e3f331df0ba8530c990ae6
customaddone <custom1485@docomo.ne.jp>
2020-03-28 22:54:12 +0900

    ABD160 D Line++

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,7 +1,14 @@
-# {}でディクショナリ型に
-# S[s]が存在する限り
-S = {c:list(input()) for c in "abc"}
-s = "a"
-while S[s]:
-    s = S[s].pop(0)
-print(s.upper())
+n, x, y = map(int, input().split())
+anslist = [0] * n
+list = []
+for m in range(1, n + 1):
+    for n in range(m, n + 1):
+        if m != n:
+            list.append([m, n])
+while len(list) != 0:
+    dis = list[0][1] - list[0][0]
+    warpdis = (abs(x - list[0][0])) + (abs(list[0][1] - y)) + 1
+    anslist[min(dis, warpdis)] += 1
+    list.pop(0)
+for i in range(1, n):
+    print(anslist[i])
