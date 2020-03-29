a4a19a831f9190c69d96f96cbe863395c7bf23a1
customaddone <custom1485@docomo.ne.jp>
2020-03-29 11:45:27 +0900

    enumerate

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,18 +1,3 @@
-n = int(input())
-t, a = map(int, input().split())
-h = list(map(int, input().split()))
-lat = 0
-abstem = 1000
-for i in range(n):
-    avetem = t - h[i] * 0.006
-    if abs(avetem - a) <= abstem:
-        abstem = abs(a - avetem)
-        lat = h[i]
-print(h.index(lat) + 1)
-
-# 模範解答
-n=int(input())
-t,a=map(int,input().split())
-h=list(map(int,input().split()))
-z = [abs(t - i * 0.006 - a) for i in h]
-print(z.index(min(z)) + 1)
+list = ["a", "b", "c"]
+for number, alphabet in enumerate(list):
+    print(number, alphabet)
