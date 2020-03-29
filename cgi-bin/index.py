e2b7d0d0fe80fa0ed64fdb2211a56072cb93113d
customaddone <custom1485@docomo.ne.jp>
2020-03-29 19:00:55 +0900

    深さ優先探索

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,12 +1,20 @@
-# n,n-1,n-2と小さく再帰し1以下になったら止まる
-# fib(n)の数をメモっておくと楽になる
-memo = [0] * 100
+# iまででsumを作る
+#
+def dfs(i, sum):
+    if i == n:
+        return sum == k
 
-def fib(n):
-    if n <= 1:
-        return n;
-    if (memo[n] != 0):
-        return memo[n]
-    memo[n] = fib(n - 1) + fib(n - 2)
-    return memo[n]
-print(fib(40))
+    if dfs(i + 1, sum):
+        return True
+
+    if dfs(i + 1, sum + a[i]):
+        return True
+
+    return False
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+
+if dfs(0, 0):
+    print("Yes")
+else:
+    print("No")
