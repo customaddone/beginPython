2750eebbddde690e7d5530c884faa84cf2190a6a
customaddone <custom1485@docomo.ne.jp>
2020-03-29 15:16:49 +0900

    bisect.bisect_left

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,13 +1,17 @@
-l = int(input())
-n = int(input())
-x = list(map(int, input().split()))
-mintime = 0
-maxtime = 0
-for i in range(n):
-    individual = min(x[i], l - x[i])
-    mintime = max(mintime, individual)
-for i in range(n):
-    individual = max(x[i], l - x[i])
-    maxtime = max(maxtime, individual)
-print(mintime)
-print(maxtime)
+# N**iの数が多くなりすぎる時は、listを思いっきり伸ばして二分探索すればOK
+import bisect
+import time
+from decimal import Decimal
+
+t1 = time.time()
+a = [x for x in range(2000000)]
+
+t1 = time.time()
+b = a.index(888888)
+t2 = time.time()
+print(Decimal(t2 - t1))
+
+t3 = time.time()
+b = bisect.bisect_left(a, 888888)
+t4 = time.time()
+print(Decimal(t4 - t3))
