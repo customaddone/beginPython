16562d5a6fc9bda68a953af00f59bf51de52600c
customaddone <custom1485@docomo.ne.jp>
2020-03-29 10:10:15 +0900

    greatest common divisor(Euclidean Algorithm)

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,14 +1,21 @@
-n, x, y = map(int, input().split())
-anslist = [0] * n
-list = []
-for m in range(1, n + 1):
-    for n in range(m, n + 1):
-        if m != n:
-            list.append([m, n])
-while len(list) != 0:
-    dis = list[0][1] - list[0][0]
-    warpdis = (abs(x - list[0][0])) + (abs(list[0][1] - y)) + 1
-    anslist[min(dis, warpdis)] += 1
-    list.pop(0)
-for i in range(1, n):
-    print(anslist[i])
+#import fractionsしてgcd関数を呼び出す
+#python3.4.3ではmathではなくfractionsに関数がある
+import fractions
+a, b = map(int, input().split())
+print(int(a * b / fractions.gcd(a, b)))
+
+#fractions使わない場合
+#ユーグリッド互除法
+def gcd(m, n):
+    x = max(m, n)
+    y = min(m, n)
+    if x % y == 0:
+        return y
+    else:
+        while x % y != 0:
+            z = x % y
+            x = y
+            y = z
+        else:
+            return z
+print(int(a * b) / gcd(a, b))
