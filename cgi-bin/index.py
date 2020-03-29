b35e07d0d449b7eddb318a9f1f27279744ba7f3a
customaddone <custom1485@docomo.ne.jp>
2020-03-29 15:31:35 +0900

    binary_search

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,17 +1,17 @@
-# N**iの数が多くなりすぎる時は、listを思いっきり伸ばして二分探索すればOK
-import bisect
-import time
-from decimal import Decimal
+# 二分探索
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-t1 = time.time()
-a = [x for x in range(2000000)]
+def binary_search(list, x):
+    low = 0
+    high = len(list)
 
-t1 = time.time()
-b = a.index(888888)
-t2 = time.time()
-print(Decimal(t2 - t1))
-
-t3 = time.time()
-b = bisect.bisect_left(a, 888888)
-t4 = time.time()
-print(Decimal(t4 - t3))
+    while low <= high:
+        mid = (low + high) // 2
+        if a[mid] == x:
+            return mid
+        elif a[mid] < x:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return None
+print(binary_search(a, 9))
