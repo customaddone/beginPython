5b50d414715bd6e4a6aed612e6a1695a7cda8578
customaddone <custom1485@docomo.ne.jp>
2020-03-29 13:31:25 +0900

    蟻本1-6 三角形

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,3 +1,10 @@
-list = ["a", "b", "c"]
-for number, alphabet in enumerate(list):
-    print(number, alphabet)
+# O(n3)程度であれば十分間に合う
+n = int(input())
+a = list(map(int, input().split()))
+ans = 0
+for l in range(n):
+    for m in range(l + 1, n):
+        for q in range(m + 1, n):
+            if a[l] + a[m] > a[q]:
+                ans = max(ans, a[l] + a[m] + a[q])
+print(ans)
