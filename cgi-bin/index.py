bafff47ef8c38df45015f4243c3b2ec4f30646b8
customaddone <custom1485@docomo.ne.jp>
2020-03-29 22:03:14 +0900

    ABC 045 C たくさんの数式

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,26 +1,12 @@
-import sys
+def dfs(i, f):
+    if i == n - 1:
+        return sum(list(map(int, f.split("+"))))
 
-graph = {
-    'a': ['c'],
-    'b': ['c','f'],
-    'c': ['a','b','d'],
-    'd': ['e','f'],
-    'e': ['d'],
-    'f': ['b','d','g'],
-    'g': ['f']
-}
-# 深さ優先探索
-def DFS(graph,start,end,mark_dict):
-    # mark_dictにないキーを指定すると勝手に設定してくれる
-    mark_dict[start] = True
-    # スタート地点から移動可能な地点を回す
-    for target in graph[start]:
-        if target == end:
-            print('Exist')
-            sys.exit()
-        # targetをスタート位置に設定してもう一度
-        elif target not in mark_dict:
-            mark_dict[target] = True
-            DFS(graph,target,end,mark_dict)
+    # 末端まで行くと数字をリターンする
+    # 2つ合わせたもの、４つ合わせたもの...が帰ってくる
+    return dfs(i + 1, f + s[i + 1]) + dfs(i + 1, f + "+" + s[i + 1])
 
-DFS(graph,'b','g',{})
+s = input()
+n = len(s)
+
+print(dfs(0, s[0]))
