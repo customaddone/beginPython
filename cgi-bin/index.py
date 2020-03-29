30301fa5e0afd0aee2d8e7c0777381f4bcd11fed
customaddone <custom1485@docomo.ne.jp>
2020-03-29 10:59:47 +0900

    ABC113 B palace

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,4 +1,18 @@
-# ナンバリング→アルファベット
-print(chr(65))
-# アルファベット→ナンバリング
-print(ord('a'))
+n = int(input())
+t, a = map(int, input().split())
+h = list(map(int, input().split()))
+lat = 0
+abstem = 1000
+for i in range(n):
+    avetem = t - h[i] * 0.006
+    if abs(avetem - a) <= abstem:
+        abstem = abs(a - avetem)
+        lat = h[i]
+print(h.index(lat) + 1)
+
+# 模範解答
+n=int(input())
+t,a=map(int,input().split())
+h=list(map(int,input().split()))
+z = [abs(t - i * 0.006 - a) for i in h]
+print(z.index(min(z)) + 1)
