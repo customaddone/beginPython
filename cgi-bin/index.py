fada28fd30de549c96e85ea0cd8412b27991db63
customaddone <custom1485@docomo.ne.jp>
2020-03-29 16:32:38 +0900

    再帰関数メモ探索

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,7 +1,12 @@
 # n,n-1,n-2と小さく再帰し1以下になったら止まる
+# fib(n)の数をメモっておくと楽になる
+memo = [0] * 100
+
 def fib(n):
     if n <= 1:
         return n;
-    return fib(n - 1) + fib(n - 2)
-
-print(fib(8))
+    if (memo[n] != 0):
+        return memo[n]
+    memo[n] = fib(n - 1) + fib(n - 2)
+    return memo[n]
+print(fib(40))
