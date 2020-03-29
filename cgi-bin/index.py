129bd5a6cc3dd8043aff913397e1921b40ae35bd
customaddone <custom1485@docomo.ne.jp>
2020-03-29 21:11:59 +0900

    グラフ　深さ優先探索

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,20 +1,26 @@
-n = 3
-k = 6
-a = [1, 2, 3, 4, 5]
+import sys
 
-# 2 ** n 通り試行がある
-def dfs(i, sum):
-    if i == n:
-        print(sum)
-        return sum == k
+graph = {
+    'a': ['c'],
+    'b': ['c','f'],
+    'c': ['a','b','d'],
+    'd': ['e','f'],
+    'e': ['d'],
+    'f': ['b','d','g'],
+    'g': ['f']
+}
+# 深さ優先探索
+def DFS(graph,start,end,mark_dict):
+    # mark_dictにないキーを指定すると勝手に設定してくれる
+    mark_dict[start] = True
+    # スタート地点から移動可能な地点を回す
+    for target in graph[start]:
+        if target == end:
+            print('Exist')
+            sys.exit()
+        # targetをスタート位置に設定してもう一度
+        elif target not in mark_dict:
+            mark_dict[target] = True
+            DFS(graph,target,end,mark_dict)
 
-    # 掘り進んだ先にTrueがあるようなら
-    if dfs(i + 1, sum):
-        return True
-
-    if dfs(i + 1, sum + a[i]):
-        return True
-
-    return False
-
-print(dfs(0, 0))
+DFS(graph,'b','g',{})
