9e260830f8a451ad345373e35f9b1b9b64d273d5
customaddone <custom1485@docomo.ne.jp>
2020-03-29 16:24:32 +0900

    roop def

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,17 +1,7 @@
-# 二分探索
-a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# n,n-1,n-2と小さく再帰し1以下になったら止まる
+def fib(n):
+    if n <= 1:
+        return n;
+    return fib(n - 1) + fib(n - 2)
 
-def binary_search(list, x):
-    low = 0
-    high = len(list)
-
-    while low <= high:
-        mid = (low + high) // 2
-        if a[mid] == x:
-            return mid
-        elif a[mid] < x:
-            low = mid + 1
-        else:
-            high = mid - 1
-    return None
-print(binary_search(a, 9))
+print(fib(8))
