9723939be3a98b416daabe992c24a840c00985fe
customaddone <custom1485@docomo.ne.jp>
2020-04-01 23:02:01 +0900

    幅優先探索 #2

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,8 +1,9 @@
-tree = [[1, 2], [3, 4], [5, 6],[7, 8]]
+import collections
+tree = [[1, 2], [3, 4], [5, 6],[],[],[],[]]
+list = collections.deque([0])
 
-data = [0]
-while len(data) > 0:
-    pos = data.pop(0)
-    print(pos, end="")
-    for i in tree[pos]:
-        data.append(i)
+while len(list) > 0:
+    pop = list.popleft()
+    print(pop, end = " ")
+    for i in tree[pop]:
+        list.append(i)
