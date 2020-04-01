2b6fd7f6e4bc3ae37f89859d15a4e18117b06a59
customaddone <custom1485@docomo.ne.jp>
2020-04-01 21:34:21 +0900

    動å的計画法ã

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,20 +1,26 @@
 def rec(i, j):
+    if dp[i][j]:
+        return dp[i][j]
     if i == n:
         res = 0
-        print([i, j, res])
-    # オーバーフロー この先０になります
     elif j < w[i]:
         res = rec(i + 1, j)
-    # v[i]の分だけ増えます
     else:
         res = max(rec(i + 1, j), rec(i + 1, j - w[i]) + v[i])
 
+    dp[i][j] = res
     return res
 
 n = int(input())
-w = list(map(int, input().split()))
-v = list(map(int, input().split()))
-
+w = []
+v = []
+for i in range(n):
+    w_, v_ = map(int, input().split())
+    w.append(w_)
+    v.append(v_)
 W = int(input())
 
-print(rec(0, W))
+# 一つ多く枠をとる
+dp = [[0] * (W + 1) for i in range(n + 1)]  # メモ化テーブル
+
+print(rec_memo(0, W))
