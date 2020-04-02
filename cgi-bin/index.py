9524470e580f78dc7685a41c8f46d044a3c68286
customaddone <custom1485@docomo.ne.jp>
2020-04-02 21:35:32 +0900

    maze 深さ優先探索

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,9 +1,22 @@
-import collections
-tree = [[1, 2], [3, 4], [5, 6],[],[],[],[]]
-list = collections.deque([0])
+pos = [[1, 1, 0]]
 
-while len(list) > 0:
-    pop = list.popleft()
-    print(pop, end = " ")
-    for i in tree[pop]:
-        list.append(i)
+def search(x, y, depth):
+    if maze[x][y] == 1:
+        print(depth)
+        exit()
+
+    maze[x][y] = 2
+
+    if maze[x - 1][y] < 2:
+        search(x - 1, y, depth + 1)
+    if maze[x + 1][y] < 2:
+        search(x - 1, y, depth + 1)
+    if maze[x][y - 1] < 2:
+        search(x, y - 1, depth + 1)
+    if maze[x][y + 1] < 2:
+        search(x, y + 1, depth + 1)
+
+    # 詰まったら元にもどす
+    maze[x][y] = 0
+
+search(1, 1, 0)
