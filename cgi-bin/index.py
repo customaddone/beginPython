769ef7b0fdf981c1647fe3d1515bc119c0db021e
customaddone <custom1485@docomo.ne.jp>
2020-04-04 11:37:08 +0900

    深さ優先探索maze

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -3,19 +3,22 @@ maze=[[9,9,9,9,9,9,9,9,9,9,9,9],[9,0,0,0,9,0,0,0,0,0,0,9],[9,0,9,0,0,0,9,9,0,9,9
 import collections
 pos = collections.deque([[1, 1, 0]])
 
-while len(pos) > 0:
-    x, y, depth = pos.popleft()
-
+def search(x, y, depth):
     if maze[x][y] == 1:
         print(depth)
-        break
+        exit()
     maze[x][y] = 2
 
     if maze[x + 1][y] < 2:
-        pos.append([x + 1, y, depth + 1])
+        search(x + 1, y, depth + 1)
     if maze[x - 1][y] < 2:
-        pos.append([x - 1, y, depth + 1])
+        search(x - 1, y, depth + 1)
     if maze[x][y - 1] < 2:
-        pos.append([x, y - 1, depth + 1])
+        search(x, y - 1, depth + 1)
     if maze[x][y + 1] < 2:
-        pos.append([x, y  + 1, depth + 1])
+        search(x, y  + 1, depth + 1)
+
+    # 前の分岐まで戻って０（通過前）にする
+    maze[x][y] = 0
+
+search(1, 1, 0)
