4e7dd9a0b2ad0944242b0a386945756a5f4664a7
customaddone <custom1485@docomo.ne.jp>
2020-04-04 13:44:43 +0900

    ベルマンフォード法

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,19 +1,23 @@
-N = 8
+def bellman_ford(edges, num_v):
+    dist = [float('inf') for i in range(num_v)]
+    dist[0] = 0
 
-def check(x, col):
-    for i, row in enumerate(reversed(col)):
-        if (x + i + 1 == row) or (x - i - 1 == row):
-            return False
-    return True
+    changed = True
+    while changed:
+        changed = False
+        # roop1
+        # edge1 [0, 1, 4]
+        # edge2 [0, 2, 3]
+        # ...
+        # edge9 [5, 6, 4]
 
-def search(col):
-    if len(col) == N:
-        print(col)
-        return
+        # 変更があったので最初からfor edge in edges:する
+        for edge in edges:
+            # dist[edge[1]]:終点 dist[edge[0]]:始点 edge[2]:コスト
+            if dist[edge[1]] > dist[edge[0]] + edge[2]:
+                dist[edge[1]] = dist[edge[0]] + edge[2]
+                changed = True
+    return dist
 
-    for i in range(N):
-        if i not in col:
-            if check(i, col):
-                col.append(i)
-                search(col)
-                col.pop()
+edges = [[0,1,4],[0,2,3],[1,2,1],[1,3,1],[1,4,5],[2,5,2],[4,6,2],[5,4,1],[5,6,4]]
+print(bellman_ford(edges,7))
