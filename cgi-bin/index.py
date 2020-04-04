d9ca3f1b69eadd1e7410f769c7f7cddd57cdedb5
customaddone <custom1485@docomo.ne.jp>
2020-04-04 12:24:45 +0900

    クイーン問題

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,24 +1,19 @@
-maze=[[9,9,9,9,9,9,9,9,9,9,9,9],[9,0,0,0,9,0,0,0,0,0,0,9],[9,0,9,0,0,0,9,9,0,9,9,9],[9,0,9,9,0,9,0,0,0,9,0,9],[9,0,0,0,9,0,0,9,9,0,9,9],[9,9,9,0,0,9,0,9,0,0,0,9],[9,0,0,0,9,0,9,0,0,9,1,9],[9,0,9,0,0,0,0,9,0,0,9,9],[9,0,0,9,0,9,0,0,9,0,0,9],[9,0,9,0,9,0,9,0,0,9,0,9],[9,0,0,0,0,0,0,9,0,0,0,9],[9,9,9,9,9,9,9,9,9,9,9,9]]
+N = 8
 
-import collections
-pos = collections.deque([[1, 1, 0]])
+def check(x, col):
+    for i, row in enumerate(reversed(col)):
+        if (x + i + 1 == row) or (x - i - 1 == row):
+            return False
+    return True
 
-def search(x, y, depth):
-    if maze[x][y] == 1:
-        print(depth)
-        exit()
-    maze[x][y] = 2
+def search(col):
+    if len(col) == N:
+        print(col)
+        return
 
-    if maze[x + 1][y] < 2:
-        search(x + 1, y, depth + 1)
-    if maze[x - 1][y] < 2:
-        search(x - 1, y, depth + 1)
-    if maze[x][y - 1] < 2:
-        search(x, y - 1, depth + 1)
-    if maze[x][y + 1] < 2:
-        search(x, y  + 1, depth + 1)
-
-    # 前の分岐まで戻って０（通過前）にする
-    maze[x][y] = 0
-
-search(1, 1, 0)
+    for i in range(N):
+        if i not in col:
+            if check(i, col):
+                col.append(i)
+                search(col)
+                col.pop()
