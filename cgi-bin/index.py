3a8e08c359a67fabc5dea2b4ccf5098816e0e410
customaddone <custom1485@docomo.ne.jp>
2020-04-04 11:32:23 +0900

    幅探索maza

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,14 +1,21 @@
-# fibonacci(40)あたりから動きが怪しくなるので、メモ作っておくと便利
-# 再帰は上限ある
-# この方法だとfibonatti(1500)ぐらいいける
-data = [0] * 100000
-def fibonacci(n):
-    if (n == 1) or (n == 2):
-        return 1
-    if data[n] != 0:
-        return data[n]
-    else:
-        data[n] = fibonacci(n - 2) + fibonacci(n - 1)
-        return data[n]
+maze=[[9,9,9,9,9,9,9,9,9,9,9,9],[9,0,0,0,9,0,0,0,0,0,0,9],[9,0,9,0,0,0,9,9,0,9,9,9],[9,0,9,9,0,9,0,0,0,9,0,9],[9,0,0,0,9,0,0,9,9,0,9,9],[9,9,9,0,0,9,0,9,0,0,0,9],[9,0,0,0,9,0,9,0,0,9,1,9],[9,0,9,0,0,0,0,9,0,0,9,9],[9,0,0,9,0,9,0,0,9,0,0,9],[9,0,9,0,9,0,9,0,0,9,0,9],[9,0,0,0,0,0,0,9,0,0,0,9],[9,9,9,9,9,9,9,9,9,9,9,9]]
 
-print(fibonacci(1500))
+import collections
+pos = collections.deque([[1, 1, 0]])
+
+while len(pos) > 0:
+    x, y, depth = pos.popleft()
+
+    if maze[x][y] == 1:
+        print(depth)
+        break
+    maze[x][y] = 2
+
+    if maze[x + 1][y] < 2:
+        pos.append([x + 1, y, depth + 1])
+    if maze[x - 1][y] < 2:
+        pos.append([x - 1, y, depth + 1])
+    if maze[x][y - 1] < 2:
+        pos.append([x, y - 1, depth + 1])
+    if maze[x][y + 1] < 2:
+        pos.append([x, y  + 1, depth + 1])
