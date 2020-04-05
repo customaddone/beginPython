ba2dc2b69764d158f95608d559b3ec9fe431547e
customaddone <custom1485@docomo.ne.jp>
2020-04-05 12:28:23 +0900

    たくさんの数式

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,14 +1,10 @@
-n, m = map(int, input().split())
-s, c = [], []
-for m in range(M):
-    s,c=map(int,input().split())
-    S.append(s)
-    C.append(c)
-# n = 3なら1000まで
-for i in range(10 ** n):
-    # 10**n以下の数字を全て並べて条件に適合するやつだけ抽出
-    if len(str(i)) == N and all(str(i)[s - 1] == str(c) for s, c in zip(s, c)):
-        print(i)
-        break
-else:
-    print(-1)
+# dfs(0, s[0])で最初の１個を使うのでやるのはi == n - 1まで
+def dfs(i, f):
+    if i == n - 1:
+        return sum(list(map(int, f.split("+"))))
+    return dfs(i + 1, f + s[i + 1]) + dfs(i + 1, f + "+" + s[i + 1])
+
+s = input()
+n = len(s)
+
+print(dfs(0, s[0]))
