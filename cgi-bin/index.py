d269481e44c41b54622783944431b4d524c2d89e
customaddone <custom1485@docomo.ne.jp>
2020-04-05 12:38:19 +0900

    ABC079 C Train Ticket

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,10 +1,13 @@
-# dfs(0, s[0])で最初の１個を使うのでやるのはi == n - 1まで
-def dfs(i, f):
-    if i == n - 1:
-        return sum(list(map(int, f.split("+"))))
-    return dfs(i + 1, f + s[i + 1]) + dfs(i + 1, f + "+" + s[i + 1])
+def dfs(i, f, sum):
+    if i == 3:
+        if sum == 7:
+            print(f + "=7")
+            exit()
+
+    else:
+        dfs(i + 1, f + "-" + s[i + 1], sum - int(s[i + 1]))
+        dfs(i + 1, f + "+" + s[i + 1], sum + int(s[i + 1]))
 
 s = input()
-n = len(s)
 
-print(dfs(0, s[0]))
+dfs(0, s[s], int(s[0]))
