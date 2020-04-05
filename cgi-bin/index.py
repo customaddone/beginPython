25ef8a73c886f55a5e52c062cfa327aceaa42c54
customaddone <custom1485@docomo.ne.jp>
2020-04-05 12:11:29 +0900

    ABC157C Guess The Number

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,11 +1,14 @@
-n = int(input())
-MOD = 10**4+7
-
-# n + 1まで0
-a=[0]*(max(4, n+1))
-a[3]=1
-# 最初にリストを作っとく
-for i in range(4, n+1):
-       a[i]=(a[i-1]+a[i-2]+a[i-3]) % MOD
-
-print(a[n])
+n, m = map(int, input().split())
+s, c = [], []
+for m in range(M):
+    s,c=map(int,input().split())
+    S.append(s)
+    C.append(c)
+# n = 3なら1000まで
+for i in range(10 ** n):
+    # 10**n以下の数字を全て並べて条件に適合するやつだけ抽出
+    if len(str(i)) == N and all(str(i)[s - 1] == str(c) for s, c in zip(s, c)):
+        print(i)
+        break
+else:
+    print(-1)
