6b203c0b3257775d0594ccf378c437e8d495ec80
customaddone <custom1485@docomo.ne.jp>
2020-04-05 21:48:16 +0900

    ATC 001A 深さ優先

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,16 +1,35 @@
 import sys
 # 再帰制限を外す
 sys.setrecursionlimit(1000000)
-lista = [0] * 10000
+n, m = map(int, input().split())
+c = [list(input()) for i in range(n)]
 
-def fib(n):
-    if lista[n] != 0:
-        return lista[n]
-    if n == 1:
-        return 0
-    elif n == 2:
-        return 1
-    lista[n] = fib(n - 1) + fib(n - 2)
-    return lista[n]
-n = int(input())
-print(fib(n))
+# 到達したかどうか（0は未到達、1は到達済み）
+d = [[0] * m for i in range(n)]
+
+# 移動する4方向
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
+def dfs(x, y):
+    d[x][y] = 1
+
+    # 移動４方向をループ
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if 0 <= nx and nx < n and 0 <= ny and ny < m and d[nx][ny] == 0 and c[nx][ny] != "#":
+            dfs(nx, ny)
+
+# スタートはどこ？
+for i in range(n):
+    for j in range(m):
+        if c[i][j] == "s":
+            dfs(i, j)
+for i in range(n):
+    for j in range(m):
+        if c[i][j] == "g" and d[i][j]:
+            print("Yes")
+            exit()
+print("No")
