85c22a1537a84c06394c5534d6aaafb3780d04eb
customaddone <custom1485@docomo.ne.jp>
2020-04-07 20:08:34 +0900

    組み合わせä¸抽出

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,35 +1,9 @@
-from collections import deque
+import itertools
 
-def bfs():
-    d = [[float('inf')] * m for in range(n)]
+# 4C3 * 3!
+for i in itertools.permutations([0, 1, 2, 3], 3):
+    print(i)
 
-    dx = [1, 0, -1, 0]
-    dy = [0, 1, 0, -1]
-
-    for i in range(n):
-        for j in range(m):
-            if maze[i][j] == "S":
-                sx = i
-                sy = j
-            if maze[i][j] == "G":
-                gx = i
-                gy = j
-
-    que = deque([])
-    que.append((sx, sy))
-    d[sx][sy] = 0
-
-    while que:
-        p = que.popleft()
-
-        if p[0] == gx and p[1] == gy:
-            break
-        for i in range(4):
-            nx = p[0] + dx[i]
-            ny = p[1] + dy[i]
-
-            if 0 <= nx < n and 0 <= ny < m and maze[nx][ny] != "#" and d[nx][ny] != float("inf"):
-                que.append((nx, ny))
-                # 移動できるならキューに入れ、その点の距離をpからの距離プラス１で確定する
-                d[nx][ny] = d[p[0]][p[1]] + 1
-    return d[gx][gy]
+# 4C3
+for i in itertools.combinations([0, 1, 2, 3], 3):
+    print(i)
