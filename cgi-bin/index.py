492d29bcf552de4ce71e0db1970d1ca5d628d055
customaddone <custom1485@docomo.ne.jp>
2020-04-07 21:06:37 +0900

    幅探索

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,10 +1,35 @@
-memo = {1: 1, 2: 1}
-def fibonacci(n):
-    if (n == 1) or (n == 2):
-        return 1
-    # 9: 34 みたいにメモられる
-    memo[n] = fibonacci(n - 1) + fibonacci(n - 2)
-    return memo[n]
+from collections import deque
+maze = [
+    [9,9,9,9,9,9,9,9,9,9,9,9],
+    [9,0,0,0,9,0,0,0,0,0,0,9],
+    [9,0,9,0,0,0,9,9,0,9,9,9],
+    [9,0,9,9,0,9,0,0,0,9,0,9],
+    [9,0,0,0,9,0,0,9,9,0,9,9],
+    [9,9,9,0,0,9,0,9,0,0,0,9],
+    [9,0,0,0,9,0,9,0,0,9,1,9],
+    [9,0,9,0,0,0,0,9,0,0,9,9],
+    [9,0,0,9,0,9,0,0,9,0,0,9],
+    [9,0,9,0,9,0,9,0,0,9,0,9],
+    [9,0,0,0,0,0,0,9,0,0,0,9],
+    [9,9,9,9,9,9,9,9,9,9,9,9]
+]
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
 
-fibonacci(10)
-print(memo)
+pos = [[1, 1, 0]]
+
+while len(pos) > 0:
+    x, y, depth =  pos.pop(0)
+
+    if maze[x][y] == 1:
+        print(depth)
+        break
+
+    maze[x][y] = 2
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if maze[nx][ny] < 2:
+            pos.append([nx, ny, depth + 1])
