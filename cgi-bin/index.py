761e58f119a0bf017af9c38918bc57ee6ac3a160
customaddone <custom1485@docomo.ne.jp>
2020-04-10 23:12:54 +0900

    K-th Common Divisor

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,11 +1,6 @@
-n = int(input())
-dp = [1]*(n + 1)
-ans = 0
-# エラストテネスの箒
-for i in range(2, n + 1):
-    div = 1
-    while i * div <= n:
-        dp[i * div] += 1
-        div += 1
-# dp[1]から１個飛ばしで表示
-print(dp[1::2].count(8))
+a, b, k = map(int, input().split())
+lista = []
+for i in range(1,min(a, b) + 1):
+    if a % i == 0 and b % i == 0:
+        lista.append(i)
+print(lista[-k])
