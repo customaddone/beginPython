493d271c44d83d715b6a096cc449bdfa43af6a3e
customaddone <custom1485@docomo.ne.jp>
2020-04-10 23:33:17 +0900

    Digits in Multiplication

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,9 +1,12 @@
-import itertools
-l = ["a", "b", "c", "d"]
-
-# 順番関係ありの組み合わせ
-for v in itertools.permutations(l, 2):
-    print(v)
-# 関係なしの組み合わせ
-for v in itertools.combinations(l, 2):
-    print(v)
+import math
+n = int(input())
+divlist = []
+ans = 100000
+for i in range(1, int(math.sqrt(n)) + 1):
+    if n % i == 0:
+        divlist.append(i)
+for i in divlist:
+    fa = len(str(i))
+    fb = len(str(int(n / i)))
+    ans = min(ans, max(fa, fb))
+print(ans)
