f6fa2e2fbf21d20fc8c933ac3f4a6df3d6694a6c
customaddone <custom1485@docomo.ne.jp>
2020-04-11 23:07:03 +0900

    ナイト巡回問題

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,25 +1,25 @@
-def rec_memo(i, j):
-    if dp[i][j]:
-        return dp[i][j]
-    if i == n:
-        res = 0
-    elif j < w[i]:
-        res = rec_memo(i + 1, j)
-    else:
-        # 次の商品を取った場合の最大値と取らない場合の最大値を比べる
-        res = max(rec_memo(i + 1, j), rec_memo(i + 1, j - w[i]) + v[i])
-        print([i, j, res])
+maze = [
+         [0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0]
+        ]
 
-    dp[i][j] = res
-    return res
-
-n = 4
-w = [2, 1, 3, 2]
-v = [3, 2, 4, 2]
-
-W = 5
-dp = [[0] * (W + 1) for i in range(n + 1)]  # メモ化テーブル
-# i = 3 5余した状態での最大値は2（最後の商品をとった場合）
-# i = 3 2余した状態での最大値は2
-# i = 2 5余した状態での最大値は6
-print(rec_memo(0, W))
+dx = [1, 2, 2, 1, -1, -2, -2, -1]
+dy = [-2, -1, 1, 2, 2, 1, -1, -2]
+counter = 0
+def dfs(x, y, depth):
+    global counter
+    if depth == 25:
+        counter += 1
+    maze[x][y] = depth
+    for i in range(8):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx <= 4 and 0 <= ny <= 4 and maze[nx][ny] == 0:
+            dfs(nx, ny, depth + 1)
+    maze[x][y] = 0
+dfs(0, 0, 1)
+# 計算量やばそう
+print(counter)
