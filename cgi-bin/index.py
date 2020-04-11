fdc8de51aa2c6138daaa3dc53df563e145c35029
customaddone <custom1485@docomo.ne.jp>
2020-04-11 14:55:38 +0900

    巡回セールスマン問題#3

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,8 +1,11 @@
 from copy import deepcopy
-
+ans = 1000000
+# dfsを完全に理解してしまったかもしれない
 def dfs(now, ignore, sumtime):
+    # ansは数値書き換え用なのでglobal宣言
+    global ans
     if len(ignore) == n:
-        print(sumtime)
+        ans = min(ans, sumtime)
     for i in dist[now]:
         if i[0] in ignore:
             continue
@@ -10,8 +13,15 @@ def dfs(now, ignore, sumtime):
         nignore.append(i[0])
         dfs(i[0], nignore, sumtime + i[1])
 
-n = 3
+n = 4
 ans = 1000000
-dist = [[[1, 5], [2, 2]], [[0, 5], [2, 4]], [[0, 2], [1, 4]]]
+dist = [
+        [[1, 8], [2, 7], [3, 3]],
+        [[0, 8], [2, 9], [3, 1]],
+        [[0, 7], [1, 9], [3, 4]],
+        [[0, 3], [1, 1], [2, 4]]
+       ]
 
-dfs(0, [0], 0)
+for i in range(n):
+    dfs(i, [i], 0)
+print(ans)
