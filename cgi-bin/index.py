4fbe0ae7f9330945ae568923ea53acb25b84c533
customaddone <custom1485@docomo.ne.jp>
2020-04-11 11:40:59 +0900

    145 C Average Length

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,24 +1,18 @@
+import math
 import itertools
-n, m = map(int, input().split())
-ks = [list(map(int, input().split())) for _ in range(m)]
-p = list(map(int, input().split()))
-
-cnt = 0
-# 複数の集合から要素を一つずつ取り出した時の組み合わせの集合
-# スイッチの全ての組み合わせ
-for bits in itertools.product([0, 1], repeat = n):
-    # 電球を並べる
-    lights = [0] * m
-
-    for i in range(n):
-        # もしスイッチがONなら
-        if bits[i] == 0:
-            sw = i + 1
-            for j in range(len(ks)):
-                if sw in ks[j][1:]:
-                    # そのスイッチが関係する電球にプラス１
-                    lights[j] += 1
-    m_lighs = [l % 2 for l in lights]
-    if m_lights == p:
-        cnt += 1
-print(cnt)
+n = int(input())
+lista = []
+anslist = 0
+for i in range(n):
+    x, y = map(int, input().split())
+    lista.append([x, y])
+# permutasion 引数１つのみ
+# len(lista)!通り出る
+for i in itertools.permutations(lista):
+    for j in range(n - 1):
+        anslist += math.sqrt((i[j + 1][0] - i[j][0]) ** 2 + (i[j + 1][1] - i[j][1]) ** 2)
+        #if i != j:
+            #print([lista[i], lista[j]])
+            #anslist += math.sqrt((lista[j][0] - lista[i][0]) ** 2 + (lista[j][1] - lista[i][1]) ** 2)
+# math.factorial 階乗
+print(anslist / math.factorial(n))
