172276212a59e5688c311841fa14f716649d028d
customaddone <custom1485@docomo.ne.jp>
2020-04-11 10:17:18 +0900

    itertools.product 128C Switches

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,6 +1,24 @@
-a, b, c, x, y = map(int, input().split())
-ans = x*a+y*b
-for i in range(max(x, y) + 1):
-    p = c * i * 2 + max(0, x - i) * a + max(0, y - i) * b
-    ans = min(ans, p)
-print(ans)
+import itertools
+n, m = map(int, input().split())
+ks = [list(map(int, input().split())) for _ in range(m)]
+p = list(map(int, input().split()))
+
+cnt = 0
+# 複数の集合から要素を一つずつ取り出した時の組み合わせの集合
+# スイッチの全ての組み合わせ
+for bits in itertools.product([0, 1], repeat = n):
+    # 電球を並べる
+    lights = [0] * m
+
+    for i in range(n):
+        # もしスイッチがONなら
+        if bits[i] == 0:
+            sw = i + 1
+            for j in range(len(ks)):
+                if sw in ks[j][1:]:
+                    # そのスイッチが関係する電球にプラス１
+                    lights[j] += 1
+    m_lighs = [l % 2 for l in lights]
+    if m_lights == p:
+        cnt += 1
+print(cnt)
