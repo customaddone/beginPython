1951f9651b4beda78a59bee98955c2f25eee4d4d
customaddone <custom1485@docomo.ne.jp>
2020-04-12 11:54:48 +0900

    グラフ理論　全ての場所に行く方法幅æ探索

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,26 +1,26 @@
-import collections
-m = int(input())
-n = 1000000000
-ans = []
-listnum = [
-            [0, 1],
-            [0, 1, 2],
-            [1, 2, 3],
-            [2, 3, 4],
-            [3, 4, 5],
-            [4, 5, 6],
-            [5, 6, 7],
-            [6, 7, 8],
-            [7, 8, 9],
-            [8, 9]
-           ]
-def dfs(s):
-    if len(str(n)) + 1 > len(s):
-        ans.append(int(s))
-        lastnum = str(s)[-1]
-        for i in listnum[int(lastnum)]:
-            dfs(s + str(i))
-for i in range(1, 10):
-    dfs(str(i))
-ans = sorted(ans)
-print(ans[m - 1])
+from copy import deepcopy
+ans = 1000000
+# dfsを完全に理解してしまったかもしれない
+def dfs(now, ignore):
+    if len(ignore) == n:
+        print(ignore)
+    for i in dist[now]:
+        if i in ignore:
+            continue
+        nignore = deepcopy(ignore)
+        nignore.append(i)
+        dfs(i, nignore)
+
+n = 8
+dist = [
+        [1, 2, 4],
+        [0, 3, 8],
+        [0, 5],
+        [1, 7, 8],
+        [0, 8],
+        [6, 8],
+        [5, 7],
+        [3, 6],
+        [1, 3, 4, 5]
+       ]
+dfs(0, [0])
