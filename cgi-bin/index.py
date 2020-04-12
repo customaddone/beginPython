5001e19ab1293d0b4dfdca93dba155f4e1c8a9fc
customaddone <custom1485@docomo.ne.jp>
2020-04-12 16:55:49 +0900

    ABC143 D Triangles

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,22 +1,8 @@
-import sys
-from copy import deepcopy
-sys.setrecursionlimit(10 ** 9)
-
+import bisect
 n = int(input())
-lista = [[] for i in range(n + 1)]
-ans = float('inf')
+l = sorted(list(map(int, input().split())))
+counter = 0
 for i in range(n):
-    a = int(input())
-    lista[i + 1].append(a)
-def dfs(now, ignore):
-    global ans
-    if now == 2:
-        ans = min(ans, len(ignore) - 1)
-    for i in lista[now]:
-        if i in ignore:
-            continue
-        nignore = deepcopy(ignore)
-        nignore.append(i)
-        dfs(i, nignore)
-dfs(1, [1])
-print(-1 if ans == float('inf') else ans)
+    for j in range(i + 1, n):
+        counter += (bisect.bisect_left(l, l[i] + l[j]) - j - 1)
+print(counter)
