21ed4ae0cec8aa08c290a1947c4c44debe8ac750
customaddone <custom1485@docomo.ne.jp>
2020-04-12 09:58:43 +0900

    ABC161 D Lunlun Number

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,16 +1,26 @@
-n = int(input())
-
-ans = 0
-
+import collections
+m = int(input())
+n = 1000000000
+ans = []
+listnum = [
+            [0, 1],
+            [0, 1, 2],
+            [1, 2, 3],
+            [2, 3, 4],
+            [3, 4, 5],
+            [4, 5, 6],
+            [5, 6, 7],
+            [6, 7, 8],
+            [7, 8, 9],
+            [8, 9]
+           ]
 def dfs(s):
-    global ans
-    if s:
-        if int(s) > n:
-            return
-        elif "3" in s and "5" in s and "7" in s:
-            ans += 1
-    dfs(s + "3")
-    dfs(s + "5")
-    dfs(s + "7")
-dfs("")
-print(ans)
+    if len(str(n)) + 1 > len(s):
+        ans.append(int(s))
+        lastnum = str(s)[-1]
+        for i in listnum[int(lastnum)]:
+            dfs(s + str(i))
+for i in range(1, 10):
+    dfs(str(i))
+ans = sorted(ans)
+print(ans[m - 1])
