6c9ff12794d373077ada4e9235aa2c1cd6b5a74f
customaddone <custom1485@docomo.ne.jp>
2020-04-12 20:10:57 +0900

    BFS (幅優先探索) 超入門！

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,8 +1,25 @@
-import bisect
-n = int(input())
-l = sorted(list(map(int, input().split())))
-counter = 0
-for i in range(n):
-    for j in range(i + 1, n):
-        counter += (bisect.bisect_left(l, l[i] + l[j]) - j - 1)
-print(counter)
+import collections
+from copy import deepcopy
+dist = [
+        [1, 2, 4],
+        [0, 3, 4, 8],
+        [0, 5],
+        [1, 7, 8],
+        [0, 1, 8],
+        [2, 6, 8],
+        [5, 7],
+        [3, 6],
+        [1, 3, 4, 5]
+        ]
+pos = collections.deque([[0, [0]]])
+
+while len(pos) > 0:
+    now, ignore = pos.popleft()
+    if now == 6:
+        print(ignore)
+    for i in dist[now]:
+        if i in ignore:
+            continue
+        nignore = deepcopy(ignore)
+        nignore.append(i)
+        pos.append([i, nignore])
