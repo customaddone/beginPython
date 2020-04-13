990f251182b4458aa8aeaee9fb7c76420b17c433
customaddone <custom1485@docomo.ne.jp>
2020-04-13 22:12:01 +0900

    ABC146 C - Buy an Integer

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,29 +1,12 @@
-from collections import deque
-maze = [
-    [9,9,9,9,9,9,9,9,9,9],
-    [9,0,9,0,0,0,0,9,1,9],
-    [9,0,9,0,9,0,0,0,0,9],
-    [9,0,0,0,9,0,9,9,0,9],
-    [9,9,0,9,9,0,0,0,9,9],
-    [9,0,0,0,9,9,9,0,9,9],
-    [9,0,9,0,0,0,0,0,9,9],
-    [9,0,0,0,9,0,9,0,0,9],
-    [9,0,0,0,0,0,0,0,0,9],
-    [9,9,9,9,9,9,9,9,9,9]
-]
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
-
-# これ
-pos = deque([[8, 8, 0]])
-
-while len(pos) > 0:
-    x, y, depth = pos.popleft()
-    if maze[x][y] == 1:
-        print(depth)
-    maze[x][y] = 2
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if maze[nx][ny] < 2:
-            pos.append([nx, ny, depth + 1])
+a,b,x = map(int,input().split())
+mi = 0
+ma = 10**9+1
+# 上限と下限を徐々に狭めていく
+# 上限-下限＝１になったら下限が答え
+while ma-mi>1:
+  me = int((mi+ma)/2)
+  if a*me+b*len(str(me))<=x:
+    mi = me
+  else:
+    ma = me
+print(mi)
