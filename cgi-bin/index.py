c2589dbed6a9f53939e232cdb169deaa57f888e2
customaddone <custom1485@docomo.ne.jp>
2020-04-13 23:00:33 +0900

    ナップサックDP #2

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,10 +1,16 @@
 a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
-# n = 20の時点で既に動きが怪しい
-def rec(i, maxsum):
+# メモ用意してスッキリ
+def rec(i):
+    if dp[i]:
+        return dp[i]
     if i == n:
-        return maxsum
-    newsum = max(rec(i + 1, maxsum), rec(i + 1, maxsum + a[i]))
-    return newsum
+        res = 0
+    else:
+        res = max(rec(i + 1), rec(i + 1)+ a[i])
+    dp[i] = res
+    return res
+
 n = 20
-print(rec(0, 0))
+dp = [0] * (n + 1)
+print(rec(0))
