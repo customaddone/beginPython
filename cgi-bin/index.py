9901d51549cc366858e019678aa2827041309a0d
customaddone <custom1485@docomo.ne.jp>
2020-04-14 20:44:34 +0900

    問題 2:　ナップサック問題

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,12 +1,18 @@
-p = 0
-if p:
-    print(True)
-else:
-    print(False)
-
-# 0はboolean型に直すとFalse
-list = [0, 0, 0]
-if list[0]:
-    print(True)
-else:
-    print(False)
+w = [2, 1, 3, 2, 1, 5]
+v = [3, 2, 6, 1, 3, 85]
+n = 6
+W = 9
+def rec_memo(i, j):
+    if dp[i][j]:
+        return dp[i][j]
+    if i == n:
+        return 0
+    elif j < w[i]:
+        res = rec_memo(i + 1, j)
+    else:
+        res = max(rec_memo(i + 1, j), rec_memo(i + 1, j - w[i]) + v[i])
+    dp[i][j] = res
+    return res
+# iが先 jが後
+dp = [[0] * (W + 1) for i in range(n + 1)]
+print(rec_memo(0, W))
