b4abb3e6c3c037aa67ce7a4cd4052f34a335eb55
customaddone <custom1485@docomo.ne.jp>
2020-04-14 21:53:11 +0900

    部部部部分和問題

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,18 +1,17 @@
-w = [2, 1, 3, 2, 1, 5]
-v = [3, 2, 6, 1, 3, 85]
 n = 6
-W = 9
-def rec_memo(i, j):
-    if dp[i][j]:
-        return dp[i][j]
+a =  [3,7,8,12,13,18]
+A = 27
+def rec_memo(i, sum):
+    if sum < A and dp[i][sum]:
+        return dp[i][sum]
     if i == n:
-        return 0
-    elif j < w[i]:
-        res = rec_memo(i + 1, j)
+        return sum == A
+    if sum > A:
+        return False
     else:
-        res = max(rec_memo(i + 1, j), rec_memo(i + 1, j - w[i]) + v[i])
-    dp[i][j] = res
-    return res
-# iが先 jが後
-dp = [[0] * (W + 1) for i in range(n + 1)]
-print(rec_memo(0, W))
+        res = (rec_memo(i + 1, sum) or rec_memo(i + 1, sum + a[i]))
+        if sum < A:
+            dp[i][sum] = res
+        return res
+dp = [[0] * (A + 1) for i in range(n + 1)]
+print(rec_memo(0, 0))
