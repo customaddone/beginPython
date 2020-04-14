193d4da620fcf7299911b8bdf74a29ee6c4bb9a6
customaddone <custom1485@docomo.ne.jp>
2020-04-14 22:58:06 +0900

    é部分和数え上げ問題 git push python index.py
    z

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,20 +1,16 @@
-def part_sum(a,A):
-    p=10**9+7
-    #初期化
-    N=len(a)
-    dp=[[0 for i in range(A+1)] for j in range(N+1)]
-    # i番目までの整数からいくつか選んで総和をjにすることができるか
-    # 0番目までの整数からいくつか選んで総和を0にすることができるか
-    dp[0][0]=1
+def part_sum(a, A):
+    p = 10 ** 9 + 7
+    N = len(a)
+    dp = [[0] * (A + 1) for i in range(N + 1)]
+    dp[0][0] = 1
 
-    #DP
     for i in range(N):
-        for j in range(A+1):
-            # dp[i][j−a[i]]がTrueなら、dp[i+1][j]もTrue（足せばいいので）
-            if a[i]<=j: #i+1番目の数字a[i]を足せるかも
-                dp[i+1][j]=dp[i][j-a[i]]+ dp[i][j]% p
-            else: #入る可能性はない
-                dp[i+1][j]=dp[i][j]%p
+        for j in range(A + 1):
+            if a[i] <= j:
+                # 足したパターンと足さなかったパターンの合計
+                dp[i + 1][j] = dp[i][j - a[i]] + dp[i][j]
+            else:
+                dp[i + 1][j] = dp[i][j]
     return dp[N][A]
 
 a = [1, 3, 5, 7, 9]
