831641f4f971de80089ac75d00e1bcc409fd67e1
customaddone <custom1485@docomo.ne.jp>
2020-04-15 22:23:41 +0900

    レーベンシュタイン距離 (diffコマンド)

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -4,14 +4,15 @@ t = 'poporinapeperuto'
 def dfs(s, t):
     lens = len(s)
     lent = len(t)
-    dp = [[0] * (lent + 1) for i in range(lens + 1)]
+    dp = [[float('inf')] * (lent + 1) for i in range(lens + 1)]
     dp[0][0] = 0
 
     for i in range(lens):
         for j in range(lent):
             if s[i] == t[j]:
-                dp[i + 1][j + 1] = max(dp[i][j] + 1, dp[i + 1][j], dp[i][j + 1])
+                # dp[i + 1][j + 1]はdp[i + 1][j]、dp[i][j + 1] と比べて削除、挿入の手間が１つ増える
+                dp[i + 1][j + 1] = min(dp[i][j], dp[i + 1][j] + 1, dp[i][j + 1] + 1)
             else:
-                dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1])
+                dp[i + 1][j + 1] = min(dp[i + 1][j] + 1, dp[i][j + 1] + 1)
     return dp[lens][lent]
 print(dfs(s, t))
