8b6184ed5ab369535492beb194c1c7c35f01c8b2
customaddone <custom1485@docomo.ne.jp>
2020-04-15 19:56:22 +0900

    最小個æ最小個数部分和問題ã

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,18 +1,19 @@
-def part_sum(a, A):
+n = 2
+a = [7, 5]
+A = 6
+
+def dfs(a, A):
     p = 10 ** 9 + 7
     N = len(a)
-    dp = [[0] * (A + 1) for i in range(N + 1)]
-    dp[0][0] = 1
+    # 一旦float('inf')に
+    dp = [[float('inf')] * (A + 1) for i in range(N + 1)]
+    dp[0][0] = 0
 
     for i in range(N):
         for j in range(A + 1):
             if a[i] <= j:
-                # 足したパターンと足さなかったパターンの合計
-                dp[i + 1][j] = dp[i][j - a[i]] + dp[i][j]
+                dp[i + 1][j] = min(dp[i][j - a[i]] + 1, dp[i][j])
             else:
                 dp[i + 1][j] = dp[i][j]
     return dp[N][A]
-
-a = [1, 3, 5, 7, 9]
-A = 10
-print(part_sum(a, A))
+print(dfs(a, A))
