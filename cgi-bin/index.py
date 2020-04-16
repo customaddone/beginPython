2f6daa7621ce8972d1127e3f8b2338904ba20aac
customaddone <custom1485@docomo.ne.jp>
2020-04-16 22:53:46 +0900

    bit dp

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,21 +1,35 @@
-n = 14
-w = [8, 7, 1, 4, 3, 5, 4, 1, 6, 8, 10, 4, 6, 5]
-# 未開発の部分は-1
-dp = [[-1] * (n + 1) for i in range(n + 1)]
+n,w = map(int,input().split())
 
-def rec(l, r):
-    if dp[l][r] != -1:
-        return dp[l][r]
+#d[i][j]:i→jへの距離
+d = [[float("inf")]*n for i in range(n)]
+for i in range(w):
+   x,y,z = map(int,input().split())
+   d[x][y] = z
 
-    if abs(l - r) <= 1:
+#     今いる点                 訪れた集合0b10110...
+dp = [[-1] * n for i in range(1<<n)]
+
+
+#訪れた集合がs、今いる点がvの時０に戻る最短経路
+def rec(s,v,dp):
+    if dp[s][v] >= 0:
+        return dp[s][v]
+
+    # 0b1000000 - 1 = 0b111111
+    if s == (1<<n)-1 and v == 0:
+        #全ての頂点を訪れた(s = 11...11 and v = 0)
+        #最短距離0
+        dp[s][v] = 0
         return 0
 
-    res = 0
+    res = float("inf")
+    for u in range(n):
+        #sをu右にずらした時の1桁目
+        if (s>>u&1) == 0:
+            #uに訪れていない時(uの箇所が0の時),次はuとすると
+            #             uの箇所を1にして訪れた状態にしてrec
+            res = min(res,rec(s|(1<<u),u,dp)+d[v][u])
 
-    if abs(w[l] - w[r - 1]) <= 1 and rec(l + 1,r - 1) == r - l - 2:
-        res = r - l
-    for i in range(l + 1, r):
-        res = max(res, rec(l, i) + rec(i, r))
-    dp[l][r] = res
+    dp[s][v] = res
     return res
-print(rec(0, n))
+print(rec(0,0,dp))
