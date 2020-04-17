e6cab29d4d3dcbd47fb3c6d28fbbc660b904c050
customaddone <custom1485@docomo.ne.jp>
2020-04-17 20:09:27 +0900

    最小巡回問題

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,35 +1,26 @@
-n,w = map(int,input().split())
+from copy import deepcopy
 
-#d[i][j]:i→jへの距離
-d = [[float("inf")]*n for i in range(n)]
-for i in range(w):
-   x,y,z = map(int,input().split())
-   d[x][y] = z
+def dfs(now, ignore, sumtime):
+    global ans
+    if len(ignore) == n:
+        ans = min(ans, sumtime)
+    for i in dist[now]:
+        if i[0] in ignore:
+            continue
+        nignore = deepcopy(ignore)
+        nignore.append(i[0])
+        dfs(i[0], nignore, sumtime + i[1])
 
-#     今いる点                 訪れた集合0b10110...
-dp = [[-1] * n for i in range(1<<n)]
+n = 4
+ans = 1000000
 
+dist = [
+        [[1, 8], [2, 7], [3, 3]],
+        [[0, 8], [2, 9]],
+        [[0, 7], [1, 9], [3, 4]],
+        [[0, 3], [2, 4]]
+       ]
 
-#訪れた集合がs、今いる点がvの時０に戻る最短経路
-def rec(s,v,dp):
-    if dp[s][v] >= 0:
-        return dp[s][v]
-
-    # 0b1000000 - 1 = 0b111111
-    if s == (1<<n)-1 and v == 0:
-        #全ての頂点を訪れた(s = 11...11 and v = 0)
-        #最短距離0
-        dp[s][v] = 0
-        return 0
-
-    res = float("inf")
-    for u in range(n):
-        #sをu右にずらした時の1桁目
-        if (s>>u&1) == 0:
-            #uに訪れていない時(uの箇所が0の時),次はuとすると
-            #             uの箇所を1にして訪れた状態にしてrec
-            res = min(res,rec(s|(1<<u),u,dp)+d[v][u])
-
-    dp[s][v] = res
-    return res
-print(rec(0,0,dp))
+for i in range(n):
+    dfs(i, [i], 0)
+print(ans)
