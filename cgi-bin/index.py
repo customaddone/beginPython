a3c7fae523a753a9f7dd5b2ea076416dacd6a0dd
customaddone <custom1485@docomo.ne.jp>
2020-04-17 21:53:00 +0900

    ワーシャルフロイド

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,29 +1,18 @@
-from copy import deepcopy
+def warshall_floyd(d):
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                d[i][j] = min(d[i][j], d[i][k] + d[k][j])
+    return d
 
-def dfs(now, ignore, sumtime):
-    global ans
-    # (i<<n) - 1 全ての場所を訪れる
-    if ignore == (1<<n) - 1:
-        ans = min(ans, sumtime)
-    dp[ignore][now] = sumtime
-    print(dp[ignore][now])
-    for i in dist[now]:
-        # i[0]の位置が1
-        if ignore>>i[0]&1 == 1:
-            continue
-        # ignore.append(i[0])
-        nignore = ignore|1<<i[0]
-        dfs(i[0], nignore, sumtime + i[1])
+n,w = map(int,input().split()) #n:頂点数　w:辺の数
 
-n = 4
-ans = 1000000
-dp = [[-1] * n for i in range(1<<n)]
-dist = [
-        [[1, 8], [2, 7], [3, 3]],
-        [[0, 8], [2, 9]],
-        [[0, 7], [1, 9], [3, 4]],
-        [[0, 3], [2, 4]]
-       ]
-
-dfs(0, 1, 0)
-print(ans)
+d = [[float("inf")]*n for i in range(n)]
+#d[u][v] : 辺uvのコスト(存在しないときはinf)
+for i in range(w):
+    x,y,z = map(int,input().split())
+    d[x][y] = z
+    d[y][x] = z
+for i in range(n):
+    d[i][i] = 0 #自身のところに行くコストは０
+print(warshall_floyd(d))
