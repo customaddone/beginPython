e5d3b77f5faf67cb32993b0e9c6d54e44bd7ff6b
customaddone <custom1485@docomo.ne.jp>
2020-04-17 20:44:04 +0900

    bit 最短経路

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -2,18 +2,22 @@ from copy import deepcopy
 
 def dfs(now, ignore, sumtime):
     global ans
-    if len(ignore) == n:
+    # (i<<n) - 1 全ての場所を訪れる
+    if ignore == (1<<n) - 1:
         ans = min(ans, sumtime)
+    dp[ignore][now] = sumtime
+    print(dp[ignore][now])
     for i in dist[now]:
-        if i[0] in ignore:
+        # i[0]の位置が1
+        if ignore>>i[0]&1 == 1:
             continue
-        nignore = deepcopy(ignore)
-        nignore.append(i[0])
+        # ignore.append(i[0])
+        nignore = ignore|1<<i[0]
         dfs(i[0], nignore, sumtime + i[1])
 
 n = 4
 ans = 1000000
-
+dp = [[-1] * n for i in range(1<<n)]
 dist = [
         [[1, 8], [2, 7], [3, 3]],
         [[0, 8], [2, 9]],
@@ -21,6 +25,5 @@ dist = [
         [[0, 3], [2, 4]]
        ]
 
-for i in range(n):
-    dfs(i, [i], 0)
+dfs(0, 1, 0)
 print(ans)
