44c8b329110eda5261aaa40739656c509b3f4c22
customaddone <custom1485@docomo.ne.jp>
2020-04-18 22:41:27 +0900

    復習１７　レーベンシュタイン距離

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,17 +1,17 @@
-s = 'pirikapirirara'
-t = 'poporinapeperuto'
+s = "pirikapirirara"
+t = "poporinapeperuto"
 
-def dfs(s, ts):
+def dfs(s, t):
     lens = len(s)
     lent = len(t)
-    dp = [[0] * (lent + 1) for i in range(lens + 1)]
+    dp = [[float('inf')] * (lent + 1) for i in range(lens + 1)]
     dp[0][0] = 0
 
     for i in range(lens):
         for j in range(lent):
             if s[i] == t[j]:
-                dp[i + 1][j + 1] = max(dp[i][j] + 1, dp[i + 1][j], dp[i][j + 1])
+                dp[i + 1][j + 1] = min(dp[i][j], dp[i + 1][j] + 1, dp[i][j + 1] + 1)
             else:
-                dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1])
+                dp[i + 1][j + 1] = min(dp[i][j] + 1, dp[i + 1][j] + 1, dp[i][j + 1] + 1)
     return dp[lens][lent]
 print(dfs(s, t))
