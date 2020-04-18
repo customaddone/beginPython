a04fdf13c56c35243e677563b11840679bad332d
customaddone <custom1485@docomo.ne.jp>
2020-04-18 18:58:56 +0900

    復習４　深さ優先探索

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,12 +1,33 @@
-def binary_search_loop(data,target):
-    imin = 0
-    imax = len(data) - 1
-    while imin <= imax:
-        imid = imin + (imax + imin) // 2
-        if target == data[imid]:
-            return True
-        elif target < data[imid]:
-            imax = imid - 1
-        else:
-            imin = imid + 1
-    return False
+from collections import deque
+maze = [
+    [9,9,9,9,9,9,9,9,9,9,9,9],
+    [9,0,0,0,9,0,0,0,0,0,0,9],
+    [9,0,9,0,0,0,9,9,0,9,9,9],
+    [9,0,9,9,0,9,0,0,0,9,0,9],
+    [9,0,0,0,9,0,0,9,9,0,9,9],
+    [9,9,9,0,0,9,0,9,0,0,0,9],
+    [9,0,0,0,9,0,9,0,0,9,1,9],
+    [9,0,9,0,0,0,0,9,0,0,9,9],
+    [9,0,0,9,0,9,0,0,9,0,0,9],
+    [9,0,9,0,9,0,9,0,0,9,0,9],
+    [9,0,0,0,0,0,0,9,0,0,0,9],
+    [9,9,9,9,9,9,9,9,9,9,9,9]
+]
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+ans = 100000
+
+def dfs(x, y, depth):
+    global ans
+    if maze[x][y] == 1:
+        ans = min(ans, depth)
+    maze[x][y] = 2
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if maze[nx][ny] < 2:
+            dfs(nx, ny, depth + 1)
+    maze[x][y] = 0
+
+dfs(1, 1, 0)
+print(ans)
