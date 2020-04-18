7d48201e4458ce26b440aae51d5aa6eb5371e75a
customaddone <custom1485@docomo.ne.jp>
2020-04-18 12:17:00 +0900

    クラスカル法å

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,51 +1,57 @@
-from heapq import *
-# 優先度付きキュー
-# 最大値、最小値をO(logn)で取り出せる
-def find(A,x) -> int:
-    p = A[x]
-    if p == x:
-        return x
-    a = find(A,p)
-    A[x] = a
-    return a
+class UnionFind():
+    def __init__(self, n):
+        # print(uf)
+        # 0: [0, 2]
+        # 1: [1]...
+        self.n = n
+        # print(uf.parents)
+        # [-1, -1, -1, -1, -1, -1]
+        self.parents = [-1] * n
 
-def union(A, x, y):
-    if find(A,x) > find(A,y):
-        bx, by = find(A,y), find(A,x)
-    else:
-        bx, by = find(A,x), find(A,y)
-    A[y] = bx
-    A[by] = bx
+    def find(self, x):
+        if self.parents[x] < 0:
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
 
-N = int( input())
-X = [(0,0)]*N
-Y = [(0,0)]*N
-E = [(0,0)]*N
-for i in range(N):
-    x, y = map( int, input().split())
-    X[i] = (x,i)
-    Y[i] = (y,i)
-    E[i] = (x,y)
-X.sort()
-Y.sort()
-H = []
-# 優先度付きキューに変換
-heapify(H)
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
 
-for i in range(N-1):
-    x1, j1 = X[i]
-    x2, j2 = X[i+1]
-    y1, k1 = Y[i]
-    y2, k2 = Y[i+1]
-    print([x2-x1, j1, j2])
-    heappush(H,(x2-x1, j1, j2))
-    heappush(H,(y2-y1, k1, k2))
-print(H)
-ans = 0
-V = [ i for i in range(N)]
-while H:
-    w,s,t = heappop(H)
-    if find(V,s) != find(V,t):
-        union(V,s,t)
-        ans += w
-print(ans)
+        if x == y:
+            return
+
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+
+        # 親に格納された要素の個数をプラス
+        self.parents[x] += self.parents[y]
+        # 移動先
+        self.parents[y] = x
+
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+
+    # 経路格納
+    def __str__(self):
+        return '\n'.join('{}: {}'.format(r, self.members(r)) for r in self.roots())
+
+V, E = map(int, input().split())
+edges = []
+for i in range(E):
+    s, t, w = map(int, input().split())
+    edges.append((w, s, t))
+edges.sort()
+print(edges)
+
+def kruskal(n, edges):#edges: wでソート済み
+    U = UnionFind(n)
+    res = 0
+    for e in edges:
+        w, s, t = e
+        if not U.same(s, t):
+            res+=w
+            U.union(s, t)
+    return res
+print(kruskal(V, edges))
