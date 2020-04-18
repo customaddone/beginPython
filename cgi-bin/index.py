2f29e3bb32bb707f05a58b5d53ac2ff743fc5ceb
customaddone <custom1485@docomo.ne.jp>
2020-04-18 19:47:12 +0900

    復習ï復習８　部分和問題

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,25 +1,16 @@
-from copy import deepcopy
+def dfs(i, sum):
+    if i == n:
+        return sum == k
+    if dfs(i + 1, sum):
+        return True
+    if dfs(i + 1, sum + a[i]):
+        return True
+    return False
+    
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
 
-def dfs(now, ignore, sumtime):
-    global ans
-    if ignore == (1 << n) - 1:
-        ans = min(ans, sumtime)
-    for i in dist[now]:
-        if ignore & (1 << i[0]):
-            continue
-        nignore = ignore | (1 << i[0])
-        dfs(i[0], nignore, sumtime + i[1])
-
-n = 4
-ans = 1000000
-# 通行止めをした
-dist = [
-        [[1, 8], [2, 7], [3, 3]],
-        [[0, 8], [2, 9]],
-        [[0, 7], [1, 9], [3, 4]],
-        [[0, 3], [2, 4]]
-       ]
-
-for i in range(n):
-    dfs(i, 1<<i, 0)
-print(ans)
+if dfs(0, 0):
+    print("Yes")
+else:
+    print("No")
