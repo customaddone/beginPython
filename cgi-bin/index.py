958198918e2e205083d9e1a4d709c943d04d358c
customaddone <custom1485@docomo.ne.jp>
2020-04-18 10:22:50 +0900

    二分探索

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,15 +1,12 @@
-n = 10
-p = [[3, 7], [2, 8]]
-lista = [0] * (n + 1)
-# for i in p:
-    # for j in range(i[0], i[1] + 1):
-        # lista[j] += 1
-# print(lista)
-listb = [0] * (n)
-for i in p:
-    listb[i[0] - 1] += 1
-    listb[i[-1] - 1] -= 1
-listc = [0]
-for i in range(n - 1):
-    listc.append(listb[i] + listc[i])
-print(listc)
+def binary_search_loop(data, target):
+    imin = 0
+    imax = len(data) - 1
+    while imin <= imax:
+        imid = imin + (imax - imin) // 2
+        if target == data[imid]:
+            return True
+        elif target < data[imid]:
+            imax = imid - 1
+        else:
+            imin = imid + 1
+    return False
