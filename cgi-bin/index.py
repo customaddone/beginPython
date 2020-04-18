e5869f01a5361fec50fc25027a01f297b04895f8
customaddone <custom1485@docomo.ne.jp>
2020-04-18 17:09:51 +0900

    How many ways?

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,18 +1,15 @@
-n, k = map(int, input().split())
-a = list(map(int, input().split()))
+from copy import deepcopy
+n = 5
+x = 9
 
-for bit in range(1 << n):
-    sum = 0
-    for i in range(n):
-        # bit と 0b1000..の論理積
-        # 合っていればTrue
-        if bit & (1 << i):
-            sum += a[i]
-    if sum == k:
-        print("Yes")
-        exit()
-
-if dfs(0, 0):
-    print("Yes")
-else:
-    print("No")
+lista = [i for i in range(1, n + 1)]
+def dfs(i, numbers):
+    if len(numbers) == 3:
+        print(numbers)
+        return sum(numbers) == x
+    elif i == n:
+        return 0
+    newnumbers = deepcopy(numbers)
+    newnumbers.append(lista[i])
+    return dfs(i + 1, numbers) + dfs(i + 1, newnumbers)
+print(dfs(0, []))
