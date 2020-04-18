5a3a3fb53732296429386ae9f08f6536bce68533
customaddone <custom1485@docomo.ne.jp>
2020-04-18 18:41:10 +0900

    復習１　フィボナッチ数列

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,33 +1,11 @@
-from collections import deque
-maze = [
-    [9,9,9,9,9,9,9,9,9,9],
-    [9,0,9,0,0,0,0,9,1,9],
-    [9,0,9,0,9,0,0,0,0,9],
-    [9,0,0,0,9,0,9,9,0,9],
-    [9,9,0,9,9,0,0,0,9,9],
-    [9,0,0,0,9,9,9,0,9,9],
-    [9,0,9,0,0,0,0,0,9,9],
-    [9,0,0,0,9,0,9,0,0,9],
-    [9,0,0,0,0,0,0,0,0,9],
-    [9,9,9,9,9,9,9,9,9,9]
-]
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
-dp = [[-1] * 10 for i in range(10)]
-dp[1][1] = 0
+memo = [0] * 100
 
-# これ
-pos = deque([[1, 1, 0]])
+def fibonatti(n):
+    if memo[n] > 0:
+        return memo[n]
+    if (n == 1) or (n == 2):
+        return 1
+    memo[n] = fibonatti(n - 1) + fibonatti(n - 2)
+    return memo[n]
 
-while len(pos) > 0:
-    x, y, depth = pos.popleft()
-    if maze[x][y] == 1:
-        print(dp[x][y])
-        break
-    maze[x][y] = 2
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if maze[nx][ny] < 2 and dp[nx][ny] == -1:
-            pos.append([nx, ny, depth + 1])
-            dp[nx][ny] = dp[x][y] + 1
+print(fibonatti(40))
