5e6a6b6e292e4a987407803d3a7ce902bf8ba4c0
customaddone <custom1485@docomo.ne.jp>
2020-04-18 09:06:04 +0900

    ABC084 D - 2017-like Number

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,9 +1,28 @@
-n, k = map(int, input().split())
-lista = []
-listb = [0]
+import math
 
-lista = list(map(int, input().split()))
-for i in range(n):
+prime = [2]
+max = 10 ** 5
+limit = int(math.sqrt(max))
+data = [i + 1 for i in range(2, max, 2)]
+
+# エラストテネスの篩
+while limit > data[0]:
+    prime.append(data[0])
+    data = [j for j in data if j % data[0] != 0]
+prime = prime + data
+
+# a[i]はlike 2017
+lista = [0] * max
+for i in prime:
+    if (i + 1) / 2 in prime:
+        lista[i - 1] = 1
+
+# 累積和
+listb = [0]
+for i in range(max):
     listb.append(lista[i] + listb[i])
-print(lista)
-print(listb)
+
+q = int(input())
+for i in range(q):
+    l, r = map(int, input().split())
+    print(listb[r] - listb[l - 1])
