4632e4804fa553305eac12ccca8d90a01a1e461e
customaddone <custom1485@docomo.ne.jp>
2020-04-18 11:49:12 +0900

    Union Find

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,12 +1,39 @@
-def binary_search_loop(data, target):
-    imin = 0
-    imax = len(data) - 1
-    while imin <= imax:
-        imid = imin + (imax - imin) // 2
-        if target == data[imid]:
-            return True
-        elif target < data[imid]:
-            imax = imid - 1
+class UnionFind():
+    def __init__(self, n):
+        # print(uf)
+        # 0: [0, 2]
+        # 1: [1]...
+        self.n = n
+        # print(uf.parents)
+        # [-1, -1, -1, -1, -1, -1]
+        self.parents = [-1] * n
+
+    def find(self, x):
+        if self.parents[x] < 0:
+            return x
         else:
-            imin = imid + 1
-    return False
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
+
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+
+        if x == y:
+            return
+
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+
+        # 親に格納された要素の個数をプラス
+        self.parents[x] += self.parents[y]
+        # 移動先
+        self.parents[y] = x
+
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+
+    # 経路格納
+    def __str__(self):
+        return '\n'.join('{}: {}'.format(r, self.members(r)) for r in self.roots())
+        
