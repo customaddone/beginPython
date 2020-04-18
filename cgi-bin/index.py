2998e7cf6915fbda829487e0c17d579741e7bac1
customaddone <custom1485@docomo.ne.jp>
2020-04-18 23:15:57 +0900

    復習２２　ダイクストラ法

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,19 +1,24 @@
-def bellman(edges, num_v):
+def dij(edges, num_v):
     dist = [float('inf') for i in range(num_v)]
     dist[0] = 0
+    q = [i for i in range(num_v)]
 
-    changed = True
-    while changed:
-        changed = False
-        for edge in edges:
-            if dist[edge[1]] > dist[edge[0]] + edge[2]:
-                dist[edge[1]] = dist[edge[0]] + edge[2]
-                changed = True
-
+    while len(q) > 0:
+        r = q[0]
+        for i in q:
+            if dist[i] < dist[r]:
+                r = i
+        u = q.pop(q.index(r))
+        for i in edges[u]:
+            if dist[i[0]] > dist[u] + i[1]:
+                dist[i[0]] = dist[u] + i[1]
     return dist
 
-edges = [[0, 1, 4], [0, 2, 3], [1, 2, 1],
-         [1, 3, 1], [1, 4, 5], [2, 5, 2],
-         [4, 6, 2], [5, 4, 1], [5, 6, 4]]
-
-print(bellman(edges, 7))
+edges = [
+         [[1, 4], [2, 3], [3, 9]],
+         [[0, 4], [2, 9]],
+         [[0, 3], [1, 9], [3, 2], [4, 5]],
+         [[0, 9], [2, 2], [4, 1]],
+         [[2, 5], [3, 1]]
+         ]
+print(dij(edges, 5))
