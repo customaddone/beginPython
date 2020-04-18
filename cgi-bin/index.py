28c79840b2e582821986ab87e732eed35cc881fe
customaddone <custom1485@docomo.ne.jp>
2020-04-18 20:14:50 +0900

    復習１２　ナップサック問題再帰

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,15 +1,19 @@
-s = input()
-n = len(s)
+def rec_memo(i, j):
+    if dp[i][j]:
+        return dp[i][j]
+    if i == n:
+        res = 0
+    elif j < w[i]:
+        res = rec_memo(i + 1, j)
+    else:
+        res = max(rec_memo(i + 1, j), rec_memo(i + 1, j - w[i]) + v[i])
+    dp[i][j] = res
+    return res
 
-ans = 0
+n = 4
+w = [2, 1, 3, 2]
+v = [3, 2, 4, 2]
 
-for bit in range(1 << n):
-    f = s[0]
-
-    for i in range(n - 1):
-        if bit & (1 << i):
-            f += '+'
-        f += s[i + 1]
-
-    ans += sum(map(int, input().split()))
-print(ans)
+W = 5
+dp = [[0] * (W + 1) for i in range(n + 1)]  # メモ化テーブル
+print(rec_memo(0, W))
