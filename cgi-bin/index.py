ec38be28cd7fddc74befc0a42a6d5998e3d8d6ee
customaddone <custom1485@docomo.ne.jp>
2020-04-18 23:00:58 +0900

    復習１９ bit DP

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,21 +1,23 @@
-n = 14
-w = [8, 7, 1, 4, 3, 5, 4, 1, 6, 8, 10, 4, 6, 5]
-# 未開発の部分は-1
-dp = [[-1] * (n + 1) for i in range(n + 1)]
+n,w = map(int,input().split())
 
-def rec(l, r):
-    if dp[l][r] != -1:
-        return dp[l][r]
+#d[i][j]:i→jへの距離
+d = [[float("inf")]*n for i in range(n)]
+for i in range(w):
+   x,y,z = map(int,input().split())
+   d[x][y] = z
 
-    if abs(l - r) <= 1:
+dp = [[-1] * n for i in range(1 << n)]
+
+def rec(s, v, dp):
+    if dp[s][v] >= 0:
+        return dp[s][v]
+    if s == (1 << n) - 1 and v == 0:
+        dp[s][v] = 0
         return 0
-
-    res = 0
-
-    if abs(w[l] - w[r - 1]) <= 1 and rec(l + 1,r - 1) == r - l - 2:
-        res = r - l
-    for i in range(l + 1, r):
-        res = max(res, rec(l, i) + rec(i, r))
-    dp[l][r] = res
+    res = float('inf')
+    for u in range(n):
+        if (s >> u & 1) == 0:
+            res = min(res,rec(s|(1 << u), u, dp) + d[v][u])
+    dp[s][v] = res
     return res
-print(rec(0, n))
+print(rec(0,0,dp))
