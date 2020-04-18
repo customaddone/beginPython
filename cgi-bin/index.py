cae6d5b1a5501ccf03754bdccb5013248ccc2fd2
customaddone <custom1485@docomo.ne.jp>
2020-04-18 18:43:25 +0900

    復習２　くじ引き　二分探索

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,11 +1,17 @@
-memo = [0] * 100
+import bisect
 
-def fibonatti(n):
-    if memo[n] > 0:
-        return memo[n]
-    if (n == 1) or (n == 2):
-        return 1
-    memo[n] = fibonatti(n - 1) + fibonatti(n - 2)
-    return memo[n]
-
-print(fibonatti(40))
+n = int(input())
+m = int(input())
+lista = []
+k = [i for i in range(1,1000)]
+for i in k:
+    for j in k:
+        lista.append(i + j)
+for i in lista:
+    # リストを伸ばしてbisect
+    # m - (i + j) を満たすものが(i + j)の集合の中にあるか
+    if bisect.bisect_left(lista, m - i):
+        print(True)
+        break
+else:
+    print(False)
