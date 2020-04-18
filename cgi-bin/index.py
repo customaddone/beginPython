5318527603f30a3dc4acdbd7d3975c34ab1a2d42
customaddone <custom1485@docomo.ne.jp>
2020-04-18 17:24:31 +0900

    たくさんの数式 bit

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,15 +1,13 @@
-from copy import deepcopy
-n = 5
-x = 9
+s = input()
+n = len(s)
 
-lista = [i for i in range(1, n + 1)]
-def dfs(i, numbers):
-    if len(numbers) == 3:
-        print(numbers)
-        return sum(numbers) == x
-    elif i == n:
-        return 0
-    newnumbers = deepcopy(numbers)
-    newnumbers.append(lista[i])
-    return dfs(i + 1, numbers) + dfs(i + 1, newnumbers)
-print(dfs(0, []))
+ans = 0
+
+for bit in range(1 << n):
+    f = s[0]
+    for i in range(n):
+        if bit & (1 << n):
+            f + "+"
+        f += s[i + 1]
+    ans += sum(map(int, f.split("+")))
+print(ans)
