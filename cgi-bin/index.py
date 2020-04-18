953288f408f8fab8147ccea97b2f353124196beb
customaddone <custom1485@docomo.ne.jp>
2020-04-18 17:39:43 +0900

    幅探索dpädp使用

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,13 +1,33 @@
-s = input()
-n = len(s)
+from collections import deque
+maze = [
+    [9,9,9,9,9,9,9,9,9,9],
+    [9,0,9,0,0,0,0,9,1,9],
+    [9,0,9,0,9,0,0,0,0,9],
+    [9,0,0,0,9,0,9,9,0,9],
+    [9,9,0,9,9,0,0,0,9,9],
+    [9,0,0,0,9,9,9,0,9,9],
+    [9,0,9,0,0,0,0,0,9,9],
+    [9,0,0,0,9,0,9,0,0,9],
+    [9,0,0,0,0,0,0,0,0,9],
+    [9,9,9,9,9,9,9,9,9,9]
+]
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+dp = [[-1] * 10 for i in range(10)]
+dp[1][1] = 0
 
-ans = 0
+# これ
+pos = deque([[1, 1, 0]])
 
-for bit in range(1 << n):
-    f = s[0]
-    for i in range(n):
-        if bit & (1 << n):
-            f + "+"
-        f += s[i + 1]
-    ans += sum(map(int, f.split("+")))
-print(ans)
+while len(pos) > 0:
+    x, y, depth = pos.popleft()
+    if maze[x][y] == 1:
+        print(dp[x][y])
+        break
+    maze[x][y] = 2
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if maze[nx][ny] < 2 and dp[nx][ny] == -1:
+            pos.append([nx, ny, depth + 1])
+            dp[nx][ny] = dp[x][y] + 1
