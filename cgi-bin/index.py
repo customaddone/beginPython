8dedac6f16d201a88a508959cf428ae2dd69a734
customaddone <custom1485@docomo.ne.jp>
2020-04-18 20:39:51 +0900

    復習１４　最小個数部分和問題

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,18 +1,19 @@
+n = 5
+a = [7, 5, 1, 3, 8]
+A = 12
 
-def part_sum(a,A):
+def dfs(a, A):
     p = 10 ** 9 + 7
     N = len(a)
-    dp = [[0] * (A + 1) for i in range(N + 1)]
-    dp[0][0] = 1
+    # 一旦float('inf')に
+    dp = [[float('inf')] * (A + 1) for i in range(N + 1)]
+    dp[0][0] = 0
 
     for i in range(N):
         for j in range(A + 1):
             if a[i] <= j:
-                dp[i + 1][j] = dp[i][j - a[i]] + dp[i][j]
+                dp[i + 1][j] = min(dp[i][j - a[i]] + 1, dp[i][j])
             else:
                 dp[i + 1][j] = dp[i][j]
     return dp[N][A]
-
-a = [1, 3, 5, 7, 9]
-A = 10
-print(part_sum(a, A))
+print(dfs(a, A))
