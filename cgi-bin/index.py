d4d96e6216fb399db011ddeff36f05896dcb97f0
customaddone <custom1485@docomo.ne.jp>
2020-04-18 20:00:18 +0900

    復習９　部分和問題bit

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,16 +1,14 @@
-def dfs(i, sum):
-    if i == n:
-        return sum == k
-    if dfs(i + 1, sum):
-        return True
-    if dfs(i + 1, sum + a[i]):
-        return True
-    return False
-    
 n, k = map(int, input().split())
 a = list(map(int, input().split()))
 
-if dfs(0, 0):
-    print("Yes")
-else:
-    print("No")
+for bit in range(1 << n):
+    sum = 0
+
+    for i in range(n):
+        if bit & (1 << i):
+            sum += a[i]
+    if sum == k:
+        print('Yes')
+        exit()
+
+print('No')
