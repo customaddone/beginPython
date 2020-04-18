33c8955bd2feb9c1ac2bf0ab192356aa4013effe
customaddone <custom1485@docomo.ne.jp>
2020-04-18 22:42:39 +0900

    復習１８　区間dp

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,17 +1,21 @@
-s = "pirikapirirara"
-t = "poporinapeperuto"
+n = 14
+w = [8, 7, 1, 4, 3, 5, 4, 1, 6, 8, 10, 4, 6, 5]
+# 未開発の部分は-1
+dp = [[-1] * (n + 1) for i in range(n + 1)]
 
-def dfs(s, t):
-    lens = len(s)
-    lent = len(t)
-    dp = [[float('inf')] * (lent + 1) for i in range(lens + 1)]
-    dp[0][0] = 0
+def rec(l, r):
+    if dp[l][r] != -1:
+        return dp[l][r]
 
-    for i in range(lens):
-        for j in range(lent):
-            if s[i] == t[j]:
-                dp[i + 1][j + 1] = min(dp[i][j], dp[i + 1][j] + 1, dp[i][j + 1] + 1)
-            else:
-                dp[i + 1][j + 1] = min(dp[i][j] + 1, dp[i + 1][j] + 1, dp[i][j + 1] + 1)
-    return dp[lens][lent]
-print(dfs(s, t))
+    if abs(l - r) <= 1:
+        return 0
+
+    res = 0
+
+    if abs(w[l] - w[r - 1]) <= 1 and rec(l + 1,r - 1) == r - l - 2:
+        res = r - l
+    for i in range(l + 1, r):
+        res = max(res, rec(l, i) + rec(i, r))
+    dp[l][r] = res
+    return res
+print(rec(0, n))
