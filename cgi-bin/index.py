88b689c61f8f493a87c41afdddca2b50fc128b28
customaddone <custom1485@docomo.ne.jp>
2020-04-19 23:03:35 +0900

    ABC163 C management #2

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,8 +1,11 @@
-import collections
+n = int(input())
+a = list(map(int, input().split()))
 
-N = int(input())
-A = list(map(int, input().split()))
-# collections.Counter(A)で速くなる！！
-cnt = collections.Counter(A)
-for i in range(1, N+1):
-    print(cnt[i])
+b = [0]*n
+
+# リスト作成するとO(n)で解ける
+for i in a:
+    b[i-1] += 1
+
+for i in b:
+    print(i)
