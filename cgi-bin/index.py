32ad3c27db2c0658410f793ac15075d26886333b
customaddone <custom1485@docomo.ne.jp>
2020-04-19 13:45:02 +0900

    ABC008 C - コイン

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,8 +1,13 @@
-import collections
+# https://atcoder.jp/contests/abc008/tasks/abc008_3
+import math
 n = int(input())
-s = [input() for _ in range(n)]
-c = collections.Counter(s)
-# c.most_common() [('taro', 2), ('jiro', 1), ('saburo', 1)]
-# c.most_common()[0] ('taro', 2)
-# c.most_common()[0][0] taro
-print(c.most_common()[0][0])
+c = [int(input()) for i in range(n)]
+sumans = 0
+
+for i in c:
+    lista = [j for j in c if i % j == 0]
+    count = len(lista)
+    # iが面になる通り
+    # math.factorial（全通り） * iの約数を一列に並べた中でiが奇数個目にある確率
+    sumans += math.ceil(count / 2) / count
+print(sumans)
