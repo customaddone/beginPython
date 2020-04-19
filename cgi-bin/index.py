7a429bf700c4c7dcc2381e6771a6c8b7bdfd675f
customaddone <custom1485@docomo.ne.jp>
2020-04-19 19:16:09 +0900

    ABC005 C - おいしいたこ焼きの売り方

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,7 +1,20 @@
-# https://atcoder.jp/contests/abc002/tasks/abc002_3
-xa, ya, xb, yb, xc, yc = map(int, input().split())
-x1, y1 = (xb - xa), (yb - ya)
-x2, y2 = (xc - xa), (yc - ya)
-# 3点(0, 0),(a, b),(c, d)で構成される三角形の面積は|ad - bc|/2
-ans = abs(x1 * y2 - y1 * x2) / 2
-print(ans)
+t = int(input())
+n = int(input())
+a = list(map(int, input().split()))
+m = int(input())
+b = list(map(int, input().split()))
+
+for bi in b:
+    sellable = False
+    # jはajのインデックス
+    for j, aj in enumerate(a):
+        if t >= bi - aj >= 0:
+            # a[j]の値を100000にして使えないようにする
+            a[j] = 100000
+            sellable = True
+            break
+    if not sellable:
+        print('no')
+        break
+else:
+    print('yes')
