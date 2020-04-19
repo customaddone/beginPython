e3db90949258b05ddb53b8ebad77b449a073d421
customaddone <custom1485@docomo.ne.jp>
2020-04-19 14:05:43 +0900

    ABC002 C - 直訴三角形の面積の求め方

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,13 +1,7 @@
-# https://atcoder.jp/contests/abc008/tasks/abc008_3
-import math
-n = int(input())
-c = [int(input()) for i in range(n)]
-sumans = 0
-
-for i in c:
-    lista = [j for j in c if i % j == 0]
-    count = len(lista)
-    # iが面になる通り
-    # math.factorial（全通り） * iの約数を一列に並べた中でiが奇数個目にある確率
-    sumans += math.ceil(count / 2) / count
-print(sumans)
+# https://atcoder.jp/contests/abc002/tasks/abc002_3
+xa, ya, xb, yb, xc, yc = map(int, input().split())
+x1, y1 = (xb - xa), (yb - ya)
+x2, y2 = (xc - xa), (yc - ya)
+# 3点(0, 0),(a, b),(c, d)で構成される三角形の面積は|ad - bc|/2
+ans = abs(x1 * y2 - y1 * x2) / 2
+print(ans)
