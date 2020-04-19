3f37484bf4954f43ce3fffebde6197288104b308
customaddone <custom1485@docomo.ne.jp>
2020-04-19 12:04:47 +0900

    復習２６　n進数

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,17 +1,9 @@
-n = 10
-p = [[3, 7], [2, 8]]
-lista = [0] * (n + 1)
-"""
-for i in p:
-    for j in range(i[0], i[1] + 1):
-        lista[j] += 1
-print(lista)
-listb = [0] * (n)
-"""
-for i in p:
-    listb[i[0] - 1] += 1
-    listb[i[-1] - 1] -= 1
-listc = [0]
-for i in range(n - 1):
-    listc.append(listb[i] + listc[i])
-print(listc)
+n = int(input())
+list = []
+digit = 7
+i = 0
+while n != 0:
+    list.insert(0, str(n % digit))
+    n //= digit
+    i += 1
+print(''.join(list))
