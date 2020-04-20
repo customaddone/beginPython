14fa1e7e08371af8fad7ae6403b97d67e145f032
customaddone <custom1485@docomo.ne.jp>
2020-04-20 19:50:41 +0900

    ABC D 158 D - String Formation dequeの文字列への使用法

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -15,9 +15,12 @@ for i in lista:
         counter = -1 * counter
     else:
         if counter * rever(i[1]) > 0:
+            # 先頭に追加
             s.appendleft(i[2])
         else:
+            # 末尾に追加
             s.append(i[2])
+# listにしてjoin
 s = "".join(list(s))
 if counter > 0:
     print(s)
