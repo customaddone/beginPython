05ae7ee3b77eb01cda58fc7b2fbd3b0523f780fb
customaddone <custom1485@docomo.ne.jp>
2020-04-20 18:55:07 +0900

    復習２８　組み合わせの個数

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,11 +1,19 @@
-n = int(input())
-a = list(map(int, input().split()))
+from operator import mul
+from functools import reduce
 
-b = [0]*n
+def cmb(n, r):
+    r = min(n - r, r)
+    if r == 0: return 1
+    over = reduce(mul, range(n, n - r, -1))
+    under = reduce(mul, range(1, r + 1))
+    return over // under
 
-# リスト作成するとO(n)で解ける
-for i in a:
-    b[i-1] += 1
-
-for i in b:
-    print(i)
+'''
+５行目:5C3 → 5C2にする
+６行目:5C5 = 1
+７行目:
+reduce:第２引数の配列の中身を第１引数の関数で順々に実行していく
+mul:i * i + 1
+over 5 * 4 * 3
+under 3 * 2 * 1
+'''
