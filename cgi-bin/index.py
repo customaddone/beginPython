b3b7c9602b4afbfc5edefe82eba589e17d628d8a
customaddone <custom1485@docomo.ne.jp>
2020-04-20 19:18:22 +0900

    ABC159 D D - Banned K

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,6 +1,9 @@
 from operator import mul
 from functools import reduce
 
+n = int(input())
+a = list(map(int, input().split()))
+
 def cmb(n, r):
     r = min(n - r, r)
     if r == 0: return 1
@@ -8,12 +11,13 @@ def cmb(n, r):
     under = reduce(mul, range(1, r + 1))
     return over // under
 
-'''
-５行目:5C3 → 5C2にする
-６行目:5C5 = 1
-７行目:
-reduce:第２引数の配列の中身を第１引数の関数で順々に実行していく
-mul:i * i + 1
-over 5 * 4 * 3
-under 3 * 2 * 1
-'''
+
+lista = [0] * (n + 1)
+sum = 0
+for j in range(n):
+    lista[a[j]] += 1
+for i in lista:
+    if i >= 2:
+        sum += cmb(i, 2)
+for i in a:
+    print(sum - lista[i] + 1)
--- cgi-bin/myfile.py
+++ cgi-bin/myfile.py
@@ -1,6 +1,23 @@
+from operator import mul
+from functools import reduce
 
-n, k = map(int, input().split())
-sum = 0
-for i in range(k, n + 2):
-    sum += n * i - (i ** 2) + i + 1
-print(sum // 10 ** 9 + 7)
+n = int(input())
+a = list(map(int, input().split()))
+
+def cmb(n, r):
+    r = min(n - r, r)
+    if r == 0: return 1
+    over = reduce(mul, range(n, n - r, -1))
+    under = reduce(mul, range(1, r + 1))
+    return over // under
+
+for i in range(n):
+    lista = [0] * (n + 1)
+    sum = 0
+    for j in range(n):
+        if i != j:
+            lista[a[j]] += 1
+    for i in lista:
+        if i >= 2:
+            sum += cmb(i, 2)
+    print(sum)
