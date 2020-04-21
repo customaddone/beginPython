e64bf295259a10e405d270c705a9013321ce6ab0
customaddone <custom1485@docomo.ne.jp>
2020-04-21 23:25:46 +0900

    復習１９ bit DP #2

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,15 +1,23 @@
-lista = [i for i in range(10)]
+n,w = map(int,input().split())
 
-def binary_search_loop(data, target):
-    imin = 0
-    imax = len(data) - 1
-    while imin <= imax:
-        imid = imin + (imax - imin) // 2
-        if target == data[imid]:
-            return imid
-        elif target < data[imid]:
-            imax = imid - 1
-        else:
-            imin = imid + 1
-    return False
-print(binary_search_loop(lista, 10))
+#d[i][j]:i→jへの距離
+d = [[float('inf')] * n for i in range(n)]
+for i in range(w):
+    x, y, z = map(int, input().split())
+    d[x][y] = z
+
+dp = [[-1] * n for i in range(1 << n)]
+
+def rec(s, v, dp):
+    if dp[s][v] >= 0:
+        return dp[s][v]
+    if s == (1 << n) - 1 and v == 0:
+        dp[s][v] = 0
+        return 0
+    res = float('inf')
+    for u in range(n):
+        if not s & (1 << u):
+            res = min(res, rec(s|(1 << u), u, dp) + d[v][u])
+    dp[s][v] = res
+    return res
+print(rec(0,0,dp))
