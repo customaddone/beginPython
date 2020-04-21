62a00730e1b943e5d1bf1ee0a3f21b11d0b0dc18
customaddone <custom1485@docomo.ne.jp>
2020-04-21 21:47:38 +0900

    復習３　二分探索自家製作

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,19 +1,15 @@
-n,a,b=map(int,input().split())
-m=10**9+7
+lista = [i for i in range(10)]
 
-# 強化版cmb
-def cmb(x,y):
-    r=1
-    for i in range(1,y+1):
-        # 5C3なら
-        # 5 * 4 * 3 / 3 * 2 * 1
-        # = 5(mod m) * 1 / 3(mod m) * 4 (mod m) * 1 / 2(mod m) * 3(mod m) * 1 / 1(mod m)
-        # (x- i + 1): 5 - 1 + 1...
-        # pow(i,m-2,m):
-        # フェルマーの小定理 a ** (m - 2) (mod m) = 1 / a (mod d)
-        # 1 / a (mod m) はaで掛けると1 (mod m)になる数
-        # 1 / 2なら500000004
-        r=(r*(x-i+1)*pow(i,m-2,m))%m
-    return r
-# powでmod累乗を楽にできる
-print((pow(2,n,m)-1-cmb(n,a)-cmb(n,b))%m)
+def binary_search_loop(data, target):
+    imin = 0
+    imax = len(data) - 1
+    while imin <= imax:
+        imid = imin + (imax - imin) // 2
+        if target == data[imid]:
+            return imid
+        elif target < data[imid]:
+            imax = imid - 1
+        else:
+            imin = imid + 1
+    return False
+print(binary_search_loop(lista, 10))
