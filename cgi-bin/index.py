51ebacf620a492c99e136e1b6002a4e4f1a0ce19
customaddone <custom1485@docomo.ne.jp>
2020-04-21 18:26:40 +0900

    復習２９　逆元の使い方

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -5,6 +5,14 @@ m=10**9+7
 def cmb(x,y):
     r=1
     for i in range(1,y+1):
+        # 5C3なら
+        # 5 * 4 * 3 / 3 * 2 * 1
+        # = 5(mod m) * 1 / 3(mod m) * 4 (mod m) * 1 / 2(mod m) * 3(mod m) * 1 / 1(mod m)
+        # (x- i + 1): 5 - 1 + 1...
+        # pow(i,m-2,m):
+        # フェルマーの小定理 a ** (m - 2) (mod m) = 1 / a (mod d)
+        # 1 / a (mod m) はaで掛けると1 (mod m)になる数
+        # 1 / 2なら500000004
         r=(r*(x-i+1)*pow(i,m-2,m))%m
     return r
 # powでmod累乗を楽にできる
