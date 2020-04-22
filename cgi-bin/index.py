3c5791949aa16516925b715b86c7689f37b0e761
customaddone <custom1485@docomo.ne.jp>
2020-04-22 21:01:57 +0900

    ABC134 D - Preparing Boxes

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,23 +1,24 @@
-n,w = map(int,input().split())
-
-#d[i][j]:i→jへの距離
-d = [[float('inf')] * n for i in range(n)]
-for i in range(w):
-    x, y, z = map(int, input().split())
-    d[x][y] = z
-
-dp = [[-1] * n for i in range(1 << n)]
-
-def rec(s, v, dp):
-    if dp[s][v] >= 0:
-        return dp[s][v]
-    if s == (1 << n) - 1 and v == 0:
-        dp[s][v] = 0
-        return 0
-    res = float('inf')
-    for u in range(n):
-        if not s & (1 << u):
-            res = min(res, rec(s|(1 << u), u, dp) + d[v][u])
-    dp[s][v] = res
-    return res
-print(rec(0,0,dp))
+#https://atcoder.jp/contests/abc134/tasks/abc134_d
+n = int(input())
+a = [-1] + [int(i) for i in input().split()]
+# 影響しないとこからやっていく
+# ボール用リスト
+b = [0 for i in range(n + 1)]
+# [::-1]つけて逆順から
+for i in range(1, n + 1)[::-1]:
+    tmp = 0
+    # i * 2, i * 3...
+    # これ覚える
+    for j in range(2 * i, n + 1, i):
+        # iの倍数のボールを全て足す
+        tmp += b[j]
+    # もしtmpの合計とa[i]が一致しなかったら
+    if tmp % 2 = a[i]:
+        # i * 1にボールを入れる
+        b[i] = 1
+ans=[]
+for i in range(N+1):
+    if b[i]==1:
+        ans.append(str(i))
+print(len(ans))
+print(" ".join(ans))
