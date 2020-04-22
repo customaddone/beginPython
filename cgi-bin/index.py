594e84694323b56b9d9a5750f6f6b755c98b7179
customaddone <custom1485@docomo.ne.jp>
2020-04-23 07:02:13 +0900

    復習３０ maze master

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,15 +1,19 @@
-m = 10 ** 9 + 7
-def cmb(x,y):
-    r = 1
-    for i in range(1, y + 1):
-        r = (r * (x - i + 1) * pow(i, m-2, m)) % m
-    return r
+from collections import deque
+h, w = map(int, input().split())
+maze = [list(input()) for i in range(h)]
+dp = [[-1] * w for i in range(h)]
+dp[2][0] = 0
+pos = deque([[0, 2]])
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
 
-x, y = map(int, input().split())
-
-a = (2 * x - y) / 3
-b = (2 * y - x) / 3
-if (x + y) % 3 == 0 and a >= 0 and b >= 0:
-    print(cmb(int((x + y) / 3), int(a)))
-else:
-    print(0)
+while len(pos) > 0:
+    x, y = pos.popleft()
+    maze[y][x] = '#'
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < w and 0 <= ny < h and maze[ny][nx] =='.' and dp[ny][nx] == -1:
+            dp[ny][nx] = dp[y][x] + 1
+            pos.append([nx, ny])
+print(dp)
