c9b5c0add3ffc6cb13f90eaf0bbe8d516a134ef1
customaddone <custom1485@docomo.ne.jp>
2020-04-23 19:57:33 +0900

    ABC152 D - Handstand 2

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,31 +1,11 @@
-# https://atcoder.jp/contests/abc142/tasks/abc142_d
-import math
-# 公倍数を列挙する
-def make_divisors(m, n):
-    divisors = []
-    numi = min(m, n)
-    numa = max(m, n)
-    for i in range(1, int(math.sqrt(numi)) + 1):
-        if numi % i == 0:
-            # 18 = 2 * 9 の2の方
-            if numa % i == 0:
-                divisors.append(i)
-            # √nで無い数についてもう一個プラス
-            # 18 = 2 * 9 の9の方
-            if i != numi // i and numa % (numi // i) == 0:
-                divisors.append(numi // i)
-    return sorted(divisors)
-
-a, b = map(int, input().split())
-lista = make_divisors(a, b)
-listb = [1]
-# listaの先頭の１を削除
-lista.pop(0)
-# エラストテネスの篩の要領
-# 先頭の数をlistbに追加
-# 先頭の数＋先頭の数の倍数を削除
-# これをlistaが消滅するまで繰り返す
-while len(lista) > 0:
-    listb.append(lista[0])
-    lista = [i for i in lista if i % lista[0] != 0]
-print(len(listb))
+# https://atcoder.jp/contests/abc152/tasks/abc152_d
+n = int(input())
+cnt = 0
+# うまく個性を捨てよう
+memo = [[0] * 10 for i in range(10)]
+for i in range(1, n + 1):
+    memo[int(str(i)[0])][int(str(i)[-1])] += 1
+for i in range(10):
+    for j in range(10):
+        cnt += memo[i][j] * memo[j][i]
+print(cnt)
