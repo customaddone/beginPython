949e88cacab6318367c794e7ebc7098991ed429d
customaddone <custom1485@docomo.ne.jp>
2020-04-24 22:33:26 +0900

    004 パ研杯2019 C - カラオケ

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,17 +1,27 @@
-#https://atcoder.jp/contests/abc122/tasks/abc122_b
-s = input()
-l = ['A', 'T', 'G', 'C']
-n = len(s)
-num = 0
-ans = 0
-# sの文字列について一文字目から見ていく
+# https://atcoder.jp/contests/pakencamp-2019-day3/tasks/pakencamp_2019_day3_c
+import itertools
+n, m = map(int, input().split())
+lista = []
 for i in range(n):
-    if s[i] in l:
-        num += 1
-    else:
-        # ansの更新
-        # numのリセット
-        ans = max(num, ans)
-        num = 0
-# 最後の一つが'A','T','G','C'だった時用
-ans = max(num, ans)
+    a = list(map(int, input().split()))
+    lista.append(a)
+# i, jで選曲
+for i in range(m):
+    for j in range(i + 1, m):
+        tmp = 0
+        for k in range(n):
+            tmp += max(a[k][i], a[k[j])
+        res = max(res, tmp)
+print(res)
+
+"""
+maxscore = 0
+# 全ての状態（何番目と何番目を選ぶか）を試す
+# combinationsはあんま使わない？
+for i in itertools.combinations(range(m), 2):
+    score = 0
+    for j in range(n):
+        score += max(lista[j][i[0]], lista[j][i[1]])
+    maxscore = max(maxscore, score)
+print(maxscore)
+"""
