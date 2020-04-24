0a4ad069a835fd5ab1a0cd83b9959705d9797ae8
customaddone <custom1485@docomo.ne.jp>
2020-04-24 21:34:29 +0900

    001 　ITP1_7_B - How Many Ways?

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,19 +1,16 @@
-# https://atcoder.jp/contests/abc114/tasks/abc114_c?lang=ja
-n = int(input())
-
-ans = 0
-
-def dfs(s):
-    global ans
-    # 最初はdfsするのみ
-    if s:
-        # int型でn超えてるか判定
-        if int(s) > n:
-            return
-        elif "3" in s and "5" in s and "7" in s:
-            ans += 1
-    dfs(s + "3")
-    dfs(s + "5")
-    dfs(s + "7")
-dfs("")
-print(ans)
+def LI(): return list(map(int,input().split()))
+while True:
+    n, x = LI()
+    # AOJのお約束
+    if n == x == 0:
+        break
+    ans = 0
+    for i in range(1, n + 1):
+        # 二段目はi + 1(i < j)からスタート
+        for j in range(i + 1, n + 1):
+            # 下記のようにすることでfor減らせる
+            k = x - i - j
+            # jより大きくnより小さい
+            if j < k <= n:
+                ans += 1
+    print(ans)
