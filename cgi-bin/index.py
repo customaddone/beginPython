d610c20a9192c2d5ade9a3a938e7f977b4ac1b25
customaddone <custom1485@docomo.ne.jp>
2020-04-24 23:00:37 +0900

    005 ABC095 C - Half and Half

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,27 +1,37 @@
-# https://atcoder.jp/contests/pakencamp-2019-day3/tasks/pakencamp_2019_day3_c
-import itertools
-n, m = map(int, input().split())
-lista = []
-for i in range(n):
-    a = list(map(int, input().split()))
-    lista.append(a)
-# i, jで選曲
-for i in range(m):
-    for j in range(i + 1, m):
-        tmp = 0
-        for k in range(n):
-            tmp += max(a[k][i], a[k[j])
-        res = max(res, tmp)
-print(res)
-
+# https://atcoder.jp/contests/abc095/tasks/arc096_a
+a, b, c, x, y = map(int, input().split())
+n = max(x, y)
+ans = float('inf')
+# 別解 場合分けしてみる
+# a + b < 2 * cなら
+# ABピザを買わない
+if a+b<2*c:
+    print(x*a+y*b)
+else:
+    # aとbの共通部分についてABピザを賈う
+    ans=2*c*min(x,y)
+    if x>y:
+        x=x-y
+        # もしAピザ1枚よりABピザ2枚の方が安かったら
+        if a<2*c:
+            ans=ans+x*a
+        else:
+            ans=ans+x*2*c
+    else:
+        y=y-x
+        if b<2*c:
+            ans=ans+y*b
+        else:
+            ans=ans+y*2*c
+    print(ans)
 """
-maxscore = 0
-# 全ての状態（何番目と何番目を選ぶか）を試す
-# combinationsはあんま使わない？
-for i in itertools.combinations(range(m), 2):
-    score = 0
-    for j in range(n):
-        score += max(lista[j][i[0]], lista[j][i[1]])
-    maxscore = max(maxscore, score)
-print(maxscore)
+# a + b < 2 * cなら
+# ABピザ買わないみたいなこともできる
+for i in range(n + 1):
+    buyab = i * c * 2
+    buya = max((x - i), 0) * a
+    buyb = max((y - i), 0) * b
+    sum = buyab + buya + buyb
+    ans = min(ans, sum)
+print(ans)
 """
