31b3d97c379e050bda752d8e75105606c9f179a4
customaddone <custom1485@docomo.ne.jp>
2020-04-25 13:50:52 +0900

    006 三井住友信託銀行プログラミングコンテスト2019 D-LuckyPIN

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,37 +1,67 @@
-# https://atcoder.jp/contests/abc095/tasks/arc096_a
-a, b, c, x, y = map(int, input().split())
-n = max(x, y)
-ans = float('inf')
-# 別解 場合分けしてみる
-# a + b < 2 * cなら
-# ABピザを買わない
-if a+b<2*c:
-    print(x*a+y*b)
-else:
-    # aとbの共通部分についてABピザを賈う
-    ans=2*c*min(x,y)
-    if x>y:
-        x=x-y
-        # もしAピザ1枚よりABピザ2枚の方が安かったら
-        if a<2*c:
-            ans=ans+x*a
-        else:
-            ans=ans+x*2*c
-    else:
-        y=y-x
-        if b<2*c:
-            ans=ans+y*b
-        else:
-            ans=ans+y*2*c
-    print(ans)
+n = int(input())
+s = input()
+cnt = 0
+
+one = [0 for i in range(10)]
+two = [0 for i in range(100)]
+thr = [0 for i in range(1000)]
+
+#1 ~ 1000内でsの各文字についてループする
+#1 ~ 1000を全通り調べる
+for i in range(1000):
+    flag = 0
+    # iが1から1000までのループ、stが入力した数字の一部
+    for st in s:
+        if flag = 0 and s == str(i // 100):
+            flag = 1
+        # 192 % 100 = 92 92 // 10 = 9
+        elif flag == 1 and s == str((i % 100) // 10)):
+            flag = 2
+        elif flag == 2 and s == str(i % 10):
+            flag = 3
+            break
+    if flag == 3:
+        ans += 1
 """
-# a + b < 2 * cなら
-# ABピザ買わないみたいなこともできる
-for i in range(n + 1):
-    buyab = i * c * 2
-    buya = max((x - i), 0) * a
-    buyb = max((y - i), 0) * b
-    sum = buyab + buya + buyb
-    ans = min(ans, sum)
-print(ans)
+#[0]224から回してできるのは
+#0[2]24から回してできるのは.,..
+for st in s:
+    # tinがインデックス、tisが実際の数字
+    for tin, tis in enumerate(two):
+        if tis == 1:
+            # '92' + '2' = 922
+            # thr[922] += 1
+            thr[int(str(tin) + st)] = 1
+
+    for oin, ois in enumerate(one):
+        # ②①の次のループで2つ目に繋げる数字も決めとく
+        if ois == 1:
+            # '0' + '2' = 02
+            # two[2] = 1
+            two[int(str(oin) + st)] = 1
+    # ①stを+=1する
+    # ③②と同時にまた新しい1桁目を+=1する
+    one[int(st)] = 1
+
+print(sum(thr))
+"""
+
+"""
+def my_index(l, x, default=-1):
+    if x in l:
+        return l.index(x)
+    else:
+        return default
+
+for i in range(10):
+    indexi = my_index(s, str(i))
+    if indexi >= 0:
+        for j in range(10):
+            indexj = my_index(s[indexi + 1:], str(j))
+            if indexj >= 0:
+                for k in range(10):
+                    indexk = my_index(s[indexi + indexj + 2:], str(k))
+                    if indexk >= 0:
+                        cnt += 1
+print(cnt)
 """
--- cgi-bin/myfile.py
+++ cgi-bin/myfile.py
@@ -1,35 +1,10 @@
-from collections import deque
-
-h, w = map(int, input().split())
-maze = []
-for i in range(h):
-    s = input()
-    maze.append(list(s))
-ans = 0
-
-def mazemax(px, py):
-    dp = [[-1] * w for i in range(h)]
-    dp[0][0] = 0
-    dx = [1, 0, -1, 0]
-    dy = [0, 1, 0, -1]
-    pos = deque([[px, py]])
-
-    while len(pos) > 0:
-        x, y = pos.popleft()
-        maze[x][y] = "#"
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < w and 0 <= ny < h and maze[nx][ny] == "." and dp[nx][ny] == -1:
-                dp[nx][ny] = dp[x][y] + 1
-                pos.append([nx, ny])
-    for i in range(w):
-        for j in range(h):
-            ans = max(ans, dp[i][j])
-
-
-for i in range(w):
-    for j in range(h):
-        if maze[i][j] == ".":
-            mazemax(i, j)
-print(ans)
+n = int(input())
+s = input()
+lista = []
+for i in range(n):
+    for j in range(i + 1, n):
+        for k in range(j + 1, n):
+            str = s[i] + s[j] + s[k]
+            if not str in lista:
+                lista.append(str)
+print(len(lista))
