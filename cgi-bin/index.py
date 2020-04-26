5fb540613aad3a7f22d5794dc13b22740be9694b
customaddone <custom1485@docomo.ne.jp>
2020-04-26 10:57:09 +0900

    011 AtCoder Beginner Contest 128 C - Switches

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,43 +1,30 @@
-#https://atcoder.jp/contests/abc002/tasks/abc002_4
+# https://atcoder.jp/contests/abc128/tasks/abc128_c
 n, m = map(int, input().split())
-lista = [list(map(int, input().split())) for i in range(m)]
-maxans = 0
+lista = []
+for i in range(m):
+    # s:判定に使用するスイッチ
+    k, *s, = map(int, input().split())
+    lista.append(list(s))
+# p:2で割った時の余りが等しいと点灯
+# 全てのpについて成り立たせよう
+p = list(map(int, input().split()))
 
-"""
-5 3
-5人の議員と3つの人間関係がある
-1 2
-2 3
-1 3
-[[1, 2], [2, 3], [1, 3]]
-議員1と2が知り合い
-議員2と3が知り合い
-議員1と3が知り合い
-1と2と3が知り合い = 111
-101と110と011がある
-"""
-listalta = set([(1 << (i[0] - 1)) | (1 << (i[1] - 1)) for i in lista])
-# n, n - 1の順に
-# nCrの議員の選び方を全探索
-# もし条件が適合していればそのrが正解
-# という方法もある
-
+#p[0]について成り立たせよう
+sumans = 0
 # 全ての状態
 for bit in range(1 << n):
-    # 状態a 01001
-    flag = True
-    for i in range(n):
-        for j in range(i + 1, n):
-            # 任意の2箇所について1を立てる
-            # judge:01001と00101の共通部分 = 00001
-            judge = bit & ((1 << i) | (1 << j))
-            # judgeに1が２つ立っているか
-            if judge == ((1 << i) | (1 << j)):
-                # 立っていればそれがlistaltaにあるか
-                if not judge in listalta:
-                    # 1つでもなければFalse
-                    flag = False
-    if flag:
-        ans = bin(bit).count("1")
-        maxans = max(maxans, ans)
-print(maxans)
+    # 任意の状態aの中
+    # 全ての電球について判定
+    for i in range(m):
+        sum = 0
+        # 電球aについて判定
+        for j in lista[i]:
+            if bit & (1 << (j - 1)):
+                sum += 1
+        # 1つの電球でもつかないものがあればbreak
+        # breakうまく使おう
+        if sum % 2 != p[i]:
+            break
+    else:
+        sumans += 1
+print(sumans)
