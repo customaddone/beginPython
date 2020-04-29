a71bee56e52734a6e7a448729f46331c6a20a7a2
customaddone <custom1485@docomo.ne.jp>
2020-04-29 22:40:13 +0900

    037 DPL_1_A - コイン問題

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,18 +1,17 @@
-# http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_1_C&lang=ja
-# 個数制限あり重複ありナップサックdp
-n, w = map(int, input().split())
-val = []
-wei = []
-for i in range(n):
-    v1, w1 = map(int, input().split())
-    val.append(v1)
-    wei.append(w1)
-dp = [[0] * (w + 1) for i in range(n + 1)]
-dp[0][0] = 0
-for i in range(n):
-    for j in range(w + 1):
-        dp[i + 1][j] = dp[i][j]
-        for r in range(n):
-            if wei[r] <= j:
-                dp[i + 1][j] = max(dp[i + 1][j], dp[i][j - wei[r]] + val[r])
-print(dp[n][w])
+# http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_1_A&lang=ja
+n, m = map(int, input().split())
+coin = list(map(int, input().split()))
+dp = [float('inf') for i in range(n + 1)]
+dp[0] = 0
+
+# n <= 50000なのでfor文ループさせるのはやめてほしい
+def rec_memo(j):
+    if dp[j] <= 50000:
+        return dp[j]
+    res = float('inf')
+    for r in range(m):
+        if coin[r] <= j:
+            res = min(res, rec_memo(j - coin[r]) + 1)
+    dp[j] = res
+    return res
+print(rec_memo(n))
