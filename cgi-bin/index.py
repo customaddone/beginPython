7200b6af518525a813409630867de130338ac887
customaddone <custom1485@docomo.ne.jp>
2020-04-29 22:00:16 +0900

    個数制限あり重複ありナップサックdp

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,5 +1,5 @@
 # http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_1_C&lang=ja
-# 個数制限なし重複ありナップサック
+# 個数制限あり重複ありナップサックdp
 n, w = map(int, input().split())
 val = []
 wei = []
@@ -7,14 +7,12 @@ for i in range(n):
     v1, w1 = map(int, input().split())
     val.append(v1)
     wei.append(w1)
-dp = [0] * (w + 1)
-# 個数制限解除のためiが消える
-def rec_memo(j):
-    if dp[j]:
-        return dp[j]
-    res = 0
-    for r in range(n):
-        if wei[r] <= j:
-            res = max(res, rec_memo(j - wei[r]) + val[r])
-    return res
-print(rec_memo(w))
+dp = [[0] * (w + 1) for i in range(n + 1)]
+dp[0][0] = 0
+for i in range(n):
+    for j in range(w + 1):
+        dp[i + 1][j] = dp[i][j]
+        for r in range(n):
+            if wei[r] <= j:
+                dp[i + 1][j] = max(dp[i + 1][j], dp[i][j - wei[r]] + val[r])
+print(dp[n][w])
