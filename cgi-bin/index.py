5600a3962e57a8787f69ae7cfb7c153bbe1ae5b4
customaddone <custom1485@docomo.ne.jp>
2020-04-29 15:16:29 +0900

    030 JOI 2011 予選 5 - チーズ

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -5,11 +5,11 @@ maze = []
 for i in range(h):
     a = input()
     maze.append(list(a))
-# スタート位置特定
+
 start = [-1, -1]
-# 寄り道はしないように
-dp = [[-1] * (w + 1) for i in range(h + 1)]
-dp[start[1]][start[0]] = 0
+end = [-1, -1]
+ans = 0
+# スタート位置特定
 for i in range(w):
     for j in range(h):
         if maze[j][i] == 'S':
@@ -20,33 +20,30 @@ for i in range(w):
     break
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
-# 第一、第二引数が座標
-# 第三引数が経過時間
-# 第四引数が訪れた工場の数
-pos = deque([[start[0], start[1], 0, 1]])
+# スタート位置破壊
 maze[start[1]][start[0]] = '.'
 
-while len(pos) > 0:
-    x, y, time, factcnt = pos.popleft()
-    if factcnt == n + 1:
-        print(time)
-        break
-    # dp[y][x]を訪れるまでにいくつチーズを食べたか
-    dp[y][x] = 0
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        # 1 ~ 9の工場を巡回する（体力は考慮しなくていい）
-        if 0 <= nx < w and 0 <= ny < h and maze[ny][nx] != 'X' and dp[ny][nx] == -1:
-            if maze[ny][nx] == str(factcnt):
-                # チーズ工場を破壊
-                maze[ny][nx] = "."
-                # 一回dequeとdpをリセット
-                pos = deque([])
-                dp[ny][nx] = 0
-                dp = [[-1] * (w + 1) for i in range(h + 1)]
-                pos.append([nx, ny, time + 1, factcnt + 1])
-                # forループから抜ける
-                break
-            else:
-                pos.append([nx, ny, time + 1, factcnt])
+# n → n + 1へ移動するまでにかかる時間
+def bfs(sta, end):
+    global start
+    pos = deque([[sta[0], sta[1], 0]])
+    # 寄り道はしないように
+    dp = [[-1] * (w + 1) for i in range(h + 1)]
+    while len(pos) > 0:
+        x, y, time = pos.popleft()
+        if maze[y][x] == str(end):
+            start = [x, y]
+            return dp[y][x] + 1
+            break
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < w and 0 <= ny < h and maze[ny][nx] != 'X' and dp[ny][nx] == -1:
+                # [x, y]に隣接する部分を+1していく
+                # ここ重要　計算量が超減る
+                dp[ny][nx] = dp[y][x] + 1
+                pos.append([nx, ny, time + 1])
+# 1→2 2→3...8→9の移動時間の合計
+for i in range(n):
+    ans += bfs([start[0], start[1]], i + 1)
+print(ans)
