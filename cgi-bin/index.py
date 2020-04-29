361be58f1a3cdf5cdcf7b49c9e3ec2c5177f69f9
customaddone <custom1485@docomo.ne.jp>
2020-04-29 12:09:55 +0900

    028ALDS_11_C - 幅優先探索

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,29 +1,27 @@
-# https://atcoder.jp/contests/joi2009yo/tasks/joi2009yo_d
-import sys
-sys.setrecursionlimit(100000)
+# http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_11_C&lang=ja
+from collections import deque
 
-m = int(input())
 n = int(input())
-maze = []
+dist = [[] for i in range(n)]
 for i in range(n):
-    maze.append(list(map(int, input().split())))
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
-ans = 0
+    d = list(map(int, input().split()))
+    if len(d) > 2:
+        # 2文字目以降をappend
+        for j in d[2:]:
+            dist[i].append(j - 1)
+ignore = [0] * n
+ignore[0] = 1
+pos = deque([[0, 0]])
+dp = [0] * n
 
-def dfs(x, y, depth):
-    global ans
-    maze[y][x] = 0
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < m and 0 <= ny < n and maze[ny][nx] == 1:
-            dfs(nx, ny, depth + 1)
-    maze[y][x] = 1
-    ans = max(ans, depth)
-for i in range(m):
-    for j in range(n):
-        if maze[j][i] == 1:
-            dfs(i, j, 1)
-# TLEにならないんですかこれ
-print(ans)
+while len(pos) > 0:
+    u, depth = pos.popleft()
+    dp[u] = depth
+    for i in dist[u]:
+        if ignore[i] == 0:
+            pos.append([i, depth + 1])
+            ignore[i] = 1
+            
+for i in range(n):
+    print(i + 1, end = " ")
+    print(dp[i])
