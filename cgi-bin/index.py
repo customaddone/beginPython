10784d38704f9442432e2e5e2633258677d0e424
customaddone <custom1485@docomo.ne.jp>
2020-04-29 20:03:34 +0900

    DPL_1_B - 0,1ナップザック問題

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,12 +1,21 @@
-# ttp://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_10_A&lang=ja
-n = int(input())
-dp = [0] * (n + 1)
-def fibonatti(n):
-    if dp[n] > 0:
-        return dp[n]
-    if (n == 0) or (n == 1):
-        return 1
+# http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_1_B&lang=ja
+n, w = map(int, input().split())
+val = []
+wei = []
+for i in range(n):
+    v1, w1 = map(int, input().split())
+    val.append(v1)
+    wei.append(w1)
+dp = [[0] * (w + 1) for i in range(n + 1)]
+def rec_memo(i, j):
+    if dp[i][j]:
+        return dp[i][j]
+    if i == n:
+        res = 0
+    elif j < wei[i]:
+        res = rec_memo(i + 1, j)
     else:
-        dp[n] = fibonatti(n - 1) + fibonatti(n - 2)
-        return dp[n]
-print(fibonatti(n))
+        res = max(rec_memo(i + 1, j), rec_memo(i + 1, j - wei[i]) + val[i])
+    dp[i][j] = res
+    return res
+print(rec_memo(0, w))
