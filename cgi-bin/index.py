39e886d2138d641f3113c60fdeab500330d0a521
customaddone <custom1485@docomo.ne.jp>
2020-04-29 09:25:31 +0900

    025 AOJ 1160 - 島はいくつある？

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,29 +1,23 @@
-#http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_11_B&lang=ja
-n = int(input())
-dist = [[] for i in range(n)]
-dp = [[0, 0] for i in range(n)]
-
-for i in range(n):
-    d = list(map(int, input().split()))
-    if len(d) > 2:
-        # 2文字目以降をappend
-        for j in d[2:]:
-            dist[i].append(j - 1)
+# http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1160&lang=jp
+w, h = map(int, input().split())
+maze = [list(map(int, input().split())) for i in range(h)]
+# 8方向のベクトル
+dx = [0, 1, 1, 1, 0, -1, -1, -1]
+dy = [1, 1, 0, -1, -1, -1, 0, 1]
 cnt = 0
-def dfs(pos):
+def dfs(x, y):
     global cnt
-    # 初めての訪問なら
-    if dp[pos][0] == 0:
-        cnt += 1
-        dp[pos][0] = cnt
-    for i in dist[pos]:
-        dfs(i)
-    # for回し終えてこれが最後の訪問なら
-    if dp[pos][1] == 0:
-        cnt += 1
-        dp[pos][1] = cnt
-dfs(0)
-for i in range(n):
-    print(i + 1, end = " ")
-    print(dp[i][0], end = " ")
-    print(dp[i][1])
+    # 陸を沈める
+    maze[y][x] = 0
+    for i in range(8):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < w and 0 <= ny < h and maze[ny][nx] == 1:
+            dfs(nx, ny)
+
+for i in range(w):
+    for j in range(h):
+        if maze[j][i] == 1:
+            dfs(i, j)
+            cnt += 1
+print(cnt)
