c88c7a27099fef36aa9396b085cbe16fea09427b
customaddone <custom1485@docomo.ne.jp>
2020-04-29 11:57:29 +0900

    027 JOI 2009 予選 4 - 薄氷渡り

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,47 +1,29 @@
-# https://atcoder.jp/contests/abc138/tasks/abc138_dfro
-from collections import deque
+# https://atcoder.jp/contests/joi2009yo/tasks/joi2009yo_d
+import sys
+sys.setrecursionlimit(100000)
 
-n, q = map(int, input().split())
-dist = [[] for i in range(n)]
-for i in range(n - 1):
-    a, b = map(int, input().split())
-    dist[a - 1].append(b - 1)
-    # aftercontest対策
-    # https://qiita.com/c-yan/items/887e2c2f410ecec60650
-    # 双方向にappendしないと
-    # dist = [[3, 2], [2], [], []]
-    #   0
-    #  / \
-    # 2   3
-    # \
-    #  1　　　にしたいところが
+m = int(input())
+n = int(input())
+maze = []
+for i in range(n):
+    maze.append(list(map(int, input().split())))
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+ans = 0
 
-    #   0      1
-    #  / \    /
-    # 2   3  2    のような変な木になる
-    dist[b - 1].append(a - 1)
-value = [0 for i in range(n)]
-
-# 1つ目の[1, 10]と2つ目の[1, 10]混ぜても問題なし
-# ここでvalueの構造を工夫して計算量を下げる努力を
-for i in range(q):
-    p, x = map(int, input().split())
-    value[p - 1] += x
-#  重複を防ぐ
-ignore = [0] * n
-ignore[0] = 1
-
-pos = deque([0])
-# 上から順に自身のvalueの値を子のvalueに加算していく
-# value [100, 10, 1, 0]
-# pos.popleft() = 1 → [100, 10 + 100, 1 + 100, 0 + 100]
-# pos.popleft() = 2 → [100, 110, 101 + 10, 100 + 10]
-while len(pos) > 0:
-    u = pos.popleft()
-    for i in dist[u]:
-        if ignore[i] == 0:
-            ignore[i] = 1
-            value[i] += value[u]
-            pos.append(i)
-# 覚える
-print(*value)
+def dfs(x, y, depth):
+    global ans
+    maze[y][x] = 0
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < m and 0 <= ny < n and maze[ny][nx] == 1:
+            dfs(nx, ny, depth + 1)
+    maze[y][x] = 1
+    ans = max(ans, depth)
+for i in range(m):
+    for j in range(n):
+        if maze[j][i] == 1:
+            dfs(i, j, 1)
+# TLEにならないんですかこれ
+print(ans)
