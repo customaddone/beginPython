408f1825360687f005009308afb5bcc7b4cf3307
customaddone <custom1485@docomo.ne.jp>
2020-04-30 19:40:27 +0900

    039 JOI 2011 予選 4 - 1 年生 #2

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,28 +1,16 @@
+# https://atcoder.jp/contests/joi2011yo/tasks/joi2011yo_d
 n = int(input())
+l = list(map(int, input().split()))
 
-first, *a, result = map(int, input().split())
-lista = list(a)
-listsum = [0]
-for i in range(n - 2):
-    listsum.append(lista[i] + listsum[i])
-# aの1 / 2 + first = resultになればいい
-target = sum(lista) // 2 + (result - first) // 2
-
-dp = [[0] * (target + 1) for i in range(n - 1)]
-dp[0][0] = 1
-# 最初と最後を取り除いているのでn - 2
-for i in range(n - 2):
-    for j in range(target + 1):
-        # プラス要素:first + j(今まで選択した数の合計)
-        # マイナス要素: listsum[i] - j
-        # first + j - (listsum[i] - j)が0未満だったり20を越えればドボン
-        if 2 * j < listsum[i] - first:
-            dp[i][j] = 0
-        if 2 * j > 20 + listsum[i] - first:
-            dp[i][j] = 0
-        # あとは普通の個数制限あり重複なしナップサックdp
-        if lista[i] <= j:
-            dp[i + 1][j] = dp[i][j - lista[i]] + dp[i][j]
-        else:
-            dp[i + 1][j] =  dp[i][j]
-print(dp[n - 2][target])
+dp = [[0] * 100 for _ in range(n + 1)]
+# 最初の１個が入った状態でスタート
+dp[1][l[0]] = 1
+# 一つ目飛ばして二つ目からループさせていく
+for i in range(1,n):
+    for j in range(21):
+        if j - l[i] >= 0:
+            # dp[i + 1][j]に合流させていく
+            dp[i + 1][j] += dp[i][j - l[i]]
+        if j + l[i - 1] <= 20:
+            dp[i + 1][j] += dp[i][j + l[i]]
+print(dp[n - 1][l[-1]])
