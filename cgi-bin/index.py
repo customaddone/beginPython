78283363ca300e1ff98221a7538185d5558b36db
customaddone <custom1485@docomo.ne.jp>
2020-04-30 21:19:34 +0900

    040 JOI 2012 予選 4 - パスタ

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,16 +1,56 @@
-# https://atcoder.jp/contests/joi2011yo/tasks/joi2011yo_d
-n = int(input())
-l = list(map(int, input().split()))
+# https://atcoder.jp/contests/joi2012yo/tasks/joi2012yo_d
+# dpの三重ループ
+n, k =map(int, input().split())
+mod = 10000
+day = [-1] * n
+for i in range(k):
+    a, b = map(int, input().split())
+    day[a - 1] = b - 1
+# dpは三重に
+# 一重目:日にち
+# 二重目:その日のパスタ
+# 三重目:前日のパスタ
+dp = [[[0] * 3 for i in range(3)] for j in range(n)]
 
-dp = [[0] * 100 for _ in range(n + 1)]
-# 最初の１個が入った状態でスタート
-dp[1][l[0]] = 1
-# 一つ目飛ばして二つ目からループさせていく
-for i in range(1,n):
-    for j in range(21):
-        if j - l[i] >= 0:
-            # dp[i + 1][j]に合流させていく
-            dp[i + 1][j] += dp[i][j - l[i]]
-        if j + l[i - 1] <= 20:
-            dp[i + 1][j] += dp[i][j + l[i]]
-print(dp[n - 1][l[-1]])
+# 既に1日目のパスタが決定されていたなら
+if day[0] >= 0:
+    # そのパスタを選択
+    d = day[0]
+    # １日目、パスタd、前日のパスタはパスタd以外という１通り
+    dp[0][d][d - 1] = 1
+# 決定されていなかったら
+else:
+    for i in range(3):
+        # 全てのパスタについて一通りをプラス
+        dp[0][i][i - 1] = 1
+for i in range(1, n):
+    # パスタが決定されているかいないか
+    d = day[i]
+    if day[i] >= 0:
+        for j in range(3):
+            for k in range(3):
+                # もし３日とも同じなら
+                if d == j and j == k:
+                    continue
+                # i日目、パスタd、前日のパスタjにi - 1日目、パスタj、前日のパスタkの通り
+                # をプラス
+                dp[i][d][j] += dp[i - 1][j][k]
+                dp[i][d][j] %= mod
+    else:
+        # その日のパスタ
+        for j in range(3):
+            # 前日のパスタ
+            for k in range(3):
+                # 前々日のパスタ
+                for l in range(3):
+                    if j == k and k == l:
+                        continue
+                    dp[i][j][k] += dp[i - 1][k][l]
+                    dp[i][j][k] %= mod
+ans = 0
+# dp[-1]の全ての通りを集計
+for i in range(3):
+    for j in range(3):
+        ans += dp[-1][i][j]
+        ans %= mod
+print(ans)
