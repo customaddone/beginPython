8a930aae2a1161e9d254a2eb86ec6ec743df408e
customaddone <custom1485@docomo.ne.jp>
2020-04-30 19:16:25 +0900

    039 JOI 2011 予選 4 - 1 年生

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,18 +1,28 @@
-# http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_10_C&lang=ja
-s = 'pirikapirirara'
-t = 'poporinapeperuto'
+n = int(input())
 
-def dfs(s, ts):
-    lens = len(s)
-    lent = len(t)
-    dp = [[0] * (lent + 1) for i in range(lens + 1)]
-    dp[0][0] = 0
+first, *a, result = map(int, input().split())
+lista = list(a)
+listsum = [0]
+for i in range(n - 2):
+    listsum.append(lista[i] + listsum[i])
+# aの1 / 2 + first = resultになればいい
+target = sum(lista) // 2 + (result - first) // 2
 
-    for i in range(lens):
-        for j in range(lent):
-            if s[i] == t[j]:
-                dp[i + 1][j + 1] = max(dp[i][j] + 1, dp[i + 1][j], dp[i][j + 1])
-            else:
-                dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1])
-    return dp[lens][lent]
-print(dfs(s, t))
+dp = [[0] * (target + 1) for i in range(n - 1)]
+dp[0][0] = 1
+# 最初と最後を取り除いているのでn - 2
+for i in range(n - 2):
+    for j in range(target + 1):
+        # プラス要素:first + j(今まで選択した数の合計)
+        # マイナス要素: listsum[i] - j
+        # first + j - (listsum[i] - j)が0未満だったり20を越えればドボン
+        if 2 * j < listsum[i] - first:
+            dp[i][j] = 0
+        if 2 * j > 20 + listsum[i] - first:
+            dp[i][j] = 0
+        # あとは普通の個数制限あり重複なしナップサックdp
+        if lista[i] <= j:
+            dp[i + 1][j] = dp[i][j - lista[i]] + dp[i][j]
+        else:
+            dp[i + 1][j] =  dp[i][j]
+print(dp[n - 2][target])
