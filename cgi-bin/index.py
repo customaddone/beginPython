00fb5147c73cc9d68f3c513ea220a2b391ce44b9
customaddone <custom1485@docomo.ne.jp>
2020-05-01 19:55:42 +0900

    041 JOI 2013 予選 4 - 暑い日々

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,56 +1,18 @@
-# https://atcoder.jp/contests/joi2012yo/tasks/joi2012yo_d
-# dpの三重ループ
-n, k =map(int, input().split())
-mod = 10000
-day = [-1] * n
-for i in range(k):
-    a, b = map(int, input().split())
-    day[a - 1] = b - 1
-# dpは三重に
-# 一重目:日にち
-# 二重目:その日のパスタ
-# 三重目:前日のパスタ
-dp = [[[0] * 3 for i in range(3)] for j in range(n)]
+d, n = map(int, input().split())
+t = [int(input()) for i in range(d)]
+lista = [list(map(int, input().split())) for i in range(n)]
 
-# 既に1日目のパスタが決定されていたなら
-if day[0] >= 0:
-    # そのパスタを選択
-    d = day[0]
-    # １日目、パスタd、前日のパスタはパスタd以外という１通り
-    dp[0][d][d - 1] = 1
-# 決定されていなかったら
-else:
-    for i in range(3):
-        # 全てのパスタについて一通りをプラス
-        dp[0][i][i - 1] = 1
-for i in range(1, n):
-    # パスタが決定されているかいないか
-    d = day[i]
-    if day[i] >= 0:
-        for j in range(3):
-            for k in range(3):
-                # もし３日とも同じなら
-                if d == j and j == k:
-                    continue
-                # i日目、パスタd、前日のパスタjにi - 1日目、パスタj、前日のパスタkの通り
-                # をプラス
-                dp[i][d][j] += dp[i - 1][j][k]
-                dp[i][d][j] %= mod
-    else:
-        # その日のパスタ
-        for j in range(3):
-            # 前日のパスタ
-            for k in range(3):
-                # 前々日のパスタ
-                for l in range(3):
-                    if j == k and k == l:
-                        continue
-                    dp[i][j][k] += dp[i - 1][k][l]
-                    dp[i][j][k] %= mod
-ans = 0
-# dp[-1]の全ての通りを集計
-for i in range(3):
-    for j in range(3):
-        ans += dp[-1][i][j]
-        ans %= mod
-print(ans)
+dp = [[0 for i in range(n)] for i in range(d)]
+# 本日の服で一つ、前日の服で一つループさせる
+# 日にち
+for i in range(1, d):
+    # 今日着る服
+    for j in range(n):
+        # 昨日着てた服
+        for k in range(n):
+            # もし今日選ぶ服が温度に適合していて           昨日着てた服が昨日の気温に適合していた時
+            if lista[j][0] <= t[i] <= lista[j][1] and lista[k][0] <= t[i - 1] <= lista[k][1]:
+                # dpはkを回した時にできた他のdp[i][j] vs 今日着た服の派手さと前日に着た服の派手さの差 + 前日のdp
+                dp[i][j] = max(dp[i][j], abs(lista[j][2] - lista[k][2]) + dp[i - 1][k])
+# dpの最後尾をprint
+print(max(dp[-1]))
