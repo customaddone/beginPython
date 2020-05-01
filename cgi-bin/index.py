9daba3e0b8bc4ae3f2d36245bd076286ddbdc212
customaddone <custom1485@docomo.ne.jp>
2020-05-01 20:42:51 +0900

    042 JOI 2015 予選 4 - シルクロード

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,18 +1,24 @@
-d, n = map(int, input().split())
-t = [int(input()) for i in range(d)]
-lista = [list(map(int, input().split())) for i in range(n)]
+# https://atcoder.jp/contests/joi2015yo/tasks/joi2015yo_d
+n, m = map(int, input().split())
+# 都市iに到達するためにdiかかる
+# 都市0には最初から到達しているので先頭に0入れる
+listd = [0]
+for i in range(n):
+    d = int(input())
+    listd.append(d)
+listc = []
+for i in range(m):
+    c = int(input())
+    listc.append(c)
 
-dp = [[0 for i in range(n)] for i in range(d)]
-# 本日の服で一つ、前日の服で一つループさせる
-# 日にち
-for i in range(1, d):
-    # 今日着る服
-    for j in range(n):
-        # 昨日着てた服
-        for k in range(n):
-            # もし今日選ぶ服が温度に適合していて           昨日着てた服が昨日の気温に適合していた時
-            if lista[j][0] <= t[i] <= lista[j][1] and lista[k][0] <= t[i - 1] <= lista[k][1]:
-                # dpはkを回した時にできた他のdp[i][j] vs 今日着た服の派手さと前日に着た服の派手さの差 + 前日のdp
-                dp[i][j] = max(dp[i][j], abs(lista[j][2] - lista[k][2]) + dp[i - 1][k])
-# dpの最後尾をprint
-print(max(dp[-1]))
+dp = [[float('inf')] * (m + 1) for i in range(n + 1)]
+dp[0] = [0] * (m + 1)
+# 移動する場合（iとjを１進める)と待機する場合（iを１進める）場合とでdpを比べる
+# nが都市数 mが日にち
+# iが現在の都市 jが現在の日にち
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        # listd[i](都市i - 1→iに移動する) * listc[j - 1]（j - 1日目に都市i - 1を出発して
+        # j日目に都市iに到達する
+        dp[i][j] = min(dp[i][j - 1], dp[i - 1][j - 1] + listd[i] * listc[j - 1])
+print(dp[n][m])
