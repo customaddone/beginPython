fecdf617cfebbd1a4f80cd814d9bc9ea70a2de1d
customaddone <custom1485@docomo.ne.jp>
2020-05-01 21:25:45 +0900

    043 パ研杯2019 D - パ研軍旗

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,24 +1,28 @@
-# https://atcoder.jp/contests/joi2015yo/tasks/joi2015yo_d
-n, m = map(int, input().split())
-# 都市iに到達するためにdiかかる
-# 都市0には最初から到達しているので先頭に0入れる
-listd = [0]
-for i in range(n):
-    d = int(input())
-    listd.append(d)
-listc = []
-for i in range(m):
-    c = int(input())
-    listc.append(c)
+# https://atcoder.jp/contests/pakencamp-2019-day3/tasks/pakencamp_2019_day3_d
+n = int(input())
+s = [list(input()) for i in range(5)]
 
-dp = [[float('inf')] * (m + 1) for i in range(n + 1)]
-dp[0] = [0] * (m + 1)
-# 移動する場合（iとjを１進める)と待機する場合（iを１進める）場合とでdpを比べる
-# nが都市数 mが日にち
-# iが現在の都市 jが現在の日にち
-for i in range(1, n + 1):
-    for j in range(1, m + 1):
-        # listd[i](都市i - 1→iに移動する) * listc[j - 1]（j - 1日目に都市i - 1を出発して
-        # j日目に都市iに到達する
-        dp[i][j] = min(dp[i][j - 1], dp[i - 1][j - 1] + listd[i] * listc[j - 1])
-print(dp[n][m])
+dp = [[5, 5, 5] for i in range(n)]
+
+# dpの初期設定が重要
+for i in range(5):
+    for j in range(n):
+        # その色が出現した都度1を引く
+        if s[i][j] == "R":
+            dp[j][0] -= 1
+        elif s[i][j] == "B":
+            dp[j][1] -= 1
+        elif s[i][j] == "W":
+            dp[j][2] -= 1
+
+# print(dp)
+# [4, 4, 4],
+# [4, 4, 3],
+# [3, 4, 4]
+
+# iが列、jが色
+for i in range(1, n):
+    for j in range(3):
+        # j以外の色で前の列を塗った時の最小値
+        dp[i][j] += min(dp[i - 1][j - 1], dp[i - 1][j - 2])
+print(min(dp[-1]))
