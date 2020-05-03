13ebda249dfd40b9d34ef7c6ec8e57b4e9ba2a91
customaddone <custom1485@docomo.ne.jp>
2020-05-03 12:08:06 +0900

    053 AtCoder Beginner Contest 006 D - トランプ挿入ソート

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,27 +1,16 @@
+# https://atcoder.jp/contests/abc006/tasks/abc006_4
 from bisect import bisect_left
 n = int(input())
 lista = [int(input()) for i in range(n)]
 
+# 最長増加部分列問題 (LIS)の問題
 def lis(A):
     L = [A[0]]
     for a in A[1:]:
-        # Lの末尾よりaが大きければ増加部分を拡張できる
         if a > L[-1]:
-            # 前から順にLに追加していく
             L.append(a)
+        # このelseに引っかかった時にトランプのソートが必要
         else:
-            """
-            [3]
-            [3, 7]
-            末尾より小さい数字が来た場合は適当にすっ飛ばす
-            末尾を小さくできる場合のみ末尾を交換する
-            [3, 4]
-            [1, 4]
-            [1, 4]
-            [1, 4, 6]
-            """
             L[bisect_left(L, a)] = a
-    # Lの配列の長さは求まる
-    # Lの中身はデタラメ
     return len(L)
-print(lis(lista))
+print(n - lis(lista))
