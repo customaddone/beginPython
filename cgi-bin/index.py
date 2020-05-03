d1bf4fe337802ab6b25d1baf4aff5f0aca662943
customaddone <custom1485@docomo.ne.jp>
2020-05-04 08:19:28 +0900

    060 GRL_1_C - 全点対間最短経路

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,11 +1,18 @@
-import math
-def make_divisors(n):
-    divisors = []
-    for i in range(1, int(math.sqrt(n)) + 1):
-        if n % i == 0:
-            divisors.append(i)
-            # √nで無い数についてもう一個プラス
-            if i != n // i:
-                divisors.append(n // i)
-    return divisors
-print(sorted(make_divisors(10 ** 5)))
+# http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_C&lang=ja
+n, w = map(int, input().split())
+
+dist = [[float('inf')] * n for i in range(n)]
+for i in range(n):
+    dist[i][i] = 0
+for i in range(w):
+    x, y, z = map(int, input().split())
+    dist[x][y] = z
+
+def warshall_floyd(dist):
+    for k in range(n):
+        # i:start j:goal k:中間地点でループ回す
+        for i in range(n):
+            for j in range(n):
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+    return dist
+print(warshall_floyd(dist))
