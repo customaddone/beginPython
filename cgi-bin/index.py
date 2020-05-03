ea7237b1315051ac2b1489a5fad70ba16f685264
customaddone <custom1485@docomo.ne.jp>
2020-05-04 08:53:04 +0900

    062 AtCoder Beginner Contest 079 D - Wall

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,26 +1,29 @@
-# https://atcoder.jp/contests/abc012/tasks/abc012_4
+# https://atcoder.jp/contests/abc079/tasks/abc079_d
 n, w = map(int, input().split())
 
-dist = [[float('inf')] * n for i in range(n)]
-for i in range(n):
-    dist[i][i] = 0
-for i in range(w):
-    x, y, z = map(int, input().split())
-    dist[x - 1][y - 1] = z
-    dist[y - 1][x - 1] = z
+dist = [[float('inf')] * 10 for i in range(10)]
+for i in range(10):
+    c = list(map(int, input().split()))
+    for j in range(10):
+        dist[i][j] = c[j]
 
+# warshall_floydを回してdistを改良
 def warshall_floyd(dist):
-    for k in range(n):
+    for k in range(10):
         # i:start j:goal k:中間地点でループ回す
-        for i in range(n):
-            for j in range(n):
+        for i in range(10):
+            for j in range(10):
                 dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
     return dist
-
 warshall_floyd(dist)
 
-ans = float('inf')
-for i in dist:
-    opt = max(i)
-    ans = min(ans, opt)
-print(ans)
+board = [list(map(int, input().split())) for i in range(n)]
+
+cnt = 0
+for row in board:
+    for i in row:
+        if (i == -1) or (i == 1):
+            continue
+        else:
+            cnt += dist[i][1]
+print(cnt)
