857ea5cd2daf7fb0df010998c21bf430f5e8e366
customaddone <custom1485@docomo.ne.jp>
2020-05-03 23:34:17 +0900

    059 JOI 2014 予選 5 - タクシー

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,31 +1,63 @@
-# https://atcoder.jp/contests/joi2008yo/tasks/joi2008yo_f
+# https://atcoder.jp/contests/joi2014yo/tasks/joi2014yo_e
+from heapq import heappop, heappush
 from collections import deque
-import heapq
+import sys
+input = sys.stdin.buffer.readline
 
-num_v = 5
+INF = 10 ** 10
+n, k = map(int,input().split())
+C = []
+R = []
+for _ in range(n):
+    c, r =map(int,input().split())
+    C.append(c)
+    R.append(r)
+edges = [[] for _ in range(n)]
+for _ in range(k):
+    a, b = map(int,input().split())
+    edges[a - 1].append(b - 1)
+    edges[b - 1].append(a - 1)
 
-def dij(start, goal):
-    dist = [float('inf') for i in range(num_v)]
-    dist[start] = 0
-    pq = [(0, start)]
+d = [[INF] * n for _ in range(n)]
+for i in range(n):
+    q = deque([i])
+    d[i][i] = 0
+    while q:
+        cur = q.popleft()
+        for to in edges[cur]:
+            if d[i][to] < INF:
+                continue
+            d[i][to] = d[i][cur] + 1
+            q.append(to)
 
-    # pqの先頭がgoal行きのものなら最短距離を返す
-    while (pq[0][1] != goal):
-        d, now = heapq.heappop(pq)
-        if (d > dist[now]):
-            continue
-        for i in edges[now]:
-            if dist[i[0]] > dist[now] + i[1]:
-                dist[i[0]] = dist[now] + i[1]
-                heapq.heappush(pq, (dist[i[0]], i[0]))
-    return pq[0][0]
+for i in range(n):
+    c, r = C[i], R[i]
+    for j in range(n):
+        if d[i][j] <= r:
+            d[i][j] = c
+        else:
+            d[i][j] = INF
+del edges, C, R
 
-edges = [
-         [[1, 4], [2, 3], [3, 9]],
-         [[0, 4], [2, 9]],
-         [[0, 3], [1, 9], [3, 2], [4, 5]],
-         [[0, 9], [2, 2], [4, 1]],
-         [[2, 5], [3, 1]]
-         ]
-print (dij(0, 1))
-# [0, 4, 3, 5, 6]
+def dijkstra(s,e):
+    ds = [INF] * n
+    used = [False] * n
+    ds[s] = 0
+
+    for _ in range(n):
+        v = -1
+        for i in range(n):
+            if used[i]:
+                continue
+            if v == -1:
+                v = i
+            elif ds[i] < ds[v]:
+                v = i
+        if v == -1:
+            break
+        used[v] = True
+        for nv in range(n):
+            ds[nv] = min(ds[nv], ds[v] + d[v][nv])
+    return ds[e]
+
+print(dijkstra(0,n-1))
--- cgi-bin/myfile.py
+++ cgi-bin/myfile.py
@@ -1,29 +1,21 @@
-from collections import deque
-import heapq
-
-num_v = 5
-
-def dij(start, goal):
-    dist = [float('inf') for i in range(num_v)]
-    dist[start] = 0
-    pq = [(0, start)]
-
-    # pqの先頭がgoal行きのものなら最短距離を返す
-    while (pq[0][1] != goal):
-        d, now = heapq.heappop(pq)
-        if (d > dist[now]):
-            continue
-        for i in edges[now]:
-            if dist[i[0]] > dist[now] + i[1]:
-                dist[i[0]] = dist[now] + i[1]
-                heapq.heappush(pq, (dist[i[0]], i[0]))
-    return pq[0][0]
-
-edges = [
-         [[1, 4], [2, 3], [3, 9]],
-         [[0, 4], [2, 9]],
-         [[0, 3], [1, 9], [3, 2], [4, 5]],
-         [[0, 9], [2, 2], [4, 1]],
-         [[2, 5], [3, 1]]
-         ]
+def dijkstra(s, n, cost):#スタート, 頂点数, cost[s][t]:辺stのコスト（存在しなければinf）
+    inf = 10**18
+    dist = [inf] * n
+    used = [False] * n
+    dist[s] = 0
+    for _ in range(n):
+        v = -1
+        for i in range(n):
+            if used[i]:
+                continue
+            if v == -1:
+                v = i
+            elif dist[i]<dist[v]:
+                v = i
+        if v == -1:
+            break
+        used[v] = True
+        for nv in range(n):
+            dist[nv] = min(dist[nv], dist[v]+cost[v][nv])
+    return dist[-1]
 print (dij(0, 4))
