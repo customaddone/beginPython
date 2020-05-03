4ea61ce45067043fea93a087217368fdb5818df8
customaddone <custom1485@docomo.ne.jp>
2020-05-03 18:08:30 +0900

    058.5改良ダイクストラ

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,71 +1,31 @@
 # https://atcoder.jp/contests/joi2008yo/tasks/joi2008yo_f
 from collections import deque
 import heapq
-INF = 10 ** 20
 
-# n: 街の数
-# m: 道路の数
-# k: ゾンビ占領地域
-# s: ゾンビの行動範囲
-N, M, K, S = map(int,input().split())
-# それぞれ安全な街、危険な街での宿泊料
-P,Q = map(int,input().split())
+num_v = 5
 
-# ゾンビ占領地域
-zombie = []
-for k in range(K):
-    zombie.append(int(input()) - 1)
+def dij(start, goal):
+    dist = [float('inf') for i in range(num_v)]
+    dist[start] = 0
+    pq = [(0, start)]
 
-# 道リスト
-link = [ [] for _ in range(N)]
-
-for m in range(M):
-    a, b = map(int, input().split())
-    link[a - 1].append(b - 1)
-    link[b - 1].append(a - 1)
-
-dist = [INF] * N
-q = deque(zombie)
-# ゾンビ占領地域はinf → 0
-for v in zombie:
-    dist[v] = 0
-while q:
-    node = q.popleft()
-    for next in link[node]:
-        if dist[next] != INF:
+    # pqの先頭がgoal行きのものなら最短距離を返す
+    while (pq[0][1] != goal):
+        d, now = heapq.heappop(pq)
+        if (d > dist[now]):
             continue
-        # ゾンビの出発先 = ゾンビの出発元 + 1
-        dist[next] = dist[node] + 1
-        q.append(next)
-
-cost = [P] * N
-for n in range(N):
-    # infじゃなければ
-    if dist[n] <= S:
-        cost[n] = Q
-    if n in zombie:
-        cost[n] = INF
-    if n == 0 or n == N - 1:
-        # 最初と最後は宿泊料いらない
-        cost[n] = 0
-
-# startとgoal
-def dijkstra(s, g):
-    dists  = [INF for i in range(N)]
-    dists[s] = 0
-    pq = [(0, s)]
-    # (0, s)の後ろの部分がgになったら
-    while(pq[0][1] != g):
-        # heapqは最小値を取り出す時に圧倒的に速い
-        # ダイクストラで使える
-        d, node = heapq.heappop(pq)
-        if (d > dists[node]):
-            continue
-        for next in link[node]:
-            # nextに行くためにかかるコスト
-            c = cost[next]
-            if d + c < dists[next]:
-                dists[next] = d + c
-                heapq.heappush(pq, (dists[next], next))
+        for i in edges[now]:
+            if dist[i[0]] > dist[now] + i[1]:
+                dist[i[0]] = dist[now] + i[1]
+                heapq.heappush(pq, (dist[i[0]], i[0]))
     return pq[0][0]
-print (dijkstra(0, N - 1))
+
+edges = [
+         [[1, 4], [2, 3], [3, 9]],
+         [[0, 4], [2, 9]],
+         [[0, 3], [1, 9], [3, 2], [4, 5]],
+         [[0, 9], [2, 2], [4, 1]],
+         [[2, 5], [3, 1]]
+         ]
+print (dij(0, 1))
+# [0, 4, 3, 5, 6]
--- cgi-bin/myfile.py
+++ cgi-bin/myfile.py
@@ -1,69 +1,29 @@
-# https://atcoder.jp/contests/joi2008yo/tasks/joi2008yo_f
 from collections import deque
 import heapq
-# 用意しとく
-inf = 10 ** 20
 
-# n: 街の数
-# m: 道路の数
-# k: ゾンビ占領地域
-# s: ゾンビの行動範囲
-n, m, k, s = map(int, input().split())
-# それぞれ安全な街、危険な街での宿泊料
-p, q = map(int, input().split())
-# ゾンビ占領地域
-zombie = []
-for i in range(k):
-    zombie.append(int(input())-1)
-# 道リスト
-link = [[] for i in range(n)]
+num_v = 5
 
-for i in range(m):
-    a, b = map(int, input().split())
-    link[a - 1].append(b - 1)
-    link[a - 1].append(a - 1)
+def dij(start, goal):
+    dist = [float('inf') for i in range(num_v)]
+    dist[start] = 0
+    pq = [(0, start)]
 
-dist = [inf] * n
-q = deque(zombie)
-
-# ゾンビ占領地域はinf → 0
-for v in zombie:
-    dist[v] = 0
-while q:
-    node = q.popleft()
-    for next in link[node]:
-        if dist[next] != inf:
+    # pqの先頭がgoal行きのものなら最短距離を返す
+    while (pq[0][1] != goal):
+        d, now = heapq.heappop(pq)
+        if (d > dist[now]):
             continue
-        # ゾンビの出発先 = ゾンビの出発元 + 1
-        dist[next] = dist[node] + 1
-        q.append(next)
-
-cost = [p] * n
-for i in range(n):
-    # infじゃなければ
-    if dist[i] <= s:
-        cost[i] = q
-    if n in zombie:
-        cost[i] = inf
-    # 最初と最後は宿泊料いらない
-    if i == 0 or i == n - 1:
-        cost[i] = 0
-# startとgoal
-def dij(s, g):
-    dist = [inf for i in range(n)]
-    dist[s] = 0
-    pq = [(0, s)]
-    # (0, s)の後ろの部分がgになったら
-    while (pq[0][1] != g):
-        d, node = heapq.heappop(pq)
-        print([d, node])
-        if (d > dist[node]):
-            continue
-        for next in link[node]:
-            # nextに行くためにかかるコスト
-            c = cost[next]
-            if d + c < dist[next]:
-                dist[next] = d + c
-                heapq.heappush(pq, (dist[next], next))
+        for i in edges[now]:
+            if dist[i[0]] > dist[now] + i[1]:
+                dist[i[0]] = dist[now] + i[1]
+                heapq.heappush(pq, (dist[i[0]], i[0]))
     return pq[0][0]
-print (dij(0, n - 1))
+
+edges = [
+         [[1, 4], [2, 3], [3, 9]],
+         [[0, 4], [2, 9]],
+         [[0, 3], [1, 9], [3, 2], [4, 5]],
+         [[0, 9], [2, 2], [4, 1]],
+         [[2, 5], [3, 1]]
+         ]
+print (dij(0, 4))
