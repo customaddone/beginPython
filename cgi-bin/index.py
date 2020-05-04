d9b52e982ea4886b97fd369fb89de066fa52e288
customaddone <custom1485@docomo.ne.jp>
2020-05-04 10:17:42 +0900

    069 AtCoder Beginner Contest 084 D - 2017-like Number

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,22 +1,42 @@
-# http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=NTL_1_A&lang=ja
+# https://atcoder.jp/contests/abc084/tasks/abc084_d
 import math
-def prime_factorize(n):
-    divisors = []
-    # 27(2 * 2 * 7)の7を出すためにtemp使う
-    temp = n
-    for i in range(2, int(math.sqrt(n)) + 1):
-        if temp % i == 0:
-            cnt = 0
-            while temp % i == 0:
-                cnt += 1
-                # 素因数を見つけるたびにtempを割っていく
-                temp //= i
-            divisors.append([i, cnt])
-    if temp != 1:
-        divisors.append([temp, 1])
-    if divisors == []:
-        divisors.append([n, 1])
 
-    return divisors
-n = int(input())
-print(prime_factorize(n))
+prime = [2]
+max = 10 ** 5
+limit = int(math.sqrt(max))
+data = [i + 1 for i in range(2, max, 2)]
+
+# エラストテネスの篩
+while limit > data[0]:
+    prime.append(data[0])
+    data = [j for j in data if j % data[0] != 0]
+prime = prime + data
+
+# if (i + 1) / 2 in primeのにぶたん
+def binary_search_loop(data, target):
+    imin = 0
+    imax = len(data) - 1
+    while imin <= imax:
+        imid = imin + (imax - imin) // 2
+        if target == data[imid]:
+            return True
+        elif target < data[imid]:
+            imax = imid - 1
+        else:
+            imin = imid + 1
+    return False
+
+# 累積和
+lista = [0] * max
+for i in prime:
+    if binary_search_loop(prime, (i + 1) / 2):
+        lista[i - 1] = 1
+listb = [0]
+for i in range(max):
+    listb.append(lista[i] + listb[i])
+
+# rまでの累積 - l - 1までの累積
+q = int(input())
+for i in range(q):
+    l, r = map(int, input().split())
+    print(listb[r] - listb[l - 1])
