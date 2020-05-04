7497746a75e4406b31f7a57d504c7429bbb24c7a
customaddone <custom1485@docomo.ne.jp>
2020-05-04 10:00:55 +0900

    068 NTL_1_A - 素因数分解

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,57 +1,22 @@
-# https://www.ioi-jp.org/camp/2010/2010-sp-tasks/2010-sp-day3_22.pdf
-class UnionFind():
-    def __init__(self, n):
-        self.n = n
-        self.parents = [-1] * n
+# http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=NTL_1_A&lang=ja
+import math
+def prime_factorize(n):
+    divisors = []
+    # 27(2 * 2 * 7)の7を出すためにtemp使う
+    temp = n
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if temp % i == 0:
+            cnt = 0
+            while temp % i == 0:
+                cnt += 1
+                # 素因数を見つけるたびにtempを割っていく
+                temp //= i
+            divisors.append([i, cnt])
+    if temp != 1:
+        divisors.append([temp, 1])
+    if divisors == []:
+        divisors.append([n, 1])
 
-    def find(self, x):
-        if self.parents[x] < 0:
-            return x
-        else:
-            self.parents[x] = self.find(self.parents[x])
-            return self.parents[x]
-
-    def union(self, x, y):
-        x = self.find(x)
-        y = self.find(y)
-
-        if x == y:
-            return
-
-        if self.parents[x] > self.parents[y]:
-            x, y = y, x
-
-        self.parents[x] += self.parents[y]
-        self.parents[y] = x
-
-    def same(self, x, y):
-        return self.find(x) == self.find(y)
-
-n,m,k = map(int,input().split())
-lines = [tuple(map(int,input().split())) for _ in range(m)]
-lines = sorted(lines, key = lambda x: x[2])
-
-uf = UnionFind(n)
-
-ans = 0
-tree_num = n
-for line in lines:
-    if(tree_num == k):
-        break
-
-    a,b,c = line
-    a,b = a - 1,b - 1
-    if(uf.same(a, b)):
-        continue
-    uf.union(a, b)
-    ans += c
-    tree_num -= 1
-
-print(ans)
-
-'''
-方針
-N個の都市を、木がK個の森にする。
-最小全域木と同じようなアルゴリズムで、木がK個になったタイミングで打ち切ればOK。
-その時に採用された通路のコスト合計が答え。
-'''
+    return divisors
+n = int(input())
+print(prime_factorize(n))
