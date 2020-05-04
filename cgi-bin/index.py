d0daa1c2780cf4e7fa406329d0907b83e2cf026f
customaddone <custom1485@docomo.ne.jp>
2020-05-04 12:00:34 +0900

    072 AtCoder Beginner Contest 034 C - 経路

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,24 +1,11 @@
-# https://atcoder.jp/contests/s8pc-1/tasks/s8pc_1_e
-n, q = map(int, input().split())
-A = list(map(int, input().split()))
-C = list(map(int, input().split()))
-
+# https://atcoder.jp/contests/abc034/tasks/abc034_c
+w, h = map(int, input().split())
 mod = 10 ** 9 + 7
 
-# 全通り使いそうなのであらかじめ列挙しとく
-# べき乗にはpow便利
-distance = [pow(A[i], A[i + 1], mod) for i in range(n - 1)]
-# 累積和
-distsum = [0]
-for i in range(n - 1):
-    distsum.append((distsum[i] + distance[i]) % mod)
+def cmb(x,y):
+    r=1
+    for i in range(1, y + 1):
+        r = (r * (x - i + 1) * pow(i, mod - 2, mod)) % mod
+    return r
 
-start = 0
-sum = 0
-for i in C:
-    walk = distsum[max(i - 1, start)] - distsum[min(i - 1, start)]
-    sum += walk
-    start = i - 1
-# 最後帰る時
-sum += distsum[max(start, 0)] - distsum[min(start, 0)]
-print(sum % mod)
+print(cmb(w + h - 2, h - 1))
