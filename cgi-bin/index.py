d36190d999fb6537f20daa24c506ebdbeba0d64c
customaddone <custom1485@docomo.ne.jp>
2020-05-04 12:19:59 +0900

    073 AtCoder Beginner Contest 145 D - Knight

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,11 +1,21 @@
-# https://atcoder.jp/contests/abc034/tasks/abc034_c
-w, h = map(int, input().split())
-mod = 10 ** 9 + 7
-
+# https://atcoder.jp/contests/abc145/tasks/abc145_d
+m = 10 ** 9 + 7
 def cmb(x,y):
-    r=1
+    r = 1
     for i in range(1, y + 1):
-        r = (r * (x - i + 1) * pow(i, mod - 2, mod)) % mod
+        r = (r * (x - i + 1) * pow(i, m-2, m)) % m
     return r
 
-print(cmb(w + h - 2, h - 1))
+x, y = map(int, input().split())
+
+# a:(i + 1, j + 2), a:(i + 1, j + 2)してx, yに辿り着きたい
+# a, bのそれぞれの回数は
+# a + 2b = x 2a + b = yを因数分岐して求める
+a = (2 * x - y) / 3
+b = (2 * y - x) / 3
+# あとはa, bをどの順番で配置するか → a + b C a
+# aもbもマイナスになるパターンがある
+if (x + y) % 3 == 0 and a >= 0 and b >= 0:
+    print(cmb(int((x + y) / 3), int(a)))
+else:
+    print(0)
