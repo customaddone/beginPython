cd2ae4cba22e4086296701787aae00136c63b7e0
customaddone <custom1485@docomo.ne.jp>
2020-05-04 15:45:46 +0900

    077 JOI 2010 本選 1 - 旅人

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,18 +1,22 @@
-# https://atcoder.jp/contests/nikkei2019-final/tasks/nikkei2019_final_a
-n = int(input())
-A = list(map(int, input().split()))
-# 累積和
+# https://www.ioi-jp.org/joi/2009/2010-ho-prob_and_sol/2010-ho.pdf#page=2
+# MLE対策
+import sys
+def input(): return sys.stdin.readline()[:-1]
+
+n, m = map(int, input().split())
+lista = [int(input()) for i in range(n - 1)]
+listb = [int(input()) for i in range(m)]
 listsum = [0]
-for i in range(n):
-    listsum.append(listsum[i] + A[i])
-ans = []
-for i in range(n):
-    res = 0
-    # 範囲はlistsum(n + 1) - 幅の大きさ(i + 1)
-    for j in range(n - i):
-        # listsum[j + 幅] - listsum[i]
-        opt = listsum[j + i + 1] - listsum[j]
-        res = max(res, opt)
-    ans.append(res)
-for i in ans:
-    print(i)
+
+for i in range(n - 1):
+    # 宿場街1からの距離
+    listsum.append(listsum[i] + lista[i])
+
+now = 0
+sum = 0
+for i in listb:
+    # 逆行する時はマイナスになるのでabsする
+    # modを忘れるな
+    sum += (abs(listsum[now + i] - listsum[now]) % (10 ** 5))
+    now += i
+print(sum % (10 ** 5))
