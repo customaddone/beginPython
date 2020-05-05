89db78147c0f397854fdbf67018dae1681246f31
customaddone <custom1485@docomo.ne.jp>
2020-05-05 11:57:12 +0900

    085 DSL_1_A - 互いに素な集合

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,14 +1,38 @@
-# https://atcoder.jp/contests/abc014/tasks/abc014_3
-n = int(input())
-lista = []
-for i in range(n):
-    a, b = map(int, input().split())
-    lista.append([a, b])
-listb = [0] * (10 ** 6 + 2)
-for i in lista:
-    listb[i[0]] += 1
-    listb[i[1] + 1] -= 1
-listc = [0]
-for i in range(10 ** 6 + 2):
-    listc.append(listb[i] + listc[i])
-print(max(listc))
+# http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_1_A&lang=ja
+class UnionFind():
+    def __init__(self, n):
+        self.n = n
+        self.parents = [-1] * n
+
+    def find(self, x):
+        if self.parents[x] < 0:
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
+
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+
+        if x == y:
+            return
+
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
+
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+
+n, m = map(int, input().split())
+lista = [list(map(int, input().split())) for i in range(m)]
+# UnionFind構築
+U = UnionFind(n)
+for query in lista:
+    if query[0] == 0:
+        U.union(query[1], query[2])
+    else:
+        print(1 if U.same(query[1], query[2]) else 0)
