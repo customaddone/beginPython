86491e1cd536096836cd13a3e945ecc72bbe3a64
customaddone <custom1485@docomo.ne.jp>
2020-05-06 19:30:49 +0900

    ABC138 ki dfs

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,14 +1,31 @@
-# https://atcoder.jp/contests/abc146/tasks/abc146_c
-a, b, x = map(int, input().split())
+import sys
+sys.setrecursionlimit(10 ** 6)
+import sys
+def input():
+    return sys.stdin.readline()[:-1]
+N , Q = map(int,input().split())
+graph = [[] for _ in range(N)]
+point = [0] * N
+for _ in range(N - 1):
+    a , b = map(int,input().split())
+    graph[a - 1].append(b - 1)
+    graph[b - 1].append(a - 1)
+#print(graph)
+for _ in range(Q):
+    a , b = map(int,input().split())
+    a = a - 1
+    point[a] += b
+# dfsを用いて累積和を計算する
+# 初期状態だと前の値がないためデフォルト引数に-1を代入
+def dfs(now , prev = -1):
+    for next in graph[now]:
+        # 次のノードが前に参照した値の時はcontinue
+        if next == prev:
+            continue
+        # 現在の値を次のポイントに加算することで累積和をとる
+        point[next] += point[now]
+        # 次のノードと現在のノードを引数にdfsを継続する
+        dfs(next , now)
 
-ok = 0
-ng = 10 ** 9 + 1
-# OKになるギリギリのラインを攻める
-# 最後の一回でabs(ok - ng) <= 1になってもokは書き換えられない
-while abs(ok - ng) > 1:
-    mid = (ok + ng) // 2
-    if a * mid + b * len(str(mid)) <= x:
-        ok = mid
-    else:
-        ng = mid
-print(ok)
+dfs(0)
+print(*point)
