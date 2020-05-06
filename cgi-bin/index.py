2b3251b048594d73b4a97177cfe670b4debe164c
customaddone <custom1485@docomo.ne.jp>
2020-05-06 13:01:05 +0900

    ABC146 C - Buy an Integer 二分探索 #2

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,20 +1,13 @@
 # https://atcoder.jp/contests/abc146/tasks/abc146_c
 a, b, x = map(int, input().split())
 
-def solve(mid):
-    ans = a * mid + b * len(str(mid))
-    if ans <= x:
-        return True
-    else:
-        return False
-
 ok = 0
 ng = 10 ** 9 + 1
 # OKになるギリギリのラインを攻める
 # 最後の一回でabs(ok - ng) <= 1になってもokは書き換えられない
 while abs(ok - ng) > 1:
     mid = (ok + ng) // 2
-    if solve(mid):
+    if a * mid + b * len(str(mid)) <= x:
         ok = mid
     else:
         ng = mid
