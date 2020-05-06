e0aee7fc0308237f724778593f42c2db039be464
customaddone <custom1485@docomo.ne.jp>
2020-05-06 22:01:47 +0900

    ABC137 C-Greenbin dictãでのカウントの仕方

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,31 +1,15 @@
-import sys
-sys.setrecursionlimit(10 ** 6)
-import sys
-def input():
-    return sys.stdin.readline()[:-1]
-N , Q = map(int,input().split())
-graph = [[] for _ in range(N)]
-point = [0] * N
-for _ in range(N - 1):
-    a , b = map(int,input().split())
-    graph[a - 1].append(b - 1)
-    graph[b - 1].append(a - 1)
-#print(graph)
-for _ in range(Q):
-    a , b = map(int,input().split())
-    a = a - 1
-    point[a] += b
-# dfsを用いて累積和を計算する
-# 初期状態だと前の値がないためデフォルト引数に-1を代入
-def dfs(now , prev = -1):
-    for next in graph[now]:
-        # 次のノードが前に参照した値の時はcontinue
-        if next == prev:
-            continue
-        # 現在の値を次のポイントに加算することで累積和をとる
-        point[next] += point[now]
-        # 次のノードと現在のノードを引数にdfsを継続する
-        dfs(next , now)
-
-dfs(0)
-print(*point)
+N = int(input())
+# 結合
+s = ["".join(sorted(input())) for i in range(N)]
+# dict使ってハッシュ使っていい感じにカウントできる
+D = dict()
+for st in s:
+    if st in D:
+        D[st] += 1
+    else:
+        D[st] = 1
+ans=0
+for i in D:
+    k = D[i]
+    ans += (k * (k-1)) // 2
+print(ans)
