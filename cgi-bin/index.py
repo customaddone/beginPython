b10c2bc0aa37d34cfe15fa1b22ea83ba576bd1df
customaddone <custom1485@docomo.ne.jp>
2020-05-09 16:17:35 +0900

    003 AtCoder Beginner Contest 122 B - ATCoder

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,22 +1,22 @@
-# https://atcoder.jp/contests/abc106/tasks/abc106_b
-# （条件)の個数を求めよ
-# N <= 200なので全探索できる
-import math
-N = int(input())
-
-# 約数の数を出してくれる
-def make_divisors(n):
-    divisors = []
-    for i in range(1, int(math.sqrt(n)) + 1):
-        if n % i == 0:
-            divisors.append(i)
-            if i != n // i:
-                divisors.append(n // i)
-    return len(divisors)
-
-cnt = 0
-# 1からNまでの奇数
-for i in range(1, N + 1, 2):
-    if make_divisors(i) == 8:
-        cnt += 1
-print(cnt)
+# https://atcoder.jp/contests/abc122/tasks/abc122_b
+# 最も長いACGT文字列の長さを求める →　前から条件についてジャッジして、NGなら0にする
+# 最長共通部分列、最長増加部分列が思い浮かぶ
+# 前から条件についてジャッジして、NGなら0にする
+s = input()
+l = ['A', 'T', 'G', 'C']
+n = len(s)
+# 現在の長さ
+num = 0
+# 一番の長さ
+ans = 0
+for i in range(n):
+    if s[i] in l:
+        num += 1
+    else:
+         # ansの更新
+        # numのリセット
+        ans = max(num, ans)
+        num = 0
+# 最後の一つが'A','T','G','C'だった時用
+ans = max(num, ans)
+print(ans)
