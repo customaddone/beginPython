6cb5401ccfc4a8a605dbe7cf7f71843018886ca1
customaddone <custom1485@docomo.ne.jp>
2020-05-09 16:02:51 +0900

    002 AtCoder Beginner Contest 106 B - 105

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,35 +1,22 @@
-# 組み合わせの数を求める
-# → DP?
-# but 重複無しで３つの数を選び
-# 一つ目の数i,二つ目の数jを決めると3つ目はx - i - j になる
-# x - i - j がj ~ xの中にあるか
-# 選択する数が二つなら二重ループでいける
+# https://atcoder.jp/contests/abc106/tasks/abc106_b
+# （条件)の個数を求めよ
+# N <= 200なので全探索できる
+import math
+N = int(input())
 
-N, X = map(int, input().split())
-ans = 0
-for i in range(1, N + 1):
-    for j in range(i + 1, N + 1):
-        k = X - i - j
-        if j < k <= N:
-            ans += 1
-print(ans)
-# 1 ~ Nから○個選んでXを作る問題はだいたいDP
-# 個数制限あり部分和問題再帰
-# 個数制限亜ありの場合再帰でやるとややめんどい
-N, X = map(int, input().split())
-lista = [i for i in range(1, N + 1)]
-dp = [[0] * (X + 1) for i in range(N)]
-ans = 0
-# 引数にplusが増える
-def rec_memo(i, plus, sum):
-    global ans
-    if i == N or plus == 3:
-        if plus == 3:
-            ans += (sum == X)
-    elif X - sum < lista[i]:
-        rec_memo(i + 1, plus, sum)
-    else:
-        rec_memo(i + 1, plus, sum)
-        rec_memo(i + 1, plus + 1, sum + lista[i])
-rec_memo(0, 0, 0)
-print(ans)
+# 約数の数を出してくれる
+def make_divisors(n):
+    divisors = []
+    for i in range(1, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            divisors.append(i)
+            if i != n // i:
+                divisors.append(n // i)
+    return len(divisors)
+
+cnt = 0
+# 1からNまでの奇数
+for i in range(1, N + 1, 2):
+    if make_divisors(i) == 8:
+        cnt += 1
+print(cnt)
