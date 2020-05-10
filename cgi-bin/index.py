fd25d03d7e9bd5880069b09b0b3c614b1c4295a8
customaddone <custom1485@docomo.ne.jp>
2020-05-10 14:34:38 +0900

    ABC015 C - 高橋くんのバグ探し

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,32 +1,17 @@
-N = int(input())
-A = list(map(int, input().split()))
-alta = []
+import itertools
+N, K = map(int, input().split())
+T = []
 for i in range(N):
-    alta.append(A[i] - (i + 1))
-# 最小値を0に揃えないとバグる
-plus = min(alta)
-for i in range(N):
-    alta[i] -= plus
-
-def f(x):
-    sum = 0
-    for i in alta:
-        sum += abs(x - i)
-    return sum
-
-# 極地があるので三分探索使う
-left, right = 0, max(alta)
-# 答えが整数になる場合このrightとleftの幅を3未満にするとバグる
-while abs(right - left) > 3:
-    mid1 = (right * 2 + left) // 3 + 1
-    mid2 = (right + left * 2) // 3
-    if f(mid1) >= f(mid2):
-        # 上限を下げる（最小値をとるxはもうちょい下めの数だな）
-        right = mid1
-    else:
-        # 下限を上げる（最小値をとるxはもうちょい上めの数だな）
-        left = mid2
-ans = float('inf')
-for i in range(left, right + 1):
-    ans = min(ans, f(i))
-print(ans)
+    t = list(map(int, input().split()))
+    T.append(t)
+flag = True
+# 各配列のデカルト積
+for ps in itertools.product(*T):
+    x1 = ps[0]
+    for i in range(1, N):
+        x2 = ps[i]
+        # 排他的論理和 x1, x2に含まれるもの - x1, x2両方に含まれるもの
+        x1 = x1 ^ x2
+    if x1 == 0:
+        flag = False
+print('Nothing' if flag else 'Found')
