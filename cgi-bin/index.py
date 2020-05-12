f37ebb25182461009199ee6620f2c107eb6d3525
customaddone <custom1485@docomo.ne.jp>
2020-05-12 22:54:00 +0900

    ABC039 D - 画像処理高橋君

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -17,22 +17,48 @@ from bisect import bisect_left, bisect_right
 import sys
 sys.setrecursionlimit(1000000000)
 # list(map(int, input().split()))
-mod = 10 ** 9 + 7
+H, W = getNM()
+maze = [list(input()) for i in range(H)]
+prevmaze = [['.'] * W for i in range(H)]
+# 8方向のベクトル
+dx = [0, 1, 1, 1, 0, -1, -1, -1]
+dy = [1, 1, 0, -1, -1, -1, 0, 1]
 
-N, Q = getNM()
-board = [0] * (N + 1)
-boardalta = [0]
-# ひっくり返す(+=1)何もしない(-=1)で累積和
-for i in range(Q):
-    l, r = getNM()
-    board[l - 1] += 1
-    # board[r + 1]より向こうは何も操作しない
-    board[r] -= 1
-for i in range(N):
-    # 累積和の亜種
-    if (boardalta[i] + board[i]) % 2 == 0:
-        boardalta.append(0)
-    else:
-        boardalta.append(1)
-# joinはlistでは使えないためmapでstrに
-print(''.join(map(str, boardalta[1:])))
+# 関数を小分けにしてみよう
+def alter(x, y):
+    for i in range(8):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < W and 0 <= ny < H and prevmaze[ny][nx] == ".":
+            prevmaze[ny][nx] = "#"
+
+grass = []
+
+def rounder(x, y):
+    flag = True
+    for i in range(8):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < W and 0 <= ny < H:
+            if maze[ny][nx] == ".":
+                flag = False
+    if flag:
+        grass.append([x, y])
+        prevmaze[y][x] = "#"
+
+for i in range(W):
+    for j in range(H):
+        if maze[j][i] == "#":
+            rounder(i, j)
+
+anscopy = copy.deepcopy(prevmaze)
+
+for i in grass:
+    px, py = i[0], i[1]
+    alter(px, py)
+if (maze == prevmaze):
+    print('possible')
+    for i in anscopy:
+        print(''.join(map(str, i)))
+else:
+    print('impossible')
