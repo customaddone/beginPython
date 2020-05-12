ed53a4bd952d99151e4a176c0762ffa85489e4c9
customaddone <custom1485@docomo.ne.jp>
2020-05-12 20:09:54 +0900

    ABC099 C Strange bank 貰うdp

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -15,28 +15,31 @@ import copy
 from bisect import bisect_left, bisect_right
 
 import sys
-# sys.setrecursionlimit(1000000)
+sys.setrecursionlimit(1000000000)
 # list(map(int, input().split()))
 mod = 10 ** 9 + 7
 
-# K以上はそれ以降の何が足されようが同じ
-# K以下とそれ以上にグルーピング
-# ここまで足すとK以上になる点を求める→二分探索
-N, K = getNM()
-A = getList()
+N = getN()
+splitlist = [1]
+splitsix = 6
+while splitsix < 100000:
+    splitlist.append(splitsix)
+    splitsix *= 6
+splitnine = 9
+while splitnine < 100000:
+    splitlist.append(splitnine)
+    splitnine *= 9
+splitlist.sort()
+# これやらないと再帰が無限に実行される
+splitlist.reverse()
 
-ans = 0
-# 累積和
-listalta = [0]
-for i in range(N):
-    # i個目まで足し合わせるとlistalta[i]になる
-    # 累積和は最初何も足し合わせない時の値(0)が入る
-    listalta.append(listalta[i] + A[i])
-for i in range(N):
-    # i + 1が連続部分列の起点
-    # listalta[i]: i + 1以前（1 ~ iまで）を足した合計
-    # index: i + 1からindexまで足し合わせるとギリギリK以上になる点
-    index = bisect_left(listalta, K + listalta[i])
-    if index <= K:
-        ans += N - index + 1
-print(ans)
+ans = float('inf')
+dp = [float('inf')] * (N + 1)
+dp[0] = 0
+
+# 貰うdp
+for i in range(N + 1):
+    for k in splitlist:
+        if i >= k:
+            dp[i] = min(dp[i], dp[i - k] + 1)
+print(dp[-1])
