312241d8bf8b8e664c4e16b77a4dc3eb40938283
customaddone <custom1485@docomo.ne.jp>
2020-05-13 20:49:37 +0900

    ABC048 C - Boxes and Candies

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -16,49 +16,28 @@ from bisect import bisect_left, bisect_right
 
 import sys
 sys.setrecursionlimit(1000000000)
-# list(map(int, input().split()))
-H, W = getNM()
-maze = [list(input()) for i in range(H)]
-prevmaze = [['.'] * W for i in range(H)]
-# 8方向のベクトル
-dx = [0, 1, 1, 1, 0, -1, -1, -1]
-dy = [1, 1, 0, -1, -1, -1, 0, 1]
 
-# 関数を小分けにしてみよう
-def alter(x, y):
-    for i in range(8):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < W and 0 <= ny < H and prevmaze[ny][nx] == ".":
-            prevmaze[ny][nx] = "#"
-
-grass = []
-
-def rounder(x, y):
-    flag = True
-    for i in range(8):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < W and 0 <= ny < H:
-            if maze[ny][nx] == ".":
-                flag = False
-    if flag:
-        grass.append([x, y])
-        prevmaze[y][x] = "#"
-
-for i in range(W):
-    for j in range(H):
-        if maze[j][i] == "#":
-            rounder(i, j)
-
-anscopy = copy.deepcopy(prevmaze)
-
-for i in grass:
-    px, py = i[0], i[1]
-    alter(px, py)
-if (maze == prevmaze):
-    print('possible')
-    for i in anscopy:
-        print(''.join(map(str, i)))
-else:
-    print('impossible')
+N, X = getNM()
+A = getList()
+ans = 0
+flag = True
+while flag:
+    flag = False
+    for i in range(N - 2):
+        # A[i1とA[i + 1]の過剰分
+        minus1 = A[i] + A[i + 1] - X
+        # A[i + 1]とA[i + 2]の過剰分
+        minus2 = A[i + 1] + A[i + 2] - X
+        if minus1 > 0 and minus2 > 0:
+            # minus1とminus2が両方プラスなら一個食べるだけでminu1とminu2の分を
+            # 両方減らせる
+            minuspoint = min(minus1, minus2, A[i + 1])
+            A[i + 1] -= minuspoint
+            ans += minuspoint
+            # これをminu1,minu2が両方プラスになる部分（お得な点)がなくなるまで繰り返す
+            flag = True
+between = [A[i] + A[i + 1] - X for i in range(N - 1)]
+for i in between:
+    if i > 0:
+        ans += i
+print(ans)
