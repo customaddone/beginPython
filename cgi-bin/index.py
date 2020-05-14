d95151d27784a0e17c2e1636f5d106ceeda3d2c9
customaddone <custom1485@docomo.ne.jp>
2020-05-14 21:40:47 +0900

    ABC070 D - Transit Tree Path

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -6,6 +6,8 @@ def getList():
     return list(map(int, input().split()))
 def getArray(intn):
     return [int(input()) for i in range(intn)]
+def input():
+    return sys.stdin.readline().rstrip()
 
 from collections import defaultdict, deque, Counter
 from sys import exit
@@ -17,21 +19,35 @@ from bisect import bisect_left, bisect_right
 import sys
 sys.setrecursionlimit(1000000000)
 
-H, W = getNM()
 N = getN()
-A = getList()
-dist = []
-for i in range(N):
-    for j in range(A[i]):
-        dist.append(i + 1)
-distalta = [[0] * W for i in range(H)]
-for i in range(H * W):
-    # ジグザグに降りていく感じで縦と横のインデックスを決める
-    hei = i // W
-    if hei % 2 == 0:
-        wid = i % W
-    else:
-        wid = -1 * (i % W) - 1
-    distalta[hei][wid] = dist[i]
-for i in distalta:
-    print(*i)
+dist = [[] for i in range(N + 1)]
+for i in range(N - 1):
+    a, b, c = getNM()
+    dist[a].append([b, c])
+    dist[b].append([a, c])
+ignore = [-1] * (N + 1)
+
+# Kからの最短距離をbfsで測る
+# 必ずKを経由するのがポイント
+# 前処理O(n)だけですむ
+def distance(sta):
+    # 木をKから順にたどる（戻るの禁止）
+    pos = deque([sta])
+
+    while len(pos) > 0:
+        u = pos.popleft()
+        for i in dist[u]:
+            if ignore[i[0]] == -1:
+                ignore[i[0]] = ignore[u] + i[1]
+                pos.append(i[0])
+
+Q, K = getNM()
+ignore[K] = 0
+distance(K)
+# 答えはK~xまでの距離+K~yまでの距離
+ans = []
+for i in range(Q):
+    x, y = getNM()
+    ans.append(ignore[x] + ignore[y])
+for i in ans:
+    print(i)
