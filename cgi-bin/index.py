e4872b9ec0413218d135fedcc88da957f4b0b7c7
customaddone <custom1485@docomo.ne.jp>
2020-05-15 22:15:40 +0900

    ABC094 D - Binomial Coefficients

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -14,24 +14,23 @@ from sys import exit
 import heapq
 import math
 import copy
+from operator import mul
+from functools import reduce
 from bisect import bisect_left, bisect_right
 
 import sys
 sys.setrecursionlimit(1000000000)
 mod = 10 ** 9 + 7
 
-N, K = getNM()
-sum = 0
-# わる数bを1〜Nに固定して考える
-# 例えばN = 1 ~ 10をb = 3で割る時
-# N      1  2  3   4  5  6   7  8  9   10
-# N % b  1  2  0|  1  2  0|  1  2  0|  1
-# あまり0, 1, 2が10 // 3 = 3回現れ、最後にあまり1が１回現れる
-for b in range(1, N + 1):
-    opt1 = (N // b) * max(0, (b - K))
-    if K == 0:
-        opt2 = N % b
-    else:
-        opt2 = max(0, (N % b) - K + 1)
-    sum += (opt1 + opt2)
-print(sum)
+N = getN()
+A = getList()
+A.sort()
+
+max = max(A)
+index = bisect_left(A, max / 2)
+# nCrのrはnの真ん中に行くほど強い
+if abs((max / 2) - A[index]) < abs((max / 2) - A[index - 1]):
+    ans = [max, A[index]]
+else:
+    ans = [max, A[index - 1]]
+print(*ans)
