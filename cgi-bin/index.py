8873c3c5ce2f82abba0a6f822072df1cdc2605a7
customaddone <custom1485@docomo.ne.jp>
2020-05-16 10:27:06 +0900

    ABC084 C - Special Trains

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -22,24 +22,32 @@ import sys
 sys.setrecursionlimit(1000000000)
 mod = 10 ** 9 + 7
 
-N, K = getNM()
-X = getList()
-ans = float('inf')
+N = getN()
+train = []
 
-# 一直線に行くのが一番いいやり方
-for i in range(N - K + 1):
-    # もしX[i],X[i + K - 1]とも0以下の場合
-    # 最短経路は0 ~ (X[i + K - 1]) ~ X[i]まで
-    if X[i] <= 0 and X[i + K - 1] <= 0:
-        ans = min(ans, -X[i])
-    # もしX[i] < 0 <= X[i + K - 1]の場合
-    # 最短経路は
-    # X[i] ~ 0, 0 ~ X[i + K - 1]のうち近い方
-    # + X[i] ~ X[i + K - 1]
-    elif X[i] < 0 <= X[i + K - 1]:
-        ans = min(ans, min(-X[i], X[i + K - 1]) + (X[i + K - 1] - X[i]))
-    # もしX[i],X[i + K - 1]とも0以上の場合
-    # 最短経路は0 ~ (X[i]) ~ X[i + K - 1]まで
-    else:
-        ans = min(ans, X[i + K - 1])
-print(ans)
+for i in range(N - 1):
+    c, s, f = getNM()
+    train.append([c, s, f])
+
+# sta~Nまで行くのにかかる時間を求める関数
+def gotowest(sta):
+    # sta ~ sta + 1まで
+    time = train[sta][0] + train[sta][1]
+    # sta + 1~ Nまで
+    for j in range(sta + 1, N - 1):
+        if time <= train[j][1]:
+            # もし到着した時間が一本目の列車が出発する前なら
+            time = train[j][0] + train[j][1]
+        else:
+            # 最初の一本のその次の便(train[j][1] + train[j][2]分後出発)から数えてwait本目の列車に乗る
+            wait = ((time - train[j][1]) + train[j][2] - 1) // train[j][2]
+            time = train[j][1] + (train[j][2] * wait) + train[j][0]
+    return time
+
+ans = []
+for i in range(N - 1):
+    ans.append(gotowest(i))
+# N~Nまで（０分）
+ans.append(0)
+for i in ans:
+    print(i)
