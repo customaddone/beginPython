fbcd2570fa8fc01a353fd83df3e4b67cd2e4df4e
customaddone <custom1485@docomo.ne.jp>
2020-05-16 13:12:24 +0900

    ABC109 D - Make Them Even

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -22,33 +22,29 @@ import sys
 sys.setrecursionlimit(1000000000)
 mod = 10 ** 9 + 7
 
-# O(n)までOK a, b, cどれかを固定（ループ）させる
-N, K = getNM()
-# Kで割ってi余る数リスト
-lista = [0] * K
-ans = 0
-for i in range(1, N + 1):
-    lista[i % K] += 1
-# 例えばN = 5, K = 3のとき1と４、2と5は同じものとして扱う
-for a in range(K):
-    # a（あまりがaであるもののどれか）に対応するb, cは
-    # b == k - a(mod K)
-    b = (K - a) % K
-    c = (K - a) % K
-    if (b + c) % K == 0:
-        ans += lista[a] * lista[b] * lista[c]
-print(ans)
+H, W = getNM()
+maze = []
+for i in range(H):
+    a = getList()
+    maze.append(a)
 
-# O(1)でやる場合
-# a + b and b + c and c + a (mod k) ⇄
-# a == b == c (mod k)
-# また a + b == 0 と上記 a == bより
-# a + b == 0 (mod K) ⇄ a + a == (mod K)
-# ⇄ 2a == 0 (mod K)
+ans = []
 
-# Kが奇数のとき
-# Kの約数に2がないのでaがKで割り切れる必要がある
-# よってKが奇数のとき問題の条件を満たすのはx ** 3通りある
+# 部屋の掃除の要領で奇数の1を右端に運ぶ
+for i in range(H):
+    for j in range(W - 1):
+        if maze[i][j] % 2 == 1:
+            maze[i][j] -= 1
+            maze[i][j + 1] += 1
+            ans.append([i + 1, j + 1, i + 1, j + 2])
 
-# Kが偶数の時
-# aがKで割り切れる、またはあまりがK / 2である必要がある
+# 右端の1を右下に運ぶ
+for i in range(H - 1):
+    if maze[i][-1] % 2 == 1:
+        maze[i][-1] -= 1
+        maze[i + 1][-1] += 1
+        ans.append([i + 1, W, i + 2, W])
+
+print(len(ans))
+for i in ans:
+    print(*i)
