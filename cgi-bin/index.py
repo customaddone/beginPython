ae1c1c7cc514c43dc0ef07a26b61722822041308
customaddone <custom1485@docomo.ne.jp>
2020-05-16 17:43:36 +0900

    ABC112 C - Pyramid

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -22,9 +22,29 @@ import sys
 sys.setrecursionlimit(1000000000)
 mod = 10 ** 9 + 7
 
-N, M = getNM()
-optmax = M // N
-for i in range(optmax, 0, -1):
-    if M % i == 0:
-        print(i)
-        break
+N = getN()
+info = []
+for i in range(N):
+    x, y, h = getNM()
+    info.append([x, y, h])
+info.sort(reverse = True,key = lambda info:info[2])
+
+ans = []
+for i in range(101):
+    for j in range(101):
+        height = -1
+        flag = True
+        for k in range(N):
+            minusx = abs(i - info[k][0])
+            minusy = abs(j - info[k][1])
+            if height == -1:
+                # 上で逆順にソートしてるので確実に１以上の高さがでる
+                height = info[k][2] + minusx + minusy
+            else:
+                # 問題の式に沿うように
+                if info[k][2] != max(height - minusx - minusy, 0):
+                    flag = False
+                    break
+        if flag:
+            ans = [i, j, height]
+print(*ans)
