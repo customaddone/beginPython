0ae2ce731f4a668cc4f9f46a0a1e5e506acd41cb
customaddone <custom1485@docomo.ne.jp>
2020-05-16 23:25:19 +0900

    ABC124 D - Handstand

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -20,17 +20,34 @@ from bisect import bisect_left, bisect_right
 
 import sys
 sys.setrecursionlimit(1000000000)
-mod = 10 ** 9 + 7
 
-N = getN()
-A = getList()
+N, K = getNM()
+S = input()
 
-numset = set()
-def bispliter(n):
-    splitint = n
-    while splitint % 2 == 0:
-        splitint //= 2
-    return splitint
-for i in A:
-    numset.add(bispliter(i))
-print(len(numset))
+flag = S[0]
+sta = 1
+lista = []
+# 14 2
+# 11101010110011なら
+# ２回まで0の箇所を消すことができる
+
+# 00010を['0', 1, 3], ['1', 4, 4], ['0', 5, 5]という風に整頓する
+for i in range(1, N):
+    if flag != S[i]:
+        # flag(0か1か), sta(どこから続いているか), i(最終的にどこまで続いたか)
+        lista.append([flag, sta, i])
+        flag = S[i]
+        sta = i + 1
+lista.append([flag, sta, N])
+ans = 0
+np = len(lista)
+for i in range(np):
+    if lista[i][0] == '0':
+        index = min(i + 2 * K - 1, np - 1)
+        opt = lista[index][2] - lista[i][1] + 1
+        ans = max(ans, opt)
+    else:
+        index = min(i + 2 * K, np - 1)
+        opt = lista[index][2] - lista[i][1] + 1
+        ans = max(ans, opt)
+print(ans)
