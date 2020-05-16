d1b3015642f3777f3d0eaa887da85ca8d5a2a37a
customaddone <custom1485@docomo.ne.jp>
2020-05-16 20:40:17 +0900

    ABC019 C - 高橋くんと魔法の箱

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -22,31 +22,15 @@ import sys
 sys.setrecursionlimit(1000000000)
 mod = 10 ** 9 + 7
 
-A, B = getNM()
-# bit1桁目のフラグの個数
-# 周期は2 ** 1
-# 0と1が交互に
-# bit2桁目のフラグの個数
-# 周期は2 ** 2
-flags1 = [0] * 61
-flags2 = [0] * 61
-# 1 ~ nまでに各桁のフラグが何本立つか計算する関数
-def bitflag(n, flaglist):
-    if n > 0:
-        for i in range(1, 61):
-            split = 2 ** i
-            flag1 = (n // split) * (split // 2)
-            flag2 = max(n % split + 1 - (split // 2), 0)
-            flaglist[i] += flag1 + flag2
-# 1 ~ A - 1について（Aは範囲に入っているため）
-bitflag(A - 1, flags1)
-bitflag(B, flags2)
+N = getN()
+A = getList()
 
-for i in range(61):
-    flags2[i] -= flags1[i]
-ans = 0
-# 奇数ならフラグが立つ
-for i in range(61):
-    if flags2[i] % 2 != 0:
-        ans += 2 ** (i - 1)
-print(ans)
+numset = set()
+def bispliter(n):
+    splitint = n
+    while splitint % 2 == 0:
+        splitint //= 2
+    return splitint
+for i in A:
+    numset.add(bispliter(i))
+print(len(numset))
