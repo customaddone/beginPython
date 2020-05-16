09a07657240a56577ac17252c8212423bf93df8c
customaddone <custom1485@docomo.ne.jp>
2020-05-16 20:21:34 +0900

    ABC 121 D - XOR World

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -22,31 +22,31 @@ import sys
 sys.setrecursionlimit(1000000000)
 mod = 10 ** 9 + 7
 
-layers = [1] * 51
-patties = [1] * 51
-for i in range(1, 51):
-    # レベルiバーガーの総枚数、パティの数を計算
-    layers[i] = 3 + layers[i - 1] * 2
-    patties[i] = 1 + patties[i - 1] * 2
+A, B = getNM()
+# bit1桁目のフラグの個数
+# 周期は2 ** 1
+# 0と1が交互に
+# bit2桁目のフラグの個数
+# 周期は2 ** 2
+flags1 = [0] * 61
+flags2 = [0] * 61
+# 1 ~ nまでに各桁のフラグが何本立つか計算する関数
+def bitflag(n, flaglist):
+    if n > 0:
+        for i in range(1, 61):
+            split = 2 ** i
+            flag1 = (n // split) * (split // 2)
+            flag2 = max(n % split + 1 - (split // 2), 0)
+            flaglist[i] += flag1 + flag2
+# 1 ~ A - 1について（Aは範囲に入っているため）
+bitflag(A - 1, flags1)
+bitflag(B, flags2)
 
-
-def count_patties(l, x):
-    # レベル0バーガーなら
-    if l == 0 and x == 1:
-        return 1
-    # レベル0以外の場合、x <= 1なら
-    if x <= 1:
-        return 0
-    y = layers[l - 1]
-    # もしB(レベルl - 1バーガー)P(レベルl - 1バーガー)B
-    #             ↑
-    # にxがあれば
-    if x <= y + 1:
-        # 左端の一枚を除いて
-        return count_patties(l-1, max(x - 1, 0))
-    else:
-        # 一枚目のl - 1バーガーの内部に                # 2枚目のl - 1バーガーのパティの数 + 中央の1枚
-        return count_patties(l-1, min(x-y-2, y)) + patties[l-1] + 1
-
-N, X = getNM()
-print(count_patties(N, X))
+for i in range(61):
+    flags2[i] -= flags1[i]
+ans = 0
+# 奇数ならフラグが立つ
+for i in range(61):
+    if flags2[i] % 2 != 0:
+        ans += 2 ** (i - 1)
+print(ans)
