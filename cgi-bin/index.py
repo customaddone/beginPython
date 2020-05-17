27a47dc2c091051022949fdff813911214825c27
customaddone <custom1485@docomo.ne.jp>
2020-05-17 13:35:05 +0900

    ABC140 D - Face Produces Unhappiness

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -22,28 +22,20 @@ import sys
 sys.setrecursionlimit(1000000000)
 mod = 10 ** 9 + 7
 
-N = getN()
-convict = [[] for _ in range(N)]
-for i in range(N):
-    a = getN()
-    for j in range(a):
-        x, y = getNM()
-        convict[i].append([x - 1, y])
-ans = 0
-# 状態を生成
-for bit in range(1 << N):
-    flag = True
-    # 人iの証言について
-    for i in range(N):
-        # 正直者のフラグが立っていれば
-        if bit & (1 << i):
-            for j in convict[i]:
-                # もし正直者が行ったことに間違いがあれば
-                if j[1] == 0 and bit & (1 << j[0]) > 0:
-                    flag = False
-                if j[1] == 1 and bit & (1 << j[0]) == 0:
-                    flag = False
-    if flag:
-        index = str(bin(bit)).count('1')
-        ans = max(ans, index)
-print(ans)
+# LRLRRL
+#    ↑
+# 反転させると
+# LLLRLL
+#  ↑↑  ↑
+# また反転させると
+# LLLLLL
+#  ↑↑↑↑↑
+
+# 反転させると最大2人まで幸福な人を増やせる
+N, K = getNM()
+s = input()
+cnt = 0
+for i in range(N - 1):
+    if s[i] == s[i + 1]:
+        cnt += 1
+print(min(cnt + 2 * K, N - 1))
