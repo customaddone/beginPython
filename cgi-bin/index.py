fac494fd8686d532f5bd7fe205f8f98f818d1165
customaddone <custom1485@docomo.ne.jp>
2020-05-17 15:07:32 +0900

    ABC165 C - Many Requirements

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -22,20 +22,33 @@ import sys
 sys.setrecursionlimit(1000000000)
 mod = 10 ** 9 + 7
 
-# LRLRRL
-#    ↑
-# 反転させると
-# LLLRLL
-#  ↑↑  ↑
-# また反転させると
-# LLLLLL
-#  ↑↑↑↑↑
+N, M, Q = getNM()
+lista = []
+for i in range(Q):
+    a, b, c, d = getNM()
+    lista.append([a - 1, b - 1, c, d])
 
-# 反転させると最大2人まで幸福な人を増やせる
-N, K = getNM()
-s = input()
-cnt = 0
-for i in range(N - 1):
-    if s[i] == s[i + 1]:
-        cnt += 1
-print(min(cnt + 2 * K, N - 1))
+ans = 0
+def dfs(i, numlist):
+    if i == N:
+        # numlistを元に得点計算
+        cnt = 0
+        for jud in lista:
+            if numlist[jud[1]] - numlist[jud[0]] == jud[2]:
+                cnt += jud[3]
+        return cnt
+
+    ans = 0
+    # もしi = 0なら
+    sta = 0
+    # もしi != 0なら一個前の数字を参照
+    if i > 0:
+        sta = numlist[i - 1]
+    # 1つ前の数字が1なら1 ~ 10までをdfs
+    # 重複組み合わせ
+    for j in range(sta, M):
+        # 数字を記録
+        numlist[i] = j
+        ans = max(ans, dfs(i + 1, numlist))
+    return ans
+print(dfs(0, [0] * N))
