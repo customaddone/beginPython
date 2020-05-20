4f5ce24361ebe787ad36472c27e7bef805ec908b
customaddone <custom1485@docomo.ne.jp>
2020-05-20 20:38:39 +0900

    ABC032 C - 列

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -15,6 +15,7 @@ import heapq
 import math
 import fractions
 import copy
+from itertools import permutations
 from operator import mul
 from functools import reduce
 from bisect import bisect_left, bisect_right
@@ -22,20 +23,31 @@ from bisect import bisect_left, bisect_right
 import sys
 sys.setrecursionlimit(1000000000)
 mod = 10 ** 9 + 7
-# https://atcoder.jp/contests/pakencamp-2019-day3/tasks/pakencamp_2019_day3_c
 
-import itertools
-n, m = map(int, input().split())
-lista = []
-for i in range(n):
-    a = getList()
-    lista.append(a)
+N, K = getNM()
+S = [getN() for i in range(N)]
 
-res = 0
-for i in range(m):
-    for j in range(i + 1, m):
-        tmp = 0
-        for k in range(n):
-            tmp += max(lista[k][i], lista[k][j])
-        res = max(res, tmp)
-print(res)
+# 条件を満たす限り右端を進める
+# 条件を満たさなくなったら左端を条件を満たすまで進める
+# かける数に0が混じる
+if 0 in S:
+    print(N)
+    exit()
+else:
+    # r:右端
+    r, ans, tmp = 0, 0, 1
+    # l:左端
+    for l in range(N):
+        while r < N and tmp * S[r] <= K:
+            # 右端を伸ばす
+            tmp *= S[r]
+            r += 1
+        # 最大まで伸ばしてところでジャッジ
+        # ここで１回ループ　左端が1進む
+        ans = max(ans, r - l)
+        if l == r:
+            r += 1
+        else:
+            # 左端が1進むと同時にtmpの左端の分を削る
+            tmp //= S[l]
+print(ans)
