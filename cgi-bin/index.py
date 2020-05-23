31188ca3ddc1ad6f261807fee02398475978221f
customaddone <custom1485@docomo.ne.jp>
2020-05-23 21:28:25 +0900

    ABC074 C - Sugar Water

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -24,29 +24,48 @@ import sys
 sys.setrecursionlimit(1000000000)
 mod = 10 ** 9 + 7
 
-A, B, C, D, E, F = map(int, input().split())
+# ABが水、CDが砂糖、Eがとけられる量、Fが上限
+A, B, C, D, E, F = getNM()
 
-def calc(a, b):
-    if a == 0 or b == 0: return 0
-    return 100 * b / (a + b)
-ans = (0, 0)
-tmp = 0
-ret = set()
+# A,Bを好きな回数使うことでi(0 <= i <= 30)の水を作り出せる
+dp1 = [0] * 31
+dp1[0] = 1
+for i in range(1, 31):
+    if i >= A:
+        dp1[i] = max(dp1[i], dp1[i - A])
+    if i >= B:
+        dp1[i] = max(dp1[i], dp1[i - B])
+waterlist = []
+for i in range(31):
+    if dp1[i] > 0:
+        waterlist.append(i)
+
+# C,Dを好きな回数使うことでi(0 <= i <= 3000)の砂糖を作り出せる
+dp2 = [0] * 3001
+dp2[0] = 1
+for i in range(1, 3001):
+    if i >= C:
+        dp2[i] = max(dp2[i], dp2[i - C])
+    if i >= D:
+        dp2[i] = max(dp2[i], dp2[i - D])
+sugerlist = []
 for i in range(3001):
-    for j in range(3001):
-        water = i * A * 100 + j * B * 100
-        if 1 > water or water > F: break
-        for k in range((F-water)//C+1):
-            for l in range((F-water)//D+1):
-                suger = k * C + l * D
-                if water + suger > F: break
-                if water * E // 100 < suger: break
-                t = calc(water, suger)
-                #ret.add((t, water+suger, suger))
-                if tmp <= t:
-                    tmp = t
-                    ans = (water+suger, suger)
+    if dp2[i] > 0:
+        sugerlist.append(i)
 
-#ret = list(ret)
-#ret.sort(reverse=True)
+ans = [0, 0]
+concent = 0
+
+for water in waterlist[1:]:
+    if 100 * water <= F:
+        left = F - (water * 100)
+        # Fから水をひいた分、溶ける砂糖の限界を超えない量の砂糖を取得する
+        index = bisect_right(sugerlist, min(left, E * water))
+        suger = sugerlist[index - 1]
+        # 濃度計算
+        optconc = (100 * suger) / (100 * water + suger)
+        # ここ>にすると濃度0%に対応できずWAに
+        if optconc >= concent:
+            concent = optconc
+            ans = [100 * water + suger, suger]
 print(*ans)
