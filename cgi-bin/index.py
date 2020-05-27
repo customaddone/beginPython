eccf6112fc11f3d96c6d3fdbb8d429fb3b4abf67
customaddone <custom1485@docomo.ne.jp>
2020-05-27 19:41:31 +0900

    bit全探索通りの数

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -24,31 +24,28 @@ import sys
 sys.setrecursionlimit(1000000000)
 mod = 10 ** 9 + 7
 
-
-N, M, Q = getNM()
+n, m = map(int, input().split())
 lista = []
-for i in range(Q):
-    a, b, c, d = getNM()
-    lista.append([a - 1, b - 1, c, d])
+for i in range(m):
+    k, *s, = map(int, input().split())
+    lista.append(list(s))
+p = list(map(int, input().split()))
 
-def dfs(i, numlist):
-    if i == N:
-        # 集計
-        cnt = 0
-        for jud in lista:
-            if numlist[jud[1]] - numlist[jud[0]] == jud[2]:
-                cnt += jud[3]
-        return cnt
+sumans = 0
 
-    ans = 0
-    sta = 0
-
-    if i > 0:
-        sta = numlist[i - 1]
-
-    # 条件についてループさせてdfs
-    for j in range(sta, M):
-        numlist[i] = j
-        ans = max(ans, dfs(i + 1, numlist))
-    return ans
-print(dfs(0, [0] * N))
+for bit in range(1 << n):
+    # 各状態でフラグを立てる
+    flag = True
+    # 条件についてループ
+    for i in range(m):
+        sum = 0
+        for j in lista[i]:
+            if bit & (1 << (j - 1)):
+                sum += 1
+        if sum % 2 != p[i]:
+            flag = False
+            break
+    # 判定
+    if flag:
+        sumans += 1
+print(sumans)
