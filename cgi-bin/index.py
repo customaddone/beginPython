f58e9feacd165d9500215689d0455542c2997575
customaddone <custom1485@docomo.ne.jp>
2020-05-27 19:30:49 +0900

    文字列条件一致の数

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -24,19 +24,22 @@ import sys
 sys.setrecursionlimit(1000000000)
 mod = 10 ** 9 + 7
 
+n = int(input())
 s = input()
-l = ['A', 'T', 'G', 'C']
-n = len(s)
+cnt = 0
 
-num = 0
-ans = 0
+for i in range(1000):
+    flag = 0
 
-for i in range(n):
-    # 条件を書く
-    if s[i] in l:
-        num += 1
-    else:
-        ans = max(num, ans)
-        num = 0
-ans = max(num, ans)
-print(ans)
+    for st in s:
+        # 条件を書き込む
+        if flag == 0 and st == str(i // 100):
+            flag = 1
+        elif flag == 1 and st == str((i % 100) // 10):
+            flag = 2
+        elif flag == 2 and st == str(i % 10):
+            flag = 3
+            break
+    if flag == 3:
+        cnt += 1
+print(cnt)
