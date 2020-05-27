d509fcd896748e8c579e9633a6745d61c5f837c1
customaddone <custom1485@docomo.ne.jp>
2020-05-27 20:47:39 +0900

    dfs maze

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -27,21 +27,35 @@ mod = 10 ** 9 + 7
 from itertools import permutations
 from math import factorial, hypot
 
-N = int(input())
+maze = [
+    [9,9,9,9,9,9,9,9,9,9,9,9],
+    [9,0,0,0,9,0,0,0,0,0,0,9],
+    [9,0,9,0,0,0,9,9,0,9,9,9],
+    [9,0,9,9,0,9,0,0,0,9,0,9],
+    [9,0,0,0,9,0,0,9,9,0,9,9],
+    [9,9,9,0,0,9,0,9,0,0,0,9],
+    [9,0,0,0,9,0,9,0,0,9,1,9],
+    [9,0,9,0,0,0,0,9,0,0,9,9],
+    [9,0,0,9,0,9,0,0,9,0,0,9],
+    [9,0,9,0,9,0,9,0,0,9,0,9],
+    [9,0,0,0,0,0,0,9,0,0,0,9],
+    [9,9,9,9,9,9,9,9,9,9,9,9]
+]
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+ans = 100000
 
-ans = 0
-numlist = [3, 5, 7]
-
-def dfs(s):
+def dfs(x, y, depth):
     global ans
-    if s:
-        # 上限
-        if int(s) > N:
-            return
-        elif "3" in s and "5" in s and "7" in s:
-            ans += 1
-    # dfs
-    for i in numlist:
-        dfs(s + str(i))
-dfs("")
+    if maze[y][x] == 1:
+        ans = min(ans, depth)
+    maze[y][x] = 2
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if maze[ny][nx] < 2:
+            dfs(nx, ny, depth + 1)
+    maze[y][x] = 0
+
+dfs(1, 1, 0)
 print(ans)
