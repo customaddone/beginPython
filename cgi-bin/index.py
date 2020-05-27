7dec749456b5ee41d479ba93f5ff6270d0a3cc94
customaddone <custom1485@docomo.ne.jp>
2020-05-27 21:02:47 +0900

    maze最短距離

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -27,30 +27,63 @@ mod = 10 ** 9 + 7
 from itertools import permutations
 from math import factorial, hypot
 
-m = int(input())
-n = int(input())
+h, w, n = map(int, input().split())
+
+"""
+10 10 9
+.X...X.S.X
+6..5X..X1X
+...XXXX..X
+X..9X...X.
+8.X2X..X3X
+...XX.X4..
+XX....7X..
+X..X..XX..
+X...X.XX..
+..X.......
+"""
+
 maze = []
-for i in range(n):
-    maze.append(list(map(int, input().split())))
+for i in range(h):
+    a = input()
+    maze.append(list(a))
+
+start = [-1, -1]
+end = [-1, -1]
+ans = 0
+# スタート位置特定
+for i in range(w):
+    for j in range(h):
+        if maze[j][i] == 'S':
+            start = [i, j]
+            break
+    else:
+        continue
+    break
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
-ans = 0
+# スタート位置破壊
+maze[start[1]][start[0]] = '.'
 
-# 最長距離を求める
-def dfs(x, y, depth):
-    global ans
-    maze[y][x] = 0
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < m and 0 <= ny < n and maze[ny][nx] == 1:
-            dfs(nx, ny, depth + 1)
-    maze[y][x] = 1
-    ans = max(ans, depth)
+# n → n + 1へ移動するまでにかかる時間
+def bfs(sta, end):
+    global start
+    pos = deque([[sta[0], sta[1], 0]])
+    dp = [[-1] * (w + 1) for i in range(h + 1)]
+    
+    while len(pos) > 0:
+        x, y, time = pos.popleft()
+        if maze[y][x] == str(end):
+            start = [x, y]
+            return dp[y][x] + 1
+            break
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < w and 0 <= ny < h and maze[ny][nx] != 'X' and dp[ny][nx] == -1:
+                dp[ny][nx] = dp[y][x] + 1
+                pos.append([nx, ny, time + 1])
 
-for i in range(m):
-    for j in range(n):
-        if maze[j][i] == 1:
-            dfs(i, j, 1)
-# TLEにならないんですかこれ
+for i in range(n):
+    ans += bfs([start[0], start[1]], i + 1)
 print(ans)
