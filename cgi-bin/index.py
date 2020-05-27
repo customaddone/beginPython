f7d98e1e9429cce37acec50512e58174bba50cf6
customaddone <custom1485@docomo.ne.jp>
2020-05-27 21:06:59 +0900

    木の点Kからの最短距離

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -27,63 +27,32 @@ mod = 10 ** 9 + 7
 from itertools import permutations
 from math import factorial, hypot
 
-h, w, n = map(int, input().split())
+N = getN()
+dist = [[] for i in range(N + 1)]
+for i in range(N - 1):
+    a, b, c = getNM()
+    dist[a].append([b, c])
+    dist[b].append([a, c])
+ignore = [-1] * (N + 1)
 
-"""
-10 10 9
-.X...X.S.X
-6..5X..X1X
-...XXXX..X
-X..9X...X.
-8.X2X..X3X
-...XX.X4..
-XX....7X..
-X..X..XX..
-X...X.XX..
-..X.......
-"""
+def distance(sta):
+    # 木をstaから順にたどる（戻るの禁止）
+    pos = deque([sta])
 
-maze = []
-for i in range(h):
-    a = input()
-    maze.append(list(a))
-
-start = [-1, -1]
-end = [-1, -1]
-ans = 0
-# スタート位置特定
-for i in range(w):
-    for j in range(h):
-        if maze[j][i] == 'S':
-            start = [i, j]
-            break
-    else:
-        continue
-    break
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
-# スタート位置破壊
-maze[start[1]][start[0]] = '.'
-
-# n → n + 1へ移動するまでにかかる時間
-def bfs(sta, end):
-    global start
-    pos = deque([[sta[0], sta[1], 0]])
-    dp = [[-1] * (w + 1) for i in range(h + 1)]
-    
     while len(pos) > 0:
-        x, y, time = pos.popleft()
-        if maze[y][x] == str(end):
-            start = [x, y]
-            return dp[y][x] + 1
-            break
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < w and 0 <= ny < h and maze[ny][nx] != 'X' and dp[ny][nx] == -1:
-                dp[ny][nx] = dp[y][x] + 1
-                pos.append([nx, ny, time + 1])
+        u = pos.popleft()
+        for i in dist[u]:
+            if ignore[i[0]] == -1:
+                ignore[i[0]] = ignore[u] + i[1]
+                pos.append(i[0])
 
-for i in range(n):
-    ans += bfs([start[0], start[1]], i + 1)
-print(ans)
+Q, K = getNM()
+ignore[K] = 0
+distance(K)
+# 答えはK~xまでの距離+K~yまでの距離
+ans = []
+for i in range(Q):
+    x, y = getNM()
+    ans.append(ignore[x] + ignore[y])
+for i in ans:
+    print(i)
