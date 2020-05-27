6c5279a01af13a1d3507491410629ad9654dc28f
customaddone <custom1485@docomo.ne.jp>
2020-05-27 20:49:42 +0900

    dfs maze最長距離

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -27,35 +27,30 @@ mod = 10 ** 9 + 7
 from itertools import permutations
 from math import factorial, hypot
 
-maze = [
-    [9,9,9,9,9,9,9,9,9,9,9,9],
-    [9,0,0,0,9,0,0,0,0,0,0,9],
-    [9,0,9,0,0,0,9,9,0,9,9,9],
-    [9,0,9,9,0,9,0,0,0,9,0,9],
-    [9,0,0,0,9,0,0,9,9,0,9,9],
-    [9,9,9,0,0,9,0,9,0,0,0,9],
-    [9,0,0,0,9,0,9,0,0,9,1,9],
-    [9,0,9,0,0,0,0,9,0,0,9,9],
-    [9,0,0,9,0,9,0,0,9,0,0,9],
-    [9,0,9,0,9,0,9,0,0,9,0,9],
-    [9,0,0,0,0,0,0,9,0,0,0,9],
-    [9,9,9,9,9,9,9,9,9,9,9,9]
-]
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
-ans = 100000
+m = int(input())
+n = int(input())
+maze = []
+for i in range(n):
+    maze.append(list(map(int, input().split())))
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+ans = 0
 
+# 最長距離を求める
 def dfs(x, y, depth):
     global ans
-    if maze[y][x] == 1:
-        ans = min(ans, depth)
-    maze[y][x] = 2
+    maze[y][x] = 0
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if maze[ny][nx] < 2:
+        if 0 <= nx < m and 0 <= ny < n and maze[ny][nx] == 1:
             dfs(nx, ny, depth + 1)
-    maze[y][x] = 0
+    maze[y][x] = 1
+    ans = max(ans, depth)
 
-dfs(1, 1, 0)
+for i in range(m):
+    for j in range(n):
+        if maze[j][i] == 1:
+            dfs(i, j, 1)
+# TLEにならないんですかこれ
 print(ans)
