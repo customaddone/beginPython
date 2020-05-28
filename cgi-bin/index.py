78ff6b7af689c8b9ec04b2b9be8e1275da25ce5f
customaddone <custom1485@docomo.ne.jp>
2020-05-28 20:33:52 +0900

    個数制限なし重複あり部分和

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -27,27 +27,21 @@ mod = 10 ** 9 + 7
 from itertools import permutations
 from math import factorial, hypot
 
-H, N = 9999, 10
-magic = [
-[540,7550],
-[691,9680],
-[700,9790],
-[510,7150],
-[415,5818],
-[551,7712],
-[587,8227],
-[619,8671],
-[588,8228],
-[176,2461]
-]
+# 1 + 3 と3 + 1 と1 + 1 + 1 + 1は違う通りになる
+# dfs使った方がいい
+def part_sum(a,A):
+    p = 10 ** 9 + 7
+    W = len(a)
 
-dp = [float('inf')] * (H + 1)
-dp[0] = 0
-# HP = iを削るための最小MP
-for i in range(1, H + 1):
-    for j in magic:
-        if i < j[0]:
-            dp[i] = min(dp[i], j[1])
-        else:
-            dp[i] = min(dp[i], dp[i - j[0]] + j[1])
-print(dp[-1])
+    dp = [0] * (A + 1)
+    dp[0] = 1
+
+    for i in range(1, A + 1):
+        for j in range(W):
+            if i >= a[j]:
+                dp[i] += dp[i - a[j]]
+    return dp
+
+a = [1, 3, 5, 7, 9]
+A = 10
+print(part_sum(a, A))
