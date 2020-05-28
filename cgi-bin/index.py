e5f644b4dbd7ec064e2be7c9688a1027df110af9
customaddone <custom1485@docomo.ne.jp>
2020-05-28 21:13:55 +0900

    bit dp

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -27,17 +27,29 @@ mod = 10 ** 9 + 7
 from itertools import permutations
 from math import factorial, hypot
 
-# 1 + 3 と3 + 1 と1 + 1 + 1 + 1は違う通りになる
-# dfs使った方がいい
-def dfs(now, sum):
-    if sum <= 0:
-        return sum == 0
-    res = 0
-    for i in range(now, W):
-        res += dfs(i, sum - a[i])
-    return res
+N = 4
+inf = float('inf')
 
-a = [1, 3, 5, 7, 9]
-W = len(a)
-A = 9
-print(dfs(0, A))
+d = [
+[0, 2, inf, inf],
+[inf, 0, 3, 9],
+[1, inf, 0, 6],
+[inf, inf, 4, 0]
+]
+
+dp = [[-1] * N for i in range(1 << N)]
+
+def rec(s, v, dp):
+    if dp[s][v] >= 0:
+        return dp[s][v]
+    if s == (1 << N) - 1 and v == 0:
+        dp[s][v] = 0
+        return 0
+    res = float('inf')
+    for u in range(N):
+        if s & (1 << u) == 0:
+            res = min(res,rec(s|(1 << u), u, dp) + d[v][u])
+    dp[s][v] = res
+    return res
+# 結局のところ0からスタートしようが1からスタートしようが同じ道を通る
+print(rec(0,0,dp))
