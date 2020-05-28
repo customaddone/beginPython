783737b752d09f44ee72c6b2eb96520ba68f24b9
customaddone <custom1485@docomo.ne.jp>
2020-05-28 19:51:19 +0900

    個数制限なし重複あり部分和

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -27,18 +27,27 @@ mod = 10 ** 9 + 7
 from itertools import permutations
 from math import factorial, hypot
 
-# 例えばジャンケンで３回同じ手を出さない場合の通り
-# N = 3のときは3 ** 3 - 3 = 24通り
-N = 5
-dp = [[[0] * 3 for i in range(3)] for j in range(N)]
-for i in range(3):
-    dp[0][i][i - 1] = 1
+H, N = 9999, 10
+magic = [
+[540,7550],
+[691,9680],
+[700,9790],
+[510,7150],
+[415,5818],
+[551,7712],
+[587,8227],
+[619,8671],
+[588,8228],
+[176,2461]
+]
 
-for i in range(1, N):
-    for l in range(3):
-        for m in range(3):
-            for n in range(3):
-                if l == m and m == n:
-                    continue
-                dp[i][l][m] += dp[i - 1][m][n]
-print(dp)
+dp = [float('inf')] * (H + 1)
+dp[0] = 0
+# HP = iを削るための最小MP
+for i in range(1, H + 1):
+    for j in magic:
+        if i < j[0]:
+            dp[i] = min(dp[i], j[1])
+        else:
+            dp[i] = min(dp[i], dp[i - j[0]] + j[1])
+print(dp[-1])
