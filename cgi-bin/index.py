12b3fdb18d62a884fc2a90caa6f689b26a7b5422
customaddone <custom1485@docomo.ne.jp>
2020-05-29 19:18:07 +0900

    任意の整数で割り続ける

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -27,47 +27,22 @@ mod = 10 ** 9 + 7
 from itertools import permutations
 from math import factorial, hypot
 
-N, K = getNM()
-query = []
-for i in range(N):
-    t = getList()
-    query.append(t)
+N = getN()
+A = getList()
 
-# n重ループは素直にデカルト積使え
-# 下のでN重ループできる
-def dfs(i, now):
-    if i == 0:
-        for m in query[0]:
-            if m == 0:
-                print('Found')
-                exit()
-            dfs(i + 1, m)
-    elif i < N:
-        for j in query[i]:
-            newnow = j ^ now
-            if newnow == 0:
-                print('Found')
-                exit()
-            else:
-                dfs(i + 1, newnow)
-dfs(0, 0)
-print('Nothing')
+def spliter(n, split):
+    splited = n
+    cnt = 0
 
-"""
-N, K = map(int, input().split())
-T = []
+    while splited % split == 0:
+        if splited == 0:
+            break
+        splited //= split
+        cnt += 1
+    # print(cnt)
+    return splited
+
+ans = set()
 for i in range(N):
-    t = list(map(int, input().split()))
-    T.append(t)
-flag = True
-# 各配列のデカルト積
-for ps in itertools.product(*T):
-    x1 = ps[0]
-    for i in range(1, N):
-        x2 = ps[i]
-        # 排他的論理和 x1, x2に含まれるもの - x1, x2両方に含まれるもの
-        x1 = x1 ^ x2
-    if x1 == 0:
-        flag = False
-print('Nothing' if flag else 'Found')
-"""
+    ans.add(spliter(A[i], 2))
+print(len(ans))
