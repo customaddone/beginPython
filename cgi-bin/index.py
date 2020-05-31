82cea4d580baa64e6ceeffe23da473e138bfedf8
customaddone <custom1485@docomo.ne.jp>
2020-05-31 11:58:02 +0900

    三分探索整数ver

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -20,15 +20,21 @@ import sys
 sys.setrecursionlimit(1000000000)
 mod = 10 ** 9 + 7
 
-prime = [2]
-max = 10 ** 5
-limit = int(math.sqrt(max))
-data = [i + 1 for i in range(2, max, 2)]
+num = []
+for i in range(100):
+    if i < 50:
+        num.append(i)
+    else:
+        num.append(100 - i)
 
-# エラストテネスの篩
-while limit > data[0]:
-    prime.append(data[0])
-    data = [j for j in data if j % data[0] != 0]
-prime = prime + data
-
-print(prime)
+left, right = 0, len(num) - 1
+while abs(right - left) > 3:
+    mid1 = (right * 2 + left) // 3 + 1
+    mid2 = (right + left * 2) // 3
+    # 最小値を求める場合は矢印逆になる
+    if num[mid1] <= num[mid2]:
+        right = mid1
+    else:
+        left = mid2
+print(right)
+print(left)
