66035a287caa3dda114e2a60941df414bfb5f127
customaddone <custom1485@docomo.ne.jp>
2020-06-04 18:17:35 +0900

    二つの直線の交点

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -29,22 +29,45 @@ mod = 10 ** 9 + 7
 # Main Code #
 #############
 
-w = getN()
-n,k = getNM()
-A = []
-B = []
-for i in range(n):
-    a_,b_ = getNM()
-    A.append(a_)
-    B.append(b_)
-# dp[n][k][w] k個足してw以内で
-dp = [[[0] * (w + 1) for i in range(k + 1)] for j in range(n + 1)]
+# y = line[0]x + line[1]
+# 傾きmaxならx = line[3]
+def line(Ax, Ay, Bx, By):
+    if Ay == By:
+        return 0, Ay, None
+    if Ax == Bx:
+        return float("inf"), 0, Ax
+    a = (Ay - By) / (Ax - Bx)
+    b = Ay - a * Ax
+    return a, b, None
 
-for i in range(n):
-    for j in range(k + 1):
-        for l in range(w + 1):
-            if l >= A[i] and j >= 1:
-                dp[i + 1][j][l] = max(dp[i][j][l],dp[i][j - 1][l - A[i]] + B[i])
-            else:
-                dp[i + 1][j][l] = dp[i][j][l]
-print(dp[n][k][w])
+# print(line(0, 2, 1, 3))
+# 二つの線がクロスするx,y座標を出す
+def cross(l1, l2):
+    a1, b1, xx1 = min(l1, l2)
+    a2, b2, xx2 = max(l1, l2)
+    if a1 == a2:
+        return None
+    if a2 == float("inf"):
+        x = xx2
+    else:
+        x = (b1 - b2) / (a2 - a1)
+    y = a1 * x + b1
+    return x, y
+
+Ax, Ay, Bx, By = getNM()
+N = getN()
+P = [tuple(getNM()) for _ in range(N)]
+P += [P[0]]
+chop = line(Ax, Ay, Bx, By)
+ans = 0
+for i in range(N):
+    p1 = P[i]
+    p2 = P[i + 1]
+    side = line(p1[0], p1[1], p2[0], p2[1])
+    c = cross(chop, side)
+    if not c:
+        continue
+    x, y = c
+    if min(p1[0], p2[0]) <= x <= max(p1[0], p2[0]) and min(Ax, Bx) <= x <= max(Ax, Bx):
+        ans += 1
+print(ans // 2 + 1)
