9166e2b0aa232757c0562972c5374161c7407196
customaddone <custom1485@docomo.ne.jp>
2020-06-07 13:36:44 +0900

    完全数

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -51,77 +51,23 @@ mod = 10 ** 9 + 7
 # Main Code #
 #############
 
-X, Y = getNM()
 N = getN()
-query = []
-for i in range(N):
-    a1, b1 = getNM()
-    query.append([a1, b1])
-query.append(query[0])
 
-# y = line[0]x + line[1]
-# 傾きmaxならx = line[3]
-def line(Ax, Ay, Bx, By):
-    if Ay == By:
-        return 0, Ay, None
-    if Ax == Bx:
-        return float("inf"), 0, Ax
-    a = (Ay - By) / (Ax - Bx)
-    b = Ay - a * Ax
-    return a, b, None
+def make_divisors(n):
+    divisors = []
+    for i in range(1, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            divisors.append(i)
+            # √nで無い数についてもう一個プラス
+            if i != n // i:
+                divisors.append(n // i)
+    return divisors
 
-# 点[p1, p2]を通り、傾きslopeの直線に直行する直線を求める
-def perp(p1, p2, slope):
-    if slope == 0:
-        return float('inf'), 0, p1
-    if slope > 10 ** 12:
-        return 0, p2, None
-    opt_slope = (1 / slope) * (-1)
-    opt_inter = p2 - (opt_slope * p1)
+ans = sum(make_divisors(N)) - N
 
-    return opt_slope, opt_inter, None
-
-def distance(Ax, Ay, Bx, By):
-    return math.hypot(Ax - Bx, Ay - By)
-
-# print(line(0, 2, 1, 3))
-# 二つの線がクロスするx,y座標を出す
-def cross(l1, l2):
-    a1, b1, xx1 = min(l1, l2)
-    a2, b2, xx2 = max(l1, l2)
-    if a1 == a2:
-        return None
-    elif a2 == float("inf"):
-        x = xx2
-    else:
-        x = (b1 - b2) / (a2 - a1)
-    y = a1 * x + b1
-    return x, y
-
-x1 = query[0][0]
-y1 = query[0][1]
-
-ans = float('inf')
-for i in range(1, N + 1):
-    opt = 0
-    x2 = query[i][0]
-    y2 = query[i][1]
-
-    # 交点を求める
-    line1 = line(x1, y1, x2, y2)
-    line2 = perp(X, Y, line1[0])
-    cross_x, cross_y = cross(line1, line2)
-
-    # 直線上にあるか
-    range_min_x, range_max_x = min(x1, x2), max(x1, x2)
-    range_min_y, range_max_y = min(y1, y2), max(y1, y2)
-    if range_min_x - 10 ** 6 <= cross_x <= range_max_x + 10 ** 6 and range_min_y - 10 ** 6 <= cross_y <= range_max_y + 10 ** 6:
-        opt = distance(X, Y, cross_x, cross_y)
-    else:
-        opt = min(distance(X, Y, x1, y1), distance(X, Y, x2, y2))
-
-    ans = min(ans, opt)
-
-    x1, y1 = x2, y2
-    
-print(ans)
+if ans == N:
+    print('Perfect')
+elif ans > N:
+    print('Abundant')
+else:
+    print('Deficient')
