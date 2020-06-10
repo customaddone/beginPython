2181857a2e6dfd0acadc6742be8d85eac5a1d388
customaddone <custom1485@docomo.ne.jp>
2020-06-10 21:23:15 +0900

    85p 食物連鎖

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -51,18 +51,85 @@ mod = 10 ** 9 + 7
 # Main Code #
 #############
 
-N = 3
-A = [8, 5, 8]
+class UnionFind():
+    def __init__(self, n):
+        self.n = n
+        self.parents = [-1] * n
 
-heapq.heapify(A)
+    def find(self, x):
+        if self.parents[x] < 0:
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
 
-ans = 0
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
 
-# heapq.heappopはlogNで実行できる
-while len(A) > 1:
-    a1 = heapq.heappop(A)
-    a2 = heapq.heappop(A)
-    ans += a1 + a2
-    
-    heapq.heappush(A, a1 + a2)
-print(ans)
+        if x == y:
+            return
+
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
+
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+
+    # これ追加
+    def count_group(self):
+        return len({self.find(x) for x in range(n)})
+
+N = 100
+K = 7
+query = [
+[1, 101, 1],
+[2, 1, 2],
+[2, 2, 3],
+[2, 3, 3],
+[1, 1, 3],
+[2, 3, 1],
+[1, 5, 5]
+]
+
+U = UnionFind(3 * N)
+
+def checker(int_input, x, y):
+    if x < 0 or x >= N or y <0 or y >= N or int_input < 1 or int_input > 2:
+        return False
+    else:
+        return True
+
+for i in query:
+    int_input = i[0]
+    x = i[1]
+    y = i[2]
+
+    if not checker(int_input, x, y):
+        print(int_input, x, y)
+        continue
+
+    if int_input == 1:
+        # xとyが異種の場合
+        if U.same(x, y + N) or U.same(x, y + 2 * N):
+            print(int_input, x, y)
+            continue
+        else:
+            # x == A and y == A
+            U.union(x, y)
+            # x == B and y == B
+            U.union(x + N, y + N)
+            U.union(x + 2 * N, y + 2 * N)
+
+    if int_input == 2:
+        # x,y が同種、もしくは逆の場合
+        if U.same(x, y) or U.same(x, y + 2 * N):
+            print(int_input, x, y)
+            continue
+        else:
+            U.union(x, y + N)
+            U.union(x + N, y + 2 * N)
+            U.union(x + 2 * N, y)
