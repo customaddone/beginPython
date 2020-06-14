ea811cb20ba9601908653ae4b4620079ebbb9ac9
customaddone <custom1485@docomo.ne.jp>
2020-06-14 15:36:47 +0900

    要素を複数個入れた時に何番目に何があるか

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -51,62 +51,26 @@ mod = 10 ** 9 + 7
 # Main Code #
 #############
 
-H, W, T = 2, 3, 10
-maze = [
-['S', '#', '#'],
-['.', '#', 'G']
+# 要素を複数個入れた時に何番目に何があるか
+N, K = 3, 3
+
+parent = [
+[1, 1],
+[2, 2],
+[3, 3]
 ]
 
-start = []
-goal = []
-for i in range(H):
-    for j in range(W):
-        if maze[i][j] == "S":
-            start = [j, i]
-        if maze[i][j] == "G":
-            goal = [j, i]
+parent.sort(key = lambda int: int[0])
 
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
+A = []
+B = []
+for a, b in parent:
+    A.append(a)
+    B.append(b)
 
-# 白マス、黒マスに侵入するコストを計算する関数
-def comp_dist(x, s):
-    return x if s == "#" else 1
+alta = [0]
+for i in range(N):
+    alta.append(alta[i] + B[i])
 
-# 二次元ダイクストラの関数
-def dij(start_x, start_y, goal_x, goal_y, dis):
-    dist = [[float("inf") for _ in range(W)] for _ in range(H)]
-    dist[start_y][start_x] = 0
-    pq = [(0, start_x, start_y)]
-
-    # pqの先頭がgoal行きのものなら最短距離を返す
-    while (pq[0][1] != goal_x or pq[0][2] != goal_y):
-        distance, x, y = heapq.heappop(pq)
-
-        if distance > dist[y][x]:
-            continue
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if 0 <= nx < W and 0 <= ny < H:
-                nd = dist[y][x] + comp_dist(dis, maze[ny][nx])
-                if dist[ny][nx] > nd:
-                    dist[ny][nx] = nd
-                    heapq.heappush(pq, [dist[ny][nx], nx, ny])
-    return pq[0][0]
-
-# にぶたん
-ok = -1
-ng = 10 ** 9 + 1
-
-while ng - ok > 1:
-    mid = (ok + ng) // 2
-
-    if dij(start[0], start[1], goal[0], goal[1], mid) > T:
-        ng = mid
-    else:
-        ok = mid
-
-print(ok)
+index = bisect_left(alta, K)
+print(A[index - 1])
--- cgi-bin/myfile.py
+++ cgi-bin/myfile.py
@@ -17,48 +17,5 @@ mod = 10 ** 9 + 7
 # Main Code #
 #############
 
-H, W, T = getNM()
-maze = [list(input()) for _ in range(H)]
-
-minT = 0
-maxT = 10 ** 9 + 1
-
-start = []
-goal = []
-for i in range(H):
-    for j in range(W):
-        if maze[i][j] == "S":
-            start = [i,j]
-        if maze[i][j] == "G":
-            goal = [i,j]
-
-def comp_dist(x, s):
-    return x if s == "#" else 1
-
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
-
-def dij(start_x, start_y, goal_x, goal_y, dis):
-    dist = [[float("inf") for _ in range(W)] for _ in range(H)]
-    dist[start_y][start_x] = 0
-    pq = [(0, start_x, start_y)]
-
-    # pqの先頭がgoal行きのものなら最短距離を返す
-    while (pq[0][1] != goal_x or pq[0][2] != goal_y):
-        distance, x, y = heapq.heappop(q)
-
-        if distance > dist[y][x]:
-            continue
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if 0 <= nx < W and 0 <= ny < H:
-                nd = dist[y][x] + comp_dist(dis, maze[ny][nx])
-                if dist[ny][nx] > nd:
-                    dist[ny][nx] = nd
-                    heapq.heappush(pq, [dist[ny][nx], nx, ny])
-    return pq[0][0]
-
-print(dij(0, 0, 2, 1, 1))
+# 文字列操作
+ABC029 C - Brute-force Attack
