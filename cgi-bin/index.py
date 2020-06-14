a18857a4fa35e16d19d13c9ca06c17f4118ffaca
customaddone <custom1485@docomo.ne.jp>
2020-06-14 10:24:53 +0900

    二分探索判定系

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -51,68 +51,116 @@ mod = 10 ** 9 + 7
 # Main Code #
 #############
 
-# N進数
-n = 12
-list = []
-digit = 7
-i = 0
+# 二分探索（特定の値を求める）
+lista = [i for i in range(1, 10, 2)]
 
-while n != 0:
-    list.insert(0, str(n % digit))
-    n //= digit
-    i += 1
-print(''.join(list))
-
-# 文字列２進数を整数に変換
-print(int('010100', 2))
-
-# nを超えない最大のbase ** mは何か
-def max_pow(n, base):
-    if n == 0:
-        return None
-    opt = 1
-    cnt = 0
-    while base ** (cnt + 1) <= n:
-        opt *= base
-        cnt += 1
-    return opt, cnt
-
-# (16, 4)
-print(max_pow(27, 2))
-
-# -2進数（いるこれ？）
-def minus_digit(rev_n):
-    if rev_n == 0:
-        print('0')
-        return
-
-    cnt = 0
-    rep = rev_n
-    lista = []
-
-    while rep != 0:
-        split = (abs(rep) % 2 ** (cnt + 1)) // 2 ** cnt
-        if split == 0:
-            lista.append(0)
+def binary_search_loop(data, target):
+    imin = 0
+    imax = len(data) - 1
+    while imin <= imax:
+        imid = imin + (imax - imin) // 2
+        if target == data[imid]:
+            return imid
+        elif target < data[imid]:
+            imax = imid - 1
         else:
-            lista.append(1)
-        rep -= (split * ((-2) ** cnt))
-        cnt += 1
-    lista.reverse()
-    return''.join(map(str, lista))
+            imin = imid + 1
+    return False
 
-# 11100
-print(minus_digit(12))
-
-# 1(1), 2(1 + 2), 3(1 + 2 + 3)...を超えられるか
-def factime(ny):
-    cnt = 1
-    sum = 0
-    while True:
-        if sum + cnt > ny:
-            return cnt - 1
-            break
-        sum += cnt
-        cnt += 1
 # 2
-print(factime(2))
+print(binary_search_loop(lista, 5))
+
+# 二分探索（条件を満たす値の最大値を求める）
+def judge(n):
+    if n < 300:
+        return True
+    else:
+        return False
+
+ok = -1
+ng = 10 ** 9 + 1
+# OKになるギリギリのラインを攻める
+# 最後の一回でabs(ok - ng) <= 1になってもokは書き換えられない
+while abs(ok - ng) > 1:
+    mid = (ok + ng) // 2
+    if judge(mid):
+        ok = mid
+    else:
+        ng = mid
+
+# 299 NGは300
+print(ok)
+
+# 二分探索（条件を満たす値の最大値を求める）
+# 複雑なパターン
+N = 5
+A = [2, 4, 6, 19, 17]
+
+ng = -1
+ok = 10 ** 12 + 1
+
+# 超えてはいけないラインを設定
+def judge(limit):
+    flag = True
+    for i in range(N):
+        if A[i] > limit:
+            flag = False
+            break
+
+    return flag
+
+while ok - ng > 1:
+    mid = (ok + ng) // 2
+    # 差がK以下であれば修正可能
+    # もうちょい下のmidも試してみる
+    if judge(mid):
+        ok = mid
+    else:
+        ng = mid
+# 19
+print(ok)
+
+# 三分探索整数ver
+num = []
+for i in range(100):
+    if i < 50:
+        num.append(i)
+    else:
+        num.append(100 - i)
+
+left, right = 0, len(num) - 1
+while abs(right - left) > 3:
+    mid1 = (right * 2 + left) // 3 + 1
+    mid2 = (right + left * 2) // 3
+    # 最小値を求める場合は矢印逆になる
+    if num[mid1] <= num[mid2]:
+        right = mid1
+    else:
+        left = mid2
+
+# どうしても間は３つ空く
+# 51
+print(right)
+# 48
+print(left)
+
+# 三分探索小数点以下ver
+def f(x):
+    # これの最小値を求める
+    return x + p / pow(2, 2 * x / 3)
+
+p = 50
+left, right = 0, 100
+
+while right > left + 10 ** -10:
+    mid1 = (right * 2 + left) / 3
+    mid2 = (right + left * 2) / 3
+    if f(mid1) >= f(mid2):
+        # 上限を下げる（最小値をとるxはもうちょい下めの数だな）
+        right = mid1
+    else:
+        # 下限を上げる（最小値をとるxはもうちょい上めの数だな）
+        left = mid2
+
+# 8.959233535496452
+print(f(right))
