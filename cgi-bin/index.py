555e54c2e850d3c68294d1462b9ab88fc859e655
customaddone <custom1485@docomo.ne.jp>
2020-06-17 23:12:02 +0900

    104p layouts

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -51,87 +51,58 @@ mod = 10 ** 9 + 7
 # Main Code #
 #############
 
-class UnionFind():
-    def __init__(self, n):
-        self.n = n
-        self.parents = [-1] * n
-
-    def find(self, x):
-        if self.parents[x] < 0:
-            return x
-        else:
-            self.parents[x] = self.find(self.parents[x])
-            return self.parents[x]
-
-    def union(self, x, y):
-        x = self.find(x)
-        y = self.find(y)
-
-        if x == y:
-            return
-
-        if self.parents[x] > self.parents[y]:
-            x, y = y, x
-
-        self.parents[x] += self.parents[y]
-        self.parents[y] = x
-
-    def same(self, x, y):
-        return self.find(x) == self.find(y)
-
-    def members(self, x):
-        root = self.find(x)
-        return [i for i in range(self.n) if self.find(i) == root]
-
-    def roots(self):
-        return [i for i, x in enumerate(self.parents) if x < 0]
-
-    def all_group_members(self):
-        return {r: self.members(r) for r in self.roots()}
-
-N = 10
-
-query = [
-[4, 8, 6831],
-[1, 8, 4583],
-[0, 5, 6592],
-[0, 6, 3063],
-[3, 8, 4975],
-[1, 8, 2049],
-[4, 7, 2104],
-[2, 7, 781]
+N = 4
+query1 = [
+[1, 3, 10],
+[2, 4, 20]
 ]
 
-U = UnionFind(N)
+query2 = [
+[2, 3, 3]
+]
 
-for i in query:
-    U.union(i[0], i[1])
+# 条件
+# d[i] <= d[i + 1]
+# d[AD] + DL >= d[BL]
+# d[AD] + DD <= d[BD]
+# 上記のような制約であればグラフの考え方を適用できる
 
-def part_spanning(members, query):
-    mem_alta = [-1] * (members[-1] + 1)
-    N_a = len(members)
-    for i in range(N_a):
-        mem_alta[members[i]] = i
+# グラフ問題も
+# コストwの辺e = (v, u)について
+# d(v) + w >= d(u)
+# vからコストwでuまでいけるが、他の道も使うことができる
 
-    part_query = []
-    for i in query:
-        if i[0] in members or i[1] in members:
-            part_query.append(i)
-    # 今回大きい順に繋いだ
-    part_query.sort(reverse = True, key = lambda i:i[2])
+# 今回の場合
+# d(4) + 0 >= d(3), d(3) + 0 >= d(2)...
+# d(1) + 10 >= d(3), d(2) + 20 >= d(4)
+# d(2) + 3 <= d(3) → d(3) - 3 >= d(2)
 
-    U1 = UnionFind(N_a)
-    res = 0
-    for e in part_query:
-        a, b, cost = e
-        # members.index(a)だと計算量が多くなる
-        if not U1.same(mem_alta[a], mem_alta[b]):
-            res += cost
-            U1.union(mem_alta[a], mem_alta[b])
-    return res
+# このグラフの最小距離が条件を全て満たす通りとなり、答えとなる最大値となる
+# (少しでも周り道するとどれかの条件を満たさなくなる。最短距離で進むことが制約を全て満たす条件)
+edges = []
+for i in range(N - 1, 1, -1):
+    edges.append([i, i - 1, 0])
 
-# 各グループごとに最小全域木
-ans = 0
-for i in U.all_group_members().values():
-    ans += (len(i) * 10000 - part_spanning(i, query))
-print(ans)
+for i in range(len(query1)):
+    a, b, dis = query1[i]
+    edges.append([a - 1, b - 1, dis])
+
+for i in range(len(query2)):
+    a, b, dis = query2[i]
+    edges.append([b - 1, a - 1, -dis])
+
+def bellman(edges, num_v):
+    dist = [float('inf') for i in range(num_v)]
+    dist[0] = 0
+
+    changed = True
+    while changed:
+        changed = False
+        for edge in edges:
+            if dist[edge[1]] > dist[edge[0]] + edge[2]:
+                dist[edge[1]] = dist[edge[0]] + edge[2]
+                changed = True
+
+    return dist
+
+print(bellman(edges, N)[-1])
