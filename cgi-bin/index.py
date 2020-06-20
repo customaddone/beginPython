94b0c9d48ac6df88866712b3698e232a1e09bb02
customaddone <custom1485@docomo.ne.jp>
2020-06-21 00:37:31 +0900

    CODE FESTIVAL 2014 Easy C - 身体バランス

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -51,146 +51,80 @@ mod = 10 ** 9 + 7
 # Main Code #
 #############
 
-# mod不使用ver
-def cmb_1(n, r):
-    r = min(n - r, r)
-    if r == 0: return 1
-    over = reduce(mul, range(n, n - r, -1))
-    under = reduce(mul, range(1, r + 1))
-    return over // under
+def rand_dist(ransize, rantime):
+    dist_alta = []
+    dist_alta_2 = set()
+    while len(dist_alta) < min(rantime, ransize * (ransize - 1) // 2):
+        a, b = rand_ints_nodup(1, ransize, 2)
+        c = rand_N(1, 10)
+        if not (a, b) in dist_alta_2:
+            dist_alta.append([a, b, c])
+            dist_alta_2.add((a, b))
+    return sorted(dist_alta)
 
-# 10
-print(cmb_1(5, 3))
+N, M = getNM()
+S, T = getNM()
+S -= 1
+T -= 1
+query = [getList() for i in range(M)]
+"""
+N, M = getNM()
+S, T = rand_List(1, N, 2)
+print(S, T)
+S -= 1
+T -= 1
+query = rand_dist(N, M)
+print(query)
+"""
 
-# mod使用ver
-# nが大きい場合に
-def cmb_2(x,y):
-    r = 1
-    for i in range(1, y + 1):
-        r = (r * (x - i + 1) * pow(i, mod - 2, mod)) % mod
-    return r
+def build_tree_dis(N, edge_list):
 
-# 10
-print(cmb_2(5, 3))
+    G = [[] for i in range(N)]
 
-# 逆元事前処理ver
-# nが小さい場合に
-N = 10
+    for i in range(len(edge_list)):
+        a, b, c = edge_list[i]
+        G[a - 1].append([b - 1, c])
+        G[b - 1].append([a - 1, c])
 
-fact =[1] #階乗
-for i in range(1, N + 1):
-    fact.append(fact[i - 1] * i % mod)
+    # 葉（末端の数）
+    leaves = []
+    for i in range(N):
+        if len(G[i]) == 1:
+            leaves.append(i)
 
-facv = [0] * (N + 1) #階乗の逆元
-facv[-1] = pow(fact[-1], mod - 2 , mod)
+    return G
 
-for i in range(N - 1, -1, -1):
-    facv[i] = facv[i + 1] * (i + 1) % mod
+edges = build_tree_dis(N, query)
 
-def cmb(n, r):
-    if n < r:
-        return 0
-    return fact[n] * facv[r] * facv[n - r] % mod
-# 120
-print(cmb(10, 3))
+def dij(start):
+    dist = [float('inf') for i in range(N)]
+    dist[start] = 0
+    pq = [(0, start)]
 
-# 重複組み合わせ
-# 10個のものから重複を許して3つとる
-print(cmb_1(10 + 3 - 1, 3))
+    # pqの先頭がgoal行きのものなら最短距離を返す
+    while len(pq) > 0:
+        d, now = heapq.heappop(pq)
+        if (d > dist[now]):
+            continue
+        for i in edges[now]:
+            if dist[i[0]] > dist[now] + i[1]:
+                dist[i[0]] = dist[now] + i[1]
+                heapq.heappush(pq, (dist[i[0]], i[0]))
+    return dist
 
-# modが素数じゃない時
-def cmb_compose(n, k, mod):
-    dp = [[0] * (k + 1) for i in range(n + 1)]
-    dp[0][0] = 1
-    for i in range(1, n + 1):
-        dp[i][0] = 1
-        for j in range(1, k + 1):
-            # nCk = n - 1Ck - 1 + n - 1Ck
-            dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % mod
+list_1 = dij(S)
+list_2 = dij(T)
 
-    return dp[n][k]
-
-print(cmb_compose(10, 3, 50))
-
-# 再帰で組み合わせ
-N = 4
-L = [1, 1]
-root = 5
-
-# root ** Nでループ
-def four_pow(i, array):
-    global cnt
-    if i == N:
-        print(array)
-        return
-    for j in range(root):
-        new_array = array + [j]
-        four_pow(i + 1, new_array)
-# four_pow(0, [])
-
-# 組み合わせ
-def comb_pow(i, array):
-    global cnt
-    if i == N:
-        print(array)
-        return
-    # ここの4を変えてrootを変更
-    last = 0
-    if len(array) > 0:
-        last = array[-1]
-
-    for j in range(last + 1, root):
-        new_array = array + [j]
-        comb_pow(i + 1, new_array)
-# comb_pow(0, [])
-
-# 1スタート
-def comb_pow_2(i, array):
-    global cnt
-    if i == N:
-        print(array)
-        return
-    # ここの4を変えてrootを変更
-    last = 0
-    if len(array) > 0:
-        last = array[-1]
-
-    for j in range(last + 1, root + 1):
-        new_array = array + [j]
-        comb_pow_2(i + 1, new_array)
-comb_pow_2(0, [])
-
-# 重複組み合わせ
-def rep_comb_pow(i, array):
-    global cnt
-    if i == N:
-        print(array)
-        return
-    # ここの4を変えてrootを変更
-    last = 0
-    if len(array) > 0:
-        last = array[-1]
-
-    for j in range(last, root):
-        new_array = array + [j]
-        rep_comb_pow(i + 1, new_array)
-# rep_comb_pow(0, [])
-
-N = 2
-root = 5
-
-# 1スタート
-def rep_comb_pow_2(i, array):
-    global cnt
-    if i == N:
-        print(array)
-        return
-
-    last = 1
-    if len(array) > 0:
-        last = array[-1]
-
-    for j in range(last, root + 1):
-        new_array = array + [j]
-        rep_comb_pow_2(i + 1, new_array)
-rep_comb_pow_2(0, [])
+ans = float('inf')
+for i in range(N):
+    if i == S or i == T:
+        continue
+    if list_1[T] + list_2[i] == list_1[i] or list_2[S] + list_1[i] == list_2[i]:
+        continue
+    if list_1[i] == list_2[i]:
+        ans = i
+        break
+if ans < mod:
+    print(i + 1)
+else:
+    print(-1)
