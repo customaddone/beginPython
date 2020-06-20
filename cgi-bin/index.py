eab87ef6f7b79a4944f1e962923e54011c78a3fb
customaddone <custom1485@docomo.ne.jp>
2020-06-20 17:23:00 +0900

    ２部グラフ判定

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -51,85 +51,36 @@ mod = 10 ** 9 + 7
 # Main Code #
 #############
 
-N, M = 6, 8
-query = [
-[0, 1, 3],
-[0, 2, 3],
-[1, 2, 2],
-[1, 3, 3],
-[2, 4, 2],
-[3, 4, 4],
-[3, 5, 2],
-[4, 5, 3]
-]
+N = 5
+#es = [[1,2,3],[0,2],[0,1],[0,4],[3]] # False
+dist = [
+[1, 3],
+[0, 2],
+[1, 3],
+[0, 2, 4],
+[3]
+] # True
 
-ans = 0
-lines = defaultdict(set)
-cost = [[0] * N for i in range(N)]
-for i in range(M):
-    a, b, c = query[i]
-    if c != 0:
-        lines[a].add(b)
-        cost[a][b] += c
+#n個の頂点の色を初期化。0:未着色、1:黒、-1:白
+colors = [0] * N
 
-# sからスタート
-def Ford_Fulkerson(sta, end):
-    global ans
-    queue = deque()
-    queue.append([sta, float('inf')])
-
-    ignore = [1] * N
-    ignore[sta] = 0
-
-    route = [0] * N
-    route[sta] = -1
-
-    while queue:
-        s, flow = queue.pop()
-        for t in lines[s]:  #s->t
-            if ignore[t]: #未到達
-                # flowは入ってくる量、出る量のうち小さい方
-                flow = min(cost[s][t], flow)
-                route[t] = s
-                queue.append([t, flow])
-                ignore[t] = 0
-                if t == end: #ゴール到達
-                    ans += flow
-                    break
-        else:
-            continue #breakされなければWhile節の先頭に戻る
-        # Falseはされない
-        break
-    else:
-        return False
-
-    t = end
-    s = route[t]
-    # goalまで流れた量はflow
-    # 逆向きの辺を貼る
-    while s != -1:
-        #s->tのコスト減少，ゼロになるなら辺を削除
-        cost[s][t] -= flow
-        if cost[s][t] == 0:
-            lines[s].remove(t)
-            #t->s(逆順)のコスト増加，元がゼロなら辺を作成
-        if cost[t][s] == 0:
-            lines[t].add(s)
-
-        cost[t][s] += flow
-
-        # 一つ上の辺をたどる
-        t = s
-        s = route[t]
-
+#頂点vをcolor(1 or -1)で塗り、再帰的に矛盾がないか調べる。矛盾があればFalse
+def dfs(v, color):
+    #今いる点を着色
+    colors[v] = color
+    #今の頂点から行けるところをチェック
+    for to in dist[v]:
+        #同じ色が隣接してしまったらFalse
+        if colors[to] == color:
+            return False
+        #未着色の頂点があったら反転した色を指定し、再帰的に調べる
+        if colors[to] == 0 and not dfs(to, -color):
+            return False
+    #調べ終わったら矛盾がないのでTrue
     return True
 
-while True:
-    # ちょびちょび流して行ってゴールまで流れなくなったら終了
-    if Ford_Fulkerson(0, N - 1):
-        continue
-    else:
-        break
+#2部グラフならTrue, そうでなければFalse
+def is_bipartite():
+    return dfs(0,1) # 頂点0を黒(1)で塗ってDFS開始
 
-# 5(3 → 5の2と4 → 5の3)
-print(ans)
+print(is_bipartite())
