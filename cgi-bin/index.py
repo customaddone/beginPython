83cafecf0d6b9496c35db0b0db685c0eae884241
customaddone <custom1485@docomo.ne.jp>
2020-06-21 12:24:37 +0900

    Indeedなう（予選B）D - 高橋くんと数列

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -51,41 +51,19 @@ mod = 998244353
 # Main Code #
 #############
 
-# N:ブロック
-# M:色（バリエーション）
-# K:隣合う組み
-# N, M, K = 6, 2, 3のとき
-# K = 0 のとき
-# 121212, 212121
-# K = 1のとき,
-# 12121 のどこかに同じ数字を挟み込めば
-# 112121, 122121という風にできる
-# K = 2のとき
-# 1212 のどこかに同じ数字を挟む　を２回行う
-# 112212, 112122もできるし
-# 111212, 122212もできる
-N, M, K = getNM()
+N, C = getNM()
+A = getList()
 
-# 重複組み合わせのため
-# 繰り返し使うのでこのタイプ
-Y = N + K + 1
-fact =[1] #階乗
-for i in range(1, Y + 1):
-    fact.append(fact[i - 1] * i % mod)
+lista = [[] for i in range(C)]
 
-facv = [0] * (Y + 1) #階乗の逆元
-facv[-1] = pow(fact[-1], mod - 2 , mod)
+for i in range(N):
+    lista[A[i] - 1].append(i)
 
-for i in range(Y - 1, -1, -1):
-    facv[i] = facv[i + 1] * (i + 1) % mod
-
-def cmb(n, r):
-    if n < r:
-        return 0
-    return fact[n] * facv[r] * facv[n - r] % mod
-
-ans = 0
-for i in range(K + 1):
-    opt = M * pow(M - 1, N - i - 1, mod) * cmb(N - i + i - 1, i)
-    ans += opt % mod
-print(ans % mod)
+for i in lista:
+    ans = 0
+    for j in range(len(i)):
+        if j == 0:
+            ans += (i[j] + 1) * (N - i[j])
+        else:
+            ans += (i[j] - i[j - 1]) * (N - i[j])
+    print(ans)
