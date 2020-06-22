280f947cf3472369053289e9e67bc6800d19ca52
customaddone <custom1485@docomo.ne.jp>
2020-06-22 18:30:33 +0900

    幾何

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -51,6 +51,80 @@ mod = 10 ** 9 + 7
 # Main Code #
 #############
 
+# angle度のtan
+def tan(angle):
+    return math.tan(math.radians(angle))
+
+# 余弦定理
+A, B, H, M = 3, 4, 10, 40
+minu = H * 60 + M
+
+# 時針の角度
+angh = minu / 2
+# 分針の角度
+angm = M * 6
+
+# 角度の差
+anglepre = abs(angh - angm)
+angle = min(360 - anglepre, anglepre)
+
+# ラジアンに直してからmath.cosとかmath.tanとか
+ans = (A ** 2) + (B ** 2) - (2 * A * B * math.cos(math.radians(angle)))
+
+# 三角形の面積4.564257194330056
+print(math.sqrt(ans))
+
+# 二つの点を通る直線の方程式を求める
+# y = line[0]x + line[1]
+# 傾きmaxならx = line[3]
+def line(Ax, Ay, Bx, By):
+    if Ay == By:
+        return 0, Ay, None
+    if Ax == Bx:
+        return float("inf"), 0, Ax
+    a = (Ay - By) / (Ax - Bx)
+    b = Ay - a * Ax
+    return a, b, None
+
+# (1.0, 2.0, None)
+p1 = line(0, 2, 1, 3)
+print(p1)
+
+# 点[p1, p2]を通り、傾きslopeの直線に直行する直線を求める
+def perp(p1, p2, slope):
+    if slope == 0:
+        return float('inf'), 0, p1
+    if slope > 10 ** 12:
+        return 0, p2, None
+    opt_slope = (1 / slope) * (-1)
+    opt_inter = p2 - (opt_slope * p1)
+
+    return opt_slope, opt_inter, None
+
+def distance(Ax, Ay, Bx, By):
+    return math.hypot(Ax - Bx, Ay - By)
+
+# (-1.0, 2.0, None)
+p2 = perp(0, 2, 1)
+print(p2)
+
+# 二つの線がクロスするx,y座標を出す
+def cross(l1, l2):
+    a1, b1, xx1 = min(l1, l2)
+    a2, b2, xx2 = max(l1, l2)
+    if a1 == a2:
+        return None
+    elif a2 == float("inf"):
+        x = xx2
+    else:
+        x = (b1 - b2) / (a2 - a1)
+    y = a1 * x + b1
+    return x, y
+
+# (0.0, 2.0)
+print(cross(p1, p2))
+
+# 正方形の点を二つ入れると残りの点２つが出てくる
 def square(x1, y1, x2, y2):
     nx_1 = x1 - (y1 - y2)
     ny_1 = y1 + (x1 - x2)
@@ -59,20 +133,4 @@ def square(x1, y1, x2, y2):
 
     return [nx_1, ny_1, nx_2, ny_2]
 
-N = getN()
-lista = []
-ans = 0
-for i in range(N):
-    x, y = getNM()
-    lista.append((x, y))
-listb = set(lista)
-
-for i in range(N):
-    for j in range(i + 1, N):
-        x1, y1 = lista[i]
-        x2, y2 = lista[j]
-        nx_1, ny_1, nx_2, ny_2 = square(x1, y1, x2, y2)
-        if (nx_1, ny_1) in listb and (nx_2, ny_2) in listb:
-            matl = (nx_1 - nx_2) ** 2 + (ny_1 - ny_2) ** 2
-            ans = max(ans, matl)
-print(ans)
+print(*square(1, 1, 2, 4))
