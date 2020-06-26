ffbb6a084f4a870c022ec68e10d9776b5ca07626
customaddone <custom1485@docomo.ne.jp>
2020-06-26 22:22:53 +0900

    ABC034 D - 食塩水

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -51,74 +51,28 @@ mod = 10 ** 9 + 7
 # Main Code #
 #############
 
-N, M, T = 8, 15, 120
-A = [1, 2, 6, 16, 1, 3, 11, 9]
+N, K = 3, 2
 query = [
-[1, 8, 1],
-[7, 3, 14],
-[8, 2, 13],
-[3, 5, 4],
-[5, 7, 5],
-[6, 4, 1],
-[6, 8, 17],
-[7, 8, 5],
-[1, 4, 2],
-[4, 7, 1],
-[6, 1, 3],
-[3, 1, 10],
-[2, 6, 5],
-[2, 4, 12],
-[5, 1, 30]
+[100, 15],
+[200, 20],
+[200, 30]
 ]
-dist_1 = []
-dist_2 = []
-for i in range(M):
-    a, b, c = query[i]
-    dist_1.append([a, b, c])
-    # 帰りがけの最短経路については全ての道を逆順にすればいい
-    dist_2.append([b, a, c])
 
-def build_tree_dis(N, edge_list):
-
-    G = [[] for i in range(N)]
-
-    for i in range(len(edge_list)):
-        a, b, c = edge_list[i]
-        G[a - 1].append([b - 1, c])
-
-    # 葉（末端の数）
-    leaves = []
+def judge(target):
+    alta = []
     for i in range(N):
-        if len(G[i]) == 1:
-            leaves.append(i)
+        salt = query[i][0] * (query[i][1] - target)
+        alta.append(salt)
+    alta.sort(reverse = True)
+    return sum(alta[:K]) >= 0
 
-    return G
+left = -1
+right = 101
 
-edges_1 = build_tree_dis(N, dist_1)
-edges_2 = build_tree_dis(N, dist_2)
-
-def dij(start, edges):
-    dist = [float('inf') for i in range(N)]
-    dist[start] = 0
-    pq = [(0, start)]
-
-    # pqの先頭がgoal行きのものなら最短距離を返す
-    while len(pq) > 0:
-        d, now = heapq.heappop(pq)
-        if (d > dist[now]):
-            continue
-        for i in edges[now]:
-            if dist[i[0]] > dist[now] + i[1]:
-                dist[i[0]] = dist[now] + i[1]
-                heapq.heappush(pq, (dist[i[0]], i[0]))
-    return dist
-
-dij_to = dij(0, edges_1)
-dij_from = dij(0, edges_2)
-ans = 0
-
-for i in range(N):
-    time = dij_to[i] + dij_from[i]
-    opt = (T - time) * A[i]
-    ans = max(ans, opt)
-print(ans)
+for i in range(100):
+    mid = left + (right - left) / 2
+    if judge(mid):
+        left = mid
+    else:
+        right = mid
+print(left)
