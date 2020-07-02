ac7153880fc5360fce4827a42bf7abef6b4bb1a6
customaddone <custom1485@docomo.ne.jp>
2020-07-02 18:07:13 +0900

    桁dpライブラリ

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -51,27 +51,100 @@ mod = 10 ** 9 + 7
 # Main Code #
 #############
 
-N, M = 5, 3
-query = [
-[1, 2],
-[1, 3],
-[1, 4],
-[2, 1],
-[2, 3]
-]
+# ABC154 E - Almost Everywhere Zero
+N = '9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999'
+K = 3
+L = len(N)
 
-A_list = [[] for i in range(10 ** 5 + 1)]
-for a, b in query:
-    A_list[a].append(b)
+def judge(a):
+    return a != 0
 
-job = []
-heapq.heapify(job)
+# N以下の数字で条件を満たす桁がk個のもの
+def digit_dp(n, k):
+    l = len(n)
+
+    dp = [[[0] * (k + 1) for _ in range(2)] for i in range(l + 1)]
+    dp[0][0][0] = 1
+
+    for i in range(l):
+        d = int(n[i])
+
+        for j in range(2):
+            for d_j in range(10 if j else d + 1):
+                for k_j in range(k + 1):
+                    if judge(d_j):
+                        if k_j + 1 <= k:
+                            dp[i + 1][j | (d_j < d)][k_j + 1] += dp[i][j][k_j]
+                    else:
+                        dp[i + 1][j | (d_j < d)][k_j] += dp[i][j][k_j]
+
+    return dp
+
+dp = digit_dp(N, K)
+print(dp[L][0][K] + dp[L][1][K])
+
+# ABC029 D - 1
+N = '999999999'
+L = len(N)
+
+def judge_2(a):
+    return a == 1
+
+# N以下の数字の中で「1が書いてある桁がk個ある数字」がいくつあるか
+# 上のものと関数の中身自体は変えていない
+def digit_dp_2(n, k):
+    l = len(n)
+
+    dp = [[[0] * (k + 1) for _ in range(2)] for i in range(l + 1)]
+    dp[0][0][0] = 1
+
+    for i in range(l):
+        d = int(n[i])
+
+        for j in range(2):
+            for d_j in range(10 if j else d + 1):
+                for k_j in range(k + 1):
+                    if judge_2(d_j):
+                        if k_j + 1 <= k:
+                            dp[i + 1][j | (d_j < d)][k_j + 1] += dp[i][j][k_j]
+                    else:
+                        dp[i + 1][j | (d_j < d)][k_j] += dp[i][j][k_j]
+
+    return dp
+
+dp = digit_dp_2(N, L)
 
 ans = 0
-for i in range(1, M + 1):
-    for j in A_list[i]:
-        heapq.heappush(job, -j)
-    if len(job) > 0:
-        u = heapq.heappop(job)
-        ans += -u
+for j in range(L + 1):
+    # dp[l]について各j(1のカウント)の通りの数 * j
+    ans += (dp[L][0][j] + dp[L][1][j]) * j
 print(ans)
+
+# ABC129 E - Sum Equals Xor
+# 通りの数を求める
+
+L = '1111111111111111111'
+
+def digit_dp_3(n):
+    l = len(n)
+
+    dp = [[[0] * 2 for _ in range(2)] for i in range(l + 1)]
+    dp[0][0][0] = 1
+
+    for i in range(l):
+        d = int(n[i])
+
+        # Lになる可能性があるかないか
+        for j in range(2):
+            # 次の桁が0か1か
+            for d_j in range(2 if j else d + 1):
+                if d_j == 0:
+                    dp[i + 1][j | (d_j < d)][d_j] += (dp[i][j][0] + dp[i][j][1])
+                    dp[i + 1][j | (d_j < d)][d_j] %= mod
+                else:
+                    dp[i + 1][j | (d_j < d)][d_j] += 2 * (dp[i][j][0] + dp[i][j][1])
+                    dp[i + 1][j | (d_j < d)][d_j] %= mod
+
+    return sum(dp[-1][0]) + sum(dp[-1][1])
+
+print(digit_dp_3(L) % mod)
