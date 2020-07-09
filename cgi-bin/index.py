8880d480a69fb83d0670cdb21114621569da6708
customaddone <custom1485@docomo.ne.jp>
2020-07-09 21:41:01 +0900

    B - マス目と駒

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -46,89 +46,37 @@ import sys
 sys.setrecursionlimit(1000000000)
 mod = 10 ** 9 + 7
 
-
 #############
 # Main Code #
 #############
 
-N = 4
-A = [
-[2, 3, 4],
-[1, 3, 4],
-[4, 1, 2],
-[3, 1, 2]
-]
+H, W = getNM()
+maze = [list(input()) for i in range(H)]
 
-# 各試合に割り振るコード番号
-def code(l, r, of):
-    if l > r:
-        l, r = r, l
-    return of[l] + r - l - 1
+dp = [[-1] * W for i in range(H)]
+# (0, 0)に置けば勝ち
+dp[H - 1][W - 1] = 0
 
-of = [0] * (N + 1)
-# i番目の選手は試合of[i - 1] + 1からof[i]番目の試合に参加する
-for i in range(N):
-    of[i + 1] = of[i] + N - i - 1
-g = [[] for i in range(N * (N - 1) // 2)]
+dx = [1, 0, 1]
+dy = [0, 1, 1]
 
-# 選手iが戦う試合について
-for i in range(N):
-    for j in range(N - 2):
-        # 試合
-        fcode = code(i, A[i][j] - 1, of)
-        # fcodeの次にくる試合
-        tcode = code(i, A[i][j + 1] - 1, of)
-        # 試合tcodeは試合fcodeより後にくる
-        g[fcode].append(tcode)
+def dfs(x, y):
+    if maze[y][x] == "#":
+        return 1
 
-# トポソする
-def sort_topologically(g):
-    N = len(g)
-    ec = [0] * N
-    for i in range(N):
-        # それぞれの試合の後に来る試合について
-        for e in g[i]:
-            # eの前に来る試合を数える
-            ec[e] += 1
-    ret = [0] * N
-    q = 0
+    if dp[y][x] != -1:
+        return dp[y][x]
 
-    for i in range(N):
-        # 後に来る試合がなければ
-        if ec[i] == 0:
-            # 先頭はret[0] = a1, ret[1] = a2...
-            ret[q] = i
-            q += 1
+    result = 0
+    for i in range(3):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < W and 0 <= ny < H and maze[ny][nx] == "." and dfs(nx, ny) == 0:
+            result = 1
+    dp[y][x] = result
+    return result
 
-    p = 0
-    # p == q　全て並び終えるまで続く
-    while p < q:
-        # 先頭に来る試合ret[0], ret[1]...のそれより後に行われる試合について
-        for to in g[ret[p]]:
-            # 前に来る試合を一つ減らす
-            ec[to] -= 1
-            # もしそれで先頭になれたなら
-            if ec[to] == 0:
-                # 記録する
-                ret[q] = to
-                q += 1
-        p += 1
-
-    # 全ての前に来る試合を消せなかったら
-    for i in range(N):
-        if ec[i] > 0:
-            return None
-    return ret
-
-od = sort_topologically(g)
-
-if not od:
-    print(-1)
+if dfs(0, 0):
+    print('First')
 else:
-    dp = [1] * (N * (N - 1) // 2)
-    # 全ての試合について
-    for i in range(N * (N - 1) // 2):
-        for e in g[od[i]]:
-            # もしod[i]（eより前にあるもの）がでかければ更新
-            dp[e] = max(dp[e], dp[od[i]] + 1)
-    print(max(dp))
+    print('Second')
