7572a574a71300085466865aeb64cdf187e551b4
customaddone <custom1485@docomo.ne.jp>
2020-07-23 17:14:12 +0900

    組み合わせ

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -50,109 +50,212 @@ mod = 10 ** 9 + 7
 # Main Code #
 #############
 
-N = getN()
-A = [getList() for i in range(N)]
-B = [getList() for i in range(N)]
+def getN():
+    return int(input())
+def getNM():
+    return map(int, input().split())
+def getList():
+    return list(map(int, input().split()))
+def getArray(intn):
+    return [int(input()) for i in range(intn)]
+def input():
+    return sys.stdin.readline().rstrip()
+def rand_N(ran1, ran2):
+    return random.randint(ran1, ran2)
+def rand_List(ran1, ran2, rantime):
+    return [random.randint(ran1, ran2) for i in range(rantime)]
+def rand_ints_nodup(ran1, ran2, rantime):
+  ns = []
+  while len(ns) < rantime:
+    n = random.randint(ran1, ran2)
+    if not n in ns:
+      ns.append(n)
+  return sorted(ns)
 
-# 点が２つだと面積ができない
-if N == 2:
-    print(((B[1][0] - B[0][0]) ** 2 + (B[1][1] - B[0][1]) ** 2) ** .5 / ((A[1][0] - A[0][0]) ** 2 + (A[1][1] - A[0][1]) ** 2) ** .5)
-    exit()
+def rand_query(ran1, ran2, rantime):
+  r_query = []
+  while len(r_query) < rantime:
+    n_q = rand_ints_nodup(ran1, ran2, 2)
+    if not n_q in r_query:
+      r_query.append(n_q)
+  return sorted(r_query)
 
-def getG(XY):
-    n = len(XY)
-    total_x, total_y = 0, 0
-    for x,y in XY:
-        total_x += x
-        total_y += y
-    return (total_x/n, total_y/n)
+from collections import defaultdict, deque, Counter
+from sys import exit
+from decimal import *
+import heapq
+import math
+from fractions import gcd
+import random
+import string
+import copy
+from itertools import combinations, permutations, product
+from operator import mul
+from functools import reduce
+from bisect import bisect_left, bisect_right
 
-def getD(XY, Gx, Gy):
-    ret = 0
-    for x,y in XY:
-        d = ((x - Gx) ** 2 + (y - Gy) ** 2) ** .5
-        ret = max(ret, d)
-    return ret
+import sys
+sys.setrecursionlimit(1000000000)
+mod = 10 ** 9 + 7
 
-# 重心 : G
-GAx, GAy = getG(A)
-GBx, GBy = getG(B)
 
-# 重心から最も遠い点までの距離 : D
-DA = getD(A, GAx, GAy)
-DB = getD(B, GBx, GBy)
+#############
+# Main Code #
+#############
 
-# 拡大比率
-ans = DB / DA
-print(ans)
+# mod不使用ver
+def cmb_1(n, r):
+    r = min(n - r, r)
+    if r == 0: return 1
+    over = reduce(mul, range(n, n - r, -1))
+    under = reduce(mul, range(1, r + 1))
+    return over // under
 
-"""
-def area(X, Y, Z):  # 三角形の符号付き面積の2倍
-    return (Y[0] - X[0]) * (Z[1] - X[1]) - (Y[1] - X[1]) * (Z[0] - X[0])
+# 10
+print(cmb_1(5, 3))
 
-def ConvexHull(A):  # Aの凸包を形成する点のリストを返す
-    A = sorted(A, key = itemgetter(0,1))  # 適宜変更
-    res = []
-    N = len(A)
-    for i in range(N):  # 下側
-        a = A[i]
-        while len(res) > 1 and area(res[-2], res[-1], a) <= 0:  # 適宜'>,<,>='に変更
-            res.pop()
-        res.append(a)
-    r = len(res)
-    for i in range(N - 2, -1, -1):  # 上側
-        a = A[i]
-        while len(res) > r and area(res[-2], res[-1], a) <= 0:  # 適宜'>,<,>='に変更
-            res.pop()
-        res.append(a)
-    return res
+# mod使用ver
+# nが大きい場合に
+def cmb_2(x,y):
+    r = 1
+    for i in range(1, y + 1):
+        r = (r * (x - i + 1) * pow(i, mod - 2, mod)) % mod
+    return r
 
-def dist(a, b):
-    return sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2)
+# 10
+print(cmb_2(5, 3))
 
-# キャリパー法(Rotating Calipers法)
-# 凸多角形を回転しながら走査し、最遠点対を求める
-def rotating_calipers(ps):
-    qs = ConvexHull(ps)
-    n = len(qs)
+# 逆元事前処理ver
+# nが小さい場合に
+lim = 10 ** 6 + 1
+fact = [1, 1]
+factinv = [1, 1]
+inv = [0, 1]
 
-    if n == 2:
-        return dist(qs[0], qs[1])
+for i in range(2, lim + 1):
+    fact.append((fact[-1] * i) % mod)
+    inv.append((-inv[mod % i] * (mod // i)) % mod)
+    # 累計
+    factinv.append((factinv[-1] * inv[-1]) % mod)
 
-    i = j = 0
+def cmb(n, r):
+    if (r < 0) or (n < r):
+        return 0
+    r = min(r, n - r)
+    return fact[n] * factinv[r] * factinv[n - r] % mod
+# 120
+print(cmb(10, 3))
 
-    for k in range(n):
-        if qs[k] < qs[i]: i = k
-        if qs[j] < qs[k]: j = k
+# 重複組み合わせ
+# 10個のものから重複を許して3つとる
+print(cmb_1(10 + 3 - 1, 3))
 
-    res = 0
-    si = i
-    sj = j
-    while i != sj or j != si:
-        res = max(res, dist(qs[i], qs[j]))
-        if area(qs[j], qs[i - n + 1], qs[j - n + 1]) < 0:
-            i = (i + 1) % n
-        else:
-            j = (j + 1) % n
-    return res
+# modが素数じゃない時
+def cmb_compose(n, k, mod):
+    dp = [[0] * (k + 1) for i in range(n + 1)]
+    dp[0][0] = 1
+    for i in range(1, n + 1):
+        dp[i][0] = 1
+        for j in range(1, k + 1):
+            # nCk = n - 1Ck - 1 + n - 1Ck
+            dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % mod
 
-A.sort()
-B.sort()
-a_d = rotating_calipers(A)
-b_d = rotating_calipers(B)
-print(b_d / a_d)
+    return dp[n][k]
 
-# グラハムスキャン(2次元の場合使用可能)
+print(cmb_compose(10, 3, 50))
 
-# 凸包の面積比の平方根
-# 凸包を形成する各頂点が出る
-C = ConvexHull(A)
-D = ConvexHull(B)
-SC = 0
-SD = 0
-for i in range(len(C) - 2):
-    SC += area(C[0], C[i + 1], C[i + 2])
-    SD += area(D[0], D[i + 1], D[i + 2])
+A, B, C = 144949225, 545897619, 393065978
 
-print((SD / SC) ** .5)
-"""
+# kCc / k+1Cc = k - c + 1 / k + 1
+# k+1Cc+1 / kCc = k + 1 / c + 1
+# Xを10 ** 9 + 7 - 2乗すると逆元が求まる
+x = (C * pow(A, mod - 2, mod)) % mod
+y = (B * pow(A, mod - 2, mod)) % mod
+
+n = (x + y - 2 * x * y) * pow(x * y - x - y, mod - 2, mod)
+k = (y - x * y) * pow(x * y - x - y, mod - 2, mod)
+print((n - k) % mod, k % mod)
+
+
+# 再帰で組み合わせ
+N = 4
+L = [1, 1]
+root = 5
+
+# root ** Nでループ
+def four_pow(i, array):
+    global cnt
+    if i == N:
+        print(array)
+        return
+    for j in range(root):
+        new_array = array + [j]
+        four_pow(i + 1, new_array)
+# four_pow(0, [])
+
+# 組み合わせ
+def comb_pow(i, array):
+    global cnt
+    if i == N:
+        print(array)
+        return
+    # ここの4を変えてrootを変更
+    last = -1
+    if len(array) > 0:
+        last = array[-1]
+
+    for j in range(last + 1, root):
+        new_array = array + [j]
+        comb_pow(i + 1, new_array)
+#comb_pow(0, [])
+
+# 1スタート
+def comb_pow_2(i, array):
+    global cnt
+    if i == N:
+        print(array)
+        return
+    # ここの4を変えてrootを変更
+    last = 0
+    if len(array) > 0:
+        last = array[-1]
+
+    for j in range(last + 1, root + 1):
+        new_array = array + [j]
+        comb_pow_2(i + 1, new_array)
+# comb_pow_2(0, [])
+
+# 重複組み合わせ
+def rep_comb_pow(i, array):
+    global cnt
+    if i == N:
+        print(array)
+        return
+    # ここの4を変えてrootを変更
+    last = 0
+    if len(array) > 0:
+        last = array[-1]
+
+    for j in range(last, root):
+        new_array = array + [j]
+        rep_comb_pow(i + 1, new_array)
+# rep_comb_pow(0, [])
+
+N = 2
+root = 5
+
+# 1スタート
+def rep_comb_pow_2(i, array):
+    global cnt
+    if i == N:
+        print(array)
+        return
+
+    last = 0
+    if len(array) > 0:
+        last = array[-1]
+
+    for j in range(last + 1, root + 1):
+        new_array = array + [j]
+        rep_comb_pow_2(i + 1, new_array)
+# rep_comb_pow_2(0, [])
