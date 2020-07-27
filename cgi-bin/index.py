6ffc792d650bfeb385b75f28859220f31378a668
customaddone <custom1485@docomo.ne.jp>
2020-07-27 19:52:21 +0900

    ABC002 D - 派閥

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -32,7 +32,7 @@ from collections import defaultdict, deque, Counter
 from sys import exit
 from decimal import *
 import heapq
-from math import sqrt
+from math import sqrt, pi
 from fractions import gcd
 import random
 import string
@@ -50,164 +50,32 @@ mod = 10 ** 9 + 7
 # Main Code #
 #############
 
-# mod不使用ver
-def cmb_1(n, r):
-    r = min(n - r, r)
-    if (r < 0) or (n < r):
-        return 0
+# 条件
+# n人の国会議員の集合A{A1, A2... An}の任意の二人i, jについて
+# (i, j)がqueryに含まれる
 
-    if r == 0:
-        return 1
+# この人数nの最大値を求める
 
-    over = reduce(mul, range(n, n - r, -1))
-    under = reduce(mul, range(1, r + 1))
-    return over // under
+# 集合Aの取り方は？
+# N <= 12なのでbit全探索で全ての集合について条件を満たすか判定できる
+N, M = getNM()
+mem = set()
+for i in range(M):
+    a, b = getNM()
+    mem.add((a - 1, b - 1))
 
-# 10
-print(cmb_1(5, 3))
-
-# mod使用ver
-# nが大きい場合に
-def cmb_2(x,y):
-    r = 1
-    for i in range(1, y + 1):
-        r = (r * (x - i + 1) * pow(i, mod - 2, mod)) % mod
-    return r
-
-# 10
-print(cmb_2(5, 3))
-
-# 逆元事前処理ver
-# nが小さい場合に
-lim = 10 ** 6 + 1
-fact = [1, 1]
-factinv = [1, 1]
-inv = [0, 1]
-
-for i in range(2, lim + 1):
-    fact.append((fact[-1] * i) % mod)
-    inv.append((-inv[mod % i] * (mod // i)) % mod)
-    # 累計
-    factinv.append((factinv[-1] * inv[-1]) % mod)
-
-def cmb(n, r):
-    if (r < 0) or (n < r):
-        return 0
-    r = min(r, n - r)
-    return fact[n] * factinv[r] * factinv[n - r] % mod
-# 120
-print(cmb(10, 3))
-
-# 重複組み合わせ
-# 10個のものから重複を許して3つとる
-print(cmb_1(10 + 3 - 1, 3))
-
-# modが素数じゃない時
-def cmb_compose(n, k, mod):
-    dp = [[0] * (k + 1) for i in range(n + 1)]
-    dp[0][0] = 1
-    for i in range(1, n + 1):
-        dp[i][0] = 1
-        for j in range(1, k + 1):
-            # nCk = n - 1Ck - 1 + n - 1Ck
-            dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % mod
-
-    return dp[n][k]
-
-print(cmb_compose(10, 3, 50))
-
-A, B, C = 144949225, 545897619, 393065978
-
-# kCc / k+1Cc = k - c + 1 / k + 1
-# k+1Cc+1 / kCc = k + 1 / c + 1
-# Xを10 ** 9 + 7 - 2乗すると逆元が求まる
-x = (C * pow(A, mod - 2, mod)) % mod
-y = (B * pow(A, mod - 2, mod)) % mod
-
-n = (x + y - 2 * x * y) * pow(x * y - x - y, mod - 2, mod)
-k = (y - x * y) * pow(x * y - x - y, mod - 2, mod)
-print((n - k) % mod, k % mod)
-
-
-# 再帰で組み合わせ
-N = 4
-L = [1, 1]
-root = 5
-
-# root ** Nでループ
-def four_pow(i, array):
-    global cnt
-    if i == N:
-        print(array)
-        return
-    for j in range(root):
-        new_array = array + [j]
-        four_pow(i + 1, new_array)
-# four_pow(0, [])
-
-# 組み合わせ
-def comb_pow(i, array):
-    global cnt
-    if i == N:
-        print(array)
-        return
-    # ここの4を変えてrootを変更
-    last = -1
-    if len(array) > 0:
-        last = array[-1]
-
-    for j in range(last + 1, root):
-        new_array = array + [j]
-        comb_pow(i + 1, new_array)
-#comb_pow(0, [])
-
-# 1スタート
-def comb_pow_2(i, array):
-    global cnt
-    if i == N:
-        print(array)
-        return
-    # ここの4を変えてrootを変更
-    last = 0
-    if len(array) > 0:
-        last = array[-1]
-
-    for j in range(last + 1, root + 1):
-        new_array = array + [j]
-        comb_pow_2(i + 1, new_array)
-# comb_pow_2(0, [])
-
-# 重複組み合わせ
-def rep_comb_pow(i, array):
-    global cnt
-    if i == N:
-        print(array)
-        return
-    # ここの4を変えてrootを変更
-    last = 0
-    if len(array) > 0:
-        last = array[-1]
-
-    for j in range(last, root):
-        new_array = array + [j]
-        rep_comb_pow(i + 1, new_array)
-# rep_comb_pow(0, [])
-
-N = 2
-root = 5
-
-# 1スタート
-def rep_comb_pow_2(i, array):
-    global cnt
-    if i == N:
-        print(array)
-        return
-
-    last = 0
-    if len(array) > 0:
-        last = array[-1]
-
-    for j in range(last + 1, root + 1):
-        new_array = array + [j]
-        rep_comb_pow_2(i + 1, new_array)
-# rep_comb_pow_2(0, [])
+ans = 0
+for bit in range(1 << N):
+    # 任意のi, jについてqueryに含まれているか判定
+    flag = True
+    for i in range(N):
+        for j in range(i + 1, N):
+            # 適当に選んだ２人がbitの中に含まれていれば
+            if bit & (1 << i) and bit & (1 << j):
+                if not (i, j) in mem:
+                    flag = False
+    # もし集合bitが条件を満たすなら人数を調べる
+    if flag:
+        opt = bin(bit).count('1')
+        ans = max(ans, opt)
+print(ans)
--- cgi-bin/myfile.py
+++ cgi-bin/myfile.py
@@ -70,54 +70,33 @@ mod = 10 ** 9 + 7
 # Main Code #
 #############
 
-N = getN()
-A = getList()
-prime_list = defaultdict(int)
+S = input()
+N = len(S)
+# dp[i][j]: i番目にjまで丸をつけ終えている通り
+dp = [[0] * 4 for _ in range(N + 1)]
+dp[0][0] = 1
 
-def prime_factorize(n):
-    divisors = []
-    # 27(2 * 2 * 7)の7を出すためにtemp使う
-    temp = n
-    for i in range(2, int(math.sqrt(n)) + 1):
-        if temp % i == 0:
-            cnt = 0
-            while temp % i == 0:
-                cnt += 1
-                # 素因数を見つけるたびにtempを割っていく
-                temp //= i
-            divisors.append([i, cnt])
-    if temp != 1:
-        divisors.append([temp, 1])
-    if divisors == []:
-        divisors.append([n, 1])
-
-    return divisors
-
-for i in range(N):
-    prime = prime_factorize(A[i])
-    for j in prime:
-        prime_list[j[0]] = max(prime_list[j[0]], j[1])
-
-num = 1
-for key, value in prime_list.items():
-    num *= key ** value
-    num %= mod
-
-# 1/A[i]のmod
-
-fact =[1] #階乗
-for i in range(1, 10 ** 6 + 2):
-    fact.append(fact[i - 1] * i % mod)
-
-facv = [0] * (10 ** 6 + 2) #階乗の逆元
-facv[-1] = pow(fact[-1], mod - 2 , mod)
-
-for i in range(10 ** 6, -1, -1):
-    facv[i] = facv[i + 1] * (i + 1) % mod
-
-print(facv[:10])
-
-ans = 0
-for i in A:
-    opt = (num * facv[i]) % mod
-    print(facv[i] * i)
+for i in range(1, N + 1):
+    for j in range(4):
+        # i番目の文字が?でなければ
+        if S[i] != '?':
+            # 一つ前のものを引き継ぐ
+            dp[i + 1][j] += dp[i][j]
+            dp[i + 1][j] %= mod
+        # ?なら
+        else:
+            # A, B, C３つ分引き継ぐ
+            dp[i + 1][j] += 3 * dp[i][j]
+            dp[i + 1][j] %= mod
+    # もしS[i]がAか?ならj == 0の場合カウントを一つ増やせる
+    # これでABC○A○B○Cと○A○B○CABCは区別される
+    if S[i] == 'A' or S[i] == '?':
+        dp[i + 1][1] += dp[i][0]
+        dp[i + 1][1] %= mod
+    if S[i] == 'B' or S[i] == '?':
+        dp[i + 1][2] += dp[i][1]
+        dp[i + 1][2] %= mod
+    if S[i] == 'C' or S[i] == '?':
+        dp[i + 1][3] += dp[i][2]
+        dp[i + 1][3] %= mod
+print(dp[N][3])
