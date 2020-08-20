2690e13afd379cc3f1954dbe294399ff7959c1eb
customaddone <custom1485@docomo.ne.jp>
2020-08-20 23:49:39 +0900

    にぶたん数え上げ

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -50,6 +50,79 @@ mod = 10 ** 9 + 7
 # Main Code #
 #############
 
+num = [i for i in range(0, 10, 2)]
+A = [2, 4, 5]
+B = [2, 3]
+
+for i in A:
+    index = bisect_right(num, i)
+    print(num[index - 1])
+
+# numの中でのi未満の数字の最大値を求める
+for i in A:
+    index = bisect_left(num, i)
+    print(num[index - 1])
+
+# numの中でのiより大きい数字の最小値を求める
+for i in B:
+    index = bisect_right(num, i)
+    print(num[index])
+
+# numの中でのi以上の数字の最小値を求める
+for i in B:
+    index = bisect_left(num, i)
+    print(num[index])
+
+A = [1, 2, 4, 8, 16, 32]
+
+def or_less(array, x):
+    # arrayの中のx以下のものの個数
+    # arrayの中のx以下のもののうちの最大値
+    index = bisect_right(array, x)
+    if index == 0:
+        or_less_int = -float('inf')
+    else:
+        or_less_int = array[index - 1]
+    return [index, or_less_int]
+
+def less_than(array, x):
+    # arrayの中のx未満のものの個数
+    # arrayの中のx未満のもののうちの最大値
+    index = bisect_left(array, x)
+    if index == 0:
+        less_than_int = -float('inf')
+    else:
+        less_than_int = array[index - 1]
+    return [index, less_than_int]
+
+print(or_less(A, 8))
+print(less_than(A, 1))
+
+def or_more(array, x):
+    # arrayの中のx以上のものの個数
+    # arrayの中のx以上のもののうちの最小値
+    n = len(array)
+    index = bisect_left(array, x)
+    if index == n:
+        or_more_int = float('inf')
+    else:
+        or_more_int = array[index]
+    return [n - index, or_more_int]
+
+def more_than(array, x):
+    # arrayの中のxより大きいものの個数
+    # arrayの中のxより大きいのもののうちの最小値
+    n = len(array)
+    index = bisect_right(array, x)
+    if index == n:
+        more_than_int = float('inf')
+    else:
+        more_than_int = array[index]
+    return [n - index, more_than_int]
+
+print(or_more(A, 32))
+print(more_than(A, 1))
+
 # ABC149 E - Handshake
 N, K = 9, 14
 A = [1, 3, 5, 110, 24, 21, 34, 5, 3]
