d45a48eaf17376c49b13f7e11a72a88bcf379cb8
customaddone <custom1485@docomo.ne.jp>
2020-08-24 07:06:26 +0900

    全方位木dp

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -64,7 +64,7 @@ class Reroot():
         self.unit = 1
         self.merge = lambda a, b: a * b % mod # マージ
         self.adj_bu = lambda a, i: a + 1 # マージする時の調整 iに都合のいい値を設定しよう
-        # iの p以外側の頂点数（サイズ）みたいなのでも大丈夫。
+        # 頂点iから最も遠い点とかの場合はa + 1（子から親に行く際にエッジを一つ遡る）
         self.adj_td = lambda a, i, p: a + 1
         self.adj_fin = lambda a, i: a
         ####################
@@ -86,11 +86,11 @@ class Reroot():
         ME = [self.unit] * N # mergeを使う
         XX = [0] * N # dpする
         for i in R[1:][::-1]: # 巡回を逆順に辿る
-            XX[i] = self.adj_bu(ME[i], i) # レコード
+            XX[i] = self.adj_bu(ME[i], i) # adj_buで子要素が白である通りを追加する
             p = P[i] # 親を取り出す
+            # 子要素jの持つ状態の個数 + 子要素が白である(1通り)を親要素に掛ける
             ME[p] = self.merge(ME[p], XX[i]) # iの親要素とiの値をマージする
         XX[R[0]] = self.adj_fin(ME[R[0]], R[0]) # 先頭については答えが求められるのでXXにレコード
-
         TD = [self.unit] * N
         for i in R: # 巡回した順番に
             ac = TD[i] # 左からDP（結果はTDに入れている）
@@ -101,7 +101,7 @@ class Reroot():
             for j in self.graph[i][::-1]: # 各子要素について逆順に
                 # TDに残っている左から累積した分とacにある右から累積した分をTDにマージ
                 TD[j] = self.adj_td(self.merge(TD[j], ac), j, i)
-                ac = self.merge(ac, XX[j]) # acの累積更新
+                ac = self.merge(ac, XX[j]) # acの累積更新 # 逆向きにマージしていく
                 # レコード 順向きのものと逆向きのものをマージする
                 XX[j] = self.adj_fin(self.merge(ME[j], TD[j]), j)
         self.res = XX
