7d29f7679ae59af2abb90232b3c51783bb4613d5
customaddone <custom1485@docomo.ne.jp>
2020-08-24 22:02:20 +0900

    グラフ

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -50,106 +50,57 @@ mod = 10 ** 9 + 7
 # Main Code #
 #############
 
-N = 4
-query = [
-[0, 1, 1],
+N = 5
+# 木グラフ
+que = [
+[1, 2],
+[1, 4],
+[2, 3],
+[2, 5]
+]
+# 重みつき
+que_dis = [
 [1, 2, 2],
-[2, 3, 4]
+[1, 4, 1],
+[2, 3, 2],
+[2, 5, 1]
 ]
 
-G = [[] for i in range(N)]
-for i in range(N - 1):
-    s, t, w = query[i]
-    G[s].append((t, w))
-    G[t].append((s, w))
+def build_tree(n, edge_list):
 
+    G = [[] for i in range(n)]
 
-def bfs(s):
-    dist = [-1] * N
-    que = deque([s])
-    dist[s] = 0
+    for a, b in edge_list:
+        G[a - 1].append(b - 1)
+        G[b - 1].append(a - 1)
 
-    while que:
-        v = que.popleft()
-        d = dist[v]
-        for w, c in G[v]:
-            if dist[w] >= 0:
-                continue
-            dist[w] = d + c
-            que.append(w)
-    d = max(dist)
-    # 全部並べて一番値がでかいやつ
-    return dist.index(d), d
+    return G
 
-u, _ = bfs(0)
-v, d = bfs(u)
+def build_tree_dis(n, edge_list):
 
-print(d)
+    G = [[] for i in range(n)]
 
-# ☦️全方位木dp☦️
-# https://qiita.com/Kiri8128/items/a011c90d25911bdb3ed3
-class Reroot():
-    def __init__(self, query):
-        graph = [[] for i in range(N)]
-        dist = [{} for i in range(N)]
-        # 今回は0-indexなんだ
-        for x, y, z in query:
-            graph[x].append(y)
-            graph[y].append(x)
-            dist[x][y] = z
-            dist[y][x] = z
+    for a, b, c in edge_list:
+        G[a - 1].append([b - 1, c])
+        G[b - 1].append([a - 1, c])
 
-        self.graph = graph
-        self.dist = dist
-        # トポソ
-        P = [-1] * N # 親
-        Q = deque([0])
-        R = [] # 巡回した順番
-        while Q:
-            i = deque.popleft(Q)
-            R.append(i)
-            for a in self.graph[i]:
-                if a != P[i]:
-                    P[a] = i
-                    # 親への辺を消す
-                    self.graph[a].remove(i)
-                    deque.append(Q, a)
+    return G
 
-        ##### Settings #####
-        self.unit = 0 # 単位元
-        self.merge = lambda a, b: max(a, b)
-        self.adj_bu = lambda a, i: a + dist[i][P[i]] # マージする時の調整 iに都合のいい値を設定しよう
-        # 頂点iから最も遠い点とかの場合はa + 1（子から親に行く際にエッジを一つ遡る）
-        self.adj_td = lambda a, i, p: a + dist[i][P[i]]
-        self.adj_fin = lambda a, i: a
-        ####################
-        # bottom-up
-        # 頂点iからその親 piに向かうもの）
-        ME = [self.unit] * N # mergeを使う
-        XX = [0] * N # dpする
-        for i in R[1:][::-1]: # 巡回を逆順に辿る
-            XX[i] = self.adj_bu(ME[i], i) # adj_buで子要素が白である通りを追加する
-            p = P[i] # 親を取り出す
-            # 子要素jの持つ状態の個数 + 子要素が白である(1通り)を親要素に掛ける
-            ME[p] = self.merge(ME[p], XX[i]) # iの親要素とiの値をマージする
-        XX[R[0]] = self.adj_fin(ME[R[0]], R[0]) # 先頭については答えが求められるのでXXにレコード
-        TD = [self.unit] * N
-        for i in R: # 巡回した順番に
-            ac = TD[i] # 左からDP（結果はTDに入れている）
-            for j in self.graph[i]: # 各子要素について順番に更新を試みる
-                TD[j] = ac # TDにはjまで累積した分が入っている
-                ac = self.merge(ac, XX[j])  # マージする
-            ac = self.unit # リセット 右からDP（結果はacに入れている）
-            for j in self.graph[i][::-1]: # 各子要素について逆順に
-                # TDに残っている左から累積した分とacにある右から累積した分をTDにマージ
-                TD[j] = self.adj_td(self.merge(TD[j], ac), j, i)
-                ac = self.merge(ac, XX[j]) # acの累積更新 # 逆向きにマージしていく
-                # レコード 順向きのものと逆向きのものをマージする
-                XX[j] = self.adj_fin(self.merge(ME[j], TD[j]), j)
-        self.res = XX
+# 木の建設
+G1 = build_tree(N, que)
+G2 = build_tree_dis(N, que_dis)
 
-# main
-N = getN()
-query = [getList() for i in range(N - 1)]
-tree = Reroot(query)
-print(max(tree.res))
+# 木を探索
+def search(n, edges, sta):
+    ignore = [0] * N
+    ignore[sta] = 1
+    pos = deque([sta])
+    # 探索
+    while len(pos) > 0:
+        u = pos.popleft()
+        for i in edges[u]:
+            if ignore[i] == 0:
+                ignore[i] = 1
+                pos.append(i)
+# [0, 1, 3, 2, 4]
+search(N, G1, 0)
