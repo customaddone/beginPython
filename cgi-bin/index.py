38ae1c4c41cab326d34cc6d61a990751f290a62b
customaddone <custom1485@docomo.ne.jp>
2020-08-27 00:24:00 +0900

    ARC026 C - 蛍光灯

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -50,102 +50,59 @@ mod = 10 ** 9 + 7
 # Main Code #
 #############
 
-# 基本の二分探索
-lista = [i for i in range(10)]
+def segfunc(x, y):
+    return min(x, y)
 
-def binary_search_loop(data, target):
-    imin = 0
-    imax = len(data) - 1
-    while imin <= imax:
-        imid = imin + (imax - imin) // 2
-        if target == data[imid]:
-            return imid
-        elif target < data[imid]:
-            imax = imid - 1
-        else:
-            imin = imid + 1
-    return False
-print(binary_search_loop(lista, 4))
+ide_ele = float('inf')
 
-# 三分探索
-# ARC054 ムーアの法則
-def f(x):
-    return x + p / pow(2, 2 * x / 3)
+class SegTree:
+    def __init__(self, init_val, segfunc, ide_ele):
+        n = len(init_val)
+        self.segfunc = segfunc
+        self.ide_ele = ide_ele
+        self.num = 1 << (n - 1).bit_length()
+        self.tree = [ide_ele] * 2 * self.num
+        # 配列の値を葉にセット
+        for i in range(n):
+            self.tree[self.num + i] = init_val[i]
+        # 構築していく
+        for i in range(self.num - 1, 0, -1):
+            self.tree[i] = self.segfunc(self.tree[2 * i], self.tree[2 * i + 1])
 
-p = float(input())
-left, right = 0, 100
+    def update(self, k, x):
+        k += self.num
+        self.tree[k] = x
+        while k > 1:
+            self.tree[k >> 1] = self.segfunc(self.tree[k], self.tree[k ^ 1])
+            k >>= 1
 
-while right > left + 10 ** -10:
-    # mid二つ
-    mid1 = (right * 2 + left) / 3
-    mid2 = (right + left * 2) / 3
-    if f(mid1) >= f(mid2):
-        right = mid1
-    else:
-        left = mid2
-print(f(right))
+    def query(self, l, r):
+        res = self.ide_ele
 
-# 三分探索整数ver
-num = []
-for i in range(100):
-    if i < 50:
-        num.append(i)
-    else:
-        num.append(100 - i)
+        l += self.num
+        r += self.num
+        while l < r:
+            if l & 1:
+                res = self.segfunc(res, self.tree[l])
+                l += 1
+            if r & 1:
+                res = self.segfunc(res, self.tree[r - 1])
+            l >>= 1
+            r >>= 1
+        return res
 
-left, right = 0, len(num) - 1
-while abs(right - left) > 3:
-    mid1 = (right * 2 + left) // 3 + 1
-    mid2 = (right + left * 2) // 3
-    # 最小値を求める場合は矢印逆になる
-    if num[mid1] <= num[mid2]:
-        right = mid1
-    else:
-        left = mid2
-print(right)
-print(left)
+N, M = getNM()
+seg = SegTree([float('inf')] * (M + 1), segfunc, ide_ele)
+L = [getList() for i in range(N)]
+L.sort()
+seg.update(0, 0)
 
-# ARC050 B - 花束
-R, B = getNM()
-x, y = getNM()
-# 赤い花束をr束, 青い花束をb束とすると
-# R >= xr + b
-# B >= r + yb
-# を満たしながらk = r + bを最大化せよ
-# r = k - b
-# R >= x(k - b) + b = xk - (x - 1)b
-# B >= (k - b) + yb = k + (y - 1)b
-# (y - 1)R >= (y - 1)xk - (x - 1)(y - 1)b
-# (x - 1)B >= (x - 1)k + (x - 1)(y - 1)b
-# (y - 1)R + (x - 1)B >= ((y - 1)x + (x - 1))k
-# 二分探索?
-
-def judge(k):
-    # あるkを決めた時に
-
-    # ①r >= 0, b >= 0
-    # ②r + b = k
-    # ③R >= xr + b = (x - 1)b
-    # ④B >= r + yb = (y - 1)b
-    # となるr, bが存在するか
-
-    # R - k >= (x - 1)r
-    # (R - k) / (x - 1) >= r
-    # B - k >= (y - 1)b
-    # (B - k) / (y - 1) >= b
-    # (R - k) // (x - 1) + (B - k) // (y - 1) >= kになるか
-    if R - k >= 0 and B - k >= 0 and (R - k) // (x - 1) + (B - k) // (y - 1) >= k:
-        return True
-    else:
-        return False
-
-ok = -1
-ng = 10 ** 18 + 1
-
-while abs(ok - ng) > 1:
-    mid = (ok + ng) // 2
-    if judge(mid):
-        ok = mid
-    else:
-        ng = mid
-print(ok)
+# [0, 1, 2, 3, 4, 5]
+# seg.query(0, 2): [0, 1]の最小値
+# seg.query(2, 2 + 1): [2]の最小値
+# seg.update(2, min(vs, opt + c)): 2をmin(vs, opt + c)に更新
+for l, r, c in L:
+    opt = seg.query(l, r)
+    vs = seg.query(r, r + 1)
+    seg.update(r, min(vs, opt + c))
+print(seg.query(M, M + 1))
