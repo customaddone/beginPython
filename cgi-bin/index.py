4ab1ccd42ed7e60093450ae1677b681d156c109b
customaddone <custom1485@docomo.ne.jp>
2020-08-26 21:56:44 +0900

    二分探索

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -50,179 +50,102 @@ mod = 10 ** 9 + 7
 # Main Code #
 #############
 
-"""
-# ABC161 D - Lunlun Number
-K = 13
-que = []
-heapify(que)
-for i in range(1, 10):
-    que.append(i)
+# 基本の二分探索
+lista = [i for i in range(10)]
 
-for i in range(K):
-    u = heappop(que)
-    if u % 10 != 0:
-        heappush(que, 10 * u + (u % 10) - 1)
-    heappush(que, 10 * u + (u % 10))
-    if u % 10 != 9:
-        heappush(que, 10 * u + (u % 10) + 1)
-print(u)
-"""
+def binary_search_loop(data, target):
+    imin = 0
+    imax = len(data) - 1
+    while imin <= imax:
+        imid = imin + (imax - imin) // 2
+        if target == data[imid]:
+            return imid
+        elif target < data[imid]:
+            imax = imid - 1
+        else:
+            imin = imid + 1
+    return False
+print(binary_search_loop(lista, 4))
 
-"""
-N, M = getNM()
-weight = []
-key = []
-for _ in range(M):
-    a, b = getNM()
-    weight.append(a)
-    c = getList()
-    key.append(c)
+# 三分探索
+# ARC054 ムーアの法則
+def f(x):
+    return x + p / pow(2, 2 * x / 3)
 
-dp = [float('inf')] * (1 << N)
-dp[0] = 0
-for i in range(M):
-    bit = 0
-    for item in key[i]:
-        bit |= (1 << (item - 1))
-    for j in range(1 << N):
-        dp[j | bit] = min(dp[j | bit], dp[j] + weight[i])
+p = float(input())
+left, right = 0, 100
 
-print(dp)
-"""
-
-"""
-ARC028 B-特別賞
-N, K = getNM()
-A = getList()
-young = [0] * N
-# i番目に若い人の順位はyoung[i]位
-for i in range(N):
-    young[A[i] - 1] = i
-
-A = [-i for i in A]
-# Aの前からK個のリストを作る
-prized = []
-for i in range(K):
-    prized.append(A[i])
-heapq.heapify(prized)
-
-# K個数字があるうちの最大値（K番目の数字）を取ってくる
-now = heapq.heappop(prized)
-print(young[-now - 1] + 1)
-
-for i in range(K, N):
-    # もし現在のK番目の数字より小さい数字がきたら
-    if A[i] > now:
-        # prizedリストに混ぜて（計K個入っている）
-        heapq.heappush(prized, A[i])
-        # 再びK個数字があるうちの最大値（K番目の数字）を取ってくる
-        now = heapq.heappop(prized)
-        print(young[-now - 1] + 1)
-    # もし現在のK番目の数字より小さい数字がきたら捨てる
+while right > left + 10 ** -10:
+    # mid二つ
+    mid1 = (right * 2 + left) / 3
+    mid2 = (right + left * 2) / 3
+    if f(mid1) >= f(mid2):
+        right = mid1
     else:
-        print(young[-now - 1] + 1)
-"""
+        left = mid2
+print(f(right))
 
-"""
-ABC062 D - 3N Numbers
-N = getN()
-A = getList()
+# 三分探索整数ver
+num = []
+for i in range(100):
+    if i < 50:
+        num.append(i)
+    else:
+        num.append(100 - i)
 
-# 前から計算
-fore = A[:N]
-# 後ろから計算
-back = A[2 * N:]
-back = [-i for i in back]
-for_sum = sum(fore)
-back_sum = sum(back)
-heapq.heapify(fore)
-heapq.heapify(back)
+left, right = 0, len(num) - 1
+while abs(right - left) > 3:
+    mid1 = (right * 2 + left) // 3 + 1
+    mid2 = (right + left * 2) // 3
+    # 最小値を求める場合は矢印逆になる
+    if num[mid1] <= num[mid2]:
+        right = mid1
+    else:
+        left = mid2
+print(right)
+print(left)
 
-fore_list = []
-back_list = []
-for i in range(N):
-    fore_list.append(for_sum)
-    back_list.append(back_sum)
-    in_fore = A[N + i]
-    heapq.heappush(fore, in_fore)
-    out_fore = heapq.heappop(fore)
-    for_sum += in_fore - out_fore
+# ARC050 B - 花束
+R, B = getNM()
+x, y = getNM()
+# 赤い花束をr束, 青い花束をb束とすると
+# R >= xr + b
+# B >= r + yb
+# を満たしながらk = r + bを最大化せよ
+# r = k - b
+# R >= x(k - b) + b = xk - (x - 1)b
+# B >= (k - b) + yb = k + (y - 1)b
+# (y - 1)R >= (y - 1)xk - (x - 1)(y - 1)b
+# (x - 1)B >= (x - 1)k + (x - 1)(y - 1)b
+# (y - 1)R + (x - 1)B >= ((y - 1)x + (x - 1))k
+# 二分探索?
 
-    in_back = (-1) * A[-N - i - 1]
-    heapq.heappush(back, in_back)
-    out_back = heapq.heappop(back)
-    back_sum += in_back - out_back
+def judge(k):
+    # あるkを決めた時に
 
-fore_list.append(for_sum)
-back_list.append(back_sum)
+    # ①r >= 0, b >= 0
+    # ②r + b = k
+    # ③R >= xr + b = (x - 1)b
+    # ④B >= r + yb = (y - 1)b
+    # となるr, bが存在するか
 
-ans = -float('inf')
-for i in range(N + 1):
-    opt = fore_list[i] + back_list[N - i]
-    ans = max(ans, opt)
-print(ans)
-"""
+    # R - k >= (x - 1)r
+    # (R - k) / (x - 1) >= r
+    # B - k >= (y - 1)b
+    # (B - k) / (y - 1) >= b
+    # (R - k) // (x - 1) + (B - k) // (y - 1) >= kになるか
+    if R - k >= 0 and B - k >= 0 and (R - k) // (x - 1) + (B - k) // (y - 1) >= k:
+        return True
+    else:
+        return False
 
-"""
-# ABC123 D - Cake 123
-X, Y, Z, K = getNM()
-A = sorted([-i for i in getList()])
-B = sorted([-i for i in getList()])
-C = sorted([-i for i in getList()])
+ok = -1
+ng = 10 ** 18 + 1
 
-pos = []
-heapify(pos)
-dict = defaultdict(int)
-u = (A[0] + B[0] + C[0], 0, 0, 0)
-heappush(pos, u)
-dict[u] = 1
-
-for i in range(K):
-    p, i, j, l = heappop(pos)
-    print(-p)
-    # 取り出すごとにA, B, Cについての次の値をpush
-    if i + 1 < X:
-        opt_a = (A[i + 1] + B[j] + C[l], i + 1, j, l)
-        if dict[opt_a] == 0:
-            heappush(pos, opt_a)
-            dict[opt_a] = 1
-    if j + 1 < Y:
-        opt_b = (A[i] + B[j + 1] + C[l], i, j + 1, l)
-        if dict[opt_b] == 0:
-            heappush(pos, opt_b)
-            dict[opt_b] = 1
-    if l + 1 < Z:
-        opt_c = (A[i] + B[j] + C[l + 1], i, j, l + 1)
-        if dict[opt_c] == 0:
-            heappush(pos, opt_c)
-            dict[opt_c] = 1
-"""
-# ABC149 E - Handshake
-# Mがクソデカイので使用不可
-# 二分探索使ってね
-N, M = getNM()
-A = sorted([-i for i in getList()])
-
-pos = []
-heapify(pos)
-dict = defaultdict(int)
-u = (A[0] + A[0], 0, 0)
-heappush(pos, u)
-dict[u] = 1
-
-ans = 0
-# 大きい値M番目まで全て求まる
-for i in range(M):
-    p, i, j = heappop(pos)
-    ans += -p
-    if i + 1 < N:
-        opt_a = (A[i + 1] + A[j], i + 1, j)
-        if dict[opt_a] == 0:
-            heappush(pos, opt_a)
-            dict[opt_a] = 1
-    if j + 1 < N:
-        opt_b = (A[i] + A[j + 1], i, j + 1)
-        if dict[opt_b] == 0:
-            heappush(pos, opt_b)
-            dict[opt_b] = 1
-print(ans)
+while abs(ok - ng) > 1:
+    mid = (ok + ng) // 2
+    if judge(mid):
+        ok = mid
+    else:
+        ng = mid
+print(ok)
