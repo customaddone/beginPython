de341e7708d7bcd081dc00c6a69841d7b6b73a4f
customaddone <custom1485@docomo.ne.jp>
2020-08-30 11:31:55 +0900

    LIS 巻き戻し

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -49,76 +49,49 @@ mod = 10 ** 9 + 7
 # Main Code #
 #############
 
-# 素因数分解
-def prime_factorize(n):
-    divisors = []
-    # 27(2 * 2 * 7)の7を出すためにtemp使う
-    temp = n
-    for i in range(2, int(math.sqrt(n)) + 1):
-        if temp % i == 0:
-            cnt = 0
-            while temp % i == 0:
-                cnt += 1
-                # 素因数を見つけるたびにtempを割っていく
-                temp //= i
-            divisors.append([i, cnt])
-    if temp != 1:
-        divisors.append([temp, 1])
-    if divisors == []:
-        divisors.append([n, 1])
+# 頂点1から頂点kまでの最短パス上
+# ルートはO(n)で求められる
+N = getN()
+A = getList()
+query = [getList() for i in range(N - 1)]
 
-    return divisors
+dist = [[] for i in range(N)]
+for i in range(N - 1):
+    a, b = query[i]
+    dist[a - 1].append(b - 1)
+    dist[b - 1].append(a - 1)
 
-# [[2, 3], [3, 1]]
-print(prime_factorize(12))
+ignore = [0] * N
+ignore[0] = 1
+lis = [A[0]]
+rec = [0] * N
+rec[0] = 1
 
-# エラストテネスの篩
-prime = [2]
-max = 12
-limit = int(math.sqrt(max))
-data = [i + 1 for i in range(2, max, 2)]
+# 行きがけ帰りがけの要領
+def dfs(u):
+    global lis
+    for i in dist[u]:
+        if ignore[i] != 1:
+            ignore[i] = 1
+            # 巻き戻し用
+            plus = 0 # true or false
+            change = (0, 0, 0) # true or false, 変更した場所、変更した数値
 
-while limit > data[0]:
-    prime.append(data[0])
-    data = [j for j in data if j % data[0] != 0]
-prime = prime + data
+            if A[i] > lis[-1]:
+                lis.append(A[i])
+                plus = 1
+            else:
+                index = bisect_left(lis, A[i])
+                change = (1, index, lis[index])
+                lis[index] = A[i]
+            rec[i] = len(lis)
+            dfs(i)
+            # 巻き戻す
+            if plus:
+                lis.pop()
+            else:
+                lis[change[1]] = change[2]
 
-# [2, 3, 5, 7, 9, 11]
-print(prime)
-
-class Osa_k():
-    def __init__(self, n):
-        self.sieve = list(range(n + 1))
-        self.sieve[0] = 1 #素数でない
-        self.sieve[1] = 1 #素数でない
-        m = int(math.sqrt(n))
-        for i in range(2, m + 1):
-            if self.sieve[i] < i:
-                continue
-            for j in range(i * i, n + 1, i):
-                if self.sieve[j] == j:
-                    self.sieve[j] = i
-
-    def factorize(self, n):
-        prime = defaultdict(int)
-        if n == 0 or n == 1:
-            return (n, 1)
-        opt = n
-        while opt > 1:
-            prime[self.sieve[opt]] += 1
-            opt //= self.sieve[opt]
-        return sorted(prime.items())
-
-N = 1000
-O = Osa_k(10 ** 6 + 1)
-
-prime = defaultdict(int)
-for i in range(2, N + 1):
-    for j in O.factorize(i):
-        prime[j[0]] += j[1]
-
-ans = 1
-for i in prime.items():
-    ans *= i[1] + 1
-    ans %= mod
-print(ans)
+dfs(0)
+for i in rec:
+    print(i)
