2e8e7599adf2685602e37b0984b3019006de0cc3
customaddone <custom1485@docomo.ne.jp>
2020-09-01 20:44:50 +0900

    Z-algorithm

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -137,3 +137,110 @@ while ng - ok > 1:
     else:
         ng = mid
 print(ok)
+
+"""
+N, K = getNM()
+S = input()
+
+if(K * 2 > N):
+    print('NO')
+    exit()
+
+def check(n, s, k, mod, alp):
+    # 各アルファベットをエンコード
+    encode = {}
+    for i, ch in enumerate(alp, 10001):
+        encode[ch] = pow(172603, i, mod)
+
+    # 文字列の先頭からm文字、k文字目からm文字をハッシュしていく
+    left = 0
+    right = 0
+    for i in range(k):
+        left += encode[s[i]]
+        left %= mod
+
+        right += encode[s[i + k]]
+        right %= mod
+
+    words = set()
+    # ローリングする
+    for i in range(n):
+        words.add(left)
+        if (right in words):
+            return True
+        if (i + 2 * k >= n):
+            break
+        left += encode[s[i + k]] - encode[s[i]]
+        left %= mod
+        right += encode[s[i + k * 2]] - encode[s[i + k]]
+        right %= mod
+
+    return False
+
+ans = True
+ps = '9999217 9999221 9999233 9999271 9999277 9999289 9999299 9999317 9999337 9999347 9999397 9999401 9999419 9999433 9999463 9999469 9999481 9999511 9999533 9999593 9999601 9999637 9999653 9999659 9999667 9999677 9999713 9999739 9999749 9999761 9999823 9999863 9999877 9999883 9999889 9999901 9999907 9999929 9999931 9999937 9999943 9999971 9999973'
+ps = list(map(int, ps.split(' ')))
+ps.append(10 ** 9 + 7)
+ps.append(3040409)
+ps.append(10 ** 20 + 9)
+# 各modについて
+for p in ps:
+    # アルファベットをランダムに並べた表でエンコード表を作る
+    for alp in ['qwertyuioplkmnjhbvgfcdxsaz','qazxcsdwertfgvbnmhjyuioklp']:
+        ans = (ans & check(N, S, K, p, alp))
+
+if(ans):
+    print('YES')
+else:
+    print('NO')
+"""
+
+N, K = getNM()
+S = input()
+
+if(K * 2 > N):
+    print('NO')
+    exit()
+
+def check(n, s, k, mod, alp):
+    # 各アルファベットをエンコード
+    encode = {}
+    for i, ch in enumerate(alp, 10001):
+        encode[ch] = pow(172603, i, mod)
+
+    # 文字列の先頭からm文字、k文字目からm文字をハッシュしていく
+    res = 0
+    for i in range(k):
+        res += encode[s[i]]
+        res %= mod
+
+    dic = {res: 0}
+    # ローリングする
+    for i in range(n - k):
+        res += encode[s[i + k]] - encode[s[i]]
+        res %= mod
+        if res in dic.keys():
+            index = dic[res]
+            if index + k <= i + 1:
+                return True
+        else:
+            dic[res] = i + 1
+
+    return False
+
+ans = True
+ps = '9999217 9999221 9999233 9999271 9999277 9999289 9999299 9999317 9999337 9999347 9999397 9999401 9999419 9999433 9999463 9999469 9999481 9999511 9999533 9999593 9999601 9999637 9999653 9999659 9999667 9999677 9999713 9999739 9999749 9999761 9999823 9999863 9999877 9999883 9999889 9999901 9999907 9999929 9999931 9999937 9999943 9999971 9999973'
+ps = list(map(int, ps.split(' ')))
+ps.append(10 ** 9 + 7)
+ps.append(3040409)
+ps.append(10 ** 20 + 9)
+# 各modについて
+for p in ps:
+    # アルファベットをランダムに並べた表でエンコード表を作る
+    for alp in ['qwertyuioplkmnjhbvgfcdxsaz','qazxcsdwertfgvbnmhjyuioklp']:
+        ans = (ans & check(N, S, K, p, alp))
+
+if(ans):
+    print('YES')
+else:
+    print('NO')
