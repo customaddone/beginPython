16e0d498ab976d8effbaa1dfe60a89a0017c0773
customaddone <custom1485@docomo.ne.jp>
2020-09-02 18:04:13 +0900

    ARC053 C - 魔法使い高橋君

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -49,198 +49,40 @@ mod = 10 ** 9 + 7
 # Main Code #
 #############
 
-"""
-Z algorithm
-def Z(s):
-    n = len(s)
-    z = [0] * n
-    z[0] = n
-    L, R = 0, 0
-    for i in range(1, n):
-        if i >= R:
-            L = R = i
-            # 一致が続く限り伸ばす
-            while(R < n and s[R - L] == s[R]):
-                R += 1
-            # LCAを書き込む
-            # 頭から一致しない場合はR - L = i - i = 0
-            z[i] = R - L
-        # 全て利用できる場合
-        elif z[i - L] < R - i:
-            z[i] = z[i - L]
-        # 一部利用できる場合
-        else:
-            L = i
-            while(R < n and s[R - L] == s[R]):
-                R += 1
-            z[i] = R - L
-    return z
-# [5, 0, 3, 0, 1]
-#print(Z('ababa'))
 N = getN()
-S = input()
-ans = 0
-for i in range(N):
-    z = Z(S[i:])
-    k = len(z)
-    for j in range(k):
-        # '' と 'ababa'
-        # 'a' と 'baba'
-        # 'ab' と 'aba'
-        # ans は j('', 'a', 'ab')の長さ以上にならない（ダブらないため）
-        ans = max(ans, min(j, z[j]))
-print(ans)
-"""
+que = [getList() for i in range(N)]
+# 正直にやればN!
 
-# Rolling hash
-N = getN()
-S = list(map(ord, list(input())))
-# 適当
-base = 1007
-# base = 1009
-power = [1] * (N + 1)
-# 部分文字列を数字に
-# ハッシュ生成
-for i in range(1, N + 1):
-    power[i] = power[i - 1] * base % mod
-print(power)
+# dpか二分探索か
+# dpだと各dp[i]:iまで進んだにどの要素を使った/使ってないかの情報が必要
+# ok, ng型二分探索　どっちみち貪欲に行った場合どのような値をとるかが必要
 
-def check(m):
-    if N - m < m:
-        return False
-    res = 0
-    # 頭m文字のハッシュ生成
-    for i in range(m):
-        res += S[i] * power[m - i - 1]
-        res %= mod
-    dic = {res: 0}
-    for i in range(N - m):
-        # ハッシュをローリングしていって次々m文字のハッシュを生成していく
-        res = ((res - S[i] * power[m - 1]) * base + S[i + m]) % mod
-        # もし既出なら
-        if res in dic.keys():
-            index = dic[res]
-            if index + m <= i + 1: # 重ならないか
-                return True
-        else:
-            dic[res] = i + 1 # i + 1:頭の位置を記録する
-        print(dic)
-    return False
+# 貪欲法　最も効率の良い並べ方は何か
 
-ok = 0
-ng = N + 1
-while ng - ok > 1:
-    mid = (ok + ng) // 2
+# 上がって下がることを考えず、上がるだけ、下がるだけの場合は？
+# まず下がるものだけを並べ、次に上がるものを並べる（谷の形にすればいい）
+# 下がるもの内の並べ方は？　自由でいい
 
-    if check(mid):
-        ok = mid
+# 今回の問題だと下がるもの内の並べ方も考える必要がありそう
+# → 上がり幅が大きいものは出来るだけ谷の底に沈めたい
+
+plus = []
+minus = []
+for i in que:
+    if i[0] - i[1] >= 0:
+        plus.append(i)
     else:
-        ng = mid
-print(ok)
+        minus.append(i)
+plus.sort(reverse = True, key = lambda i:i[1])
+minus.sort()
 
-"""
-N, K = getNM()
-S = input()
+now = 0
+res = -float('inf')
+for i in minus:
+    res = max(res, now + i[0])
+    now += i[0] - i[1]
 
-if(K * 2 > N):
-    print('NO')
-    exit()
-
-def check(n, s, k, mod, alp):
-    # 各アルファベットをエンコード
-    encode = {}
-    for i, ch in enumerate(alp, 10001):
-        encode[ch] = pow(172603, i, mod)
-
-    # 文字列の先頭からm文字、k文字目からm文字をハッシュしていく
-    left = 0
-    right = 0
-    for i in range(k):
-        left += encode[s[i]]
-        left %= mod
-
-        right += encode[s[i + k]]
-        right %= mod
-
-    words = set()
-    # ローリングする
-    for i in range(n):
-        words.add(left)
-        if (right in words):
-            return True
-        if (i + 2 * k >= n):
-            break
-        left += encode[s[i + k]] - encode[s[i]]
-        left %= mod
-        right += encode[s[i + k * 2]] - encode[s[i + k]]
-        right %= mod
-
-    return False
-
-ans = True
-ps = '9999217 9999221 9999233 9999271 9999277 9999289 9999299 9999317 9999337 9999347 9999397 9999401 9999419 9999433 9999463 9999469 9999481 9999511 9999533 9999593 9999601 9999637 9999653 9999659 9999667 9999677 9999713 9999739 9999749 9999761 9999823 9999863 9999877 9999883 9999889 9999901 9999907 9999929 9999931 9999937 9999943 9999971 9999973'
-ps = list(map(int, ps.split(' ')))
-ps.append(10 ** 9 + 7)
-ps.append(3040409)
-ps.append(10 ** 20 + 9)
-# 各modについて
-for p in ps:
-    # アルファベットをランダムに並べた表でエンコード表を作る
-    for alp in ['qwertyuioplkmnjhbvgfcdxsaz','qazxcsdwertfgvbnmhjyuioklp']:
-        ans = (ans & check(N, S, K, p, alp))
-
-if(ans):
-    print('YES')
-else:
-    print('NO')
-"""
-
-N, K = getNM()
-S = input()
-
-if(K * 2 > N):
-    print('NO')
-    exit()
-
-def check(n, s, k, mod, alp):
-    # 各アルファベットをエンコード
-    encode = {}
-    for i, ch in enumerate(alp, 10001):
-        encode[ch] = pow(172603, i, mod)
-
-    # 文字列の先頭からm文字、k文字目からm文字をハッシュしていく
-    res = 0
-    for i in range(k):
-        res += encode[s[i]]
-        res %= mod
-
-    dic = {res: 0}
-    # ローリングする
-    for i in range(n - k):
-        res += encode[s[i + k]] - encode[s[i]]
-        res %= mod
-        if res in dic.keys():
-            index = dic[res]
-            if index + k <= i + 1:
-                return True
-        else:
-            dic[res] = i + 1
-
-    return False
-
-ans = True
-ps = '9999217 9999221 9999233 9999271 9999277 9999289 9999299 9999317 9999337 9999347 9999397 9999401 9999419 9999433 9999463 9999469 9999481 9999511 9999533 9999593 9999601 9999637 9999653 9999659 9999667 9999677 9999713 9999739 9999749 9999761 9999823 9999863 9999877 9999883 9999889 9999901 9999907 9999929 9999931 9999937 9999943 9999971 9999973'
-ps = list(map(int, ps.split(' ')))
-ps.append(10 ** 9 + 7)
-ps.append(3040409)
-ps.append(10 ** 20 + 9)
-# 各modについて
-for p in ps:
-    # アルファベットをランダムに並べた表でエンコード表を作る
-    for alp in ['qwertyuioplkmnjhbvgfcdxsaz','qazxcsdwertfgvbnmhjyuioklp']:
-        ans = (ans & check(N, S, K, p, alp))
-
-if(ans):
-    print('YES')
-else:
-    print('NO')
+for i in plus:
+    res = max(res, now + i[0])
+    now += i[0] - i[1]
+print(res)
