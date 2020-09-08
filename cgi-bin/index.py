802a3d7b1fb4b4159e50ad1c448a1a077da0ee4b
customaddone <custom1485@docomo.ne.jp>
2020-09-08 19:58:53 +0900

    UnionFind

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -49,30 +49,76 @@ mod = 10 ** 9 + 7
 # Main Code #
 #############
 
-S = 'aaaaa'
-N = len(S)
-# i文字目以降で最初に
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-alp = {}
-for i in range(26):
-    alp[alphabet[i]] = i
+class UnionFind:
+    def __init__(self, N):
+        """
+        N:要素数
+        root:各要素の親要素の番号を格納するリスト.
+             ただし, root[x] < 0 ならその頂点が根で-root[x]が木の要素数.
+        rank:ランク
+        """
+        self.N = N
+        self.root = [-1] * N
+        self.rank = [0] * N
 
-# i文字目以降で最初に文字sが登場するのは
-next = [[-1] * 26 for i in range(N)]
-for i in range(N - 1, -1, -1):
-    for j in range(26):
-        if i == N - 1:
-            break
-        next[i][j] = next[i + 1][j]
-    next[i][alp[S[i]]] = i
+    def __repr__(self):
+        return '\n'.join('{}: {}'.format(r, self.members(r)) for r in self.roots())
 
-dp = [0] * (N + 1) # 1-indexになる
-dp[0] = 1
-# 配るdpでやる
-for i in range(N):
-    for j in range(26):
-        if next[i][j] == -1:
-            continue
-        # 0-indexを1-indexに直す
-        dp[next[i][j] + 1] += dp[i]
-print(sum(dp))
+    def find(self, x):
+        """頂点xの根を見つける"""
+        if self.root[x] < 0:
+            return x
+        else:
+            while self.root[x] >= 0:
+                x = self.root[x]
+            return x
+
+    def union(self, x, y):
+        """x,yが属する木をunion"""
+        x = self.find(x)
+        y = self.find(y)
+        if x == y:
+            return
+        elif self.rank[x] > self.rank[y]:
+            self.root[x] += self.root[y]
+            self.root[y] = x
+        else:
+            self.root[y] += self.root[x]
+            self.root[x] = y
+            if self.rank[x] == self.rank[y]:
+                self.rank[y] += 1
+
+    def same(self, x, y):
+        """xとyが同じグループに属するかどうか"""
+        return self.find(x) == self.find(y)
+
+    def count(self, x):
+        """頂点xが属する木のサイズを返す"""
+        return - self.root[self.find(x)]
+
+    def members(self, x):
+        """xが属する木の要素を列挙"""
+        _root = self.find(x)
+        return [i for i in range(self.N) if self.find == _root]
+
+    def roots(self):
+        """森の根を列挙"""
+        return [i for i, x in enumerate(self.root) if x < 0]
+
+    def group_count(self):
+        """連結成分の数"""
+        return len(self.roots())
+
+    def all_group_members(self):
+        """{ルート要素: [そのグループに含まれる要素のリスト], ...}の辞書を返す"""
+        return {r: self.members(r) for r in self.roots()}
+
+n, q = getNM()
+
+uf = UnionFind(n)
+for i in range(q):
+    t, u, v = map(int, input().split())
+    if t:
+        print(int(uf.same(u, v)))
+    else:
+        uf.union(u, v)
