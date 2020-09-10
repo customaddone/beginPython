5635f58a362e32ab8a9e9d25a65276ab7639a5e5
customaddone <custom1485@docomo.ne.jp>
2020-09-10 20:32:28 +0900

    bit遷移dp

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -49,153 +49,30 @@ mod = 10 ** 9 + 7
 # Main Code #
 #############
 
-# ABC154 E - Almost Everywhere Zero
-N = '9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999'
-K = 3
-L = len(N)
+N = getN()
+X = getList()
 
-def judge(a):
-    return a != 0
+dp = [float('inf')] * (1 << 16)
+dp[0] = 0
 
-# N以下の数字で条件を満たす桁がk個のもの
-def digit_dp(n, k):
-    l = len(n)
+for bit_state in range(1 << 16):
+    for i in range(1, 16):
+        cnt = 0
+        tmp = 0
+        if (1 << (i - 1)) & bit_state:
+            cnt += 1
+            # bit_stateから(1 << (i - 1))のフラグを消したもの
+            tmp += (1 / 3) * dp[~(1 << (i - 1)) & bit_state]
+        if (1 << i) & bit_state:
+            cnt += 1
+            tmp += (1 / 3) * dp[~(1 << (i)) & bit_state]
+        if (1 << (i + 1)) & bit_state:
+            cnt += 1
+            tmp += (1 / 3) * dp[~(1 << (i + 1)) & bit_state]
+        if cnt != 0:
+            dp[bit_state] = min((tmp + cnt / 3 + (3 - cnt) / 3) * 3 / cnt, dp[bit_state])
 
-    dp = [[[0] * (k + 1) for _ in range(2)] for i in range(l + 1)]
-    dp[0][0][0] = 1
-
-    for i in range(l):
-        d = int(n[i])
-
-        for j in range(2):
-            for d_j in range(10 if j else d + 1):
-                for k_j in range(k + 1):
-                    if judge(d_j):
-                        if k_j + 1 <= k:
-                            dp[i + 1][j | (d_j < d)][k_j + 1] += dp[i][j][k_j]
-                    else:
-                        dp[i + 1][j | (d_j < d)][k_j] += dp[i][j][k_j]
-
-    return dp
-
-dp = digit_dp(N, K)
-print(dp[L][0][K] + dp[L][1][K])
-
-# ABC029 D - 1
-N = '999999999'
-L = len(N)
-
-def judge_2(a):
-    return a == 1
-
-# N以下の数字の中で「1が書いてある桁がk個ある数字」がいくつあるか
-# 上のものと関数の中身自体は変えていない
-def digit_dp_2(n, k):
-    l = len(n)
-
-    dp = [[[0] * (k + 1) for _ in range(2)] for i in range(l + 1)]
-    dp[0][0][0] = 1
-
-    for i in range(l):
-        d = int(n[i])
-
-        for j in range(2):
-            for d_j in range(10 if j else d + 1):
-                for k_j in range(k + 1):
-                    if judge_2(d_j):
-                        if k_j + 1 <= k:
-                            dp[i + 1][j | (d_j < d)][k_j + 1] += dp[i][j][k_j]
-                    else:
-                        dp[i + 1][j | (d_j < d)][k_j] += dp[i][j][k_j]
-
-    return dp
-
-dp = digit_dp_2(N, L)
-
-ans = 0
-for j in range(L + 1):
-    # dp[l]について各j(1のカウント)の通りの数 * j
-    ans += (dp[L][0][j] + dp[L][1][j]) * j
-print(ans)
-
-A, B = 1, 1000000000000000000
-
-# 4, 9の個数については求めない簡易版
-def judge_3(a):
-    return a in [4, 9]
-
-def digit_dp(n):
-    l = len(n)
-
-    dp = [[[0] * 2 for _ in range(2)] for i in range(l + 1)]
-    dp[0][0][0] = 1
-
-    for i in range(l):
-        d = int(n[i])
-
-        for j in range(2):
-            for d_j in range(10 if j else d + 1):
-                # 0:4,9が含まれない　1:4,9が含まれる
-                for k_j in range(2):
-                    if k_j == 0 and judge_3(d_j):
-                        dp[i + 1][j | (d_j < d)][k_j + 1] += dp[i][j][k_j]
-                    else:
-                        dp[i + 1][j | (d_j < d)][k_j] += dp[i][j][k_j]
-    return dp[l][0][1] + dp[l][1][1]
-
-print(digit_dp(str(B)) - digit_dp(str(A - 1)))
-
-# ABC129 E - Sum Equals Xor
-# 通りの数を求める
-
-L = '1111111111111111111'
-
-def digit_dp_3(n):
-    l = len(n)
-
-    dp = [[[0] * 2 for _ in range(2)] for i in range(l + 1)]
-    dp[0][0][0] = 1
-
-    for i in range(l):
-        d = int(n[i])
-
-        # Lになる可能性があるかないか
-        for j in range(2):
-            # 次の桁が0か1か
-            for d_j in range(2 if j else d + 1):
-                if d_j == 0:
-                    dp[i + 1][j | (d_j < d)][d_j] += (dp[i][j][0] + dp[i][j][1])
-                    dp[i + 1][j | (d_j < d)][d_j] %= mod
-                else:
-                    dp[i + 1][j | (d_j < d)][d_j] += 2 * (dp[i][j][0] + dp[i][j][1])
-                    dp[i + 1][j | (d_j < d)][d_j] %= mod
-
-    return sum(dp[-1][0]) + sum(dp[-1][1])
-
-print(digit_dp_3(L) % mod)
-
-# tipycal dp contest
-# N以下の正の整数であって十進法表記したときの各桁の数の和がi mod Dであるものの個数
-D = getN()
-N = input()
-
-def digit_dp_2(n, k):
-    l = len(n)
-
-    dp = [[[0] * (k + 1) for _ in range(2)] for i in range(l + 1)]
-    dp[0][0][0] = 1
-
-    for i in range(l):
-        d = int(n[i])
-
-        for j in range(2):
-            for d_j in range(10 if j else d + 1):
-                # mod k_j
-                for k_j in range(k + 1):
-                    dp[i + 1][j | (d_j < d)][(k_j + d_j) % D] += dp[i][j][k_j]
-                    dp[i + 1][j | (d_j < d)][(k_j + d_j) % D] %= mod
-
-    return dp
-
-dp = digit_dp_2(N, D)
-print((dp[-1][0][0] + dp[-1][1][0]) % mod - 1) # 0は除く
+ans_state = 0
+for i in X:
+    ans_state += (1 << i)
+print(dp[ans_state])
