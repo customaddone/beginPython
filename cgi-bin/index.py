a97587105dfbcf35b4dd594cb326d3b3fd5defd6
customaddone <custom1485@docomo.ne.jp>
2020-09-27 13:58:21 +0900

    C - All Green

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -49,223 +49,129 @@ mod = 998244353
 # Main Code #
 #############
 
-N, K = 7, 6
-S = [4, 3, 1, 1, 2, 10, 2]
-
-# 左を伸ばしていく
-# その部分列に含まれる全ての要素の値の積は「K以下」である。
-# lはrをオーバーすることもある
-
-if 0 in S:
-    print(N)
-    exit()
-else:
-    l, ans, total = 0, 0, 1
-    for r in range(N):
-        total *= S[r]
-        while total > K and l <= r:
-            total //= S[l]
-            l += 1
-        ans = max(ans, r - l + 1)
-print(ans)
-
-# (条件) 連続部分列に含まれる全ての要素の値の和は、「K以上」である。
-N, K = 4, 10
-A = [6, 1, 2, 7]
-
-left = 0
-total = 0
-ans = 0
-
-for right in range(0, N):
-    total += A[right]
-    while total >= K:
-        ans += N - right
-        total -= A[left]
-        left += 1
-print(ans)
-
-N = 10
-S = 15
-A = [5, 1, 3, 5, 10, 7, 4, 9, 2, 8]
-right = 0
-total = 0
-ans = 0
-# S以上を求める場合にはこの形で
-for left in range(N):
-    while right < N and total < S:
-        total += A[right]
-        right += 1
-    if total < S:
-        break
-    if left == right:
-        right += 1
-    total -= A[left]
-
-# 要素の種類についての問題
-# 全ての要素を含む
-P = 5
-A = [1, 8, 8, 8, 1]
-dict = {}
-for i in A:
-    dict[i] = 0
-# 要素の種類数
-V = len(dict.items())
-
-# 事象の数をカウント
-cnt = 0
-right = 0
-# １つ目から全ての事象をカバーするまでrightを進める
-while right < P:
-    if dict[A[right]] == 0:
-        cnt += 1
-    dict[A[right]] += 1
-
-    if cnt == len(dict.items()):
-        break
-
-    right += 1
-print(l, r)
-
-l = 0
-# 右を一つ進めて左をできる限り進める
-for r in range(right + 1, P):
-    # 新しく一つ加える
-    dict[A[r]] += 1
-    while True:
-        # もし要素が一つしか無かったら削れない
-        if dict[A[l]] == 1:
-            break
-        dict[A[l]] -= 1
-        l += 1
-    print(l, r)
-
-# 各要素にダブりがない範囲
-N = 6
-A = [1, 2, 2, 3, 4, 4]
-
-dict = defaultdict(int)
-l = 0
-for r in range(N):
-    while dict[A[r]] == 1:
-        dict[A[l]] -= 1
-        l += 1
-    print(l, r)
-    dict[A[r]] += 1
-
 N = 4
-A = 2, 5, 4, 6
+inf = float('inf')
 
-r, tmp = 0, 0
-# l:左端
+d = [
+[0, 2, inf, inf],
+[inf, 0, 3, 9],
+[1, inf, 0, 6],
+[inf, inf, 4, 0]
+]
+
+dp = [[-1] * N for i in range(1 << N)]
+
+def rec(s, v, dp):
+    if dp[s][v] >= 0:
+        return dp[s][v]
+    if s == (1 << N) - 1 and v == 0:
+        dp[s][v] = 0
+        return 0
+    res = float('inf')
+    for u in range(N):
+        if s & (1 << u) == 0:
+            res = min(res,rec(s|(1 << u), u, dp) + d[v][u])
+    dp[s][v] = res
+    return res
+# 結局のところ0からスタートしようが1からスタートしようが同じ道を通る
+print(rec(0,0,dp))
+
+# ABC054 C - One-stroke Path
+
+N, M = getNM()
+dist = [[] for i in range(N + 1)]
+for i in range(M):
+    a, b = getNM()
+    dist[a - 1].append(b - 1)
+    dist[b - 1].append(a - 1)
+
 cnt = 0
-# 一つオーバーさせる
-for l in range(N):
-    while r < N and tmp ^ A[r] == tmp + A[r]:
-        # 右端を伸ばす
-        tmp += A[r]
-        r += 1
-    # 計算
-    # r を一個進めて条件を満たさなくなった時点でループを終了しているので
-    # (r - l + 1) - 1
-    cnt += r - l
 
-    if l == r:
-        r += 1
-    tmp -= A[l]
+pos = deque([[1 << 0, 0]])
+
+while len(pos) > 0:
+    s, v = pos.popleft()
+    if s == (1 << N) - 1:
+        cnt += 1
+    for u in dist[v]:
+        if s & (1 << u):
+            continue
+        pos.append([s | (1 << u), u])
 print(cnt)
 
-# ABC017 D - サプリメント
+"""
+全ての場所を一度だけ通り巡回する通りの数
+bit(1 << N)を小さい順から探索する
+①bit & (1 << 0)
+最初に0を踏んでないということだから飛ばす
+②現在の場所sを探すためN個探索する
+③次の場所tを探すためN個探索する
+④渡すdpする
+"""
+
 N, M = getNM()
-F = getArray(N)
+G = [[0] * N for i in range(N)]
+for i in range(M):
+    a, b = getNM()
+    G[a - 1][b - 1] = 1 # a ~ bのエッジが存在する
+    G[b - 1][a - 1] = 1
 
-# 何通り　→ comb or dp
-# O(N)で
-# 同じ味のサプリメントを摂取しない
-# dp[i]:i日目までにサプリを摂取する通りが何通りあるか
-# dp[i]:サプリi個目までにサプリを摂取する通りが何通りあるか
+# 状態bitから次の状態へ渡すdpするというのはよくやる
+# [0](001) → [0, 1](011) → [0, 1, 2](111)
+#          → [0, 2](101) → [0, 1, 2](111)
+def counter(sta):
+    # dp[bit][i]これまでに踏んだ場所がbitであり、現在の場所がiである
+    dp = [[0] * N for i in range(1 << N)]
+    dp[1 << sta][sta] = 1
 
-# N, M = 5, 2
-# L = [1, 2, 1, 2, 2]の場合
-# dp[0] = 1
-# dp[1] = 1 1個目を新たに食べた場合、それ以前の通りは1通り
-# dp[2] = 2 2個目を新たに食べた場合、それ以前の通りは2通り
-# (前回1個目を食べたかもしれないし、今回1個目と合わせて2個目を食べたかもしれない)
-# dp[i] += dp[（最後にF[i]が登場した場所）] ~ dp[i - 1]
+    for bit in range(1, 1 << N):
+        if not bit & (1 << sta):
+            continue
+        # s:現在の場所
+        for s in range(N):
+            # sを踏んだことになっていなければ飛ばす
+            if not bit & (1 << s):
+                continue
+            # t:次の場所
+            for t in range(N):
+                # tを過去踏んでいない and s → tへのエッジがある
+                if (bit & (1 << t)) == 0 and G[s][t]:
+                    dp[bit|(1 << t)][t] += dp[bit][s]
 
-dp = [0] * (N + 1) # dpだけ1-index
-dp[0] = 1
-ignore = [0] * (M + 1)
-l = 0
-now = dp[0]
-for r in range(N):
-    # 最初ignoreのフラグが立っていないが,nowにはdp[0]の値が入っている状態
-    while ignore[F[r]]:
-        ignore[F[l]] = 0 # F[l]のフラグを消す
-        now -= dp[l] # lの直前のdpを引く
-        now %= mod
-        l += 1
-    # dpをレコード（範囲の合計を足す）
-    dp[r + 1] = now
-    # rを1個ずらして更新
-    now += dp[r + 1]
-    now %= mod
-    ignore[F[r]] = 1
+    return sum(dp[(1 << N) - 1])
 
-print(dp)
+print(counter(0))
 
-# ABC102 D - Equal Cut
-# 中央の境界をスライドさせる → １番目の境界は尺取りで求められる
-N = getN()
-A = getList()
-fore = copy.deepcopy(A)
-back = copy.deepcopy(A)
+# ABC104 C - All Green
+# 特別ボーナスがある問題は大抵bit dp
+# 目標700点
+D, G = getNM()
+query = []
+for i in range(D):
+    p, c = getNM()
+    query.append([i + 1, p, c])
 
-for i in range(N - 1):
-    fore[i + 1] += fore[i]
-    back[N - i - 2] += back[N - i - 1]
+ans_cnt = float('inf')
 
-left = 0
-total = A[0]
+for bit in range(1 << D):
+    sum = 0
+    cnt = 0
+    for i in range(D):
+        if bit & (1 << i):
+            sum += query[i][0] * query[i][1] * 100 + query[i][2]
+            cnt += query[i][1]
 
-fore_list = []
-for right in range(1, N - 2):
-    while abs((fore[right] / 2) - (total + A[left + 1])) < abs((fore[right] / 2) - total):
-        left += 1
-        total += A[left]
-    fore_list.append([right, total, fore[right] - total])
+    if sum < G:
+        for j in range(D - 1, -1, -1):
+            if not bit & (1 << j):
+                left = G - sum
+                fire = query[j][0] * 100
+                opt = min(query[j][1], (left + fire - 1) // fire)
+                sum += opt * query[j][0] * 100
+                cnt += opt
+                break
 
-right = N - 1
-total = A[-1]
+    if sum >= G:
+        ans_cnt = min(ans_cnt, cnt)
 
-back_list = []
-for left in range(N - 2, 1, -1):
-    while abs((back[left] / 2) - (total + A[right - 1])) < abs((back[left] / 2) - total):
-        right -= 1
-        total += A[right]
-    back_list.append([left, total, back[left] - total])
-back_list.sort()
-
-ans = float('inf')
-for i in range(len(back_list)):
-    ind1, a, b = fore_list[i]
-    ind2, c, d = back_list[i]
-    opt = max(a, b, c, d) - min(a, b, c, d)
-    ans = min(ans, opt)
-print(ans)
-
-N, K = 10, 4
-A = [100, 300, 600, 700, 800, 400, 500, 800, 900, 900]
-
-right, ans = 0, 0
-for left in range(N):
-    # 単調増加するとこまでもしくは長さKになるまで
-    while right < N - 1 and A[right] < A[right + 1] and right - left < K - 1:
-        right += 1
-    # もし長さKまで伸ばせたらans += 1
-    if right - left == K - 1:
-        ans += 1
-    # 前に進めないならright += 1
-    if left == right and right < N:
-        right += 1
-print(ans)
+print(ans_cnt)
