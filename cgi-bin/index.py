209ec2ea7e290c8f00039339483853aba32450e1
customaddone <custom1485@docomo.ne.jp>
2020-09-28 20:56:48 +0900

    木

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -49,238 +49,308 @@ mod = 998244353
 # Main Code #
 #############
 
-num = [i for i in range(0, 10, 2)]
-A = [2, 4, 5]
-B = [2, 3]
+N = 5
+# 木グラフ
+que = [
+[1, 2],
+[1, 4],
+[2, 3],
+[2, 5]
+]
+# 重みつき
+que_dis = [
+[1, 2, 2],
+[1, 4, 1],
+[2, 3, 2],
+[2, 5, 1]
+]
 
-for i in A:
-    index = bisect_right(num, i)
-    print(num[index - 1])
+def build_tree(n, edge_list):
 
-# numの中でのi未満の数字の最大値を求める
-for i in A:
-    index = bisect_left(num, i)
-    print(num[index - 1])
+    G = [[] for i in range(n)]
 
-# numの中でのiより大きい数字の最小値を求める
-for i in B:
-    index = bisect_right(num, i)
-    print(num[index])
+    for a, b in edge_list:
+        G[a - 1].append(b - 1)
+        G[b - 1].append(a - 1)
 
-# numの中でのi以上の数字の最小値を求める
-for i in B:
-    index = bisect_left(num, i)
-    print(num[index])
+    return G
 
-A = [1, 2, 4, 8, 16, 32]
+def build_tree_dis(n, edge_list):
 
-def or_less(array, x):
-    # arrayの中のx以下のものの個数
-    # arrayの中のx以下のもののうちの最大値
-    index = bisect_right(array, x)
-    if index == 0:
-        or_less_int = -float('inf')
-    else:
-        or_less_int = array[index - 1]
-    return [index, or_less_int]
+    G = [[] for i in range(n)]
 
-def less_than(array, x):
-    # arrayの中のx未満のものの個数
-    # arrayの中のx未満のもののうちの最大値
-    index = bisect_left(array, x)
-    if index == 0:
-        less_than_int = -float('inf')
-    else:
-        less_than_int = array[index - 1]
-    return [index, less_than_int]
+    for a, b, c in edge_list:
+        G[a - 1].append([b - 1, c])
+        G[b - 1].append([a - 1, c])
 
-print(or_less(A, 8))
-print(less_than(A, 1))
+    return G
 
-def or_more(array, x):
-    # arrayの中のx以上のものの個数
-    # arrayの中のx以上のもののうちの最小値
-    n = len(array)
-    index = bisect_left(array, x)
-    if index == n:
-        or_more_int = float('inf')
-    else:
-        or_more_int = array[index]
-    return [n - index, or_more_int]
+# 木の建設
+G1 = build_tree(N, que)
+G2 = build_tree_dis(N, que_dis)
 
-def more_than(array, x):
-    # arrayの中のxより大きいものの個数
-    # arrayの中のxより大きいのもののうちの最小値
-    n = len(array)
-    index = bisect_right(array, x)
-    if index == n:
-        more_than_int = float('inf')
-    else:
-        more_than_int = array[index]
-    return [n - index, more_than_int]
+# 木を探索
+def search(n, edges, sta):
+    ignore = [0] * N
+    ignore[sta] = 1
+    pos = deque([sta])
+    # 探索
+    while len(pos) > 0:
+        u = pos.popleft()
+        for i in edges[u]:
+            if ignore[i] == 0:
+                ignore[i] = 1
+                pos.append(i)
+# [0, 1, 3, 2, 4]
+search(N, G1, 0)
 
-print(or_more(A, 32))
-print(more_than(A, 1))
+# staからの距離
+def distance(n, edges, sta):
+    # 木をKから順にたどる（戻るの禁止）
+    ignore = [-1] * N
+    ignore[sta] = 0
+    pos = deque([sta])
 
-# ABC122 D - We Like AGC
-X, Y, Z, K = getNM()
-A = sorted([-i for i in getList()])
-B = sorted([-i for i in getList()])
-C = sorted([-i for i in getList()])
+    while len(pos) > 0:
+        u = pos.popleft()
+        for i in edges[u]:
+            if ignore[i[0]] == -1:
+                ignore[i[0]] = ignore[u] + i[1]
+                pos.append(i[0])
+    return ignore
+# [0, 2, 4, 1, 3]
+print(distance(N, G2, 0))
 
-pos = []
-heapify(pos)
-dict = defaultdict(int)
-u = (A[0] + B[0] + C[0], 0, 0, 0)
-heappush(pos, u)
-dict[u] = 1
+# ABC067 D - Fennec VS. Snuke
+N = 12
+query = [
+[1, 3],
+[2, 3],
+[3, 4],
+[3, 5],
+[5, 11],
+[6, 12],
+[7, 9],
+[8, 9],
+[9, 10],
+[9, 11],
+[11, 12]
+]
 
-for i in range(K):
-    p, i, j, l = heappop(pos)
-    print(-p)
-    # 取り出すごとにA, B, Cについての次の値をpush
-    if i + 1 < X:
-        opt_a = (A[i + 1] + B[j] + C[l], i + 1, j, l)
-        if dict[opt_a] == 0:
-            heappush(pos, opt_a)
-            dict[opt_a] = 1
-    if j + 1 < Y:
-        opt_b = (A[i] + B[j + 1] + C[l], i, j + 1, l)
-        if dict[opt_b] == 0:
-            heappush(pos, opt_b)
-            dict[opt_b] = 1
-    if l + 1 < Z:
-        opt_c = (A[i] + B[j] + C[l + 1], i, j, l + 1)
-        if dict[opt_c] == 0:
-            heappush(pos, opt_c)
-            dict[opt_c] = 1
+dist = [[] for i in range(N)]
+for i in range(N - 1):
+    a, b = query[i]
+    dist[a - 1].append(b - 1)
+    dist[b - 1].append(a - 1)
 
-# ABC149 E - Handshake
-N, K = 9, 14
-A = [1, 3, 5, 110, 24, 21, 34, 5, 3]
+# nowからNまでのルート
+def router(n, sta, end):
+    pos = deque([sta])
+    ignore = [0] * n
+    path = [0] * n
+    path[sta] = -1
 
-A.sort(reverse = True)
+    while pos[0] != end:
+        u = pos.popleft()
+        ignore[u] = 1
 
-# 各ペアを大きい順に並べるのはムリ
-# ただし、合計がx以上/以下になるペアが何個あるかは求められる
-# x以上になるペアが何個あるか
-def cnt(x):
-    r = 0
-    ans = 0
-    list_num = [0] * N
-    for i in range(N - 1, -1, -1):
-        while r < N and A[r] + A[i] >= x:
-            r += 1
-        list_num[i] = r
-        ans += r
+        for i in dist[u]:
+            if ignore[i] != 1:
+                path[i] = u
+                pos.append(i)
 
-    return [ans, list_num]
+    route = deque([end])
+    while True:
+        next = path[route[0]]
+        route.appendleft(next)
+        if route[0] == sta:
+            break
 
-ng = 0
-ok = 2 * (10 ** 6)
+    return list(route)
 
-while ok - ng > 1:
-    mid = (ok + ng) // 2
-    if cnt(mid)[0] <= K:
-        ok = mid
-    else:
-        ng = mid
+route = router(N, 0, N - 1)
+print(route)
 
-# 境界がわかったら
-imos = [0] + copy.deepcopy(A)
+# NG以外のところで辿れるところの数
+def dfs_ter(sta, ng):
+    pos = deque([sta])
+
+    ignore = [0] * N
+    for i in ng:
+        ignore[i] = 1
+
+    cnt = 0
+    while len(pos) > 0:
+        u = pos.popleft()
+        ignore[u] = 1
+        cnt += 1
+        for i in dist[u]:
+            if ignore[i] != 1:
+                pos.append(i)
+
+    return cnt
+
+L = len(route)
+fen_ter = route[:(L + 2 - 1) // 2]
+snu_ter = route[(L + 2 - 1) // 2:]
+
+fen_ans = dfs_ter(0, snu_ter)
+
+if fen_ans > N - fen_ans:
+    print('Fennec')
+else:
+    print('Snuke')
+
+# ABC070 D - Transit Tree Path
+N = getN()
+dist = [[] for i in range(N + 1)]
+for i in range(N - 1):
+    a, b, c = getNM()
+    dist[a].append([b, c])
+    dist[b].append([a, c])
+ignore = [-1] * (N + 1)
+
+# Kからの最短距離をbfsで測る
+def distance(sta):
+    # 木をKから順にたどる（戻るの禁止）
+    pos = deque([sta])
+
+    while len(pos) > 0:
+        u = pos.popleft()
+        for i in dist[u]:
+            if ignore[i[0]] == -1:
+                ignore[i[0]] = ignore[u] + i[1]
+                pos.append(i[0])
+
+Q, K = getNM()
+ignore[K] = 0
+distance(K)
+# 答えはK~xまでの距離+K~yまでの距離
+ans = []
+for i in range(Q):
+    x, y = getNM()
+    ans.append(ignore[x] + ignore[y])
+for i in ans:
+    print(i)
+
+# ABC087 D - People on a Line
+
+N, M = getNM()
+dist = [[] for i in range(N)]
+for i in range(M):
+    l, r, d = getNM()
+    dist[l - 1].append([r - 1, d])
+    dist[r - 1].append([l - 1, -d])
+
+dis = [float('inf')] * N
+
+pos = deque([i for i in range(N)])
+while len(pos) > 0:
+    u = pos.popleft()
+    # 始めの第一歩
+    if dis[u] == float('inf'):
+        dis[u] = 0
+    # 行き先の位置が未確定なら確定させる
+    # 既に確定しているなら判定
+    for to, d in dist[u]:
+        if dis[to] == float('inf'):
+            dis[to] = dis[u] + d
+            # 位置をレコードした頂点を優先的に処理する必要があるためappendleft
+            # 例 que = [0, 2, 1], [1, 2, 3]
+            # pos = [0, 1, 2] の時
+            # dis = [0, -2, 1]でYesになるはず
+            # 0を探索, 1の距離をレコードしてappend disは[0, inf, 1]
+            # pos = [1, 2, 2]のため次は1を探索
+            # dis[1] == infなのでdis[1] = 0にする
+            # dis[1] = 0, dis[2] = 1なのでNo
+            pos.appendleft(to)
+        else:
+            if dis[u] + d != dis[to]:
+                print('No')
+                exit()
+print('Yes')
+
+# ABC126 D - Even Relation
+# 頂点0からの距離を調べるだけ
+N = getN()
+edges = [[] for i in range(N)]
+for i in range(N - 1):
+    u, v, d = getNM()
+    edges[u - 1].append([v - 1, d])
+    edges[v - 1].append([u - 1, d])
+
+def dij(start):
+    dist = [float('inf') for i in range(N)]
+    dist[start] = 0
+    pq = [(0, start)]
+
+    while len(pq) > 0:
+        d, now = heapq.heappop(pq)
+        if (d > dist[now]):
+            continue
+        for i in edges[now]:
+            if dist[i[0]] > dist[now] + i[1]:
+                dist[i[0]] = dist[now] + i[1]
+                heapq.heappush(pq, (dist[i[0]], i[0]))
+    return dist
+
+ans = [0] * N
+dij_list = dij(0)
 for i in range(N):
-    imos[i + 1] += imos[i]
+    if dij_list[i] % 2 == 0:
+        ans[i] = 0
+    else:
+        ans[i] = 1
+for i in ans:
+    print(i)
 
+# ARC037 B - バウムテスト
+N, M = 11, 11
+query = [
+[1, 2],
+[1, 3],
+[2, 4],
+[3, 5],
+[4, 6],
+[5, 7],
+[6, 8],
+[7, 9],
+[8, 10],
+[9, 11],
+[10, 11]
+]
+dist = [[] for i in range(N)]
+for i in range(M):
+    a, b = query[i]
+    a -= 1
+    b -= 1
+    dist[a].append(b)
+    dist[b].append(a)
+
+ignore = [0] * N
 ans = 0
-plus = cnt(ok)[1]
+# 閉路検出
+def search(x, dist):
+    global ans
+    # 現在の位置とparent
+    pos = deque([[x, -1]])
+    ignore[x] = 1
+    flag = True
 
-# cntから得た情報を元に上から順番に足していく
+    while pos:
+        u, parent = pos.popleft()
+        for i in dist[u]:
+            if i != parent:
+                if ignore[i] == 1:
+                    flag = False
+                    continue
+                ignore[i] = 1
+                pos.append([i, u])
+    if flag:
+        ans += 1
+
+# 一つの木の頂点は全て一回のsearchで塗りつぶされる
 for i in range(N):
-    ans += (A[i] * plus[i]) + imos[plus[i]]
-# 合計がngになるペアを足りない分K - sum(plus)個足す
-ans += ng * (K - sum(plus))
+    if ignore[i] == 0:
+        search(i, dist)
 print(ans)
-
-# ABC155 D - Pairs
-N, K = 10, 40
-A = [5, 4, 3, 2, -1, 0, 0, 0, 0, 0]
-minus = [-x for x in A if x < 0]
-plus = [x for x in A if x >= 0]
-
-minus.sort()
-plus.sort()
-
-def cnt(x):
-    ans = 0
-    if x < 0:
-        x = -x
-        r = 0
-        # - * +
-        for num in minus[::-1]:
-            while r < len(plus) and plus[r] * num < x:
-                r += 1
-            ans += len(plus) - r
-        return ans
-
-    r = 0
-    for num in minus[::-1]:
-        if num * num <= x:
-            ans -= 1
-        while r < len(minus) and minus[r] * num <= x:
-            r += 1
-        ans += r
-    r = 0
-    for num in plus[::-1]:
-        if num * num <= x:
-            ans -= 1
-        while r < len(plus) and plus[r] * num <= x:
-            r += 1
-        ans += r
-    ans //= 2
-    # -になるものはまとめて計算
-    ans += len(minus) * len(plus)
-    return ans
-
-bottom = 0
-top = 2 * (10 ** 18) + 2
-
-
-while top - bottom > 1:
-    mid = (top + bottom) // 2
-    if cnt(mid - 10 ** 18 - 1) < K:
-        bottom = mid
-    else:
-        top = mid
-
-print(int(top - 10 ** 18 - 1))
-
-# ARC037 C - 億マス計算
-N, K = getNM()
-A = sorted(getList())
-B = sorted(getList())
-
-# かけ合わせるとx以下になるものが何個あるか
-def cnt(x):
-    r = 0
-    ans = 0
-    list_num = [0] * N
-    # BiについてAの各要素とペアになれるのがいくつあるか
-    for i in range(N - 1, -1, -1):
-        while r < N and A[r] * B[i] <= x:
-            r += 1
-        list_num[i] = r
-        ans += r
-
-    return [ans, list_num]
-
-ng = 0
-ok = 2 * (10 ** 18)
-
-while ok - ng > 1:
-    mid = (ok + ng) // 2
-    if cnt(mid)[0] >= K:
-        ok = mid
-    else:
-        ng = mid
-
-print(ok)
