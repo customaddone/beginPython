9cc33c48f3d8242403e2ea3d69b5ded875fb8270
customaddone <custom1485@docomo.ne.jp>
2020-09-28 22:12:54 +0900

    貪欲

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -49,240 +49,242 @@ mod = 998244353
 # Main Code #
 #############
 
-# ABC008 C - コイン
-n = getN()
-c = getArray(n)
-sumans = 0
-
-for i in c:
-    lista = [j for j in c if i % j == 0]
-    count = len(lista)
-    sumans += math.ceil(count / 2) / count
-print(sumans)
-
-# ABC011 D - 大ジャンプ
-def cmb_1(n, r):
-    r = min(n - r, r)
-    if (r < 0) or (n < r):
-        return 0
-
-    if n == 0:
-        return 1
-
-    if r == 0:
-        return 1
-    over = reduce(mul, range(n, n - r, -1))
-    under = reduce(mul, range(1, r + 1))
-    return over // under
-
-N, D = getNM()
-X, Y = getNM()
-X = abs(X)
-Y = abs(Y)
-
-# X軸に平行に正の向きに飛ぶ回数はx_time + α回
-# X軸に平行に負の向きに飛ぶ回数はα回
-x_time = X // D
-y_time = Y // D
-
-if X % D != 0 or Y % D != 0 or N < x_time + y_time:
-    print(0)
-    exit()
-
-N_a = N - (x_time + y_time)
-if N_a % 2 != 0:
-    print(0)
-    exit()
-N_a //= 2
-
-ans = 0
-for i in range(N_a + 1):
-    ans += cmb_1(N, x_time + i) * cmb_1(N - (x_time + i), i) * cmb_1(N - (x_time + 2 * i), y_time + N_a - i)
-print(ans / (4 ** N))
-
-# ABC024 D - 動的計画法
-A, B, C = getArray(3)
-
-# kCc / k+1Cc = k - c + 1 / k + 1
-# k+1Cc+1 / kCc = k + 1 / c + 1
-# Xを10 ** 9 + 7 - 2乗すると逆元が求まる
-x = (C * pow(A, mod - 2, mod)) % mod
-y = (B * pow(A, mod - 2, mod)) % mod
-
-n = (x + y - 2 * x * y) * pow(x * y - x - y, mod - 2, mod)
-k = (y - x * y) * pow(x * y - x - y, mod - 2, mod)
-print((n - k) % mod, k % mod)
-
-# ABC058 D - いろはちゃんとマス目
-# 総計する
-lim = 10 ** 6 + 1
-fact = [1, 1]
-factinv = [1, 1]
-inv = [0, 1]
-
-for i in range(2, lim + 1):
-    fact.append((fact[-1] * i) % mod)
-    inv.append((-inv[mod % i] * (mod // i)) % mod)
-    # 累計
-    factinv.append((factinv[-1] * inv[-1]) % mod)
-
-def cmb(n, r):
-    if (r < 0) or (n < r):
-        return 0
-    r = min(r, n - r)
-    return fact[n] * factinv[r] * factinv[n - r] % mod
-
-H, W, A, B = getNM()
-
-mother = cmb((H - 1) + (W - 1), (W - 1))
-
-goban = cmb((H - A) + (B - 1), B - 1)
-togoal = cmb((A - 1) + (W - B - 1), (W - B - 1))
-
-for i in range(A):
-    row = H - A + i
-    col = B - 1
-    row_left = A - 1 - i
-    col_left = W - B - 1
-    mother -= cmb(row + col, row) * cmb(row_left + col_left, row_left)
-    mother %= mod
-print(mother)
-
-# ABC057 D - Maximum Average Sets
-N, A, B = getNM()
-V = sorted(getList(), reverse = True)
-
-# 1行目の答え　上からA個
-print(sum(V[:A]) / A)
-
-# Vの各要素の数を数える
-dict = defaultdict(int)
-for i in range(N):
-    dict[V[i]] += 1
-
-r = A
-n = 0
-for i in dict.items():
-    print(i)
-    if r - i[1] >= 0:
-        r -= i[1]
-    else:
-        n = i[1] # A個まで残りr個であり、そこには要素i[j]がn個入れられる
-        break
-
-ans = 0
-# V[0] == V[A - 1]ならA個を超えても平均値が下がらない
-# 残りr個(合計でA個)決める、残りr + 1個(合計でA + 1個)決める...合計でB個決める
-if r > 0 and V[0] == V[A - 1]:
-    for i in range(r, B + 1):
-        ans += cmb_1(n, i)
-else:
-    ans += cmb_1(n, r) # A個まであとr個残っており、n個のうちr個選ぶ
-
-print(ans)
-
-# ABC066 D - 11
-# 普通にやれば(cmb(N + 1, i)だが、今回ダブりがある
-# 29 19 ~ 19 31 9の場合
-# [29]のうちいくつか + １番目の19 + [31, 9]のうちいくつかと
-# [29]のうちいくつか + ２番目の19 + [31, 9]のうちいくつかはダブル
-# i個要素を選ぶとすると、19を選び、外側の要素からi - 1個選ぶ全通りについてダブルので１回引く
+# ABC005 C - おいしいたこ焼きの売り方
+# マッチング問題だが貪欲
+T = getN()
 N = getN()
-A = getList()
+sell = getList()
+M = getN()
+buy = getList()
 
-lista = [0] * (max(A) + 1)
-double = 0
-# ダブり位置1を決める
-dou_ind_2 = 0
-for i in range(N + 1):
-    if lista[A[i]] > 0:
-        double = A[i]
-        dou_ind_2 = i
-        break
-    lista[A[i]] += 1
-# ダブり位置2を決める
-dou_ind_1 = 0
-for i in range(N + 1):
-    if A[i] == double:
-        dou_ind_1 = i
-        break
+# 来る客1, 2に売れるか
+for cus in buy:
+    flag = False
+    for i in range(N):
+        if sell[i] <= cus <= sell[i] + T:
+            flag = True
+            sell[i] = mod
+            break
+    if not flag:
+        print('no')
+        exit()
+print('yes')
 
-outer = dou_ind_1 + (N - dou_ind_2)
-
-for i in range(1, N + 2):
-    if i == 1:
-        print(N)
-        continue
-    print((cmb(N + 1, i) - cmb(outer, i - 1)) % mod)
-
-# ABC105 D - Candy Distribution
-# M人に配る mod M
-N, M = getNM()
-A = getList()
-
-alta = []
-for i in A:
-    alta.append(i % M)
-
-imos = [0]
+# ABC080 D - Recording
+# 使ってない録画機は他のチャンネルにスイッチできる
+# 同時にいくつ放送が流れているか
+N, C = getNM()
+query = [getList() for i in range(N)]
+dp = [[0] * (C + 1) for i in range(10 ** 5 + 2)]
 for i in range(N):
-    imos.append((alta[i] + imos[i]) % M)
+    s, t, c = query[i]
+    dp[s][c] += 1
+    dp[t + 1][c] -= 1
+
+for i in range(1, 10 ** 5 + 2):
+    for j in range(C + 1):
+        dp[i][j] += dp[i - 1][j]
 
 ans = 0
-for i in Counter(imos).values():
-    ans += cmb_1(i, 2)
+for i in range(10 ** 5 + 2):
+    cnt = 0
+    for j in dp[i]:
+        if j > 0:
+            cnt += 1
+    ans = max(ans, cnt)
 print(ans)
 
-# ABC136 E - Max GCD
+# ABC085 D - Katana Thrower
+N, H = getNM()
+
+a = []
+b = []
+
+for i in range(N):
+  x, y = map(int, input().split())
+  a.append(x)
+  b.append(y)
+
+# 振った場合の最大値
+max_a = max(a)
+
+ans = 0
+# 振る刀の最大攻撃力より高い攻撃力を持つ投げ刀を高い順にソートする
+# 刀iで好きなだけ振って攻撃する→気が済んだら投げることで振りの攻撃力と投げの攻撃力を
+# 両方利用することができる
+# 実は投げてしまった刀も振ることができるというルールに変更しても
+# 問題の答えは変わらない
+# 実際のムーブとしては
+# ①最も攻撃力が高い振り刀で攻撃する
+# ②一定の体力以下になると攻撃力が高い順に投げ刀で攻撃していって撃破
+# という流れになる
+for x in reversed(sorted(filter(lambda x: x >= max_a, b))):
+    H -= x
+    ans += 1
+    if H <= 0: break
+
+ans += max(0, (H + max_a - 1) // max_a)
+print(ans)
+
+# ABC091 C - 2D Plane 2N Points
+
+N = getN()
+# Rはループさせるのでソートさせる必要ない
+R = [getList() for i in range(N)]
+R_l = [1] * N
+
+B = [getList() for i in range(N)]
+B.sort()
+
+# 貪欲法でペア作りする問題
+# ABC005 C - おいしいたこ焼きの売り方の時と同様に
+# それとしか繋げないもの　を優先的に繋いでいく
+
+# 条件Aの通過が厳しい順に対象bをソートし、
+# たこ焼き　条件A:客が来る前にたこ焼きができてないといけない
+#  　　　　      客を来るのが早い順に並べる（最初から並んでる）
+# 今回     条件A:赤星のx座標が青星のx座標より小さくないといけない
+#  　　　　　　　 青星をx座標が小さい順に並べる
+
+# 条件A, 条件Bをクリアしたものの中で、最も条件Bの通過が厳しい対象aと結ぶ
+# たこ焼き　条件B:たこ焼きが賞味期限より前のものでないといけない
+#  　　　　      できるだけ古いものを売る（最初から並んでる）
+# 今回     条件B:赤星のy座標が青星のy座標より小さくないといけない
+#  　　　　　　　 条件をクリアしたもののうちでできるだけy座標が大きいものを選ぶ
+
+ans = 0
+for b in B:
+    max_y = -1
+    max_index = -1
+    for i, a in enumerate(R):
+        # x, y座標が小さいもののうちでまた使ってないもの
+        if a[0] < b[0] and a[1] < b[1] and R_l[i] == 1:
+            # あるならY座標が最も大きいもの
+            if a[1] > max_y:
+                max_y = a[1]
+                max_index = i
+    if max_y >= 0:
+        R_l[max_index] = 0
+        ans += 1
+
+# ABC100 D - Patisserie ABC
+# 8パターン全部調べる
+
+N,M = getNM()
+data = [[] for i in range(8)]
+for _ in range(N):
+    x,y,z = getNM()
+    data[0].append(x + y + z)
+    data[1].append(x + y - z)
+    data[2].append(x - y + z)
+    data[3].append(x - y - z)
+    data[4].append(- x + y + z)
+    data[5].append(- x + y - z)
+    data[6].append(- x - y + z)
+    data[7].append(- x - y - z)
+
+ans = -mod
+for i in range(8):
+    data[i].sort(reverse = True)
+    ans = max(ans,sum(data[i][:M]))
+print(ans)
+
+# ABC116 D - Various Sushi
 N, K = getNM()
-A = getList()
+various = defaultdict(list)
+que = [getList() for i in range(N)]
 
-# 操作後のAの要素全てが因数にkを持つ
+ans = 0
+num = []
+var_s = set()
 
-# 好きな回数操作を行えるとすると、Aの要素全てに因数kを持たせることができるか？
-# 操作前と操作後とではAの総和は変わらない → A = akとするとa個のkをN個の要素に配分する
-# ことでAの要素全てに因数kを持たせることができる。
-# 好きな回数操作を行える、操作後のAの全ての要素を割り切る正の整数は、sum(A)の約数になる
+# 美味しい順にK個とった時の幸福度
+que.sort(reverse = True, key = lambda i: i[1])
+for i in range(K):
+    ans += que[i][1]
+    # もし２番手以降ならあとで交換する用にとっておく
+    if que[i][0] in var_s:
+        num.append(que[i][1])
+    var_s.add(que[i][0])
 
-def make_divisors(n):
-    divisors = []
-    for i in range(1, int(math.sqrt(n)) + 1):
-        if n % i == 0:
-            divisors.append(i)
-            # √nで無い数についてもう一個プラス
-            if i != n // i:
-                divisors.append(n // i)
-    return sorted(divisors)
+var = len(var_s)
+ans += var ** 2
 
-# K回以下の操作の条件付きでも題意を満たすか
-# alta % kをし、あまりが小さい順にソートする
-# 前からi番目までをプラス、i + 1番目以降をマイナスにする
-# どちらか大きい方がK以下なら条件を満たす
+# 使ってない種類について各種類で一番大きさが大きいもの
+left_l = defaultdict(int)
+for i in range(N):
+    if not que[i][0] in var_s:
+        left_l[que[i][0]] = max(left_l[que[i][0]], que[i][1])
 
-def judger(x):
-    alta = [i % x for i in A]
-    alta.sort()
-    alta_b = [x - i for i in alta]
-    # 前からi番目までプラスにする
-    imos_f = copy.deepcopy(alta)
-    # 前からi + 1番目以降をマイナスにする
-    imos_b = copy.deepcopy(alta_b)
+num.sort(reverse = True)
+left_l = [i[1] for i in left_l.items()]
+left_l.sort()
 
-    for i in range(N - 1):
-        imos_f[i + 1] += imos_f[i]
-        imos_b[-i - 2] += imos_b[-i - 1]
+# M回交換する
+opt = ans
+M = min(len(num), len(left_l))
+for i in range(M):
+    u = num.pop()
+    s = left_l.pop()
+    # 寿司単体の幸福度
+    opt -= (u - s)
+    # 種類が増える分
+    opt += 2 * var + 1
+    var += 1
+    ans = max(opt, ans)
 
-    # 全部マイナス、全部プラスの場合
-    imos_f.insert(0, 0)
-    imos_b.append(0)
+print(ans)
 
-    ans = float('inf')
-    for i in range(N + 1):
-        ans = min(ans, max(imos_f[i], imos_b[i]))
+# ABC119 D - Lazy Faith
+A, B, Q = getNM()
+# 神社
+S = getArray(A)
+# 寺
+T = getArray(B)
+query = getArray(Q)
 
-    return ans
+S.insert(0, -float('inf'))
+T.insert(0, -float('inf'))
+S.append(float('inf'))
+T.append(float('inf'))
 
-for i in make_divisors(sum(A))[::-1]:
-    if judger(i) <= K:
-        print(i)
-        break
+def close(data, point):
+    west = data[bisect_left(data, point) - 1]
+    east = data[bisect_left(data, point)]
+
+    return west, east
+
+for i in range(Q):
+    now = query[i]
+    shrine_west, shrine_east = close(S, now)
+    temple_west, temple_east = close(T, now)
+
+    ww = now - min(shrine_west, temple_west)
+    we_1 = (now - shrine_west) * 2 + (temple_east - now)
+    we_2 = (now - temple_west) * 2 + (shrine_east - now)
+    ee = max(shrine_east, temple_east) - now
+    ew_1 = (shrine_east - now) * 2 + (now - temple_west)
+    ew_2 = (temple_east - now) * 2 + (now - shrine_west)
+
+    print(min(ww, we_1, we_2, ee, ew_1, ew_2))
+
+# ABC137 D - Summer Vacation
+# ヒープ使った貪欲
+N, M = getNM()
+query = [getList() for i in range(N)]
+
+A_list = [[] for i in range(10 ** 5 + 1)]
+for a, b in query:
+    A_list[a].append(b)
+
+job = []
+heapq.heapify(job)
+
+ans = 0
+for i in range(1, M + 1):
+    for j in A_list[i]:
+        heapq.heappush(job, -j)
+    if len(job) > 0:
+        u = heapq.heappop(job)
+        ans += -u
+print(ans)
