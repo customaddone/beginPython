1f1076aa71bdab882f072ff52acdd732ae646ae4
customaddone <custom1485@docomo.ne.jp>
2020-09-28 20:04:43 +0900

    unionfind

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -49,221 +49,246 @@ mod = 998244353
 # Main Code #
 #############
 
-# ABC005 C - おいしいたこ焼きの売り方
-# マッチング問題だが貪欲
-T = getN()
-N = getN()
-sell = getList()
-M = getN()
-buy = getList()
+class UnionFind():
+    def __init__(self, n):
+        self.n = n
+        self.parents = [-1] * n
 
-# 来る客1, 2に売れるか
-for cus in buy:
-    flag = False
-    for i in range(N):
-        if sell[i] <= cus <= sell[i] + T:
-            flag = True
-            sell[i] = mod
-            break
-    if not flag:
-        print('no')
-        exit()
-print('yes')
+    def find(self, x):
+        if self.parents[x] < 0:
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
 
-# ABC080 D - Recording
-# 使ってない録画機は他のチャンネルにスイッチできる
-# 同時にいくつ放送が流れているか
-N, C = getNM()
-query = [getList() for i in range(N)]
-dp = [[0] * (C + 1) for i in range(10 ** 5 + 2)]
-for i in range(N):
-    s, t, c = query[i]
-    dp[s][c] += 1
-    dp[t + 1][c] -= 1
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
 
-for i in range(1, 10 ** 5 + 2):
-    for j in range(C + 1):
-        dp[i][j] += dp[i - 1][j]
+        if x == y:
+            return
 
-ans = 0
-for i in range(10 ** 5 + 2):
-    cnt = 0
-    for j in dp[i]:
-        if j > 0:
-            cnt += 1
-    ans = max(ans, cnt)
-print(ans)
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
 
-# ABC085 D - Katana Thrower
-N, H = getNM()
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
 
-a = []
-b = []
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
 
-for i in range(N):
-  x, y = map(int, input().split())
-  a.append(x)
-  b.append(y)
+    def size(self, x):
+        return -self.parents[self.find(x)]
 
-# 振った場合の最大値
-max_a = max(a)
+    def members(self, x):
+        root = self.find(x)
+        return [i for i in range(self.n) if self.find(i) == root]
 
-ans = 0
-# 振る刀の最大攻撃力より高い攻撃力を持つ投げ刀を高い順にソートする
-# 刀iで好きなだけ振って攻撃する→気が済んだら投げることで振りの攻撃力と投げの攻撃力を
-# 両方利用することができる
-# 実は投げてしまった刀も振ることができるというルールに変更しても
-# 問題の答えは変わらない
-# 実際のムーブとしては
-# ①最も攻撃力が高い振り刀で攻撃する
-# ②一定の体力以下になると攻撃力が高い順に投げ刀で攻撃していって撃破
-# という流れになる
-for x in reversed(sorted(filter(lambda x: x >= max_a, b))):
-    H -= x
-    ans += 1
-    if H <= 0: break
+    def roots(self):
+        return [i for i, x in enumerate(self.parents) if x < 0]
 
-ans += max(0, (H + max_a - 1) // max_a)
-print(ans)
+    def all_group_members(self):
+        return {r: self.members(r) for r in self.roots()}
 
-# ABC091 C - 2D Plane 2N Points
+# ABC002 派閥
+# 条件
+# n人の国会議員の集合A{A1, A2... An}の任意の二人i, jについて
+# (i, j)がqueryに含まれる
 
-N = getN()
-# Rはループさせるのでソートさせる必要ない
-R = [getList() for i in range(N)]
-R_l = [1] * N
+# この人数nの最大値を求める
 
-B = [getList() for i in range(N)]
-B.sort()
-
-# 貪欲法でペア作りする問題
-# ABC005 C - おいしいたこ焼きの売り方の時と同様に
-# それとしか繋げないもの　を優先的に繋いでいく
-
-# 条件Aの通過が厳しい順に対象bをソートし、
-# たこ焼き　条件A:客が来る前にたこ焼きができてないといけない
-#  　　　　      客を来るのが早い順に並べる（最初から並んでる）
-# 今回     条件A:赤星のx座標が青星のx座標より小さくないといけない
-#  　　　　　　　 青星をx座標が小さい順に並べる
-
-# 条件A, 条件Bをクリアしたものの中で、最も条件Bの通過が厳しい対象aと結ぶ
-# たこ焼き　条件B:たこ焼きが賞味期限より前のものでないといけない
-#  　　　　      できるだけ古いものを売る（最初から並んでる）
-# 今回     条件B:赤星のy座標が青星のy座標より小さくないといけない
-#  　　　　　　　 条件をクリアしたもののうちでできるだけy座標が大きいものを選ぶ
-
-ans = 0
-for b in B:
-    max_y = -1
-    max_index = -1
-    for i, a in enumerate(R):
-        # x, y座標が小さいもののうちでまた使ってないもの
-        if a[0] < b[0] and a[1] < b[1] and R_l[i] == 1:
-            # あるならY座標が最も大きいもの
-            if a[1] > max_y:
-                max_y = a[1]
-                max_index = i
-    if max_y >= 0:
-        R_l[max_index] = 0
-        ans += 1
-
-# ABC100 D - Patisserie ABC
-# 8パターン全部調べる
-
-N,M = getNM()
-data = [[] for i in range(8)]
-for _ in range(N):
-    x,y,z = getNM()
-    data[0].append(x + y + z)
-    data[1].append(x + y - z)
-    data[2].append(x - y + z)
-    data[3].append(x - y - z)
-    data[4].append(- x + y + z)
-    data[5].append(- x + y - z)
-    data[6].append(- x - y + z)
-    data[7].append(- x - y - z)
-
-ans = -mod
-for i in range(8):
-    data[i].sort(reverse = True)
-    ans = max(ans,sum(data[i][:M]))
-print(ans)
-
-# ABC116 D - Various Sushi
-N, K = getNM()
-various = defaultdict(list)
-que = [getList() for i in range(N)]
-
-ans = 0
-num = []
-var_s = set()
-
-# 美味しい順にK個とった時の幸福度
-que.sort(reverse = True, key = lambda i: i[1])
-for i in range(K):
-    ans += que[i][1]
-    # もし２番手以降ならあとで交換する用にとっておく
-    if que[i][0] in var_s:
-        num.append(que[i][1])
-    var_s.add(que[i][0])
-
-var = len(var_s)
-ans += var ** 2
-
-# 使ってない種類について各種類で一番大きさが大きいもの
-left_l = defaultdict(int)
-for i in range(N):
-    if not que[i][0] in var_s:
-        left_l[que[i][0]] = max(left_l[que[i][0]], que[i][1])
-
-num.sort(reverse = True)
-left_l = [i[1] for i in left_l.items()]
-left_l.sort()
-
-# M回交換する
-opt = ans
-M = min(len(num), len(left_l))
+# 集合Aの取り方は？
+# N <= 12なのでbit全探索で全ての集合について条件を満たすか判定できる
+N, M = getNM()
+mem = set()
 for i in range(M):
-    u = num.pop()
-    s = left_l.pop()
-    # 寿司単体の幸福度
-    opt -= (u - s)
-    # 種類が増える分
-    opt += 2 * var + 1
-    var += 1
-    ans = max(opt, ans)
+    a, b = getNM()
+    mem.add((a - 1, b - 1))
 
+ans = 0
+for bit in range(1 << N):
+    # 任意のi, jについてqueryに含まれているか判定
+    flag = True
+    for i in range(N):
+        for j in range(i + 1, N):
+            # 適当に選んだ２人がbitの中に含まれていれば
+            if bit & (1 << i) and bit & (1 << j):
+                if not (i, j) in mem:
+                    flag = False
+    # もし集合bitが条件を満たすなら人数を調べる
+    if flag:
+        opt = bin(bit).count('1')
+        ans = max(ans, opt)
 print(ans)
 
-# ABC119 D - Lazy Faith
-A, B, Q = getNM()
-# 神社
-S = getArray(A)
-# 寺
-T = getArray(B)
-query = getArray(Q)
+# ABC040 D - 道路の老朽化対策について
+# 人によって通れる橋が限定される場合がある
+# クエリソートしてUnion Find
 
-S.insert(0, -float('inf'))
-T.insert(0, -float('inf'))
-S.append(float('inf'))
-T.append(float('inf'))
-
-def close(data, point):
-    west = data[bisect_left(data, point) - 1]
-    east = data[bisect_left(data, point)]
-
-    return west, east
-
+N, M = getNM()
+bridge = [getList() for i in range(M)]
+Q = getN()
+resident = []
 for i in range(Q):
-    now = query[i]
-    shrine_west, shrine_east = close(S, now)
-    temple_west, temple_east = close(T, now)
+    a, b = getNM()
+    resident.append([a, b, i])
 
-    ww = now - min(shrine_west, temple_west)
-    we_1 = (now - shrine_west) * 2 + (temple_east - now)
-    we_2 = (now - temple_west) * 2 + (shrine_east - now)
-    ee = max(shrine_east, temple_east) - now
-    ew_1 = (shrine_east - now) * 2 + (now - temple_west)
-    ew_2 = (temple_east - now) * 2 + (now - shrine_west)
+bridge.sort(reverse = True, key = lambda i:i[2])
+resident.sort(reverse = True, key = lambda i:i[1])
 
-    print(min(ww, we_1, we_2, ee, ew_1, ew_2))
+U = UnionFind(N)
+
+ans = []
+index = 0
+for i in range(Q):
+    # 建築年が新しい順に橋をかけていく
+    for j in range(index, M):
+        if bridge[j][2] > resident[i][1]:
+            a, b, c = bridge[j]
+            U.union(a - 1, b - 1)
+        else:
+            index = j
+            break
+    # U.sizeで判定
+    ans.append([resident[i][2], U.size(resident[i][0] - 1)])
+
+# 国民を登場順にソート
+ans.sort(key = lambda i: i[0])
+for i in ans:
+    print(i[1])
+
+# ABC097 D - Equals
+# 同じ島のところにしか飛べない
+N, M = getNM()
+# 1 ~ 5の並び替え
+# これを1, 2, 3, 4, 5にしたい
+P = getList()
+# Pのうちのペア
+query = []
+for i in range(M):
+    a, b = getNM()
+    query.append([a, b])
+
+U = UnionFind(N)
+for i in range(M):
+    a, b = query[i]
+    U.union(a - 1, b - 1)
+
+cnt = 0
+for i in range(N):
+    if U.same(P[i] - 1, i):
+        cnt += 1
+
+print(cnt)
+
+# ABC120 D - Decayed Bridges
+# クエリソート
+N, M = getNM()
+query = [getNM() for i in range(M)]
+
+U = UnionFind(N)
+now = cmb_list[N]
+ans = [now]
+
+for i in range(M - 1, 0, -1):
+    a, b = query[i]
+    size_a = 0
+    size_b = 0
+    if not U.same(a - 1, b - 1):
+        size_a = U.size(a - 1)
+        size_b = U.size(b - 1)
+
+        U.union(a - 1, b - 1)
+        size_after = U.size(a - 1)
+
+        now -= (cmb_list[size_after] - cmb_list[size_a] - cmb_list[size_b])
+    else:
+        U.union(a - 1, b - 1)
+    ans.append(now)
+
+for i in range(M - 1, -1, -1):
+    print(ans[i])
+
+# 各1 ~ Nに交易所を立てるのを0~Nにエッジを貼るのに見立てる
+N, M = getNM()
+edges = []
+for i in range(N):
+    c = getN()
+    edges.append((c, 0, i + 1))
+for i in range(M):
+    s, t, w = getNM()
+    edges.append((w, s, t))
+edges.sort()
+
+def kruskal(n, edges):
+    U = UnionFind(n)
+    res = 0
+    for e in edges:
+        w, s, t = e
+        if not U.same(s, t):
+            res += w
+            U.union(s, t)
+    return res
+print(kruskal(N + 1, edges))
+
+# 駐車場
+N, M, S = getNM()
+S -= 1
+dist = [[] for i in range(N)]
+for i in range(M):
+    v1, v2 = getNM()
+    v1 -= 1
+    v2 -= 1
+    v1, v2 = min(v1, v2), max(v1, v2)
+    dist[v1].append(v2)
+
+U = UnionFind(N)
+
+ans = []
+for i in range(N - 1, -1, -1):
+    # 地点iに車を駐める場合、一端がiの道は使えない
+    # → iに車を停める以前であれば,一端がiの道を使える
+    for j in dist[i]:
+        U.union(i, j)
+    if U.same(i, S):
+        ans.append(i + 1)
+ans.sort()
+for i in ans:
+    print(i)
+
+# ABC065 built?
+# xでソート、yでソートし、それぞれ
+# abs(a - b)とabs(c - d)のエッジをそれぞれ加える
+# どちらか短い方が使われる
+N = getN()
+query = []
+for i in range(N):
+    a, b = getNM()
+    query.append([a, b, i])
+
+q_a = sorted(query, key = lambda i: i[0])
+q_b = sorted(query, key = lambda i: i[1])
+edges = []
+
+a1 = q_a[0]
+b1 = q_b[0]
+for i in range(1, N):
+    a2 = q_a[i]
+    b2 = q_b[i]
+    edges.append([abs(a1[0] - a2[0]), a1[2], a2[2]])
+    edges.append([abs(b1[1] - b2[1]), b1[2], b2[2]])
+    a1, b1 = a2, b2
+edges.sort()
+
+def kruskal(n, edges):
+    U = UnionFind(n)
+    res = 0
+    for e in edges:
+        w, s, t = e
+        if not U.same(s, t):
+            res += w
+            U.union(s, t)
+        if U.size(0) == N:
+            break
+    return res
+print(kruskal(N, edges))
