edd1c6ddecfc604616ede90187b797b347636fa8
customaddone <custom1485@docomo.ne.jp>
2020-09-29 19:04:59 +0900

    F - Strivore

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -49,199 +49,295 @@ mod = 998244353
 # Main Code #
 #############
 
-# ABC006 D - トランプ挿入ソート
+# ABC008 C - コイン
 n = getN()
-lista = [getList() for i in range(n)]
+c = getArray(n)
+sumans = 0
 
-# 最長増加部分列問題 (LIS)の問題
-def lis(A):
-    L = [A[0]]
-    for a in A[1:]:
-        if a > L[-1]:
-            L.append(a)
-        # このelseに引っかかった時にトランプのソートが必要
-        else:
-            L[bisect_left(L, a)] = a
-    return len(L)
-print(n - lis(lista))
+for i in c:
+    lista = [j for j in c if i % j == 0]
+    count = len(lista)
+    sumans += math.ceil(count / 2) / count
+print(sumans)
 
-# Donutsプロコンチャレンジ2015 C - 行列のできるドーナツ屋
-# LISの応用
+# ABC011 D - 大ジャンプ
+def cmb_1(n, r):
+    r = min(n - r, r)
+    if (r < 0) or (n < r):
+        return 0
 
-N = getN()
-H = getList()
+    if n == 0:
+        return 1
 
-# まあBITだろう
-# 逆から置くBITではない？
-# 累積和？
-# 個数だけ求めればいい
+    if r == 0:
+        return 1
+    over = reduce(mul, range(n, n - r, -1))
+    under = reduce(mul, range(1, r + 1))
+    return over // under
 
-# 地点iからは
-# i - 1起点の単純増加列
-# LIS？
-ans = [0] * N
-L = []
-# 人0 ~ iまでがどのように見えるか これを人i + 1が見る
-for i in range(N - 1):
-    # もしLの一番小さいやつよりH[i]が大きければ
-    while L and L[-1] < H[i]:
-        L.pop()
-    L.append(H[i])
-    ans[i + 1] = len(L)
+N, D = getNM()
+X, Y = getNM()
+X = abs(X)
+Y = abs(Y)
 
-for i in ans:
+# X軸に平行に正の向きに飛ぶ回数はx_time + α回
+# X軸に平行に負の向きに飛ぶ回数はα回
+x_time = X // D
+y_time = Y // D
+
+if X % D != 0 or Y % D != 0 or N < x_time + y_time:
+    print(0)
+    exit()
+
+N_a = N - (x_time + y_time)
+if N_a % 2 != 0:
+    print(0)
+    exit()
+N_a //= 2
+
+ans = 0
+for i in range(N_a + 1):
+    ans += cmb_1(N, x_time + i) * cmb_1(N - (x_time + i), i) * cmb_1(N - (x_time + 2 * i), y_time + N_a - i)
+print(ans / (4 ** N))
+
+# ABC024 D - 動的計画法
+A, B, C = getArray(3)
+
+# kCc / k+1Cc = k - c + 1 / k + 1
+# k+1Cc+1 / kCc = k + 1 / c + 1
+# Xを10 ** 9 + 7 - 2乗すると逆元が求まる
+x = (C * pow(A, mod - 2, mod)) % mod
+y = (B * pow(A, mod - 2, mod)) % mod
+
+n = (x + y - 2 * x * y) * pow(x * y - x - y, mod - 2, mod)
+k = (y - x * y) * pow(x * y - x - y, mod - 2, mod)
+print((n - k) % mod, k % mod)
+
+# ABC058 D - いろはちゃんとマス目
+# 総計する
+lim = 10 ** 6 + 1
+fact = [1, 1]
+factinv = [1, 1]
+inv = [0, 1]
+
+for i in range(2, lim + 1):
+    fact.append((fact[-1] * i) % mod)
+    inv.append((-inv[mod % i] * (mod // i)) % mod)
+    # 累計
+    factinv.append((factinv[-1] * inv[-1]) % mod)
+
+def cmb(n, r):
+    if (r < 0) or (n < r):
+        return 0
+    r = min(r, n - r)
+    return fact[n] * factinv[r] * factinv[n - r] % mod
+
+H, W, A, B = getNM()
+
+mother = cmb((H - 1) + (W - 1), (W - 1))
+
+goban = cmb((H - A) + (B - 1), B - 1)
+togoal = cmb((A - 1) + (W - B - 1), (W - B - 1))
+
+for i in range(A):
+    row = H - A + i
+    col = B - 1
+    row_left = A - 1 - i
+    col_left = W - B - 1
+    mother -= cmb(row + col, row) * cmb(row_left + col_left, row_left)
+    mother %= mod
+print(mother)
+
+# ABC057 D - Maximum Average Sets
+N, A, B = getNM()
+V = sorted(getList(), reverse = True)
+
+# 1行目の答え　上からA個
+print(sum(V[:A]) / A)
+
+# Vの各要素の数を数える
+dict = defaultdict(int)
+for i in range(N):
+    dict[V[i]] += 1
+
+r = A
+n = 0
+for i in dict.items():
     print(i)
-
-# ABC134 E - Sequence Decomposing
-N = getN()
-A = getArray(N)
-
-def lis(A):
-    L = deque([A[0]])
-    for a in A[1:]:
-        # Lのどの数より小さくければ
-        # 繋げられるものがない
-        if a <= L[0]:
-            L.appendleft(a)
-        else:
-            # L[bisect_left(L, a) - 1]
-            # Lの中のa未満の数字のうちの最大値
-            L[bisect_left(L, a) - 1] = a
-    return len(L)
-
-print(lis(A))
-
-N = 10
-A = [0, 6, 9, 9, 2, 3, 4, 5, 10, 3]
-
-ans = deque([A[0]])
-for i in range(1, N):
-    # ans[index] A[i]が挟みこめる場所
-    # A[0] <= A[i]なら0になる
-    # ans[index - 1]: A[i]未満で一番大きい数字
-    index = bisect_left(ans, A[i])
-    if index == 0:
-        ans.appendleft(A[i])
+    if r - i[1] >= 0:
+        r -= i[1]
     else:
-        # 同じ数が複数ある場合は一番最後の数字が更新される
-        ans[index - 1] = A[i]
-print(len(ans))
+        n = i[1] # A個まで残りr個であり、そこには要素i[j]がn個入れられる
+        break
 
-# ABC165 F - LIS on Tree
-# 頂点1から頂点kまでの最短パス上
-# ルートはO(n)で求められる
+ans = 0
+# V[0] == V[A - 1]ならA個を超えても平均値が下がらない
+# 残りr個(合計でA個)決める、残りr + 1個(合計でA + 1個)決める...合計でB個決める
+if r > 0 and V[0] == V[A - 1]:
+    for i in range(r, B + 1):
+        ans += cmb_1(n, i)
+else:
+    ans += cmb_1(n, r) # A個まであとr個残っており、n個のうちr個選ぶ
+
+print(ans)
+
+# ABC066 D - 11
+# 普通にやれば(cmb(N + 1, i)だが、今回ダブりがある
+# 29 19 ~ 19 31 9の場合
+# [29]のうちいくつか + １番目の19 + [31, 9]のうちいくつかと
+# [29]のうちいくつか + ２番目の19 + [31, 9]のうちいくつかはダブル
+# i個要素を選ぶとすると、19を選び、外側の要素からi - 1個選ぶ全通りについてダブルので１回引く
 N = getN()
 A = getList()
-query = [getList() for i in range(N - 1)]
 
-dist = [[] for i in range(N)]
-for i in range(N - 1):
-    a, b = query[i]
-    dist[a - 1].append(b - 1)
-    dist[b - 1].append(a - 1)
+lista = [0] * (max(A) + 1)
+double = 0
+# ダブり位置1を決める
+dou_ind_2 = 0
+for i in range(N + 1):
+    if lista[A[i]] > 0:
+        double = A[i]
+        dou_ind_2 = i
+        break
+    lista[A[i]] += 1
+# ダブり位置2を決める
+dou_ind_1 = 0
+for i in range(N + 1):
+    if A[i] == double:
+        dou_ind_1 = i
+        break
 
-ignore = [0] * N
-ignore[0] = 1
-lis = [A[0]]
-rec = [0] * N
-rec[0] = 1
+outer = dou_ind_1 + (N - dou_ind_2)
 
-# 行きがけ帰りがけの要領
-def dfs(u):
-    global lis
-    for i in dist[u]:
-        if ignore[i] != 1:
-            ignore[i] = 1
-            # 巻き戻し用
-            plus = 0 # true or false
-            change = (0, 0, 0) # true or false, 変更した場所、変更した数値
-
-            if A[i] > lis[-1]:
-                lis.append(A[i])
-                plus = 1
-            else:
-                index = bisect_left(lis, A[i])
-                change = (1, index, lis[index])
-                lis[index] = A[i]
-            rec[i] = len(lis)
-            dfs(i)
-            # 巻き戻す
-            if plus:
-                lis.pop()
-            else:
-                lis[change[1]] = change[2]
-
-dfs(0)
-for i in rec:
-    print(i)
-
-# ACLC1 A - Reachable Towns
-
-N = getN()
-Q = [getList() for i in range(N)]
-que = deepcopy(Q)
-que.sort(key = lambda i:i[1], reverse = True)
-que.sort()
-
-# xy座標が共に大きいもの
-# 順列になっている？
-
-"""
-O(n**2)
-U = UnionFind(N)
-for i in range(N):
-    for j in range(i + 1, N):
-        if que[i][1] < que[j][1]:
-            U.union(i, j)
-for i in range(N):
-    print(U.count(i))
-"""
-#　ソート方法はこれでOK
-# やらなくていい探索がある　それを減らす
-# 4 3
-# 4 1
-# 4 2
-# 3 1
-# 3 2
-# 1 2 これだけいる
-
-# 4 3 1 2でi < jになるものをペアに
-# 1とペアにできるのは2, 3, 4
-# 2とペアにできるのは3, 4
-# 3は4
-# それぞれ右側にあれば
-
-# 6 7 5 3 2 4 1
-# グループ１ 6 7
-# グループ2 5
-# グループ3 3 2 4
-# グループ4 1
-# どれか１つのグループに属する
-
-U = UnionFind(N + 1)
-group = []
-
-for x, y in que:
-    # １番目のものは必ずグループのリーダーになれる
-    if not group:
-        group.append(y)
+for i in range(1, N + 2):
+    if i == 1:
+        print(N)
         continue
-    # リーダーが降順に並ぶように
-    if y < group[-1]:
-        group.append(y)
-        continue
-    opt = float('inf')
-    # グループ再編成
-    while group:
-        # yより小さいものは全てyが所属するグループに入る
-        if y > group[-1]:
-            l = group.pop()
-            U.union(l, y)
-            opt = min(opt, l) # yが所属するグループの中のリーダー　一番最初のものが記録される
+    print((cmb(N + 1, i) - cmb(outer, i - 1)) % mod)
+
+# ABC105 D - Candy Distribution
+# M人に配る mod M
+N, M = getNM()
+A = getList()
+
+alta = []
+for i in A:
+    alta.append(i % M)
+
+imos = [0]
+for i in range(N):
+    imos.append((alta[i] + imos[i]) % M)
+
+ans = 0
+for i in Counter(imos).values():
+    ans += cmb_1(i, 2)
+print(ans)
+
+# ABC136 E - Max GCD
+N, K = getNM()
+A = getList()
+
+# 操作後のAの要素全てが因数にkを持つ
+
+# 好きな回数操作を行えるとすると、Aの要素全てに因数kを持たせることができるか？
+# 操作前と操作後とではAの総和は変わらない → A = akとするとa個のkをN個の要素に配分する
+# ことでAの要素全てに因数kを持たせることができる。
+# 好きな回数操作を行える、操作後のAの全ての要素を割り切る正の整数は、sum(A)の約数になる
+
+def make_divisors(n):
+    divisors = []
+    for i in range(1, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            divisors.append(i)
+            # √nで無い数についてもう一個プラス
+            if i != n // i:
+                divisors.append(n // i)
+    return sorted(divisors)
+
+# K回以下の操作の条件付きでも題意を満たすか
+# alta % kをし、あまりが小さい順にソートする
+# 前からi番目までをプラス、i + 1番目以降をマイナスにする
+# どちらか大きい方がK以下なら条件を満たす
+
+def judger(x):
+    alta = [i % x for i in A]
+    alta.sort()
+    alta_b = [x - i for i in alta]
+    # 前からi番目までプラスにする
+    imos_f = copy.deepcopy(alta)
+    # 前からi + 1番目以降をマイナスにする
+    imos_b = copy.deepcopy(alta_b)
+
+    for i in range(N - 1):
+        imos_f[i + 1] += imos_f[i]
+        imos_b[-i - 2] += imos_b[-i - 1]
+
+    # 全部マイナス、全部プラスの場合
+    imos_f.insert(0, 0)
+    imos_b.append(0)
+
+    ans = float('inf')
+    for i in range(N + 1):
+        ans = min(ans, max(imos_f[i], imos_b[i]))
+
+    return ans
+
+for i in make_divisors(sum(A))[::-1]:
+    if judger(i) <= K:
+        print(i)
+
+# ABC154 F - Many Many Paths
+"""
+g(r,c) を 0 ≤ i ≤ r かつ 0 ≤ j ≤ c を満たす全ての整数の組 (i,j) に対する f(i,j) の総和とする。
+ここでf(r + 1, c) = f(r, c) + f(r, c - 1)...f(r, 0)
+f(r, c)からr方向へ1つ（一通り）
+f(r, c - 1)からr方向へ1つ, c方向に1つ（一通り）
+...
+つまりf(r2 + 1, c2) = f(r2, c) + f(r2, c - 1) + ... f(r2, 0)
+これをf(0, c2)からf(r2 + 1, c2)まで求めればg(r2, c2)が求まる
+"""
+r1, c1, r2, c2 = getNM()
+
+ans = 0
+for i in range(r1, r2 + 1):
+    ans = (ans + cmb(c2 + i + 1, i + 1) - cmb(c1 + i, i + 1)) % mod
+print(ans)
+
+# ABC171 F - Strivore
+"""
+dp[i][j]を
+「i文字目まででj回Sの文字を使ったか」とする
+
+K = 5
+S = 'oof'
+dp = [[0] * (len(S) + 1) for i in range(K + len(S) + 1)]
+dp[0][0] = 1
+
+for i in range(1, K + len(S) + 1):
+    for j in range(len(S) + 1):
+        if j < len(S):
+            dp[i][j] += dp[i - 1][j] * 25
         else:
-            break
-    # リーダー変更
-    group.append(opt)
+            dp[i][j] += dp[i - 1][j] * 26 # j回使い切るともうSは関係なくなるので *= 26になる
+        if j >= 1:
+            dp[i][j] += dp[i - 1][j - 1]
 
-for i in range(N):
-    print(U.count(Q[i][1])) # yがiのもののサイズの大きさ　uf.funcはインデックスで呼ばなくてもいい
+l_s = len(S)
+どのタイミングで「この先ずっと*= 26」になるか
+後ろからi文字目にSを使い切る: pow(25, K - k, mod) * cmb(N + K - k - 1, N - 1) * pow(26, k, mod)
+...
+"""
+
+K = getN()
+S = input()
+N = len(S)
+
+ans = 0
+# l_s + i文字目に文字を使い切る
+# 次から *= 26
+for k in range(K + 1):
+    # 逆からやってる
+    ans += cmb(N + K - k - 1, N - 1) * pow(26, k, mod) * pow(25, K - k, mod) % mod
+    ans %= mod
+
+print(ans)
