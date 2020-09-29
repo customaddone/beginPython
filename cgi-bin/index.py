c7e7675cce64943a89b968db2cbcb98d4ccec1d7
customaddone <custom1485@docomo.ne.jp>
2020-09-29 18:54:16 +0900

    lis

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -49,272 +49,199 @@ mod = 998244353
 # Main Code #
 #############
 
-def prime_factorize(n):
-    divisors = []
-    temp = n
-    for i in range(2, int(math.sqrt(n)) + 1):
-        if temp % i == 0:
-            cnt = 0
-            while temp % i == 0:
-                cnt += 1
-                # 素因数を見つけるたびにtempを割っていく
-                temp //= i
-            divisors.append([i, cnt])
-    if temp != 1:
-        divisors.append([temp, 1])
-    if divisors == []:
-        divisors.append([n, 1])
+# ABC006 D - トランプ挿入ソート
+n = getN()
+lista = [getList() for i in range(n)]
 
-    return divisors
-
-# ABC052 C - Factors of Factorial
-N = getN()
-
-# N!の因数 = (2の因数) + (3の因数)...
-# 約数の個数 = (因数の個数 + 1) * (因数の個数 + 1)...
-mod = 10 ** 9 + 7
-ans = 1
-# それぞれの因数となる素数の数をセットする
-dp = [0] * (N + 1)
-
-for i in range(1, N + 1):
-    for j in prime_factorize(i):
-        if j[0] > 1:
-            dp[j[0]] += j[1]
-# 約数の数:それぞれの因数の(因数の数 + 1)を掛け合わせたもの
-for i in dp:
-    if i > 0:
-        ans = (ans * (i + 1)) % mod
-print(ans % mod)
-
-# ABC090 D - Remainder Reminder
-# 数え上げ
-N, K = getNM()
-sum = 0
-for b in range(1, N + 1):
-    opt1 = (N // b) * max(0, (b - K))
-    if K == 0:
-        opt2 = N % b
-    else:
-        opt2 = max(0, (N % b) - K + 1)
-    sum += (opt1 + opt2)
-print(sum)
-
-# 094 D - Binomial Coefficients
-# combはrを真ん中に設定すると大きくなる
-
-N = getN()
-A = getList()
-A.sort()
-
-max = max(A)
-index = bisect_left(A, max / 2)
-if abs((max / 2) - A[index]) < abs((max / 2) - A[index - 1]):
-    ans = [max, A[index]]
-else:
-    ans = [max, A[index - 1]]
-print(*ans)
-
-# ABC096 D - Five, Five Everywhere
-# 素数はmod nでグルーピングできる
-N = getN()
-
-# エラストテネスの篩
-prime = [2]
-max = 55555
-limit = int(math.sqrt(max))
-data = [i + 1 for i in range(2, max, 2)]
-
-while limit > data[0]:
-    prime.append(data[0])
-    data = [j for j in data if j % data[0] != 0]
-prime = prime + data
-
-prime = sorted(prime)
-
-prime = [i for i in prime if i % 5 == 1]
-print(*prime[:N])
-
-# ABC114 D - 756
-N = getN()
-
-def prime_factorize(n):
-    divisors = []
-    # 27(2 * 2 * 7)の7を出すためにtemp使う
-    temp = n
-    for i in range(2, int(math.sqrt(n)) + 1):
-        if temp % i == 0:
-            cnt = 0
-            while temp % i == 0:
-                cnt += 1
-                # 素因数を見つけるたびにtempを割っていく
-                temp //= i
-            divisors.append([i, cnt])
-    if temp != 1:
-        divisors.append([temp, 1])
-    if divisors == [] and n != 1:
-        divisors.append([n, 1])
-
-    return divisors
-
-primli = [0] * 101
-# N! の因数を計算する
-for i in range(1, N + 1):
-    for j in prime_factorize(i):
-        primli[j[0]] += j[1]
-# 約数を75個持つとは(因数 + 1)をかけ合わせると75になるということ
-# 75 = 3 * 3 * 5なので例えば
-# (因数aが2個 + 1) * (因数bが2個 + 1) * (因数cが4個 + 1)なら約数が75個になる
-alta = []
-for i in primli:
-    if i != 0:
-        alta.append(i + 1)
-
-prim3 = 0
-prim5 = 0
-prim15 = 0
-prim25 = 0
-prim75 = 0
-for i in alta:
-    if i >= 75:
-        prim75 += 1
-    if i >= 25:
-        prim25 += 1
-    if i >= 15:
-        prim15 += 1
-    if i >= 5:
-        prim5 += 1
-    if i >= 3:
-        prim3 += 1
-
-ans = 0
-if prim3 >= 1 and prim5 >= 2:
-    # prim5 C 2
-    ans += prim5 * (prim5 - 1) // 2 * (prim3 - 2)
-if prim15 >= 1 and prim5 >= 1:
-    ans += prim15 * (prim5 - 1)
-if prim25 >= 1 and prim3 >= 1:
-    ans += prim25 * (prim3 - 1)
-if prim75 >= 1:
-    ans += prim75
-print(ans)
-
-N, M = getNM()
-A = [int(i) // 2 for i in input().split()]
-
-# 4と8の場合
-# 2 6 10 14 18...
-# 4 12 20 28... これを２で割ると
-
-# 1 3 5 7 9...
-# 2 4 10 14... 起点が偶数と奇数なため永遠に一致しない
-
-# 4と12なら
-# 2 6 10 14 18...
-# 6 18 30 42... これを２で割ると
-# 1 3 5 7 9...
-# 3 9 15 21...　になり、起点が奇数と奇数になるためどこかで一致する
-
-# Aの各要素がどれも2でn回ちょうど割れる必要がある
-def div_2(n):
-    cnt = n
-    res = 0
-    while cnt > 0:
-        if cnt % 2 == 0:
-            cnt //= 2
-            res += 1
+# 最長増加部分列問題 (LIS)の問題
+def lis(A):
+    L = [A[0]]
+    for a in A[1:]:
+        if a > L[-1]:
+            L.append(a)
+        # このelseに引っかかった時にトランプのソートが必要
         else:
-            return res
+            L[bisect_left(L, a)] = a
+    return len(L)
+print(n - lis(lista))
 
-def lcm(x, y):
-    return x * (y // gcd(x, y))
+# Donutsプロコンチャレンジ2015 C - 行列のできるドーナツ屋
+# LISの応用
 
-judge = [div_2(i) for i in A]
+N = getN()
+H = getList()
 
-if min(judge) != max(judge):
-    print(0)
-    exit()
-L = 1
-for i in range(N):
-    L = lcm(L, A[i])
+# まあBITだろう
+# 逆から置くBITではない？
+# 累積和？
+# 個数だけ求めればいい
 
-# Ai * 0.5, Ai * 1, Ai * 1.5...の個数 - Ai * 1, Ai * 2...の個数
-print(M // L - M // (2 * L))
+# 地点iからは
+# i - 1起点の単純増加列
+# LIS？
+ans = [0] * N
+L = []
+# 人0 ~ iまでがどのように見えるか これを人i + 1が見る
+for i in range(N - 1):
+    # もしLの一番小さいやつよりH[i]が大きければ
+    while L and L[-1] < H[i]:
+        L.pop()
+    L.append(H[i])
+    ans[i + 1] = len(L)
 
-# ABC152 E - Flatten
-# 大きい数は因数で持つ
+for i in ans:
+    print(i)
+
+# ABC134 E - Sequence Decomposing
+N = getN()
+A = getArray(N)
+
+def lis(A):
+    L = deque([A[0]])
+    for a in A[1:]:
+        # Lのどの数より小さくければ
+        # 繋げられるものがない
+        if a <= L[0]:
+            L.appendleft(a)
+        else:
+            # L[bisect_left(L, a) - 1]
+            # Lの中のa未満の数字のうちの最大値
+            L[bisect_left(L, a) - 1] = a
+    return len(L)
+
+print(lis(A))
+
+N = 10
+A = [0, 6, 9, 9, 2, 3, 4, 5, 10, 3]
+
+ans = deque([A[0]])
+for i in range(1, N):
+    # ans[index] A[i]が挟みこめる場所
+    # A[0] <= A[i]なら0になる
+    # ans[index - 1]: A[i]未満で一番大きい数字
+    index = bisect_left(ans, A[i])
+    if index == 0:
+        ans.appendleft(A[i])
+    else:
+        # 同じ数が複数ある場合は一番最後の数字が更新される
+        ans[index - 1] = A[i]
+print(len(ans))
+
+# ABC165 F - LIS on Tree
+# 頂点1から頂点kまでの最短パス上
+# ルートはO(n)で求められる
 N = getN()
 A = getList()
-prime_list = defaultdict(int)
+query = [getList() for i in range(N - 1)]
 
-for i in range(N):
-    prime = prime_factorize(A[i])
-    for j in prime:
-        prime_list[j[0]] = max(prime_list[j[0]], j[1])
+dist = [[] for i in range(N)]
+for i in range(N - 1):
+    a, b = query[i]
+    dist[a - 1].append(b - 1)
+    dist[b - 1].append(a - 1)
 
-num = 1
-for key, value in prime_list.items():
-    num *= key ** value
-    num %= mod
+ignore = [0] * N
+ignore[0] = 1
+lis = [A[0]]
+rec = [0] * N
+rec[0] = 1
 
-# 1/A[i]のmod
-lim = 10 ** 6 + 1
-fact = [1, 1]
-inv = [0, 1]
+# 行きがけ帰りがけの要領
+def dfs(u):
+    global lis
+    for i in dist[u]:
+        if ignore[i] != 1:
+            ignore[i] = 1
+            # 巻き戻し用
+            plus = 0 # true or false
+            change = (0, 0, 0) # true or false, 変更した場所、変更した数値
 
-for i in range(2, lim + 1):
-    fact.append((fact[-1] * i) % mod)
-    inv.append((-inv[mod % i] * (mod // i)) % mod)
+            if A[i] > lis[-1]:
+                lis.append(A[i])
+                plus = 1
+            else:
+                index = bisect_left(lis, A[i])
+                change = (1, index, lis[index])
+                lis[index] = A[i]
+            rec[i] = len(lis)
+            dfs(i)
+            # 巻き戻す
+            if plus:
+                lis.pop()
+            else:
+                lis[change[1]] = change[2]
 
-ans = 0
-for i in A:
-    opt = (num * inv[i]) % mod
-    ans += opt
-    ans %= mod
-print(ans % mod)
+dfs(0)
+for i in rec:
+    print(i)
 
-# ABC161 F - Division or Subtraction
+# ACLC1 A - Reachable Towns
+
 N = getN()
+Q = [getList() for i in range(N)]
+que = deepcopy(Q)
+que.sort(key = lambda i:i[1], reverse = True)
+que.sort()
 
-# 手順としては
-# ①　kで出来るだけ割る
-# ②　kで引いていく　N = mk + d(d = 1, 2, 3...)とすると,　引いて残る数はm(k - 1) + d
-# つまりkで割り切れず、引いても引いても永遠に①に戻ることはない
+# xy座標が共に大きいもの
+# 順列になっている？
 
-# N = k ** i * (mk + 1)となるkの数を求める
-# i == 0の時
-# kがなんであれk ** iは１になるので
-# N = mk + 1、つまりN - 1がkの倍数であればそのkは条件を満たす
-# N - 1の約数（１以外）が候補
+"""
+O(n**2)
+U = UnionFind(N)
+for i in range(N):
+    for j in range(i + 1, N):
+        if que[i][1] < que[j][1]:
+            U.union(i, j)
+for i in range(N):
+    print(U.count(i))
+"""
+#　ソート方法はこれでOK
+# やらなくていい探索がある　それを減らす
+# 4 3
+# 4 1
+# 4 2
+# 3 1
+# 3 2
+# 1 2 これだけいる
 
-ans = set()
-for i in make_divisors(N - 1):
-    if i != 1:
-        ans.add(i)
+# 4 3 1 2でi < jになるものをペアに
+# 1とペアにできるのは2, 3, 4
+# 2とペアにできるのは3, 4
+# 3は4
+# それぞれ右側にあれば
 
-# 割れるだけ割る関数
-def dividor(x, k):
-    if k == 1:
-        return 0
-    n = x
-    while True:
-        if n % k == 0:
-            n //= k
+# 6 7 5 3 2 4 1
+# グループ１ 6 7
+# グループ2 5
+# グループ3 3 2 4
+# グループ4 1
+# どれか１つのグループに属する
+
+U = UnionFind(N + 1)
+group = []
+
+for x, y in que:
+    # １番目のものは必ずグループのリーダーになれる
+    if not group:
+        group.append(y)
+        continue
+    # リーダーが降順に並ぶように
+    if y < group[-1]:
+        group.append(y)
+        continue
+    opt = float('inf')
+    # グループ再編成
+    while group:
+        # yより小さいものは全てyが所属するグループに入る
+        if y > group[-1]:
+            l = group.pop()
+            U.union(l, y)
+            opt = min(opt, l) # yが所属するグループの中のリーダー　一番最初のものが記録される
         else:
             break
-    return n
+    # リーダー変更
+    group.append(opt)
 
-# i >= 1の時
-# 候補はNの約数
-for prim in make_divisors(N):
-    if prim == 1:
-        continue
-    # Nを割れるだけ割る
-    alta = dividor(N, prim)
-    if alta == 1:
-        ans.add(prim)
-        continue
-    if alta >= prim and alta % prim == 1:
-        ans.add(prim)
-
-print(len(ans))
+for i in range(N):
+    print(U.count(Q[i][1])) # yがiのもののサイズの大きさ　uf.funcはインデックスで呼ばなくてもいい
