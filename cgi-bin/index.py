9cdf7adc0e6f09b6eda69d51bf74c7d1fb5112e9
customaddone <custom1485@docomo.ne.jp>
2020-10-02 22:02:11 +0900

    木dp

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -49,264 +49,156 @@ mod = 10 ** 9 + 7
 # Main Code #
 #############
 
-# 基本の二分探索
-lista = [i for i in range(10)]
+# ABC036 D - 塗り絵
+# 木dp
+# 葉からボトムアップか頂点からdfs
 
-def binary_search_loop(data, target):
-    imin = 0
-    imax = len(data) - 1
-    while imin <= imax:
-        imid = imin + (imax - imin) // 2
-        if target == data[imid]:
-            return imid
-        elif target < data[imid]:
-            imax = imid - 1
-        else:
-            imin = imid + 1
-    return False
-print(binary_search_loop(lista, 4))
-
-# 三分探索
-# ARC054 ムーアの法則
-def f(x):
-    return x + p / pow(2, 2 * x / 3)
-
-p = float(input())
-left, right = 0, 100
-
-while right > left + 10 ** -10:
-    # mid二つ
-    mid1 = (right * 2 + left) / 3
-    mid2 = (right + left * 2) / 3
-    if f(mid1) >= f(mid2):
-        right = mid1
-    else:
-        left = mid2
-print(f(right))
-
-# 三分探索整数ver
-num = []
-for i in range(100):
-    if i < 50:
-        num.append(i)
-    else:
-        num.append(100 - i)
-
-left, right = 0, len(num) - 1
-while abs(right - left) > 3:
-    mid1 = (right * 2 + left) // 3 + 1
-    mid2 = (right + left * 2) // 3
-    # 最小値を求める場合は矢印逆になる
-    if num[mid1] <= num[mid2]:
-        right = mid1
-    else:
-        left = mid2
-print(right)
-print(left)
-
-# ARC050 B - 花束
-R, B = getNM()
-x, y = getNM()
-# 赤い花束をr束, 青い花束をb束とすると
-# R >= xr + b
-# B >= r + yb
-# を満たしながらk = r + bを最大化せよ
-# r = k - b
-# R >= x(k - b) + b = xk - (x - 1)b
-# B >= (k - b) + yb = k + (y - 1)b
-# (y - 1)R >= (y - 1)xk - (x - 1)(y - 1)b
-# (x - 1)B >= (x - 1)k + (x - 1)(y - 1)b
-# (y - 1)R + (x - 1)B >= ((y - 1)x + (x - 1))k
-# 二分探索?
-
-def judge(k):
-    # あるkを決めた時に
-
-    # ①r >= 0, b >= 0
-    # ②r + b = k
-    # ③R >= xr + b = (x - 1)b
-    # ④B >= r + yb = (y - 1)b
-    # となるr, bが存在するか
-
-    # R - k >= (x - 1)r
-    # (R - k) / (x - 1) >= r
-    # B - k >= (y - 1)b
-    # (B - k) / (y - 1) >= b
-    # (R - k) // (x - 1) + (B - k) // (y - 1) >= kになるか
-    if R - k >= 0 and B - k >= 0 and (R - k) // (x - 1) + (B - k) // (y - 1) >= k:
-        return True
-    else:
-        return False
-
-ok = -1
-ng = 10 ** 18 + 1
-
-while abs(ok - ng) > 1:
-    mid = (ok + ng) // 2
-    if judge(mid):
-        ok = mid
-    else:
-        ng = mid
-print(ok)
-
-# AGC041 B - Voting Judges
-
-"""
-# 問題:N問、ジャッジ:M人
-# M人のジャッジがそれぞれV問を選び、問題のスコアを１ずつあげる
-# M人の投票の後、大きい方からP問が選ばれる
-# 問題セットに選ばれる可能性があるのは何問あるか
-M人全員が投票すれば選ばれやすくなる
-選ばれるとは？
-可能性がないものを数えた方が早いのでは
-P番目以内にあれば無条件で通過
-現在のP番目 <= A[i] + M
-V <= Pなら
-上からP - 1番目までのどれか + A[i]を加算させることでA[i]を強くできる　
-V > Pなら？
-上からP - 1番目までとA[i]を強化するとして、残りのV - P個は小さいものから順に選ぶ
-A[i]を抜かせないようにしたい
-A[i]より大きい数字も一緒に足される場合にはP以内に入れない
-"""
-N, M, V, P = getNM()
-A = getList()
-A.sort(reverse = True)
-
-def judge(x):
-    if x < P:
-        return True
-    if A[P - 1] > A[x] + M:
-        return False
-    # P - 1番目まで + 自身以降の数字についてはM個足す
-    left = (V - (P - 1) - (N - x)) * M
-    # P個目からx-1まで A[x] + Mを超えない分足す
-    for i in range(P - 1, x):
-        left -= A[x] + M - A[i]
-
-    return left <= 0
-
-ok = -1
-ng = N
-
-while abs(ok - ng) > 1:
-    mid = (ok + ng) // 2
-    if judge(mid):
-        ok = mid
-    else:
-        ng = mid
-print(ok + 1)
-
-# ABC023 D - 射撃王
 N = getN()
-listh = []
-lists = []
+query = [getList() for i in range(N - 1)]
+
+dist = [[] for i in range(N)]
+for i in range(N - 1):
+    a, b = query[i]
+    dist[a - 1].append(b - 1)
+    dist[b - 1].append(a - 1)
+
+dp = [[0, 0] for i in range(N)]
+sta = 0
 for i in range(N):
-    h, s = getNM()
-    listh.append(h)
-    lists.append(s)
-left = 0
-right = 10 ** 15
+    if len(dist[i]) == 1:
+        sta = i
+        break
 
-for _ in range(50):
-    flag = True
-    mid = (left + right) // 2
-    costtime = [0] * N
-    for i in range(N):
-        costtime[i] = (mid - listh[i]) / lists[i]
-    costtime.sort()
-    for i in range(N):
-        if costtime[i] - i < 0:
-            flag = False
-    if flag:
-        right = mid
-    else:
-        left = mid
-print(right)
+for i in range(N):
+    if len(dist[i]) == 1 and i != sta:
+        dp[i] = [1, 1]
 
-# ABC034 D - 食塩水
+ignore = [0] * N
+ignore[sta] = 1
+def dfs(now):
+    white = 1
+    black = 1
+    for i in dist[now]:
+        if ignore[i] != 1:
+            ignore[i] = 1
+            w_cnt, b_cnt = dfs(i)
+            white = (white * (w_cnt + b_cnt)) % mod
+            black = (black * w_cnt) % mod
+    dp[now] = [white % mod, black % mod]
+    return dp[now]
+
+print(sum(dfs(sta)) % mod)
+
+# ABC133 E - Virus Tree 2
+# 木dp
+
+lim = 10 ** 6 + 1
+fact = [1, 1]
+factinv = [1, 1]
+inv = [0, 1]
+
+for i in range(2, lim + 1):
+    fact.append((fact[-1] * i) % mod)
+    inv.append((-inv[mod % i] * (mod // i)) % mod)
+    # 累計
+    factinv.append((factinv[-1] * inv[-1]) % mod)
+
+def cmb(n, r):
+    if (r < 0) or (n < r):
+        return 0
+    r = min(r, n - r)
+    return fact[n] * factinv[r] * factinv[n - r] % mod
+    
+N, K = getNM()
+query = [getList() for i in range(N - 1)]
+
+dist = [[] for i in range(N)]
+for a, b in query:
+    dist[a - 1].append(b - 1)
+    dist[b - 1].append(a - 1)
+
+max_root = 0
+max_root_index = 0
+for i in range(N):
+    if len(dist[i]) >= max_root:
+        max_root = len(dist[i])
+        max_root_index = i
+
+pos = deque([max_root_index])
+
+ans = 1
+ignore = [-1] * N
+ignore[max_root_index] = 1
+ans *= cmb(K, max_root + 1) * math.factorial(max_root + 1)
+
+while len(pos) > 0:
+    u = pos.popleft()
+    for i in dist[u]:
+        if ignore[i] == -1:
+            ignore[i] = 1
+            if len(dist[i]) >= 2:
+                ans *= cmb(K - 2, len(dist[i]) - 1) * math.factorial(len(dist[i]) - 1)
+                ans %= mod
+            pos.append(i)
+
+print(ans % mod)
+
+# dfs
 
 N, K = getNM()
-query = [getList() for i in range(N)]
+dist = [[] for i in range(N)]
+for i in range(N - 1):
+    a, b = getNM()
+    dist[a - 1].append(b - 1)
+    dist[b - 1].append(a - 1)
 
-def judge(target):
-    alta = []
-    for i in range(N):
-        salt = query[i][0] * (query[i][1] - target)
-        alta.append(salt)
-    alta.sort(reverse = True)
-    return sum(alta[:K]) >= 0
+# 木dp的な
+# 上から子ノードの塗り分け方、その子ノードの塗り分け方...をかけていく
+def dfs(now, root): # 子ノードの塗り分け方は親がいればK-2Pr通り
+    res = 1
+    for i in dist[now]:
+        if i != root:
+            res *= dfs(i, now)
+            res %= mod
 
-left = -1
-right = 101
+    vari = (root != -1) + 1 # 親 + 自身 roots[u] == -1 なら親なし
+    r = len(dist[now]) - (root != -1)
+    res *= cmb(K - vari, r) * math.factorial(r)
 
-for i in range(100):
-    mid = left + (right - left) / 2
-    if judge(mid):
-        left = mid
-    else:
-        right = mid
-print(left)
+    return res % mod
 
-# ABC063 D - Widespread
-# 最小で何回の　二分探索
-# 通常攻撃time回を全員に食らわせる→爆発time回を食らわせる
-# 魔物は生き残ることができるか
-N, A, B = getNM()
-H = getArray(N)
+print((dfs(0, -1) * K) % mod)
 
-def judge(time, main, sub, hp):
-    diff = main - sub
-    cnt = 0
-    for i in range(len(hp)):
-        left = hp[i] - time * sub
-        if left > 0:
-            cnt += (left + diff - 1) // diff
+# ABC146 D - Coloring Edges on Tree
+N = getN()
 
-    return cnt
+dist = [[] for i in range(N)]
+edges = {}
+for i in range(N - 1):
+    a, b = getNM()
+    dist[a - 1].append(b - 1)
+    dist[b - 1].append(a - 1)
+    edges[(a - 1, b - 1)] = i
+    edges[(b - 1, a - 1)] = i
 
-ok = 10 ** 12 + 1
-ng = -1
+color = [-1] * N
+color[0] = 0
+ans = [0] * (N - 1)
+pos = deque([0])
 
-while ok - ng > 1:
-    mid = (ok + ng) // 2
-    opt = judge(mid, A, B, H)
+while pos:
+    u = pos.popleft()
+    j = 1
+    for i in dist[u]:
+        if color[i] != -1:
+            continue
+        if j == color[u]:
+            j += 1
+        color[i] = j
+        ans[edges[(i, u)]] = j
+        pos.append(i)
+        j += 1
 
-    # mid:仮のの回数
-    # opt:midを定めた時必要な爆発の回数
-    # 仮の回数が必要な回数より多ければokを緩和
-    if mid >= opt:
-        ok = mid
-    else:
-        ng = mid
-print(ok)
-
-# 範囲指定してエッジを引く最短路問題は大体セグ木
-# seg解
-# M = 3の時、1から2, 3, 4に行ける
-# dp[i]: iまで行く最短の通り
-# dp[i] = seg.query(i - M, i) + 1
-# dpが出来上がったらdp = [1, 1, 2, inf, inf, 3, 3...]みたいなのを
-# 1がある位置、2がある位置...[[0, 1], [2], [5, 6]...]としていき各子要素の一番最初の数字をとる
-
-# 現在地点 - Mまでで最も大きく戻れる地点を探す
-# 最も大きく戻っていけば最初の1 ~ i間を最小にできる
-N, M = getNM()
-S = input()
-opt = []
-for i in range(N + 1):
-    if S[i] == '0':
-        opt.append(i)
-
-now = opt.pop()
-ans = []
-while opt:
-    index = bisect_left(opt, now - M) # 現在地点 - Mまでで最も大きく戻れる地点を探す
-    if index == len(opt): # なければ-1
-        print(-1)
-        exit()
-    ans.append(now - opt[index])
-    while len(opt) - 1 != index: # indexの場所まで掘る
-        opt.pop()
-    now = opt.pop()
-
-print(*ans[::-1])
+print(max(ans))
+for i in ans:
+    print(i)
