e63cfe1e99690f292a758ef1e6deb37616b38392
customaddone <custom1485@docomo.ne.jp>
2020-10-20 21:40:15 +0900

    dfs

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -49,224 +49,248 @@ mod = 10 ** 9 + 7
 # Main Code #
 #############
 
-# NOMURA プログラミングコンテスト 2020 C - Folia
-"""
-N = 3
-A = [0, 1, 1, 2]の場合
-葉の数を求める
-まず完全二分木から考える
-ここから取り除く
-深さ3の葉は4つある
-深さ4の葉は8つある
-深さnの葉は2 ** (n - 1)つある
-
-深さ3の葉は4つある完全二分木について
-A = [0, 0, 0, 4]
-深さ3の葉を一つ刈ると
-A = [0, 0, 0, 3]
-二つ刈ってみる　この時、１つ目と同じ親のを刈ると
-A = [0, 0, 1, 2]
-違うのを刈ると
-A = [0, 0, 0, 2]
-根となる頂点を１つ作る
-A[0] = 1ならその頂点は葉（下に頂点を繋げない)
-A[0] = 0ならその頂点は生きる
-
-頂点の最大値を求めるなら
-なるべく大きく分岐させた方がいい
-エッジ貼らなくても頂点数求めるだけでいい
-A[i]を探索するたびに ans += する
-
-現在用意している仮の頂点数を保持しておく
-確定させた頂点数も抑えておく
-
-A = [0, 0, 1, 0, 2]を逆から見ると
-psuedo = [1, 2, 4, 8, 16]
-psuedoのそれぞれとA[4]どちらか小さい方をpsuedoから引く
-psuedo = [0, 0, 2, 6, 14]
-
-left[0] ~ left[i]のそれぞれでA[i]を引けるだけ引く
-順に見ると
-psuedo = []
-left = [] # ansに加える値
-A[0] = 0
-psuedo = [1]
-left = [1]
-A[1] = 0
-psuedo = [1, 2]
-left = [1, 2]
-A[2] = 1
-psuedo = [1, 2, 3]
-left = [0, 1, 3]
-A[3] = 0
-psuedo = [1, 2, 3, 6]
-left = [0, 1, 3, 6]
-A[4] = 2
-psuedo = [1, 2, 3, 6, 10]
-left = [0, 0, 1, 4, 10]
-
-葉にならない点の上限を考える
-"""
-
-# 根から探索するか
-# 葉から探索するか
-
-# 私は根から
-# 総和には累積和が効く
+# ABC114 C - 755
 N = getN()
-A = getList()
+rength = len(str(N))
+numlist = [3, 5, 7]
+cnt = 0
 
-# 深さ0の二分木の場合
-if N == 0:
-    if A[0] == 1:
-        print(1)
+def sevfivthr(i, strint):
+    global cnt
+    if i == rength:
+        return
     else:
-        print(-1)
-    exit()
+        for num in numlist:
+            newstr = strint + str(num)
+            if ('3' in newstr) and ('5' in newstr) and ('7' in newstr):
+                if int(newstr) <= N:
+                    cnt += 1
+            sevfivthr(i + 1, newstr)
+for i in numlist:
+    sevfivthr(1, str(i))
+print(cnt)
 
-if A[0] == 0:
-    psuedo = [1]
-    left = [1] # 確定させる用
-else:
-    psuedo = [0]
-    left = [0]
+# ABC115 D - Christmas
+# レベルNバーガーの下からX層目まで
+N, X = getNM()
 
-alta = deepcopy(A)
-for i in range(N - 1, -1, -1):
-    alta[i] += alta[i + 1]
-ma = max(alta)
+# レベルNバーガーの中間地点、全体のサイズ
+cnt_burger = [[0 for i in range(2)] for i in range(51)]
+cnt_burger[0] = [1, 1]
+for i in range(1, 51):
+    cnt_burger[i][0] = 1 + cnt_burger[i - 1][1] + 1
+    cnt_burger[i][1] = cnt_burger[i][0] + cnt_burger[i - 1][1] + 1
 
-# i + 1番目について調べる
-for i in range(1, N + 1):
-    opt = min(psuedo[-1] * 2, ma) # stop指数爆発 今回max(alta)以上の数字は必要ない
-    psuedo.append(opt)
-    left.append(opt)
-    if psuedo[-1] - A[i] < 0:
-        print(-1)
-        exit()
-    psuedo[-1] -= A[i]
-    # 確定させていく
-    """
-    明らか追いつかないので累積する
-    for j in range(i, -1, -1):
-        if dete[j] == 0:
-            break
-        add = min(dete[j], A[i])
-        ans += add
-        dete[j] -= add
-    """
+# レベルNバーガーにパティが何枚含まれる？
+cnt_patty = [0] * 51
+cnt_patty[0] = 1
+for i in range(1, 51):
+    cnt_patty[i] = 2 * cnt_patty[i - 1] + 1
 
-ans = 0
+# レベルNの下からX番目までにパティが何枚含まれるか
+# xが大きいのでdpはできない
+def count(n, x):
+    # レベル0バーガーの場合
+    if n == 0 and x == 1:
+        return 1
 
-for i in range(N + 1):
-    ans += min(left[i], alta[i])
-print(ans)
+    # バーガーの一番下のパンのみ食べる場合
+    if x == 1:
+        return 0
 
-# ARC011 ダブレット
-# つまり最短経路問題
-s1, s2 = input().split(' ')
+    # 中間地点以前のどこかまで食べる場合
+    elif 1 < x < cnt_burger[n][0]:
+        # レベルn - 1バーガーの下からx - 1層目まで
+        return count(n - 1, x - 1)
+
+    # 中間地点まで食べる
+    elif x == cnt_burger[n][0]:
+        return cnt_patty[n - 1] + 1
+
+    # 中間地点 ~ 最後以前のうちのどこか
+    elif cnt_burger[n][0] < x < cnt_burger[n][1]:
+        return cnt_patty[n - 1] + 1 + count(n - 1, x - cnt_burger[n][0])
+
+    # 最後
+    else:
+         return 2 * cnt_patty[n - 1] + 1
+
+print(count(N, X))
+
+# ABC122 D - We Like AGC
 N = getN()
-S = set()
+# 文字の個数を求める
+# 755を思い出す
+# dfsする
 
-S.add(s1)
-S.add(s2)
+memo = [{} for i in range(N + 1)]
 
-for i in range(N):
-    S.add(input())
+# 判定用
+def ok(last4):
+    # 1234, 2134, 1324, 1243を調べる
+    for i in range(4):
+        t = list(last4)
+        if i >= 1:
+            t[i - 1], t[i] = t[i], t[i - 1]
+        if ''.join(t).count('AGC') >= 1:
+            return False
+    return True
 
-if s1 == s2:
-    print(0)
-    print(s1)
-    print(s2)
-    exit()
+# 文字を伸ばしていく
+# memo[cur][last3]:cur文字目まで決定しており、そのラスト3文字がlast3のもの
+# 判定に必要なのは後ろ3文字 + ['A', 'G', 'C', 'T']
+def dfs(cur, last3):
+    if last3 in memo[cur]:
+        return memo[cur][last3]
+    # 全部決定したら
+    if cur == N:
+        return 1
 
-S = list(S)
-N = len(S)
+    res = 0
+    # last3に['A', 'G', 'C', 'T']をくっつけてみる
+    for s in 'AGCT':
+        if ok(last3 + s):
+            res = (res + dfs(cur + 1, last3[1:] + s)) % mod
+    memo[cur][last3] = res
+    return res
 
-# 2つにエッジを貼れるか
-def judge(s1, s2):
-    cnt = 0
-    n = len(s1)
-    for i in range(n):
-        if s1[i] != s2[i]:
-            cnt += 1
-    if cnt <= 1:
-        return True
-    else:
-        return False
+print(dfs(0, 'TTT'))
 
-dist = [[] for i in range(N)]
-d = [[float('inf')] * N for i in range(N)]
-for i in range(N):
-    for j in range(i + 1, N):
-        if judge(S[i], S[j]):
-            dist[i].append(j)
-            dist[j].append(i)
-            d[i][j] = 1
-            d[j][i] = 1
+# AGC009 B - Tournament
 
-sta = 0
-end = 0
-for i in range(N):
-    if S[i] == s1:
-        sta = i
-        break
-for i in range(N):
-    if S[i] == s2:
-        end = i
-        break
+N = getN()
+A = [-1, -1] + getArray(N - 1)
 
-# ダイクストラする
-def dij(start, edges):
-    dist = [float('inf') for i in range(N)]
-    dist[start] = 0
-    pq = [(0, start)]
+"""
+トーナメントの深さの最小値を求めよ
+何もなければbitの長さになる
+a2...anは人aiとの試合で負けた
+A = [1, 1, 2, 4]の場合
+1が優勝（デフォルト）
+2は1との試合で負けました
+3は1との試合で負けました
+4は2との試合で負けました
+5は4との試合で負けました
+1は2, 3と戦った 2, 3に勝った
+2は1, 4と戦った 4に勝った
+3は1と戦った
+4は2, 5と戦った 5に勝った
+5は4と戦った
+4がネックになりそう
+4をいい感じに配置して
+1から辿る
+1は3に勝った 深さは
+各頂点をrootとした部分木のサイズを求める
+A = [1, 2, 1, 3, 1, 4]の場合
+1の相手は2, 4, 6 部分木の深さは4
+葉から求める
+5の部分木のサイズは1(葉なので)
+7の部分木のサイズは1(葉なので)
+3の部分木のサイズは最小で子要素の数n + 1(自分)
+①3の子要素を逆順にソートする
+②子要素の深さはn, n-1...1まで許容される　越えたら求める部分木のサイズが大きくなる
+選手iの各相手(子要素)が[r1, r2, r3...]だとする
+これらを選手iが戦った試合だけを見たトーナメント表上に配置する
+深さの最小mi_resは子要素の数n + 1(自分)
+ただし1回戦で戦った相手の部分木の深さがn1ならmi_res = min(mi_res, n1 - 1)
+     2回戦で戦った相手の部分木の深さがn2ならmi_res = min(mi_res, n2 - 2)
+     ...
+と更新されていく
+つまり深さが深い相手を後ろの方に配置した方がお得
+"""
 
-    # pqの先頭がgoal行きのものなら最短距離を返す
-    while len(pq) > 0:
-        di, now = heapq.heappop(pq)
-        if (di > dist[now]):
-            continue
-        for i in edges[now]:
-            if dist[i] > dist[now] + d[i][now]:
-                dist[i] = dist[now] + d[i][now]
-                heapq.heappush(pq, (dist[i], i))
-    return dist
+dist = [[] for i in range(N + 1)]
+for i in range(2, N + 1):
+    dist[A[i]].append(i)
 
-distance = dij(sta, dist)
-if distance[end] == float('inf'):
-    print(-1)
-    exit()
+def depth_cnt(x):
+    l = []
+    for i in dist[x]:
+        l.append(depth_cnt(i))
+    l.sort(reverse = True) # xが最後に勝った相手に深さが一番深いやつを持ってくるとお得
+    mi_depth = len(l)
+    res = mi_depth
+    for i in range(mi_depth):
+        # xが最後に勝った相手、xが最後から2番目に勝った相手...
+        # 今回は逆にやる
+        res = max(res, l[i] + i)
+    return res + 1 # 部分木のサイズは最小で子要素の数n + 1(自分)
 
-# 最短経路を示す矢印を求める
-# ダイクストラと同じ要領で
-def router(n, sta):
-    pos = deque([sta])
-    ignore = [0] * n
-    path = [0] * n
-    ignore[sta] = 0
-    path[sta] = -1
+print(depth_cnt(1) - 1)
 
-    while pos:
-        u = pos.popleft()
+# AGC044 A - pay to win
 
-        for i in dist[u]:
-            if ignore[i] != 1 and distance[i] == ignore[u] + d[i][u]:
-                path[i] = u
-                ignore[i] = ignore[u] + d[i][u]
-                pos.append(i)
+"""
+あなたは 0 という数を持っており
+A(*= 2)
+B(*= 3)
+C(*= 5)
+D(+=1, -=1)
+Dは最初に一回する
+Dはいつやればお得？ 最小に行った方がお得
 
-    return path
+指数は爆発するのでそんなに数は大きくならない
+1 → 2 (B or D)
+1 → 3 (C or D)
 
-path = router(N, sta)
-ans = [S[end]]
-now = end
-while True:
-    now = path[now]
-    ans.append(S[now])
-    if now == sta:
-        break
+Nについて2, 3, 5で出来るだけ割って見るか
+Dを最初に少々足して
+2, 3, 5を出来るだけ積んでNの近辺を目指せ
+Dをどこまで少々足せばいいか
 
-print(len(ans) - 2)
-for i in range(len(ans)):
-    print(ans[-i - 1])
+小さい数で試してみる
+11 は　12 - 1だったり 10 + 1だったりする
+着地場所の候補は6(11 // 6 + 1) ~ 21
+もし因数に2, 3, 5がなければDのみで行くことになる　←　
+途中でいい具合にDをするといい感じになるが
+dfs(x) xに着地する時の最小コスト
+メモdfsでなんとかできない？
+
+着地点の範囲は
+N - 5 ~ N + 5ぐらいでいいのでは
+
+N から 0を目指す
+上下の直近の2,3,5の倍数まで移動してから割り算で移動するような遷移しか考える必要がない
+13 なら 14, 6
+分岐先はそんなに多くない
+"""
+
+T = getN()
+
+for _ in range(T):
+    n, A, B, C, D = getNM()
+    memo = {}
+
+    def solve(N):
+        if N in memo:
+            return memo[N]
+        if N == 0:
+            return 0
+        if N == 1:
+            return D # Dを使う
+
+        tmp = D * N # 最初の候補、全てDで
+
+        if N % 2 == 0: # 割り切れるならそうすればいい
+            tmp = min(tmp, solve(N // 2) + A)
+        else:
+            tmp = min(tmp, solve(N // 2) + A + D, solve(N // 2 + 1) + A + D)
+
+        if N % 3 == 0:
+            tmp = min(tmp, solve(N // 3) + B)
+        else:
+            d = N % 3
+            u = 3 - d
+            tmp = min(tmp, solve(N // 3) + B + d * D, solve(N // 3 + 1) + B + u * D)
+
+        if N % 5 == 0:
+            tmp = min(tmp, solve(N // 5) + C)
+        else:
+            d = N % 5
+            u = 5 - d
+            tmp = min(tmp, solve(N // 5) + C + d * D, solve(N // 5 + 1) + C + u * D)
+
+        memo[N] = tmp
+        return tmp
+
+    print(solve(n))
