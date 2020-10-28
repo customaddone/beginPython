e170b755550d0d1dd5bf6caaea87f700deba3a40
customaddone <custom1485@docomo.ne.jp>
2020-10-28 22:37:47 +0900

    とぽそ

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -49,175 +49,140 @@ mod = 10 ** 9 + 7
 # Main Code #
 #############
 
-# ABC007 幅優先探索
-r, c = map(int, input().split())
-sy, sx = map(int, input().split())
-gx, gy = map(int, input().split())
-sy -= 1
-sx -= 1
-gx -= 1
-gy -= 1
+# https://qiita.com/Morifolium/items/6c8f0a188af2f9620db2
+N = 8
 
-maze = []
-ans = float('inf')
-
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
-
-pos = deque([[sx, sy, 0]])
-dp = [[-1] * (c + 1) for i in range(r + 1)]
-dp[sx][sy] = 0
-
-for i in range(r):
-    c = input()
-    maze.append(list(c))
-
-while len(pos) > 0:
-    x, y, depth = pos.popleft()
-    if x == gx and y == gy:
-        break
-    maze[x][y] = '#'
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if maze[nx][ny] == "." and dp[nx][ny] == -1:
-            pos.append([nx, ny, depth + 1])
-            dp[nx][ny] = dp[x][y] + 1
-print(dp[gx][gy])
-
-# ABC176 D - Wizard in Maze
-H, W = getNM()
-Ch, Cw = getNM()
-Dh, Dw = getNM()
-maze = [input() for i in range(H)]
-Ch -= 1
-Cw -= 1
-Dh -= 1
-Dw -= 1
-
-# ワープを最低で何回使うか
-# 上下左右2つ向こうまでの範囲内でワープできる
-# 隣接する'.'が領域
-
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
-
-pos = deque([[Ch, Cw]])
-dp = [[-1] * W for i in range(H)]
-dp[Ch][Cw] = 0
-
-while len(pos) > 0:
-    y, x = pos.popleft()
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        # 歩いて移動
-        if 0 <= nx < W and 0 <= ny < H and maze[ny][nx] == "." and (dp[ny][nx] == -1 or dp[y][x] < dp[ny][nx]):
-            # 0-1 bfs
-            # 先頭に置く
-            pos.appendleft([ny, nx])
-            dp[ny][nx] = dp[y][x]
-    # ワープ
-    for i in range(-2, 3):
-        for j in range(-2, 3):
-            wy = y + i
-            wx = x + j
-            # 歩いて移動不可能でないと使わない
-            if 0 <= wx < W and 0 <= wy < H and maze[wy][wx] == "." and dp[wy][wx] == -1:
-                pos.append([wy, wx])
-                dp[wy][wx] = dp[y][x] + 1
-
-print(dp[Dh][Dw])
-
-# AGC033 A - Darker and Darker
-
+ab = [
+[1, 6],
+[2, 5],
+[3, 1],
+[3, 2],
+[4, 1],
+[4, 6],
+[5, 1],
+[6, 7],
+[7, 8]
+]
 """
-一番近い#までの距離
-100万マスあるので一回の探索で済むように
-黒マスの周囲４マスを探索
-用が済めばポイ　同じマスについて探索する必要はない
-これで計算量は4 * H * W
+for _ in range(N + M - 1):
+    ab.append(tuple(map(int, input().split())))
 """
 
-H, W = getNM()
-maze = [list(input()) for i in range(H)]
-prev = []
-for i in range(H):
-    for j in range(W):
-        if maze[i][j] == '#':
-            prev.append((i, j))
+def topological(n, dist):
+    in_cnt = defaultdict(int)
+    outs = defaultdict(list)
 
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
+    for a, b in ab:
+        in_cnt[b - 1] += 1
+        outs[a - 1].append(b - 1)
 
-flag = True
-ans = 0
-while flag:
-    flag = False
-    next = []
-    while prev:
-        y, x = prev.pop()
-        for i in range(4):
-            ny = y + dy[i]
-            nx = x + dx[i]
-            if 0 <= ny < H and 0 <= nx < W and maze[ny][nx] == '.':
-                flag = True
-                maze[ny][nx] = '#'
-                next.append((ny, nx))
-    prev = next
-    if flag:
-        ans += 1
+    res = []
+    queue = deque([i for i in range(n) if in_cnt[i] == 0])
 
-print(ans)
+    while len(queue) != 0:
+        v = queue.popleft()
+        res.append(v)
+        for v2 in outs[v]:
+            in_cnt[v2] -= 1
+            if in_cnt[v2] == 0:
+                queue.append(v2)
 
-# AGC043 A - Range Flip Find Route
+    return res
+
+# [2, 3, 1, 4, 0, 5, 6, 7]
+# queryに閉路ができる道を追加するとバグってlen = 8未満の配列を返す
+print(topological(N, ab))
+
+# ABC041 D - 徒競走
+# トポロジカルソートの種類の数
+N, M = 3, 2
+query = [
+[2, 1],
+[2, 3]
+]
+
+X = [0] * N
+for i in range(M):
+    x, y = query[i]
+    # xにある矢印を集計
+    X[x - 1] |= 1 << (y - 1)
+
+DP = [0] * (1 << N)
+DP[0] = 1
+
+# jの左に置くものとしてどのような組み合わせがあるか
+for bit in range(1, 1 << N):
+    for j in range(N):
+        # j番目が含まれる場合において
+        if bit & (1 << j):
+            if not (X[j] & (bit ^ (1 << j))):
+                # 上のbitまで運送してってdp[-1]で集計
+                DP[bit] += DP[bit ^ (1 << j)]
+print(DP)
+
+# 全国統一プログラミング王決定戦予選 D - Restore the Tree
 
 """
-白いとこだけ踏んでゴールを目指す
-スタートやゴールが黒いこともある
+元のN頂点N - 1辺の根付き有向辺グラフ + 新たにM本の有向辺
+元の木は一意に定まることが示せる。
 
-操作をすると選択した長方形空間内の白黒が反転する
-最小で何回操作するか
-効率の良い操作方法を考える
+邪魔なM本を消せ
+木にするためには
+ループを消す
+B側に根以外の各頂点がN - 1個あるようにすればいい
+他には？
+適当に辺を選んでいくが、最終的に連結である必要がある
 
-黒い部分を白くすることだけを考える？
-白だけ踏んでいけるとは？
-二回反転させれば元どおり
+6 3
+2 1
+2 3
+4 1
+4 2
+6 1
+2 6
+4 6
+6 5の場合
 
-白から黒に、黒から白に侵入するときだけ += 1する？
+1: [2, 1], [4, 1], [6, 1]
+2: [4, 2]
+3: [6, 3]
+4: [] 親になるものがすぐわかることもある
+5: [6, 5]
+6: [2, 6], [4, 6]
+
+切り離しても連結のママのものは？
+MのA,Bについて、Bは元の根付き木に置けるAの子孫である
+親方向へは伸びない
+なのでトポソする
 """
 
-H, W = getNM()
-maze = [list(input()) for i in range(H)]
-dp = [[float('inf')] * W for i in range(H)]
-dp[0][0] = 0
-if maze[0][0] == "#":
-    dp[0][0] = 1
+N, M = getNM()
+dist = [getList() for i in range(N + M - 1)]
+edges = [[] for i in range(N)] # 親要素の候補
+for a, b in dist:
+    edges[b - 1].append(a - 1)
 
-dy = [0, 1]
-dx = [1, 0]
+# トポソする
+# 順番が求まる
+res = topological(N, dist)
 
-pos = deque([[0, 0]])
+ans = [-1] * N
+ans[res[0]] = 0
+depth = [-1] * N
+depth[res[0]] = 0
 
-# 0 - 1bfs?
-while pos:
-    y, x = pos.popleft()
-    for i in range(2):
-        ny = y + dy[i]
-        nx = x + dx[i]
-        if 0 <= ny < H and 0 <= nx < W: # 領域内
-            # 同じ色の場合
-            if maze[y][x] == maze[ny][nx] and dp[ny][nx] > dp[y][x]:
-                pos.appendleft([ny, nx])
-                dp[ny][nx] = dp[y][x]
-            # 違う色の場合
-            if maze[y][x] != maze[ny][nx]:
-                # 入るときだけでいい
-                if maze[y][x] == "." and dp[ny][nx] > dp[y][x] + 1:
-                    pos.append([ny, nx])
-                    dp[ny][nx] = dp[y][x] + 1
-                elif maze[y][x] == "#" and dp[ny][nx] > dp[y][x]:
-                    pos.appendleft([ny, nx])
-                    dp[ny][nx] = dp[y][x]
+# 追加のM辺はショートカットになるので
+# 元の根付き木は辺のうち深さが最も深くなるもの
 
-print(dp[H - 1][W - 1])
+for i in res[1:]: # 二番手以降について調べる
+    parent = 0
+    dep_opt = 0
+    for j in edges[i]: # iの各親について深さを調べる
+        if depth[j] + 1 > dep_opt: # 更新できるなら
+            parent = j
+            dep_opt = depth[j] + 1
+    ans[i] = parent + 1
+    depth[i] = dep_opt
+
+for i in ans:
+    print(i)
