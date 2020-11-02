7206c6c98eb73285cd8214e66e93fb3a729ccb05
customaddone <custom1485@docomo.ne.jp>
2020-11-02 21:06:36 +0900

    クラスカル法

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -49,71 +49,185 @@ mod = 998244353
 # Main Code #
 #############
 
-# CODE FESTIVAL 2016 Final D. Pair Cards
+# プリム法
+n = 7
+edge = [
+[[1, 1]],
+[[1, 0], [2, 2], [3, 3], [7, 4]],
+[[2, 1], [10, 5]],
+[[3, 1], [1, 4], [5, 6]],
+[[7, 1], [1, 3], [8, 6], [5, 5]],
+[[10, 2], [5, 4]],
+[[5, 3], [8, 4]]
+]
+
+def prim_heap():
+    used = [1] * n #True:未使用
+
+    edgelist = []
+    for e in edge[0]:
+        heapq.heappush(edgelist,e)
+
+    used[0] = 0
+    res = 0
+
+    while len(edgelist) != 0:
+        minedge = heapq.heappop(edgelist)
+        # もし使用済なら飛ばす
+        if not used[minedge[1]]:
+            continue
+
+        # 距離最小のものを使用する
+        # エッジを一つ使うたびに頂点を消す
+        # これをN - 1回繰り返す
+        v = minedge[1]
+        used[v] = 0
+
+        for e in edge[v]:
+            if used[e[1]]:
+                heapq.heappush(edgelist,e)
+        res += minedge[0]
+
+    return res
+
+print(prim_heap())
+
+# クラスカル法
+
+class UnionFind():
+    def __init__(self, n):
+        self.n = n
+        self.parents = [-1] * n
+
+    def find(self, x):
+        if self.parents[x] < 0:
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
+
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+
+        if x == y:
+            return
+
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
+
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+
+V, E = map(int, input().split())
+edges = []
+for i in range(E):
+    s, t, w = map(int, input().split())
+    edges.append((w, s, t))
+edges.sort()
+
+def kruskal(n, edges):
+    U = UnionFind(n)
+    res = 0
+    for e in edges:
+        w, s, t = e
+        if not U.same(s, t):
+            res += w
+            U.union(s, t)
+    return res
+print(kruskal(V, edges))
+
+# CODE FESTIVAL 2016 qual B C - Gr-idian MST
+# クラスカル法的な考え方
 
 """
-個数の最大値を求める
-ペア
+HW領域は広い
+(H + 1) * (W + 1)個の家がある
+辺には道路がある
+i ~ i + 1の縦方向の舗装がコストp
+j ~ j + 1の横方向の舗装がコストq
 
-最も条件がきついものからペアにしていく
-条件　どちらか
-同じ整数
-Mの倍数になる
+最小全域木を作るためのコスト
+木になるんだからH * W - 1本舗装する
+もちろんダイクストラ無理
+2 2
+P = [3, 5]
+Q = [2, 7]
+  _ _
+5 _| |
+3 _| |
+  2  7 これが最小
 
-Mが奇数の場合と偶数の場合
-奇数の場合
-mod mが
-0: 自身と
-i: m - iと
-偶数の場合
-0, m // 2: 自身と
-i: m - iと
+繋げる点１つにつき辺が１つ - 1c
 
-効率のいい組み合わせ
-きつい順から
-min(len(i), len(m - i))になるが差分については同じ数同士をペアにできる
+クラスカル法: コストが小さいものから繋げてU.same
 
-同じ数がグループにある数はその同じ数と結ぶことができるし、m - iのグループの数と結ぶことも
-できる　条件は緩い
-そうではない条件が厳しいものを優先して結ぶ
+縦と横を代替できる
+小さいものから順にグループが繋がるように
+[0, 0], [0, 1], [0, 2]は全て繋がってるものとする
+これに[1, 0], [1, 1], [1, 2]を繋げるには横棒を引くか縦棒を引き、他の横棒を借りて連結する
+縦一行をグループにするのに縦線が必要な訳ではない
+
+a2
+
+a1
+   b1   b2 について
+
+一行目のグループを全て繋げるには
+    _
+a2 |_
+a1 |_ か
+
+a2 |_|
+a1 | | の２つの方法がある
+
+縦の合計 + 横1本か全て横か
+できたら縦と横入れ替えて実施
+
+次のグループを繋げるためには横一本は絶対使う
+後の4本は縦（各1本ずつ）横（たくさん）から4本選ぶ
+
+i行目にある点を全て同じグループにし、motherに統合するイメージ
+
+クラスカル法的な考え方だと
+PとQを一緒くたにし、小さい順から取っていく
+縦/横棒を拾ったら全ての横/縦棒とバトル
+バトルに勝った本数を引ける
 """
 
-N, M = getNM()
-X = getList()
+W, H = getNM()
+P = getArray(W)
+Q = getArray(H)
+P.sort()
+Q.sort()
 
-modulo = [[] for i in range(M)]
-for x in X:
-    modulo[x % M].append(x)
-
-ans = (len(modulo[0]) // 2) + (len(modulo[M // 2]) // 2) * (M % 2 == 0)
-half = M // 2 if M % 2 == 0 else (M // 2) + 1
-
-for i in range(1, half):
-    pair1 = 0
-    dict1 = defaultdict(int)
-    for j in modulo[i]: # 同じ数
-        if dict1[j] == 1:
-            dict1[j] -= 1
-            pair1 += 1
-        else:
-            dict1[j] += 1
-
-    pair2 = 0
-    dict2 = defaultdict(int)
-    for j in modulo[M - i]:
-        if dict2[j] == 1:
-            dict2[j] -= 1
-            pair2 += 1
-        else:
-            dict2[j] += 1
-
-    if len(modulo[i]) > len(modulo[M - i]): # pair1を使う
-        ans += len(modulo[M - i])
-        diff = abs(len(modulo[i]) - len(modulo[M - i])) // 2
-        ans += min(diff, pair1)
+su = sum(P)
+opt1 = su # 横線を最初に引いた状態でスタート
+P_imos = deepcopy(P)
+for i in range(1, W):
+    P_imos[i] += P_imos[i - 1]
+for i in range(H):
+    # Q[i]は一本は必ず使う
+    # 残りW本は好きに
+    yoko = bisect_left(P, Q[i])
+    if yoko == 0: # 縦線より小さいものがなかった
+        opt1 += Q[i] * (W + 1)
     else:
-        ans += len(modulo[i])
-        diff = abs(len(modulo[i]) - len(modulo[M - i])) // 2
-        ans += min(diff, pair2)
+        opt1 += P_imos[yoko - 1] + (W + 1 - yoko) * Q[i]
 
-print(ans)
+su = sum(Q)
+opt2 = su # 縦線を初めに引いて
+Q_imos = deepcopy(Q)
+for i in range(1, H):
+    Q_imos[i] += Q_imos[i - 1]
+for i in range(W):
+    tate = bisect_left(Q, P[i])
+    if tate == 0:
+        opt2 += P[i] * (H + 1)
+    else:
+        opt2 += Q_imos[tate - 1] + (H + 1 - tate) * P[i]
+
+print(min(opt1, opt2))
