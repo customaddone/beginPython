3a5b15158bef723f3f839db52d8b5fa9a66bcdaf
customaddone <custom1485@docomo.ne.jp>
2020-11-03 10:50:30 +0900

    組み合わせ

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -49,185 +49,295 @@ mod = 998244353
 # Main Code #
 #############
 
-# プリム法
-n = 7
-edge = [
-[[1, 1]],
-[[1, 0], [2, 2], [3, 3], [7, 4]],
-[[2, 1], [10, 5]],
-[[3, 1], [1, 4], [5, 6]],
-[[7, 1], [1, 3], [8, 6], [5, 5]],
-[[10, 2], [5, 4]],
-[[5, 3], [8, 4]]
-]
+# mod不使用ver
+def cmb_1(n, r):
+    r = min(n - r, r)
+    if (r < 0) or (n < r):
+        return 0
 
-def prim_heap():
-    used = [1] * n #True:未使用
+    if r == 0:
+        return 1
 
-    edgelist = []
-    for e in edge[0]:
-        heapq.heappush(edgelist,e)
+    over = reduce(mul, range(n, n - r, -1))
+    under = reduce(mul, range(1, r + 1))
+    return over // under
 
-    used[0] = 0
-    res = 0
+# 10
+print(cmb_1(5, 3))
 
-    while len(edgelist) != 0:
-        minedge = heapq.heappop(edgelist)
-        # もし使用済なら飛ばす
-        if not used[minedge[1]]:
-            continue
+# mod使用ver
+# nが大きい場合に
+def cmb_2(x,y):
+    r = 1
+    for i in range(1, y + 1):
+        r = (r * (x - i + 1) * pow(i, mod - 2, mod)) % mod
+    return r
 
-        # 距離最小のものを使用する
-        # エッジを一つ使うたびに頂点を消す
-        # これをN - 1回繰り返す
-        v = minedge[1]
-        used[v] = 0
+# 10
+print(cmb_2(5, 3))
 
-        for e in edge[v]:
-            if used[e[1]]:
-                heapq.heappush(edgelist,e)
-        res += minedge[0]
+# 逆元事前処理ver
+# nが小さい場合に
+lim = 10 ** 6 + 1
+fact = [1, 1]
+factinv = [1, 1]
+inv = [0, 1]
 
-    return res
+for i in range(2, lim + 1):
+    fact.append((fact[-1] * i) % mod)
+    inv.append((-inv[mod % i] * (mod // i)) % mod)
+    # 累計
+    factinv.append((factinv[-1] * inv[-1]) % mod)
 
-print(prim_heap())
+def cmb(n, r):
+    if (r < 0) or (n < r):
+        return 0
+    r = min(r, n - r)
+    return fact[n] * factinv[r] * factinv[n - r] % mod
+# 120
+print(cmb(10, 3))
 
-# クラスカル法
+lim = 10 ** 6 + 1
+fact = [1, 1]
+factinv = [1, 1]
+inv = [0, 1]
 
-class UnionFind():
-    def __init__(self, n):
-        self.n = n
-        self.parents = [-1] * n
+for i in range(2, lim + 1):
+    fact.append((fact[-1] * i) % mod)
+    inv.append((-inv[mod % i] * (mod // i)) % mod)
+    # 累計
+    factinv.append((factinv[-1] * inv[-1]) % mod)
 
-    def find(self, x):
-        if self.parents[x] < 0:
-            return x
-        else:
-            self.parents[x] = self.find(self.parents[x])
-            return self.parents[x]
+# 階乗
+def factorial(n, r):
+    if (r < 0) or (n < r):
+        return 0
+    return fact[n] * factinv[n - r] % mod
 
-    def union(self, x, y):
-        x = self.find(x)
-        y = self.find(y)
+# print(factorial(5, 3))
 
-        if x == y:
-            return
+# 重複組み合わせ
+# 10個のものから重複を許して3つとる
+print(cmb_1(10 + 3 - 1, 3))
 
-        if self.parents[x] > self.parents[y]:
-            x, y = y, x
+# modが素数じゃない時
+def cmb_compose(n, k, mod):
+    dp = [[0] * (k + 1) for i in range(n + 1)]
+    dp[0][0] = 1
+    for i in range(1, n + 1):
+        dp[i][0] = 1
+        for j in range(1, k + 1):
+            # nCk = n - 1Ck - 1 + n - 1Ck
+            dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % mod
 
-        self.parents[x] += self.parents[y]
-        self.parents[y] = x
+    return dp[n][k]
 
-    def same(self, x, y):
-        return self.find(x) == self.find(y)
+print(cmb_compose(10, 3, 50))
 
-V, E = map(int, input().split())
-edges = []
-for i in range(E):
-    s, t, w = map(int, input().split())
-    edges.append((w, s, t))
-edges.sort()
+A, B, C = 144949225, 545897619, 393065978
 
-def kruskal(n, edges):
-    U = UnionFind(n)
-    res = 0
-    for e in edges:
-        w, s, t = e
-        if not U.same(s, t):
-            res += w
-            U.union(s, t)
-    return res
-print(kruskal(V, edges))
+# kCc / k+1Cc = k - c + 1 / k + 1
+# k+1Cc+1 / kCc = k + 1 / c + 1
+# Xを10 ** 9 + 7 - 2乗すると逆元が求まる
+x = (C * pow(A, mod - 2, mod)) % mod
+y = (B * pow(A, mod - 2, mod)) % mod
 
-# CODE FESTIVAL 2016 qual B C - Gr-idian MST
-# クラスカル法的な考え方
+n = (x + y - 2 * x * y) * pow(x * y - x - y, mod - 2, mod)
+k = (y - x * y) * pow(x * y - x - y, mod - 2, mod)
+print((n - k) % mod, k % mod)
+
+
+# 再帰で組み合わせ
+N = 4
+L = [1, 1]
+root = 5
+
+# root ** Nでループ
+def four_pow(i, array):
+    global cnt
+    if i == N:
+        print(array)
+        return
+    for j in range(root):
+        new_array = array + [j]
+        four_pow(i + 1, new_array)
+# four_pow(0, [])
+
+# 組み合わせ
+def comb_pow(i, array):
+    global cnt
+    if i == N:
+        print(array)
+        return
+    # ここの4を変えてrootを変更
+    last = -1
+    if len(array) > 0:
+        last = array[-1]
+
+    for j in range(last + 1, root):
+        new_array = array + [j]
+        comb_pow(i + 1, new_array)
+#comb_pow(0, [])
+
+# 1スタート
+def comb_pow_2(i, array):
+    global cnt
+    if i == N:
+        print(array)
+        return
+    # ここの4を変えてrootを変更
+    last = 0
+    if len(array) > 0:
+        last = array[-1]
+
+    for j in range(last + 1, root + 1):
+        new_array = array + [j]
+        comb_pow_2(i + 1, new_array)
+# comb_pow_2(0, [])
+
+# 重複組み合わせ
+def rep_comb_pow(i, array):
+    global cnt
+    if i == N:
+        print(array)
+        return
+    # ここの4を変えてrootを変更
+    last = 0
+    if len(array) > 0:
+        last = array[-1]
+
+    for j in range(last, root):
+        new_array = array + [j]
+        rep_comb_pow(i + 1, new_array)
+# rep_comb_pow(0, [])
+
+N = 2
+root = 5
+
+# 1スタート
+def rep_comb_pow_2(i, array):
+    global cnt
+    if i == N:
+        print(array)
+        return
+
+    last = 0
+    if len(array) > 0:
+        last = array[-1]
+
+    for j in range(last + 1, root + 1):
+        new_array = array + [j]
+        rep_comb_pow_2(i + 1, new_array)
+# rep_comb_pow_2(0, [])
+
+N, K = 10, 5
+
+c1 = cmb(N, K)
+
+# 完全順列（モンモール数）
+dp = [0] * (K + 1)
+dp[2] = 1
+for i in range(3, K + 1):
+    dp[i] = (i - 1) * (dp[i - 1] + dp[i - 2]) % mod
+c2 = dp[K]
+
+ans = c1 * c2 % mod
+print(ans)
+
+# ABC008 C - コイン
+
+n = getN()
+c = getArray(n)
+sumans = 0
+
+for i in c:
+    lista = [j for j in c if i % j == 0]
+    count = len(lista)
+    sumans += math.ceil(count / 2) / count
+print(sumans)
+
+# 第6回 ドワンゴからの挑戦状 予選 B - Fusing Slimes
 
 """
-HW領域は広い
-(H + 1) * (W + 1)個の家がある
-辺には道路がある
-i ~ i + 1の縦方向の舗装がコストp
-j ~ j + 1の横方向の舗装がコストq
+操作をN - 1回行う
+1 ~ N - 1のどれかを右隣のスライムの位置まで移動させる　そして消す
+その通りは(N - 1)!通りあるがそれの総和を求めよ
+考え方を変えてみるか
 
-最小全域木を作るためのコスト
-木になるんだからH * W - 1本舗装する
-もちろんダイクストラ無理
-2 2
-P = [3, 5]
-Q = [2, 7]
-  _ _
-5 _| |
-3 _| |
-  2  7 これが最小
+3
+1 2 3　の場合
 
-繋げる点１つにつき辺が１つ - 1c
+  2 3 →    3 1 + 1移動
+1   3 →    3 1 + 2移動 答えは5
 
-クラスカル法: コストが小さいものから繋げてU.same
+1の移動する距離 + 2の移動する距離 +...
+1が移動する距離の通りは？
+1 2 3 4 の場合 3! = 6通り
+距離1: 1番目に1が選択される 2! = 2, 3 → 1 3通り
+2が選択されない状態で1が選ばれる
+距離2: 2 → 1 1通り
+距離3: 3 → 2 → 1, 2 → 3 → 1
 
-縦と横を代替できる
-小さいものから順にグループが繋がるように
-[0, 0], [0, 1], [0, 2]は全て繋がってるものとする
-これに[1, 0], [1, 1], [1, 2]を繋げるには横棒を引くか縦棒を引き、他の横棒を借りて連結する
-縦一行をグループにするのに縦線が必要な訳ではない
+距離1:
+1 2この順番は確定
+残りの3については○ 1 ○ 2 ○　の3箇所のどこかに置く
 
-a2
+距離2:
+同様に○ 1 ○ 3 ○ だが
+○ 1 2 3 ○ と ○ 1 ○ 3 2　はダブりがあるので引く
 
-a1
-   b1   b2 について
+(既に置いたものの順列) 1 (target) その他についてはこれの間に自由に置いて良い
+startが2の場合は1がどの時点で選ばれても強制でその他にしても問題ない
+N <= 10 ** 5
+うまくまとめる
 
-一行目のグループを全て繋げるには
-    _
-a2 |_
-a1 |_ か
+case1
+start1: 1
+next1: 2
+と
+case2
+start2: 2
+next2: 3
+はそれぞれ
 
-a2 |_|
-a1 | | の２つの方法がある
+○ start ○ next ○　になるので通りの数は同じ
+つまり同じ距離のindexを移動する場合は通りの数が同じ　その通りの数は距離をiとすると
+n = (i - 1)! + 2 既に置いたものの順列 + start + next + 1
+r = (N - 1) - (i + 1) の nPr i <= N - 2
+距離N - 1は(N - 2)!
 
-縦の合計 + 横1本か全て横か
-できたら縦と横入れ替えて実施
+同じ距離のindexを移動する場合は通りの数が同じ
+o = (既に置いたもの(i - 1) + 自身)とすると
+o + 1に残りのもの - 1を置いていく
+(i - 1)! * (N - 2) // oが N-1Po+1が通りの数
 
-次のグループを繋げるためには横一本は絶対使う
-後の4本は縦（各1本ずつ）横（たくさん）から4本選ぶ
+N個目がゴールであるものは別にする
 
-i行目にある点を全て同じグループにし、motherに統合するイメージ
+まとめて計算するのには変わりはない
+1 2 3 4の時
+3 が 4に飛ぶ確率 1
+2 が 4に飛ぶ確率　先に3が選ばれないといけない 1 / 2
+1 が 4に飛ぶ確率　先に2, 3が選ばれないといけない 1 / 3 * 1 / 2 * 2!
+...
+1 が xに飛ぶ確率　先に2, 3...x - 1が選ばれないと 1 / (x - 1) * (1 / (x - 2)... * (x - 2)!)消える
+これに(n - 1)!をかける
 
-クラスカル法的な考え方だと
-PとQを一緒くたにし、小さい順から取っていく
-縦/横棒を拾ったら全ての横/縦棒とバトル
-バトルに勝った本数を引ける
+各区間についてどのスライムが通過するかをまとめる
 """
 
-W, H = getNM()
-P = getArray(W)
-Q = getArray(H)
-P.sort()
-Q.sort()
+N = getN()
+X = getList()
+MOD = 10 ** 9 + 7
 
-su = sum(P)
-opt1 = su # 横線を最初に引いた状態でスタート
-P_imos = deepcopy(P)
-for i in range(1, W):
-    P_imos[i] += P_imos[i - 1]
-for i in range(H):
-    # Q[i]は一本は必ず使う
-    # 残りW本は好きに
-    yoko = bisect_left(P, Q[i])
-    if yoko == 0: # 縦線より小さいものがなかった
-        opt1 += Q[i] * (W + 1)
-    else:
-        opt1 += P_imos[yoko - 1] + (W + 1 - yoko) * Q[i]
-
-su = sum(Q)
-opt2 = su # 縦線を初めに引いて
-Q_imos = deepcopy(Q)
-for i in range(1, H):
-    Q_imos[i] += Q_imos[i - 1]
-for i in range(W):
-    tate = bisect_left(Q, P[i])
-    if tate == 0:
-        opt2 += P[i] * (H + 1)
-    else:
-        opt2 += Q_imos[tate - 1] + (H + 1 - tate) * P[i]
-
-print(min(opt1, opt2))
+S = 0
+res = 0
+F = 1
+# 小さいスケールのものから考える
+for i in range(1, N): # 区間0 ~ 1, 1 ~ 2...を通過するか
+    S += pow(i, MOD - 2, MOD) # 1, 1 / 2, 1 / 3...をその都度足していく
+    res += (X[i] - X[i - 1]) * S # S = 1 + 1 / 2 + 1 / 3...
+                                 # これは1: i - 1がi - 1 ~ iを通過する確率、 i - 2が...
+    res %= MOD
+    F *= i # 最終的に(N - 1)!になる
+    F %= MOD
+print(res * F % MOD)
