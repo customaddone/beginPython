639ebfb1777dcbf905e6a1ec951d9c7b9a21debf
customaddone <custom1485@docomo.ne.jp>
2020-11-04 21:44:47 +0900

    nim

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -49,352 +49,150 @@ mod = 998244353
 # Main Code #
 #############
 
-# NOMURA プログラミングコンテスト 2020 C - Folia
-"""
-N = 3
-A = [0, 1, 1, 2]の場合
-葉の数を求める
-まず完全二分木から考える
-ここから取り除く
-深さ3の葉は4つある
-深さ4の葉は8つある
-深さnの葉は2 ** (n - 1)つある
-深さ3の葉は4つある完全二分木について
-A = [0, 0, 0, 4]
-深さ3の葉を一つ刈ると
-A = [0, 0, 0, 3]
-二つ刈ってみる　この時、１つ目と同じ親のを刈ると
-A = [0, 0, 1, 2]
-違うのを刈ると
-A = [0, 0, 0, 2]
-根となる頂点を１つ作る
-A[0] = 1ならその頂点は葉（下に頂点を繋げない)
-A[0] = 0ならその頂点は生きる
-頂点の最大値を求めるなら
-なるべく大きく分岐させた方がいい
-エッジ貼らなくても頂点数求めるだけでいい
-A[i]を探索するたびに ans += する
-現在用意している仮の頂点数を保持しておく
-確定させた頂点数も抑えておく
-A = [0, 0, 1, 0, 2]を逆から見ると
-psuedo = [1, 2, 4, 8, 16]
-psuedoのそれぞれとA[4]どちらか小さい方をpsuedoから引く
-psuedo = [0, 0, 2, 6, 14]
-left[0] ~ left[i]のそれぞれでA[i]を引けるだけ引く
-順に見ると
-psuedo = []
-left = [] # ansに加える値
-A[0] = 0
-psuedo = [1]
-left = [1]
-A[1] = 0
-psuedo = [1, 2]
-left = [1, 2]
-A[2] = 1
-psuedo = [1, 2, 3]
-left = [0, 1, 3]
-A[3] = 0
-psuedo = [1, 2, 3, 6]
-left = [0, 1, 3, 6]
-A[4] = 2
-psuedo = [1, 2, 3, 6, 10]
-left = [0, 0, 1, 4, 10]
-葉にならない点の上限を考える
-"""
+# ABC027 C - 倍々ゲーム
+# ２人が最善を尽くす時、どちらが勝つか
+# パターン1:ある状態になるように収束させれば必ず勝つ
+# パターン2:ある場所を目指せば必ず勝つようになる
+# パターン3:最初の配置のためどんな方法を取っても必ず勝つ
 
-# 根から探索するか
-# 葉から探索するか
-
-# 私は根から
-# 総和には累積和が効く
+# まずは全通り試してみる　その中で勝ちが偏っている部分がある
 N = getN()
-A = getList()
-
-# 深さ0の二分木の場合
-if N == 0:
-    if A[0] == 1:
-        print(1)
+k = N
+depth = 0
+while k > 1:
+    k //= 2
+    depth += 1
+x = 1
+cnt = 1
+if depth % 2:
+    while x <= N:
+        if cnt % 2:
+            x *= 2
+        else:
+            x *= 2
+            x += 1
+        cnt += 1
+    if cnt % 2:
+        print("Takahashi")
     else:
-        print(-1)
-    exit()
-
-if A[0] == 0:
-    psuedo = [1]
-    left = [1] # 確定させる用
+        print("Aoki")
 else:
-    psuedo = [0]
-    left = [0]
-
-alta = deepcopy(A)
-for i in range(N - 1, -1, -1):
-    alta[i] += alta[i + 1]
-ma = max(alta)
-
-# i + 1番目について調べる
-for i in range(1, N + 1):
-    opt = min(psuedo[-1] * 2, ma) # stop指数爆発 今回max(alta)以上の数字は必要ない
-    psuedo.append(opt)
-    left.append(opt)
-    if psuedo[-1] - A[i] < 0:
-        print(-1)
-        exit()
-    psuedo[-1] -= A[i]
-    # 確定させていく
-    """
-    明らか追いつかないので累積する
-    for j in range(i, -1, -1):
-        if dete[j] == 0:
-            break
-        add = min(dete[j], A[i])
-        ans += add
-        dete[j] -= add
-    """
-
-ans = 0
-
-for i in range(N + 1):
-    ans += min(left[i], alta[i])
-print(ans)
-
-# ARC011 ダブレット
-# つまり最短経路問題
-s1, s2 = input().split(' ')
-N = getN()
-S = set()
-
-S.add(s1)
-S.add(s2)
-
-for i in range(N):
-    S.add(input())
-
-if s1 == s2:
-    print(0)
-    print(s1)
-    print(s2)
-    exit()
-
-S = list(S)
-N = len(S)
-
-# 2つにエッジを貼れるか
-def judge(s1, s2):
-    cnt = 0
-    n = len(s1)
-    for i in range(n):
-        if s1[i] != s2[i]:
-            cnt += 1
-    if cnt <= 1:
-        return True
+    while x <= N:
+        if cnt % 2:
+            x *= 2
+            x += 1
+        else:
+            x *= 2
+        cnt += 1
+    if cnt % 2:
+        print("Takahashi")
     else:
-        return False
+        print("Aoki")
 
-dist = [[] for i in range(N)]
-d = [[float('inf')] * N for i in range(N)]
-for i in range(N):
-    for j in range(i + 1, N):
-        if judge(S[i], S[j]):
-            dist[i].append(j)
-            dist[j].append(i)
-            d[i][j] = 1
-            d[j][i] = 1
+# ABC048 D - An Ordinary Game
+# 最終的にどのような形で終わるか
+S = input()
+if (S[0] != S[-1]) ^ (len(S) % 2):
+    print('Second')
+else:
+    print('First')
 
-sta = 0
-end = 0
-for i in range(N):
-    if S[i] == s1:
-        sta = i
-        break
-for i in range(N):
-    if S[i] == s2:
-        end = i
-        break
+# ABC059 D - Alice&Brown
+# まずは全通り試してみる
 
-# ダイクストラする
-def dij(start, edges):
-    dist = [float('inf') for i in range(N)]
-    dist[start] = 0
-    pq = [(0, start)]
+# 最終形をイメージする →
+# 1 0 操作出来ない　終わり
+# 1 1 操作出来ない　終わり
+# 逆に2 0 や 3 0 なら操作できる
+# 2以上開く、０か１開くを繰り返す
+X, Y = getNM()
+X = int(X)
+Y = int(Y)
 
-    # pqの先頭がgoal行きのものなら最短距離を返す
-    while len(pq) > 0:
-        di, now = heapq.heappop(pq)
-        if (di > dist[now]):
-            continue
-        for i in edges[now]:
-            if dist[i] > dist[now] + d[i][now]:
-                dist[i] = dist[now] + d[i][now]
-                heapq.heappush(pq, (dist[i], i))
-    return dist
+if (X - Y) ** 2 > 1:
+    print("Alice")
+else:
+    print("Brown")
 
-distance = dij(sta, dist)
-if distance[end] == float('inf'):
-    print(-1)
-    exit()
-
-# 最短経路を示す矢印を求める
-# ダイクストラと同じ要領で
-def router(n, sta):
-    pos = deque([sta])
-    ignore = [0] * n
-    path = [0] * n
-    ignore[sta] = 0
-    path[sta] = -1
-
-    while pos:
-        u = pos.popleft()
-
-        for i in dist[u]:
-            if ignore[i] != 1 and distance[i] == ignore[u] + d[i][u]:
-                path[i] = u
-                ignore[i] = ignore[u] + d[i][u]
-                pos.append(i)
-
-    return path
-
-path = router(N, sta)
-ans = [S[end]]
-now = end
-while True:
-    now = path[now]
-    ans.append(S[now])
-    if now == sta:
-        break
-
-print(len(ans) - 2)
-for i in range(len(ans)):
-    print(ans[-i - 1])
-
-# AGC014 B - Unplanned Queries
+# AGC033 LRUD Game
 
 """
-どこかの点を基準とする
-・どの辺を見ても書かれている数が偶数になった
-性質を満たす木が存在するか
-色々な木で考えてみよう
-最終的に最適な木の構造がわかる？
-頂点xを基準に見ると
-x - 1 - 2の木の場合 [1, 2]のクエリは
-x - 1 ① 2 xを経由すると
-x ② 1 ① 2
-他の辺の偶奇を変えずに1 - 2間だけ数字を加算することができた
-最も単純なスターグラフを考えると、x - i間の辺の数字 = クエリに何回iが出たか
-適当な頂点を根として、根付き木にする。この根を r とする。
-・クエリ(p, a) + (p, b)とクエリ(a, b)は同じ
-まず、クエリ (a, b) を考えたとき、これ を (r, a), (r, b) と分解することができる。
-これは、(a, b) の LCA を p としたとき、クエリ (a, b) ではパス a − p, b − p に +1 しており、
-クエリ (r, a), (r, b) ではパス a − p, b − p に +1、パス r − p に +2 するため、
-mod 2 で考えると同一視できるので明らかである。
-基準点をrとすると、クエリ全体では (r, i (i = 1 ~ N))を繰り返すことと同値である
-(r, v(vはrから一番深い点))のクエリを偶数回行う時、途中の辺についても偶数回加算されている
+制約よりゲーム木とも違う
+つまりnim
+スタート地点にコマが置いてある　これを２人で動かす
+S[i]の方向に動かす、もしくは動かさない
+盤上から落ちるか残るか
+逆から見てみると
+
+最適行動　とは？
+中央に寄るように、もしくは遠ざかるように？
+
+全ての行動をシミュレートして偏りを探す
+RとLの数の差、UとDの数の差によって勝敗がわかる
+
+高橋くんが如何なる行為をしても青木くんは盤上に残すことができる
+青木くんが如何なる行為をしても高橋くんは盤上から落とすことができる
+
+高橋くんは駒の振れ幅を大きくする
+青木くんは駒の振れ幅を小さくする
+基本高橋くん有利？
+
+2人は相手の行動を先読みできるか
+あとで相手がRを大量に持っている　→　できるだけ左にずらす
+最後のL or Rの処理のあと、コマが残っている
+相手の最悪の行動に対応できるか
+高橋と青木のLRUDの差で考える
+LL
+  RRRの場合
+
+２人が最適な行動をするとは
+相手が最悪の行動をしてくるということ
+コマが一番左にいても絶対に落とせる
+L, R, U, Dのそれぞれが独立に
+R, L, D, Uと対戦できる
 """
 
-N, M = getNM()
-list = [0] * (N + 1)
-for i in range(M):
-    a, b = getNM()
-    list[a] += 1
-    list[b] += 1
+H, W, N = getNM() # H:縦 W:横
+start = getList()
+S = input()
+T = input()
 
-for i in range(1, N + 1):
-    if list[i] % 2 != 0:
+LRUD_range = [start[1], start[1], start[0], start[0]]
+
+for i in range(N):
+    # 高橋のターン
+    if S[i] == 'L': # 高橋のL対青木のR
+        LRUD_range[0] -= 1
+    elif S[i] == 'R':
+        LRUD_range[1] += 1
+    elif S[i] == 'U':
+        LRUD_range[2] -= 1
+    else:
+        LRUD_range[3] += 1
+
+    if LRUD_range[0] < 1 or W < LRUD_range[1] or LRUD_range[2] < 1 or H < LRUD_range[3]:
         print('NO')
         exit()
+
+    # 青木のターン
+    if T[i] == 'L': # 高橋のR対青木のL ただしleftできるのは1まで
+        LRUD_range[1] = max(1, LRUD_range[1] - 1)
+    elif T[i] == 'R':
+        LRUD_range[0] = min(W, LRUD_range[0] + 1)
+    elif T[i] == 'U':
+        LRUD_range[3] = max(1, LRUD_range[3] - 1)
+    else:
+        LRUD_range[2] = min(H, LRUD_range[2] + 1)
+
 print('YES')
 
-# AGC013 B - Hamiltonish Path
-
-"""
-木構造ではない
-N頂点にM辺
-・２個以上の頂点を通る
-・同じ頂点を通らない
-・パスの少なくとも一方の端点と直接辺で結ばれている頂点は、必ずパスに含まれる
-いくつかあるうちの一つ答えよ
-端点と端点をつなぐと輪になる
-端点の頂点数は2でないといけないのでは　そんなことはない
-端点の候補はどんなものがあるか
-端点の１つ目はなんでもいいのか
-グリッドグラフを考えた場合に中央の点を指定してもうまく条件を満たすのでいけそう
-ただし、子要素に葉があればだめ
-端点に葉が2つあれば余裕
-端点を一つ選ぶ　→
-隣接する辺を全て通るパスを考える　
-7 8
-1 2
-2 3
-3 4
-4 5
-5 6
-6 7
-3 5
-2 6　の時
-もし2 6のパスがなければ3を端点にできない
-パスグラフを考えると
-パスグラフの端っこ２つを端点にしないと作れない
-大元となるパスグラフを見つける
-寄り道しないといけない場合であってもすぐ戻ってこれるような
-端点を一つ決める
-端点を2にしてもう片方の端点を考える時、1が邪魔
-端点を2から1にずらせれば楽
-適当な点１つを選んでそこから端点1に行くパス、端点2に行くパスを書いていく
-これ完全グラフだとTLEするんじゃ？
-今回Mの数が小さい
-作れる最大の完全グラフはN = 400 (N(N + 1) // 2 = 80000ぐらい)なのでOK
-"""
-
-N, M = getNM()
-dist = [[] for i in range(N)]
-for i in range(M):
-    a, b = getNM()
-    dist[a - 1].append(b - 1)
-    dist[b - 1].append(a - 1)
-
-parent = [-1] * N
-ignore = [0] * N
-ignore[0] = 1
-dot1 = 0
-dot1_flag = True
-dot2 = 0
-dot2_flag = True
-
-# 端点が見つかる（行き止まりになる）まで探索
-while dot1_flag or dot2_flag:
-    dot1_flag = False
-    for i in dist[dot1]:
-        if ignore[i] == 0:
-            ignore[i] = 1
-            parent[dot1] = i
-            dot1 = i
-            dot1_flag = True
-            break
-
-    dot2_flag = False
-    for i in dist[dot2]:
-        if ignore[i] == 0:
-            ignore[i] = 1
-            parent[i] = dot2
-            dot2 = i
-            dot2_flag = True
-            break
-
-ans = [dot2 + 1]
-now = dot2
-while now != dot1:
-    now = parent[now]
-    ans.append(now + 1)
-
-ans = list(reversed(ans))
-
-print(len(ans))
-print(*ans)
-
 # AGC033 C - Removing Coins
-# 木の直径 + nim 
+# 木の直径 + nim
 
 """
 最適行動する
 ある形に収束する
+
 相手の手がどうであれ
 全ての通りを試してみる
 
@@ -402,15 +200,14 @@ print(*ans)
 
 一つ頂点を選び、コインを取る
 その後他のコインを吸い寄せる
-
 ある点iを選択する
 iから各コインへの距離が1小さくなる
 
 各コインへの最短距離の最大値が1になると負け
 木なのでコインが２つ隣り合うだけになったら負け
-
 コインの木の直径が2で回ってきたら負け
 コインの木はちぎれることはない
+
 コインの木の直径を2にする
 
 1 - 2 - 3 - 4 - 5のパスグラフを考える
@@ -420,14 +217,15 @@ iから各コインへの距離が1小さくなる
 端以外を取ると木の直径が2減る
 
 1 - 2 - 3 - 4 - 5
-
 firが1取る 1 - 2 - 3 - 4 → secが2取る 2 - 3
 firが2取る 2 - 3 - 4 → secが4取る 3 - 4
 
 1 - 2 - 3 - 4 - 5 - 6
 fir 1取る 1 - 2 - 3 - 4 - 5 firの勝ち
+
 相手に直径5になるように回す　
 firは6か7で回ってきたら勝ち　8で回ってきたら
+
 2 + 3 * iで回ってきたら負け 2, 5, 8...
 2 * (3 * i) + 1 端を選ぶ
 2 * (3 * i) + 2 真ん中を選ぶ
@@ -473,100 +271,36 @@ if (d - 2) % 3 == 0:
 else:
     print('First')
 
-
-# AGC039 B - Graph Partition
+# 全国統一プログラミング王 予選　C - Different Strokes
 
 """
-N頂点M辺
-N <= 200
-O(n ** 3)までいける
-ワーシャルフロイドか
-01
-10
-1 - 2
-
-頂点集合V1, V2, V3...をできる限り作る
-頂点集合の最大値
-どの辺も番号が隣り合う頂点集合の頂点同士をつなぐ
-頂点を分解する
-いろいろやってみる
-
-頂点を一つずつ前から調べてみる
-頂点1をV1に入れると
-V1とVkだけ不利な気がする
-
-隣接する頂点は隣に置く
-距離が2ある頂点については自身と同じ場所に置ける
-[[1, 3, 4], [0, 2, 5], [1, 3], [0, 2], [0], [1]]の場合
-
-0を置く 0
-1を置く 0 1
-2を置く 0 1 2
-3を置く時 0 - 3にエッジがあるため2の向こうに置くことはできない
-0 1 2
-  3
-
-全ての辺について条件を満たす
-辺をいい感じに配置する
-1, 3について、[0, 2]は共通して隣にいないといけない
-輪ができる　輪を潰す
-
-最短距離の位置に配置すれば良い
-繋がってなければ何してもいい
-
-偶数か奇数か　繋がっていれば隣
-２つに分けることは可能か
-距離奇数のものを向こうに、偶数のものをこっちに
-判定可能
-距離を伸ばすということは？
-
-まず２つのグループに分けられるか
-分けられるならAi ~ Ajの最短距離の最長が答え
+一つの数字を最大化できないか
+結局青木さんの点数は高橋くんが選ばなかったもののBの総和になる
+つまり高橋くんがi個目を選ぶとAi獲得するだけでなくBi特することになる
+逆に青木さんがj個目を選ぶとBi獲得するだけでなくAi高橋くんを損させられる
+高橋くん、青木さんはAi + Biが大きい順に取っていく
 """
 
 N = getN()
-S = [[int(i) for i in list(input())] for j in range(N)]
-
-edges = [[] for i in range(N)]
+dish = []
 for i in range(N):
-    for j in range(N):
-        if S[i][j] == 1:
-            edges[i].append(j)
+    a, b = getNM()
+    dish.append([a, b, a + b])
 
-# まず２つのグループに分けられるか
-group = [-1] * N
-group[0] = 0
+dish.sort(key = lambda i:i[2])
 
-pos = deque([0])
-while pos:
-    u = pos.popleft()
-    for i in edges[u]:
-        if group[i] == -1: # 未確定
-            group[i] = 1 if group[u] == 0 else 0
-            pos.append(i)
-        else: # 確定済み
-            if group[u] == group[i]:
-                print(-1)
-                exit()
+ans = 0
+while True:
+    if dish: # 高橋くん
+        a, b, total = dish.pop()
+        ans += a
+    else:
+        break
 
-# ２つのグループに分けられるなら
-# Sをdistに使う
-for i in range(N):
-    for j in range(N):
-        if i != j and S[i][j] == 0:
-            S[i][j] = float('inf')
-
-def warshall_floyd(dist):
-    for k in range(N):
-        # i:start j:goal k:中間地点でループ回す
-        for i in range(N):
-            for j in range(N):
-                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
-    return dist
-
-S = warshall_floyd(S)
-ans = 2 # 連結ってことは絶対に2以上
-for i in S:
-    ans = max(ans, max(i) + 1)
+    if dish: # 青木さん
+        a, b, total = dish.pop()
+        ans -= b
+    else:
+        break
 
 print(ans)
