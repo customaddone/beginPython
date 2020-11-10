825162e28744fdf433744d1e4726fc3ed9708f04
customaddone <custom1485@docomo.ne.jp>
2020-11-10 19:29:04 +0900

    二分探索

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -49,340 +49,116 @@ mod = 998244353
 # Main Code #
 #############
 
-# ABC161 D - Lunlun Number
-K = 13
-que = []
-heapify(que)
-for i in range(1, 10):
-    que.append(i)
-for i in range(K):
-    u = heappop(que)
-    if u % 10 != 0:
-        heappush(que, 10 * u + (u % 10) - 1)
-    heappush(que, 10 * u + (u % 10))
-    if u % 10 != 9:
-        heappush(que, 10 * u + (u % 10) + 1)
-print(u)
+# エイシング プログラミング コンテスト 2019 D - Nearest Card Game
 
-N, M = getNM()
-weight = []
-key = []
-for _ in range(M):
-    a, b = getNM()
-    weight.append(a)
-    c = getList()
-    key.append(c)
-dp = [float('inf')] * (1 << N)
-dp[0] = 0
-for i in range(M):
-    bit = 0
-    for item in key[i]:
-        bit |= (1 << (item - 1))
-    for j in range(1 << N):
-        dp[j | bit] = min(dp[j | bit], dp[j] + weight[i])
-print(dp)
+"""
+つまりnim
+青木君がXiを指定する
+高橋くん→青木くんの順番でAから数字を取っていく
+高橋くん: 最も大きい数字を取る
+青木くん: Xに最も近い数字を取る　複数あれば小さい方
+カードがなくなれば終了　
+各Xについて高橋くんが取るカードの合計を求めよ
+O(1)でやる　テーブルで累積和とかなんかを前処理してO(1)で参照する
+Nが偶数: 高橋くんはN // 2枚のカードを取る
+Nが奇数: 高橋くんはN // 2 + 1枚のカードを取る
 
+Xが変化するごとに取るカードがどのように変化するか
+5 5
+3 5 7 11 13
+X = 1の時、近いカードは順に3, 5, 7, 11, 13
+つまり高橋くんは13, 11, 7を取る
+X = 4の時、近いカードは順に3, 5, 7, 11, 13
+X = 9の時、近いカードは順に7, 11, 5, 13, 3
+X = 10の時             11,  7,13,  5, 3
 
-# ARC028 B-特別賞
-N, K = getNM()
+X = 10の場合　7,  5, 3...
+            11, 13...
+高橋くんの取るカードは    13, 11, 7,  5, 3
+7と5がスキップされるので、13 + 11 + 3 = 27
+高橋くん、青木くんの取る順番がわかれば得点をO(1)でもとめれればいいのだが
+13, 11, 7, 5, 3
+7, 11, 5, 13, 3 互いにスキップされ合う
+高橋くんがiを取るスピードが青木くんより早い、もしくは同じなら取得できる
+そうでなければ青木くんが取得する
+X = 23の場合
+高橋: 13, 11, 7, 5, 3
+青木: 13, 11, 7, 5, 3
+13 + 7 + 3で23 これを各O(1)で求めたいが
+累積和しそうだ
+NlogNまでならいける　各XiについてlogNで
+青木くんが取る順番は求めなくていい？
+高橋くんが取れるどうかだけ？
+X = 4からの距離
+1, 1, 3, 7, 9: 3 5 7 11 13
+X = 5
+2, 0, 2, 6, 8: 5 3 7 11 13
+X = 6
+3, 1, 1, 5, 7: 5 7 3 11 13
+X = 7
+4, 2, 0, 4, 6: 7 5 3 11 13
+X = 8
+5, 3, 1, 3, 5: 7 5 11 3 13
+X = 9
+6, 4, 2, 2, 4: 7 11 5 13 3
+スワップしていく？
+
+X = 1の時
+3, 5, 7, 11, 13
+X = 4の時
+5, 7, 11, 13
+3
+X = 9の時
+11, 13
+7, 5, 3
+Xiから近い順とA[-1]から近い順
+3 5 7 11 13
+     10  13
+bisect_rightでXiでのスタート地点は求められる
+Xi-1の時と何が違うか
+
+A1, A3...の合計とA2, A4...の合計を求める
+Aは[(高橋と青木が交互に取るゾーン), (青木が取るゾーン), (高橋が取るゾーン)]になる
+その境界を求める
+"""
+
+N, Q = getNM()
 A = getList()
-young = [0] * N
-# i番目に若い人の順位はyoung[i]位
+X = getArray(Q)
+
+csum = [0]
 for i in range(N):
-    young[A[i] - 1] = i
-A = [-i for i in A]
-# Aの前からK個のリストを作る
-prized = []
-for i in range(K):
-    prized.append(A[i])
-heapq.heapify(prized)
-# K個数字があるうちの最大値（K番目の数字）を取ってくる
-now = heapq.heappop(prized)
-print(young[-now - 1] + 1)
-for i in range(K, N):
-    # もし現在のK番目の数字より小さい数字がきたら
-    if A[i] > now:
-        # prizedリストに混ぜて（計K個入っている）
-        heapq.heappush(prized, A[i])
-        # 再びK個数字があるうちの最大値（K番目の数字）を取ってくる
-        now = heapq.heappop(prized)
-        print(young[-now - 1] + 1)
-    # もし現在のK番目の数字より小さい数字がきたら捨てる
+    csum.append(csum[-1] + A[i])
+csum_even = [0]
+for i in range(0, N, 2):
+    csum_even.append(csum_even[-1] + A[i])
+csum_odd = [0]
+for i in range(1, N, 2):
+    csum_odd.append(csum_odd[-1] + A[i])
+
+#t回でX以下のAのみとなってしまうような最小のtを求める。
+def calc_t(X):
+    ok = -1
+    ng = N + 1
+    while ng - ok > 1:
+        mid = (ok + ng) // 2
+        aoki = mid // 2
+        taka = mid - aoki
+        aoki_max = A[-taka]
+        aoki_min_index = bisect_left(A, 2 * X - aoki_max)
+        if N - taka - aoki_min_index < aoki:
+            ng = mid
+        else:
+            ok = mid
+
+    return ok
+
+for x in X:
+    t = calc_t(x)
+    s = csum[N] - csum[N - (t + 1) // 2]
+
+    if N % 2 == 0:
+        s += csum_odd[(N - t) // 2]
     else:
-        print(young[-now - 1] + 1)
-
-# ABC062 D - 3N Numbers
-N = getN()
-A = getList()
-# foreとbackの境界線を移動させる
-# [3 1 4 1 5 9]の場合
-# foreは[3 1], [3 1 4], [3, 1, 4, 1]の場合
-# backは[5 9], [1 5 9], [4, 1, 5, 9]の場合を前計算
-# 前から計算
-fore = A[:N]
-# 後ろから計算
-back = A[2 * N:]
-back = [-i for i in back]
-for_sum = sum(fore)
-back_sum = sum(back)
-heapify(fore)
-heapify(back)
-
-fore_list = []
-back_list = []
-for i in range(N):
-    fore_list.append(for_sum)
-    back_list.append(back_sum)
-    in_fore = A[N + i]
-    heappush(fore, in_fore)
-    out_fore = heappop(fore)
-    for_sum += in_fore - out_fore
-
-    in_back = (-1) * A[-N - i - 1]
-    heappush(back, in_back)
-    out_back = heappop(back)
-    back_sum += in_back - out_back
-
-fore_list.append(for_sum)
-back_list.append(back_sum)
-
-ans = -float('inf')
-for i in range(N + 1):
-    opt = fore_list[i] + back_list[N - i]
-    ans = max(ans, opt)
-print(ans)
-
-# ABC123 D - Cake 123
-X, Y, Z, K = getNM()
-A = sorted([-i for i in getList()])
-B = sorted([-i for i in getList()])
-C = sorted([-i for i in getList()])
-pos = []
-heapify(pos)
-dict = defaultdict(int)
-u = (A[0] + B[0] + C[0], 0, 0, 0)
-heappush(pos, u)
-dict[u] = 1
-for i in range(K):
-    p, i, j, l = heappop(pos)
-    print(-p)
-    # 取り出すごとにA, B, Cについての次の値をpush
-    if i + 1 < X:
-        opt_a = (A[i + 1] + B[j] + C[l], i + 1, j, l)
-        if dict[opt_a] == 0:
-            heappush(pos, opt_a)
-            dict[opt_a] = 1
-    if j + 1 < Y:
-        opt_b = (A[i] + B[j + 1] + C[l], i, j + 1, l)
-        if dict[opt_b] == 0:
-            heappush(pos, opt_b)
-            dict[opt_b] = 1
-    if l + 1 < Z:
-        opt_c = (A[i] + B[j] + C[l + 1], i, j, l + 1)
-        if dict[opt_c] == 0:
-            heappush(pos, opt_c)
-            dict[opt_c] = 1
-
-# ABC137 D - Summer Vacation
-
-N, M = getNM()
-query = [getList() for i in range(N)]
-
-A_list = [[] for i in range(10 ** 5 + 1)]
-for a, b in query:
-    A_list[a].append(b)
-
-job = []
-heapq.heapify(job)
-
-ans = 0
-for i in range(1, M + 1):
-    for j in A_list[i]:
-        heapq.heappush(job, -j)
-    if len(job) > 0:
-        u = heapq.heappop(job)
-        ans += -u
-print(ans)
-
-# ABC149 E - Handshake
-# Mがクソデカイので使用不可
-# 二分探索使ってね
-N, M = getNM()
-A = sorted([-i for i in getList()])
-
-pos = []
-heapify(pos)
-dict = defaultdict(int)
-u = (A[0] + A[0], 0, 0)
-heappush(pos, u)
-dict[u] = 1
-
-ans = 0
-# 大きい値M番目まで全て求まる
-for i in range(M):
-    p, i, j = heappop(pos)
-    ans += -p
-    if i + 1 < N:
-        opt_a = (A[i + 1] + A[j], i + 1, j)
-        if dict[opt_a] == 0:
-            heappush(pos, opt_a)
-            dict[opt_a] = 1
-    if j + 1 < N:
-        opt_b = (A[i] + A[j + 1], i, j + 1)
-        if dict[opt_b] == 0:
-            heappush(pos, opt_b)
-            dict[opt_b] = 1
-print(ans)
-
-# Code Formula 2014 予選A C - 決勝進出者
-
-"""
-N: 予選の回数
-K: 招待人数
-最高順位が高い順に　どこかの予選でハイスコアを出せばOK
-最高順位が同じ場合は、最高順位を取った予選が開かれた時期が早い方から先に選ばれる。
-現在の試合を含めた残り試合数 = dとすると
-(K + d - 1) // dの人数分上から順番にとる
-2 11
-1 2 3 4 5 6 7 8 9 10 11
-1 2 15 14 13 16 17 18 19 20 21
-
-の場合
-[[1, 2, 3, 4, 5, 6], [15, 14, 13]] ここまでいける
-[1 2 3 4 5 6] 7 8 9 10 11
-[1 2 15 14 13] 16 17 18 19 20 21
-1番目の6位までと2番目以降の5位までは問答無用で確定する
-後をどうするか
-枠が空く　このまま順調に取っていっても枠が余る場合は
-枠が空いた場合は再計算
-N <= 50しかない
-優先度何番目かをレコードする
-枠が空くたびにボーダーが下がる
-
-4 5
-1 2 3 4 5
-2 1 3 4 5
-1 2 3 4 5
-2 1 3 4 5 の場合
-
-一番最初に2人通過できる？
-制約が小さいので50回全探索できる
-
-iを一つ進めるごとに候補がA[i]の分だけ増える
-これをヒープキューで優先度が高い（数字が小さい）順に取る
-"""
-
-N, K = getNM()
-A = [[] for i in range(N)]
-for i in range(N):
-    a = getList()
-    for j in range(K):
-        A[i].append([j * N + i + 1, a[j]])
-
-ans = [[] for i in range(N)]
-L = []
-heapify(L)
-passed = set()
-
-for i in range(N):
-    for j in A[i]:
-        heappush(L, j)
-    while L and L[0][0] <= K: # whileで抜き取る時は要素が残っているか気をつけよう
-        pref, id = heappop(L)
-        if id in passed:
-            K += 1
-        else:
-            ans[i].append(id)
-            passed.add(id)
-
-for i in ans:
-    print(*sorted(i))
-
-# ARC098 E - Range Minimum Queries
-
-"""
-数列A
-長さKの連続する部分列を1つ選ぶ　
-その中の最小のものを取り除く　infにすれば？
-取り除いた要素の最大値 - 最小値をマイナスにしたい　二分探索とかできる?
-最終形をイメージする
-
-一番望ましいのは
-Q個について最小区間のQ個を取ること
-それより小さい要素を取らずに都合のいいとこだけ取りたい
-小さい順に仕切りを立てていく
-まず小さい順に1 2 3 4...これは必ず取れる　（1 2 3 5...とかは1234より大きくなる）
-次に2 3 4 5を取れるか
-N個目の数って難しくない？
-
-5 3 2
-4 3 1 5 2 の場合
-4 3 [1 5 2]
-4 [3 5 2]
-
-N <= 2000なので 1, 2, 3, 4で区切っていくのはできそう
-Qの中に1を入れる場合、求める値はAq - A1
-
-1 1 3 5 6 7 の場合
-1番目の1以降を使うと 1 1 3 5
-2番目の1以降を使うと 1 3 5 6
-3以降を使うと       3 5 6 7
-なので3以降を使う方がいい
-Q = 4の時、候補となるのは
-[小さい方から1番目、2番目、3番目...] or
-[小さい方から2番目、3番目、4番目...] or...
-
-ただし、[小さい方から2番目、3番目、4番目...]を作るには選択範囲に小さい方から1番目を含めないことが必要
-4 3 1 5 2 の場合
-　　 ×     1は障害物になる
-ブロック1:[4, 3]
-ブロック2:[5, 2] の中でしかKを回せない
-[小さい方から3番目、4番目、5番目...]の場合
-ブロック1:[4, 3]
-ブロック2:[5]
-"""
-
-N, K, Q = getNM()
-A = getList()
-A = [[A[i], i] for i in range(N)]
-
-flag = [0] * N
-# 区切り0
-l = deepcopy(sorted(A))
-opt = []
-l.sort()
-for i in range(Q):
-    opt.append(l[i][0])
-ans = opt[-1] - opt[0]
-
-# 区切り1個以上
-for i in range(N):
-    # indexの位置はlを再利用
-    flag[l[i][1]] = 1 # A[i][1]はindex
-    parent = []
-    child = []
-    # フラグの立っているところで区切る
-    # 要素の探索はAを使う
-    for j in range(N):
-        if flag[j] == 0:
-            child.append(A[j][0])
-        else:
-            child.sort()
-            parent.append(child)
-            child = []
-    if len(child):
-        child.sort()
-        parent.append(child)
-
-    # 値を求める
-    # 各childから取れるだけ取る(配列操作を行う)
-    opt = []
-    for array in parent:
-        for j in range(len(array) - K + 1): # childの長さ - K + 1だけ値を取れる
-            opt.append(array[j])
-    # Q個取れたなら
-    if len(opt) >= Q:
-        opt.sort()
-        ans = min(ans, opt[Q - 1] - opt[0])
-
-print(ans)
+        s += csum_even[(N - t + 1) // 2]
+    print(s)
