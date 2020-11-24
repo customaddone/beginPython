05c67335139e4734e3093dee6146577de39c6335
customaddone <custom1485@docomo.ne.jp>
2020-11-24 20:57:12 +0900

    文字列

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -51,274 +51,487 @@ dy = [0, 1, 0, -1]
 # Main Code #
 #############
 
-"""
-Z algorithm
-def Z(s):
-    n = len(s)
-    z = [0] * n
-    z[0] = n
-    L, R = 0, 0
-    for i in range(1, n):
-        if i >= R:
-            L = R = i
-            # 一致が続く限り伸ばす
-            while(R < n and s[R - L] == s[R]):
-                R += 1
-            # LCAを書き込む
-            # 頭から一致しない場合はR - L = i - i = 0
-            z[i] = R - L
-        # 全て利用できる場合
-        elif z[i - L] < R - i:
-            z[i] = z[i - L]
-        # 一部利用できる場合
-        else:
-            L = i
-            while(R < n and s[R - L] == s[R]):
-                R += 1
-            z[i] = R - L
-    return z
-# [5, 0, 3, 0, 1]
-#print(Z('ababa'))
-N = getN()
-S = input()
-ans = 0
-for i in range(N):
-    z = Z(S[i:])
-    k = len(z)
-    for j in range(k):
-        # '' と 'ababa'
-        # 'a' と 'baba'
-        # 'ab' と 'aba'
-        # ans は j('', 'a', 'ab')の長さ以上にならない（ダブらないため）
-        ans = max(ans, min(j, z[j]))
-print(ans)
-"""
+# 文字列を整数に変換
+N = 26
 
-# Rolling hash
-N = getN()
-S = list(map(ord, list(input())))
-# 適当
-base = 1007
-# base = 1009
-power = [1] * (N + 1)
-# 部分文字列を数字に
-# ハッシュ生成
-for i in range(1, N + 1):
-    power[i] = power[i - 1] * base % mod
-print(power)
-
-# 長さmの文字列がダブらずに存在するか
-def check(m):
-    if N - m < m:
-        return False
-    res = 0
-    # 頭m文字のハッシュ生成
-    for i in range(m):
-        res += S[i] * power[m - i - 1]
-        res %= mod
-    dic = {res: 0}
-    for i in range(N - m):
-        # ハッシュをローリングしていって次々m文字のハッシュを生成していく
-        res = ((res - S[i] * power[m - 1]) * base + S[i + m]) % mod
-        # もし既出なら
-        if res in dic.keys():
-            index = dic[res]
-            if index + m <= i + 1: # 重ならないか
-                return True
-        else:
-            dic[res] = i + 1 # i + 1:頭の位置を記録する
-        print(dic)
-    return False
-
-ok = 0
-ng = N + 1
-while ng - ok > 1:
-    mid = (ok + ng) // 2
-
-    if check(mid):
-        ok = mid
+def num2alpha(num):
+    if num <= 26:
+        return chr(96 + num)
+    elif num % 26 == 0:
+        return num2alpha(num // 26 - 1) + chr(122)
     else:
-        ng = mid
-print(ok)
+        return num2alpha(num // 26) + chr(96 + num % 26)
 
-"""
-N, K = getNM()
-S = input()
-if(K * 2 > N):
-    print('NO')
-    exit()
-def check(n, s, k, mod, alp):
-    # 各アルファベットをエンコード
-    encode = {}
-    for i, ch in enumerate(alp, 10001):
-        encode[ch] = pow(172603, i, mod)
-    # 文字列の先頭からm文字、k文字目からm文字をハッシュしていく
-    left = 0
-    right = 0
-    for i in range(k):
-        left += encode[s[i]]
-        left %= mod
-        right += encode[s[i + k]]
-        right %= mod
-    words = set()
-    # ローリングする
-    for i in range(n):
-        words.add(left)
-        if (right in words):
-            return True
-        if (i + 2 * k >= n):
+# z
+print(num2alpha(N))
+
+n = N
+lista = []
+digit = 26
+i = 0
+
+while n != 0:
+    opt = n % digit
+    lista.insert(0, opt)
+    if n % digit == 0:
+        n = n // digit - 1
+    else:
+        n = n // digit
+    i += 1
+
+str_list = 'abcdefghijklmnopqrstuvwxyz'
+ans = ''
+for i in range(len(lista)):
+    ans += str_list[lista[i] - 1]
+
+# z
+print(ans)
+
+#  最長共通部分列
+s = 'pirikapirirara'
+t = 'poporinapeperuto'
+
+def dfs(s, ts):
+    lens = len(s)
+    lent = len(t)
+    dp = [[0] * (lent + 1) for i in range(lens + 1)]
+    dp[0][0] = 0
+
+    for i in range(lens):
+        for j in range(lent):
+            if s[i] == t[j]:
+                dp[i + 1][j + 1] = max(dp[i][j] + 1, dp[i + 1][j], dp[i][j + 1])
+            else:
+                dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1])
+    return dp[lens][lent]
+print(dfs(s, t))
+
+# レーベンシュタイン距離
+s = "pirikapirirara"
+t = "poporinapeperuto"
+
+def dfs(s, t):
+    lens = len(s)
+    lent = len(t)
+    dp = [[float('inf')] * (lent + 1) for i in range(lens + 1)]
+    dp[0][0] = 0
+
+    for i in range(lens):
+        for j in range(lent):
+            if s[i] == t[j]:
+                dp[i + 1][j + 1] = min(dp[i][j], dp[i + 1][j] + 1, dp[i][j + 1] + 1)
+            else:
+                dp[i + 1][j + 1] = min(dp[i][j] + 1, dp[i + 1][j] + 1, dp[i][j + 1] + 1)
+    return dp[lens][lent]
+print(dfs(s, t))
+
+# digital arts B - Password
+
+# 文字配列を数字配列に
+# pypyだといる
+def ord_chr(array, fanc):
+    if fanc == 0:
+        res = [ord(s) - ord('a') for s in array]
+        return res
+
+    if fanc == 1:
+        res = [chr(i + ord('a')) for i in array]
+        res = ''.join(res)
+        return res
+
+S = ord_chr(input(), 0)
+S = [i + 1 for i in S]
+
+if S == [1] or S == [26] * 20:
+	print("NO")
+	exit()
+
+h = sum(S)
+opt1 = [h % 26] * (h % 26 > 0) + [26] * (h // 26)
+
+if opt1 == S: # 逆向きにしてみる
+    opt1 = opt1[::-1]
+
+if opt1 == S: # 逆むきにしてむ同じなら'zzz'
+    opt1[-1] -= 1
+    opt1.append(1)
+
+opt1 = [i - 1 for i in opt1]
+print(ord_chr(opt1, 1))
+
+# ABC009 C - 辞書式順序ふたたび
+
+N,K = getNM()
+S = list(input())
+T = sorted(S)
+diff = 0
+ans = ""
+
+for i in range(N):
+    s = S[i]
+    # 残りの文字を全ループさせる
+    for t in T:
+        # tを追加して良いか確かめる
+        diff1 = diff + (s != t)
+        count = Counter(T)
+        count[t] -= 1
+        diff2 = sum((Counter(S[i + 1:]) - count).values())
+        # 追加していいなら
+        if diff1 + diff2 <= K:
+            diff = diff1
+            ans += t
+            T.remove(t)
             break
-        left += encode[s[i + k]] - encode[s[i]]
-        left %= mod
-        right += encode[s[i + k * 2]] - encode[s[i + k]]
-        right %= mod
-    return False
-ans = True
-ps = '9999217 9999221 9999233 9999271 9999277 9999289 9999299 9999317 9999337 9999347 9999397 9999401 9999419 9999433 9999463 9999469 9999481 9999511 9999533 9999593 9999601 9999637 9999653 9999659 9999667 9999677 9999713 9999739 9999749 9999761 9999823 9999863 9999877 9999883 9999889 9999901 9999907 9999929 9999931 9999937 9999943 9999971 9999973'
-ps = list(map(int, ps.split(' ')))
-ps.append(10 ** 9 + 7)
-ps.append(3040409)
-ps.append(10 ** 20 + 9)
-# 各modについて
-for p in ps:
-    # アルファベットをランダムに並べた表でエンコード表を作る
-    for alp in ['qwertyuioplkmnjhbvgfcdxsaz','qazxcsdwertfgvbnmhjyuioklp']:
-        ans = (ans & check(N, S, K, p, alp))
-if(ans):
-    print('YES')
-else:
-    print('NO')
-"""
-# ARC024 だれじゃ
-N, K = getNM()
-S = input()
+print(ans)
 
-if(K * 2 > N):
-    print('NO')
+# ABC031 語呂合わせ
+
+# 1 ~ Kまでの数字がどの単語に当てはまるか
+# 1 ~ Kに対し文字の候補は26 ** 3通り?
+
+# 文字列は総文字数、アルファベットの種類（２６種類、定数倍）で捉えられる
+
+N, M = getNM()
+que = []
+for i in range(M):
+    v, w = input().split()
+    que.append([v, w])
+root = 3
+
+def judge(array):
+    # 1 ~ Kに割り当てた文字数が正しいか
+    for v, w in que:
+        cnt = 0
+        for i in range(len(v)):
+            cnt += array[int(v[i]) - 1]
+        if cnt != len(w):
+            return
+    # 文字数が適合するなら
+    str_list = [''] * N
+    for v, w in que:
+        cnt = 0
+        # 文字を区切っていく
+        for i in range(len(v)):
+            str_len = array[int(v[i]) - 1]
+            opt = w[cnt: cnt + str_len]
+            if str_list[int(v[i]) - 1] == '':
+                str_list[int(v[i]) - 1] = opt
+            else:
+                if str_list[int(v[i]) - 1] != opt:
+                    return
+            cnt += str_len
+
+    # 全て適合するなら
+    for i in str_list:
+        print(i)
     exit()
 
-# ダブらない場所に長さKの同じ文字の種類と数で構成された部分があるか
-def check(n, s, k, mod, alp):
-    # 各アルファベットをエンコード
-    encode = {}
-    for i, ch in enumerate(alp, 10001):
-        encode[ch] = pow(172603, i, mod)
+# 1 ~ Kの文字数が何文字かについて3 ** Kを全探索
+def four_pow(i, array):
+    global cnt
+    if i == N:
+        judge(array)
+        return
+    for j in range(1, root + 1):
+        new_array = array + [j]
+        four_pow(i + 1, new_array)
+four_pow(0, [])
 
-    # 文字列の先頭からm文字、k文字目からm文字をハッシュしていく
-    res = 0
-    for i in range(k):
-        res += encode[s[i]]
-        res %= mod
+K, N = getNM()
+G = []
+for i in range(N):
+    v, w = map(str, input().split())
+    # 桁ごとに数字を分ける
+    v = list(v)
+    v = [int(d) - 1 for d in v]
+    G.append((v, w))
 
-    dic = {res: 0}
-    # ローリングする
-    for i in range(n - k):
-        res += encode[s[i + k]] - encode[s[i]]
-        res %= mod
-        if res in dic.keys():
-            index = dic[res]
-            if index + k <= i + 1:# ダブって無いか
-                return True
+# それぞれの語呂数に対して長さ1 ~ 3を割り当てる
+for p in product(range(1, 4), repeat = K):
+    S = [[] for _ in range(K)]
+    for v, w in G:
+        c = 0
+        # 長さが正しいか判定するパート
+        for d in v:
+            # 使われた語呂数の長さを足し合わせる
+            c += p[d]
+        if c != len(w):
+            break
+        # 文字列を割り当てるパート
         else:
-            dic[res] = i + 1
+            cur = 0
+            for d in v:
+                # 長さごとに文字列を切っていく
+                S[d].append(w[cur: cur + p[d]])
+                cur += p[d]
+    # 長さが整合したものが見つかれば
+    else:
+        for i in range(K):
+            # 任意の語呂数に対する文字列が一意に定まらなければ
+            # 112: abcで 1 = a, 1 = B, 2 = cになるみたいなケース
+            if len(set(S[i])) != 1:
+                break
+        else:
+            for i in range(K):
+                print(S[i][0])
+            exit()
 
-    return False
+# ABC043 D - アンバランス
+# i文字目を見る場合
+# i - 1文字目が同じ文字ならアウト
+# i - 2文字目が同じでもアウト
+S = input()
+N = len(S)
 
-ans = True
-ps = '9999217 9999221 9999233 9999271 9999277 9999289 9999299 9999317 9999337 9999347 9999397 9999401 9999419 9999433 9999463 9999469 9999481 9999511 9999533 9999593 9999601 9999637 9999653 9999659 9999667 9999677 9999713 9999739 9999749 9999761 9999823 9999863 9999877 9999883 9999889 9999901 9999907 9999929 9999931 9999937 9999943 9999971 9999973'
-ps = list(map(int, ps.split(' ')))
-ps.append(10 ** 9 + 7)
-ps.append(3040409)
-ps.append(10 ** 20 + 9)
-# 各modについて
-for p in ps:
-    # アルファベットをランダムに並べた表でエンコード表を作る
-    for alp in ['qwertyuioplkmnjhbvgfcdxsaz','qazxcsdwertfgvbnmhjyuioklp']:
-        ans = (ans & check(N, S, K, p, alp))
+ans = [-1, -1]
+for i in range(1, N):
+    if S[i] == S[i - 1]:
+        ans = [i, i + 1]
+        break
+    if i > 1 and S[i] == S[i - 2]:
+        ans = [i - 1, i + 1]
+        break
+print(*ans)
 
-if(ans):
-    print('YES')
+# ABC049 C - 白昼夢
+
+S = input()
+
+while len(S) >= 5:
+    # Sを４つの単語で順に調べて刈っていく
+    if len(S) >= 7 and S[-7:] == "dreamer":
+        S = S[:-7]
+        continue
+
+    if len(S) >= 6 and S[-6:] == "eraser":
+        S = S[:-6]
+        continue
+
+    elif S[-5:] == "dream" or S[-5:] == "erase":
+        S = S[:-5]
+        continue
+
+    else:
+        break
+
+if len(S) == 0:
+    print("YES")
 else:
-    print('NO')
+    print("NO")
+
+# ARC019 B - こだわりの名前
+S = input()
+N = len(S)
+bi = N // 2
+str_f = []
+for i in range(bi):
+    str_f.append(S[i])
+str_b = []
+for i in range(bi):
+    str_b.append(S[-i - 1])
+
+cnt = 0
+for i in range(bi):
+    if str_f[i] != str_b[i]:
+        cnt += 1
+
+# 全て一致
+if cnt == 0:
+    # 真ん中以外は何に変えても回文にならない
+    # 真ん中は何に変えても回文になる
+    print(2 * bi * 25)
+elif cnt == 1:
+    if N % 2 == 0:
+        print(25 * bi * 2 - 2)
+    else:
+        # 真ん中は何に変えても回文にならない
+        print(25 * bi * 2 - 2 + 25)
+else:
+    print(25 * N)
+
+# AGC048 A - atcoder < S
+
+"""
+スワップの最小回数は
+１文字目 a
+どこかにaより上がいたらそれをスワップして終了
+aならそのまま
+a以下なら？
+counterする？
+
+前から探索する
+スワップしなくていいならスワップしない
+target[i] > alta[i]の時スワップする
+前のとスワップするかも
+そもそもatcoderは6文字
+スワップの最大回数は6回
+次の文字にいくのはtarget[i] = alta[i]だった時のみ
+target[i] < alta[i]: 終了
+target[i] = alta[i]: 次に
+target[i] > alta[i]: スワップ要
+
+target'atcoder'が任意の文字(例:topcoder)、任意の二箇所（隣接しなくていい）をスワップできるなら
+一文字目を見る
+target[i] < alta[i]: 終了
+target[i] = alta[i]: 次に
+target[i] > alta[i]: スワップ要
+その文字以降を探索 target[i]を上回るものがあればスワップ += 1終了
+無い場合　target[i]と同じものが見つかればそのうち一番右のものとスワップ
+　　　　　target[i]を下回るものしかなければ終了
+
+たぶん
+"""
+
+T = getN()
+S = [input() for i in range(T)]
+
+for s in S:
+    if s.count('a') == len(s):
+        print(-1)
+        continue
+
+    if s > 'atcoder':
+        print(0)
+        continue
+
+    n = len(s)
+    for i in range(n):
+        # aより上の要素をスワップを繰り返し運送する
+
+        # i - 1回運送すると2番目の位置にくる
+        # もしord('t') < ord(i)なら条件を満たし終了
+        # そうでなければもう一つ前に運送する
+
+        # i回運送すると１番目の位置に来る
+        # ord('a') < ord(i)より条件を満たす
+        if s[i] > 'a':
+            if s[i] > 't':
+                print(i - 1)
+            else:
+                print(i)
+            break
+
+# C - String Coloring
+
+"""
+長さ2N
+何通りありますか　comboかdp
+Sの各文字を赤か青で塗る
+2 ** 2Nあるがこれを2 ** Nに落としたい
+2NからN個選ぶ
+36C18 9,075,135,300通り
+意外とでかいな　これを減らす
+
+まず構成する数字の個数が一致してないと
+4
+cabaacba の場合
+[[1, 3, 4, 7], [2, 6], [0, 5], [], [], [],
+a * 2, b * 1, c * 1
+indexの選び方は左右対象でなければならない
+1選ぶと7も選ばれる
+3を選ばない（相手側になる）と4も選ばれない（相手側になる）
+aの場所の配分は
+1, 3 + 4, 7でも1, 7 + 3, 4でもいい
+他の文字との位置関係が大事
+
+もちろん全通り出すのは無理
+左から右に読んだ文字列と右から左に読んだ文字列が一致するとは
+最終的には2 ** Nでいい
+明らかダメそうなやつを削る
+もちろん全ての文字を使わないといけない
+掛け算する形になると思う
+
+cabaacba で最初のcを選んだら最後のbaが青になるのは許されない
+グループ１になる組み合わせとグループ２になる組み合わせの裏表
+順番があるのでindexをとってうにうにはできない
+
+各要素を赤か青か　これを2 ** 18
+2 ** Nまでしかできない　2 ** Nを
+fore = S[:N]
+back = list(reversed(S[N:])) の両方でやってdict
+
+やってることはただのbit全探索半分全列挙
+"""
+
+string = 'abcdefghijklmnopqrstuvwxyz'
+N = getN()
+S = list([ord(i) - ord('a') for i in input()])
+
+fore = S[:N]
+back = list(reversed(S[N:]))
+
+# 26進数シリーズ
+# アルファベット → 数値
+def alpha2num(alpha):
+    num=0
+    for index, item in enumerate(list(alpha)):
+        num += pow(26, len(alpha) - index - 1) * (ord(item) - ord('a') + 1)
+    return num
+
+# 数値 → アルファベット
+def num2alpha(num):
+    if num <= 26:
+        return chr(96 + num)
+    elif num % 26 == 0:
+        return num2alpha(num // 26 - 1) + chr(122)
+    else:
+        return num2alpha(num // 26) + chr(96 + num % 26)
+
+def array2num(array):
+    num = 0
+    for index, item in enumerate(array):
+        num += pow(26, len(array) - index - 1) * (item + 1)
+    return num
+
+l = defaultdict(lambda: defaultdict(int))
+for bit in range(1 << N):
+    g1 = [] # 赤色
+    g2 = [] # 青色
+    for i in range(N):
+        if bit & (1 << i):
+            g1.append(fore[i])
+        else:
+            g2.append(fore[i])
+    g1 = array2num(g1)
+    g2 = array2num(g2)
+    l[g1][g2] += 1
+
+ans = 0
+for bit in range(1 << N):
+    g1 = [] # 赤色
+    g2 = [] # 青色
+    for i in range(N):
+        if bit & (1 << i):
+            g1.append(back[i])
+        else:
+            g2.append(back[i])
+
+    g1 = array2num(g1)
+    g2 = array2num(g2)
+    ans += l[g1][g2]
+
+print(ans)
 
 # 天下一プログラマーコンテスト2014予選B B - エターナルスタティックファイナル
-
-# ローリングハッシュ
-# sの連続部分列とkがマッチした部分s[i:j]についてのi, jを求める
-# 文字列を数字に変換してから使おう
-def check(stri, targ, base):
-    s = list(map(ord, list(stri)))
-    k = list(map(ord, list(targ)))
-    n = len(s)
-    m = len(k)
-    if n - m < 0:
-        return set()
-
-    pm = 2 ** 61 - 1
-    power = [1] * (n + 1)
-
-    # 第i文字目のハッシュ生成
-    for i in range(1, n + 1):
-        power[i] = power[i - 1] * base % pm
-
-    res = 0 # ハッシュを保存
-    # sの頭m文字のハッシュ生成
-    for i in range(m):
-        res += s[i] * power[m - i - 1]
-        res %= pm
-
-    # kのハッシュを生成
-    target = 0
-    for i in range(m):
-        target += k[i] * power[m - i - 1]
-        target %= pm
-
-    # sをローリングさせて調べる
-    opt = set()
-    for i in range(n - m):
-        # マッチしたら
-        # 半開区間で返す（変更可能）
-        if res == target:
-            opt.add((i, i + m))
-        # 0 ~ m文字目, 1 ~ m+1文字目...を生成しkと比べる
-        res = ((res - s[i] * power[m - 1]) * base + s[i + m]) % pm
-    # 最後の1回
-    if res == target:
-        opt.add((n - m, n))
-
-    return opt
-
-def rolling_hash(s, t):
-    # ほんとは原子根乱択がいい
-    # 基数baseと法pmをたくさん用意しないとfalseのものがtrueになるため、
-    # 全部trueのものだけを返す
-    res = check(s, t, 161971)
-    for base in [167329, 191911]: # 適当に手動で乱択してね
-        # これ一回の計算量はO(N)
-        res &= check(s, t, base)
-    return res
+# 文字列は二分探索できる
 
 N = getN()
 S = input()
-T = [input() for i in range(N)]
+T = [input() for _ in range(N)]
 
-opt = []
-for i in range(N):
-    # ロリハするとO(S + T)になる（もともとはO(S * (T ** 2)))
-    for j in list(rolling_hash(S, T[i])):
-        opt.append(j)
+T.sort() # 文字列を二分探索する
 
-dict = defaultdict(list)
-for l, r in opt:
-    dict[r].append(l)
-
-# opt(ST)の大きさがネックになり、前進型貰うdpで計算量はO(ST)
-# 合計で計算量はO(ST + T ** 2)(ロリハの部分)　なお定数倍
 dp = [0] * (len(S) + 1)
 dp[0] = 1
-for i in range(len(S) + 1): # iが右端
-    for l in dict[i]:
-        dp[i] += dp[l]
-    dp[i] %= mod
+for l in range(len(S)):
+    s = ''
+    for r in range(l, len(S)):
+        # l, rを決め, S[l:r+1]を出す
+        # これがTの中にいくつあるか
+        # O(N ** 2 * S) → O(S ** 2logN)
+        s += S[r]
+        cnt = bisect_right(T, s) - bisect_left(T, s)
+        dp[r + 1] += dp[l] * cnt
+        dp[r + 1] %= mod
 
-print(dp[-1] % mod)
+print(dp[-1])
