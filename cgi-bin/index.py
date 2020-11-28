b54a2a048763da3fa68100f10ae8deef7764a141
customaddone <custom1485@docomo.ne.jp>
2020-11-28 11:39:52 +0900

    lis

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -51,451 +51,364 @@ dy = [0, 1, 0, -1]
 # Main Code #
 #############
 
-# ABC005 C - おいしいたこ焼きの売り方
-# マッチング問題だが貪欲
-T = getN()
-N = getN()
-sell = getList()
-M = getN()
-buy = getList()
+# ABC006 D - トランプ挿入ソート
+n = getN()
+lista = [getList() for i in range(n)]
 
-# 来る客1, 2に売れるか
-for cus in buy:
-    flag = False
-    for i in range(N):
-        if sell[i] <= cus <= sell[i] + T:
-            flag = True
-            sell[i] = mod
-            break
-    if not flag:
-        print('no')
-        exit()
-print('yes')
+# 最長増加部分列問題 (LIS)の問題
+def lis(A):
+    L = [A[0]]
+    for a in A[1:]:
+        if a > L[-1]:
+            L.append(a)
+        # このelseに引っかかった時にトランプのソートが必要
+        else:
+            L[bisect_left(L, a)] = a
+    return len(L)
+print(n - lis(lista))
 
-# ABC080 D - Recording
-# 使ってない録画機は他のチャンネルにスイッチできる
-# 同時にいくつ放送が流れているか
-N, C = getNM()
-query = [getList() for i in range(N)]
-dp = [[0] * (C + 1) for i in range(10 ** 5 + 2)]
-for i in range(N):
-    s, t, c = query[i]
-    dp[s][c] += 1
-    dp[t + 1][c] -= 1
+# AGC024 B - Backfront
 
-for i in range(1, 10 ** 5 + 2):
-    for j in range(C + 1):
-        dp[i][j] += dp[i - 1][j]
+"""
+バブルソートのやつみたいになるんやろ
+適当な順番に数を選べば最小回数で
+3 2 5 1 4 6 の場合
+1 2より右にあるので左に
+2 3より右にあるので左に
+4 5より右にあるので左に
 
-ans = 0
-for i in range(10 ** 5 + 2):
-    cnt = 0
-    for j in dp[i]:
-        if j > 0:
-            cnt += 1
-    ans = max(ans, cnt)
-print(ans)
+4が6より左にある場合は？
 
-# ABC085 D - Katana Thrower
-N, H = getNM()
-
-a = []
-b = []
-
-for i in range(N):
-  x, y = map(int, input().split())
-  a.append(x)
-  b.append(y)
-
-# 振った場合の最大値
-max_a = max(a)
-
-ans = 0
-# 振る刀の最大攻撃力より高い攻撃力を持つ投げ刀を高い順にソートする
-# 刀iで好きなだけ振って攻撃する→気が済んだら投げることで振りの攻撃力と投げの攻撃力を
-# 両方利用することができる
-# 実は投げてしまった刀も振ることができるというルールに変更しても
-# 問題の答えは変わらない
-# 実際のムーブとしては
-# ①最も攻撃力が高い振り刀で攻撃する
-# ②一定の体力以下になると攻撃力が高い順に投げ刀で攻撃していって撃破
-# という流れになる
-for x in reversed(sorted(filter(lambda x: x >= max_a, b))):
-    H -= x
-    ans += 1
-    if H <= 0: break
-
-ans += max(0, (H + max_a - 1) // max_a)
-print(ans)
-
-# ABC091 C - 2D Plane 2N Points
-
-# 貪欲法でペア作りする問題
-# ABC005 C - おいしいたこ焼きの売り方の時と同様に
-# それとしか繋げないもの　を優先的に繋いでいく
-
-# 条件Aの通過が厳しい順に対象bをソートし、
-# たこ焼き　条件A:客が来る前にたこ焼きができてないといけない
-#  　　　　      客を来るのが早い順に並べる（最初から並んでる）
-# 今回     条件A:赤星のx座標が青星のx座標より小さくないといけない
-#  　　　　　　　 青星をx座標が小さい順に並べる
-
-# 条件A, 条件Bをクリアしたものの中で、最も条件Bの通過が厳しい対象aと結ぶ
-# たこ焼き　条件B:たこ焼きが賞味期限より前のものでないといけない
-#  　　　　      できるだけ古いものを売る（最初から並んでる）
-# 今回     条件B:赤星のy座標が青星のy座標より小さくないといけない
-#  　　　　　　　 条件をクリアしたもののうちでできるだけy座標が大きいものを選ぶ
+場所がおかしいやつは取り除く　あとで並べる
+i < i + 1 < i + 2...で単調増加してる分だけ
+普通のlis
+def lis(A):
+    L = [A[0]]
+    for a in A[1:]:
+        if a > L[-1]:
+            L.append(a)
+        # このelseに引っかかった時にトランプのソートが必要
+        else:
+            L[bisect_left(L, a)] = a
+    return len(L)
+print(n - lis(lista))
+"""
 
 N = getN()
-ab = []
-cd = []
-for i in range(N):
-    ab.append(getList())
-for j in range(N):
-    cd.append(getList())
-ab.sort(reverse = True)
+P = getArray(N)
 
+P = [[P[i], i] for i in range(N)]
+P.sort()
+
+now = 1 # 最初の1個は自明にok
 ans = 0
 
-for a,b in ab:
-    cd.sort(key = lambda x:x[1]) # ソートし直し
-    for i in range(len(cd)):
-        c, d= cd[i]
-        if a < c and b < d: # 店を探す
-            cd.pop(i)
-            ans += 1
-            break
-
-print(ans)
-
-N = getN()
-ab = set(tuple(getList()) for _ in range(N))
-cd = sorted(tuple(getList()) for _ in range(N))
-
-ab.add((-1, -1)) # 番兵
-for c, d in cd:
-    # 条件を満たすものの中でbが一番でかいもの
-    tmp = max((b, a) for a, b in ab if (a < c and b < d))[::-1]
-    if tmp != (-1, -1):
-        ab.remove(tmp) # match
-
-print(N - (len(ab) - 1))
-
-# ABC100 D - Patisserie ABC
-# 8パターン全部調べる
-
-N,M = getNM()
-data = [[] for i in range(8)]
-for _ in range(N):
-    x,y,z = getNM()
-    data[0].append(x + y + z)
-    data[1].append(x + y - z)
-    data[2].append(x - y + z)
-    data[3].append(x - y - z)
-    data[4].append(- x + y + z)
-    data[5].append(- x + y - z)
-    data[6].append(- x - y + z)
-    data[7].append(- x - y - z)
-
-ans = -mod
-for i in range(8):
-    data[i].sort(reverse = True)
-    ans = max(ans,sum(data[i][:M]))
-print(ans)
-
-# ABC103 D - Islands War
-N, M = getNM()
-que = []
-for i in range(M):
-    a, b = getNM()
-    que.append([a - 1, b - 1])
-
-# N個の島　各島間には橋がある
-# 要望が1 ~ 5, 1 ~ 3の時
-# 例えば1 - 2間を取り除けばいい
-# 要望が1 ~ 2, 1 ~ 3, 1 ~ 4の時
-# 1 - 2間の橋を取り除く
-# 逆に言えば1 ~ 2の時は1 ~ 2間の橋を取り除くしかない
-
-# 間隔が狭いものから調べていく
-# 間隔1の橋を取り除く、間隔2の橋を取り除く...
-# 間隔2のどちらの橋を取り除く
-
-# 1 ~ 2, 1 ~ 3, 1 ~ 4の時
-# 1 - 2間の橋を取り除く
-# 2 ~ 3, 1 ~ 3, 1 ~ 4の時
-# 1 - 2の橋は取り除かなくていい
-# するとこれは2スタートの2 ~ 3, 2 ~ 3, 2 ~ 4の要望に答える問題に変換できる
-# 終点をソートして左側から順に橋を落とすか落とさないか決めていく
-
-que.sort(key = lambda i:i[1])
-# a ~ bで繋がっているならb - 1 ~ b間の橋を取り除く
-# queryの区間は左にスライドしていき、次のqueryに最も関係あるのは右の方の橋なので
-
-ans = 0
-destroy = -1 # 最後に落とした橋
-for a, b in que:
-    if destroy <= a: # 範囲外なら橋を新しく落とす
-        ans += 1
-        destroy = b
-print(ans)
-
-
-# ABC116 D - Various Sushi
-N, K = getNM()
-various = defaultdict(list)
-que = [getList() for i in range(N)]
-
-ans = 0
-num = []
-var_s = set()
-
-# 美味しい順にK個とった時の幸福度
-que.sort(reverse = True, key = lambda i: i[1])
-for i in range(K):
-    ans += que[i][1]
-    # もし２番手以降ならあとで交換する用にとっておく
-    if que[i][0] in var_s:
-        num.append(que[i][1])
-    var_s.add(que[i][0])
-
-var = len(var_s)
-ans += var ** 2
-
-# 使ってない種類について各種類で一番大きさが大きいもの
-left_l = defaultdict(int)
-for i in range(N):
-    if not que[i][0] in var_s:
-        left_l[que[i][0]] = max(left_l[que[i][0]], que[i][1])
-
-num.sort(reverse = True)
-left_l = [i[1] for i in left_l.items()]
-left_l.sort()
-
-# M回交換する
-opt = ans
-M = min(len(num), len(left_l))
-for i in range(M):
-    u = num.pop()
-    s = left_l.pop()
-    # 寿司単体の幸福度
-    opt -= (u - s)
-    # 種類が増える分
-    opt += 2 * var + 1
-    var += 1
-    ans = max(opt, ans)
-
-print(ans)
-
-# ABC119 D - Lazy Faith
-A, B, Q = getNM()
-# 神社
-S = getArray(A)
-# 寺
-T = getArray(B)
-query = getArray(Q)
-
-S.insert(0, -float('inf'))
-T.insert(0, -float('inf'))
-S.append(float('inf'))
-T.append(float('inf'))
-
-def close(data, point):
-    west = data[bisect_left(data, point) - 1]
-    east = data[bisect_left(data, point)]
-
-    return west, east
-
-for i in range(Q):
-    now = query[i]
-    shrine_west, shrine_east = close(S, now)
-    temple_west, temple_east = close(T, now)
-
-    ww = now - min(shrine_west, temple_west)
-    we_1 = (now - shrine_west) * 2 + (temple_east - now)
-    we_2 = (now - temple_west) * 2 + (shrine_east - now)
-    ee = max(shrine_east, temple_east) - now
-    ew_1 = (shrine_east - now) * 2 + (now - temple_west)
-    ew_2 = (temple_east - now) * 2 + (now - shrine_west)
-
-    print(min(ww, we_1, we_2, ee, ew_1, ew_2))
-
-# ABC137 D - Summer Vacation
-# ヒープ使った貪欲
-N, M = getNM()
-query = [getList() for i in range(N)]
-
-A_list = [[] for i in range(10 ** 5 + 1)]
-for a, b in query:
-    A_list[a].append(b)
-
-job = []
-heapq.heapify(job)
-
-ans = 0
-for i in range(1, M + 1):
-    for j in A_list[i]:
-        heapq.heappush(job, -j)
-    if len(job) > 0:
-        u = heapq.heappop(job)
-        ans += -u
-print(ans)
-
-# ABC169 E - Count Median
-N = getN()
-A = []
-B = []
-for i in range(N):
-    a, b = getNM()
-    A.append(a)
-    B.append(b)
-A.sort()
-B.sort()
-
-# 範囲がN個ある
-# Xは整数
-# 中央値のmin, maxは？
-# Nが偶数、奇数の場合
-# 奇数の場合 中央値は絶対に整数
-# 中央値のmin: Aの中央値、max: Bの中央値
-# 偶数の場合
-# 中央値のmin: (Ai-1 + Ai) / 2 max: (Bi-1 + Bi) / 2
-# いくつある？
-
-# 中央値は最低でも0.5刻み
-# 偶数の場合は奇数の2N - 1になる？
-# Ai-1とAiを自由にいじることで0.5, 1, 1.5と言う風に中間値を作れそう
-
-if N % 2 == 0:
-    opt_a = (A[(N // 2) - 1] + A[N // 2]) / 2
-    opt_b = (B[(N // 2) - 1] + B[N // 2]) / 2
-    # opt_b - opt_aを0.5で割って +1
-    # intで出せ
-    print(int((opt_b - opt_a) * 2 + 1))
-else:
-    opt_a = A[N // 2]
-    opt_b = B[N // 2]
-    # 中央値は絶対に整数
-    print(opt_b - opt_a + 1)
-
-# AGC029 B - Powers of two
-# N = getN()
-# A = getList() # ボール
-N = getN()
-A = getList()
-A.sort()
-# ボールからペアを作ってその和が２冪(2 ** iになる)になるようにしたい
-
-# 最大でいくつペアを作れるか
-# 最大流？Nが大きいから無理
-
-# M <= 2 * 10 ** 5
-# 数字aのペアの候補は？
-# 1なら3 (4), 7 (8), 15(16)...
-# 2なら2 (4), 6 (8), 14(16)...
-# 探索して求められるか？
-# 2冪の数はだいたい30個 各要素のペアになるものは求められる
-powers = [1] * 32
-for i in range(1, 32):
-    powers[i] = powers[i - 1] * 2
-
-# Aの中に30個の候補のどれかが含まれているか
-# 二分探索でどのペアかもわかる
-
-# ペアとなるbがあったとして、どれにくっつけるのが有効か？
-# 数を絞れば最大流できる Aが同じ数の場合があるのでできない
-# dp?
-
-# 実は大きい数の方がペアになる条件は厳しくなる
-# 貪欲ペアは厳しい順からペアにしていく
-flag = [0] * N
-ans = 0
-while A:
-    u = A.pop() # 自身とペアにならないようにpop
-    if flag[len(A)]: # uのフラグが立っていたなら飛ばす
-        continue
-    # 自身より大きい２冪の数の中で最も小さいもの
-    # これがAの中に存在するか
-    opt = powers[bisect_right(powers, u)] - u
-    # 存在するか二分探索
-    left = bisect_left(A, opt)
-    right = bisect_left(A, opt + 1)
-    # 存在する'1, 1, 1...'のうちフラグが立っていないものとペアに
-    # 後ろから探索する
-    for i in range(right - 1, left - 1, -1):
-        if not flag[i]:
-            ans += 1
-            flag[i] = 1
-            break
-print(ans)
-
-# キーエンス プログラミング コンテスト 2020 B - Robot Arms
-# 区間スケジューリング問題
-# 終点をソート
-N = getN()
-query = [getList() for i in range(N)]
-
-r_l = []
-for x, l in query:
-    r_l.append([x - l, x + l])
-r_l.sort(key = lambda i:i[1])
-
-cnt = 0
-last = r_l[0][1]
 for i in range(1, N):
-    if r_l[i][0] < last:
-        cnt += 1
+    # 前のより左にあれば更新
+    if P[i][1] < P[i - 1][1]:
+        ans = max(ans, now)
+        now = 1
     else:
-        last = r_l[i][1]
-print(N - cnt)
+        now += 1
 
-# CODE FESTIVAL 2016 qual B D - Greedy customers
+ans = max(ans, now)
 
-"""
-greedy?
-Aは各人の所持金
-Pを指定する　減らせる人については減らす
-1で売ったらええんちゃう？
+print(N - ans)
 
-Aの最小値 - 1を各要素から引く(できる限り1で売る)
-3
-2 3 5の時
-1 2 4 になる
-もう一度1は不可能
-2もだめ
-3売るか
-Aでかいしループは無理　リストにもできない
-セグ？セグ×　貪欲でなんとかなる
+# Donutsプロコンチャレンジ2015 C - 行列のできるドーナツ屋
+# LISの応用
 
-前の奴 + 1でできる限り売る
-買うのは最初の人だけか
+N = getN()
+H = getList()
 
-A [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9]
-3に1を２回提示 3が1に
-4に2を1回提示 4が2に
-5に3を1回提示 5が2に
+# まあBITだろう
+# 逆から置くBITではない？
+# 累積和？
+# 個数だけ求めればいい
 
-後ろの奴を効率的に優先席にできない？
-間の邪魔な奴を1にできれば
-"""
+# 地点iからは
+# i - 1起点の単純増加列
+# LIS？
+ans = [0] * N
+L = []
+# 人0 ~ iまでがどのように見えるか これを人i + 1が見る
+for i in range(N - 1):
+    # もしLの一番小さいやつよりH[i]が大きければ
+    while L and L[-1] < H[i]:
+        L.pop()
+    L.append(H[i])
+    ans[i + 1] = len(L)
 
+for i in ans:
+    print(i)
+
+# ABC134 E - Sequence Decomposing
 N = getN()
 A = getArray(N)
 
-ans = A[0] - 1 # 最初は先頭にA[0] - 1回売りつける
-now = 2 # 先頭に売らないよう2に
-
-for i in range(1, N):
-    if now == A[i]: # 減らせないし次からnow円で売れない
-        now += 1
-    if now < A[i]: # 売れる　その場合は適当に調整して1にする
-        # now = 2 A[i] = 8なら
-        # 2, 2, 3を売りつければA[i] = 1になる
-        if A[i] % now == 0:
-            cnt = (A[i] // now) - 1
-            ans += cnt
+def lis(A):
+    L = deque([A[0]])
+    for a in A[1:]:
+        # Lのどの数より小さくければ
+        # 繋げられるものがない
+        if a <= L[0]:
+            L.appendleft(a)
         else:
-            cnt = A[i] // now
-            ans += cnt
+            # L[bisect_left(L, a) - 1]
+            # Lの中のa未満の数字のうちの最大値
+            L[bisect_left(L, a) - 1] = a
+    return len(L)
 
-print(ans)
+print(lis(A))
+
+N = 10
+A = [0, 6, 9, 9, 2, 3, 4, 5, 10, 3]
+
+ans = deque([A[0]])
+for i in range(1, N):
+    # ans[index] A[i]が挟みこめる場所
+    # A[0] <= A[i]なら0になる
+    # ans[index - 1]: A[i]未満で一番大きい数字
+    index = bisect_left(ans, A[i])
+    if index == 0:
+        ans.appendleft(A[i])
+    else:
+        # 同じ数が複数ある場合は一番最後の数字が更新される
+        ans[index - 1] = A[i]
+print(len(ans))
+
+# ABC165 F - LIS on Tree
+# 頂点1から頂点kまでの最短パス上
+# ルートはO(n)で求められる
+N = getN()
+A = getList()
+query = [getList() for i in range(N - 1)]
+
+dist = [[] for i in range(N)]
+for i in range(N - 1):
+    a, b = query[i]
+    dist[a - 1].append(b - 1)
+    dist[b - 1].append(a - 1)
+
+ignore = [0] * N
+ignore[0] = 1
+lis = [A[0]]
+rec = [0] * N
+rec[0] = 1
+
+# 行きがけ帰りがけの要領
+def dfs(u):
+    global lis
+    for i in dist[u]:
+        if ignore[i] != 1:
+            ignore[i] = 1
+            # 巻き戻し用
+            plus = 0 # true or false
+            change = (0, 0, 0) # true or false, 変更した場所、変更した数値
+
+            if A[i] > lis[-1]:
+                lis.append(A[i])
+                plus = 1
+            else:
+                index = bisect_left(lis, A[i])
+                change = (1, index, lis[index])
+                lis[index] = A[i]
+            rec[i] = len(lis)
+            dfs(i)
+            # 巻き戻す
+            if plus:
+                lis.pop()
+            else:
+                lis[change[1]] = change[2]
+
+dfs(0)
+for i in rec:
+    print(i)
+
+# ACLC1 A - Reachable Towns
+
+N = getN()
+Q = [getList() for i in range(N)]
+que = deepcopy(Q)
+que.sort(key = lambda i:i[1], reverse = True)
+que.sort()
+
+# xy座標が共に大きいもの
+# 順列になっている？
+
+"""
+O(n**2)
+U = UnionFind(N)
+for i in range(N):
+    for j in range(i + 1, N):
+        if que[i][1] < que[j][1]:
+            U.union(i, j)
+for i in range(N):
+    print(U.count(i))
+"""
+#　ソート方法はこれでOK
+# やらなくていい探索がある　それを減らす
+# 4 3
+# 4 1
+# 4 2
+# 3 1
+# 3 2
+# 1 2 これだけいる
+
+# 4 3 1 2でi < jになるものをペアに
+# 1とペアにできるのは2, 3, 4
+# 2とペアにできるのは3, 4
+# 3は4
+# それぞれ右側にあれば
+
+# 6 7 5 3 2 4 1
+# グループ１ 6 7
+# グループ2 5
+# グループ3 3 2 4
+# グループ4 1
+# どれか１つのグループに属する
+
+U = UnionFind(N + 1)
+group = []
+
+for x, y in que:
+    # １番目のものは必ずグループのリーダーになれる
+    if not group:
+        group.append(y)
+        continue
+    # リーダーが降順に並ぶように
+    if y < group[-1]:
+        group.append(y)
+        continue
+    opt = float('inf')
+    # グループ再編成
+    while group:
+        # yより小さいものは全てyが所属するグループに入る
+        if y > group[-1]:
+            l = group.pop()
+            U.union(l, y)
+            opt = min(opt, l) # yが所属するグループの中のリーダー　一番最初のものが記録される
+        else:
+            break
+    # リーダー変更
+    group.append(opt)
+
+for i in range(N):
+    print(U.count(Q[i][1])) # yがiのもののサイズの大きさ　uf.funcはインデックスで呼ばなくてもいい
+
+# ARC091 E - LISDL
+
+"""
+最長増加部分列の長さはA　
+最長減少部分列の長さはB
+使える数字は1 ~ N
+一番都合のいいものを探す
+A + B > N + 1ならダメっぽい
+A + B = Nの場合
+2 3 5 + 5 4
+2 3 1 5 4
+1を適当な所に置けば良い
+A + B = N + 1の場合は簡単
+N = 5
+A, B = 3, 3
+3 4 5 + 5 2 1
+3 4 5 2 1
+
+A + B < N の場合
+いらないものを適当に置けば
+いいのでは？
+N = 5
+A, B = 2, 2 は無理 A + B <= 4は無理
+2 3 + 3 1 (4, 5はいらない)
+2 3 1 + 4 5
+
+(5, 6, 3, 1, 4, 2)
+4 5 6 1 2 3
+
+def lis(A):
+    L = [A[0]]
+    for a in A[1:]:
+        if a > L[-1]:
+            L.append(a)
+        # このelseに引っかかった時にトランプのソートが必要
+        else:
+            L[bisect_left(L, a)] = a
+    return len(L)
+
+for i in permutations([1, 2, 3, 4, 5, 6, 7]):
+    if lis(i) + lis(list(reversed(i))) <= 5:
+        print(lis(i), lis(list(reversed(i))), i)
+
+相方を2にした場合の下限は(N + 1) // 2 ?
+5なら3 2
+6なら3 2
+7なら4 2
+8なら4 2
+3 4 5 + 5 2
+3 4 5 1 2
+4 5 6 + 6 3
+4 5 6 1 2 3
+4 5 6 7 + 6 3
+4 5 6 7 1 2 3
+細かく区切ればもっといける
+N = 16なら
+[13 14 15 16] [9 10 11 12] [5 6 7 8] [1 2 3 4]
+A = 4, B = 4
+A * B >= Nであれば作れる
+
+LISはグループで分割しよう！！！
+"""
+
+N, A, B = getNM()
+
+# mainの長さが半分より下ならout
+if A * B < N or N + 1 < A + B:
+    print(-1)
+    exit()
+
+if A == 1:
+    if B == N:
+        print(*[i for i in range(N, 0, -1)])
+    else:
+        print(-1)
+    exit()
+
+if B == 1:
+    if A == N:
+        print(*[i for i in range(1, N + 1)])
+    else:
+        print(-1)
+    exit()
+
+# グループ内の要素の数がA,グループの数がB
+res1 = [i + 1 for i in range(N - A, N)]
+res2 = []
+L = [i + 1 for i in range(N - A - 1, -1, -1)] # 残りN - A個
+
+# 残りN - A個をB - 1個で分割する
+ind = (N - A) // (B - 1)
+for i in range(B - 1):
+    opt = []
+    for j in range(ind + (i < (N - A) % (B - 1))):
+        u = L.pop()
+        opt.append(u)
+    res2.append(opt)
+
+res2 = list(reversed(res2))
+
+ans = res1
+for i in range(B - 1):
+    ans += res2[i]
+
+def lis(A):
+    L = [A[0]]
+    for a in A[1:]:
+        if a > L[-1]:
+            L.append(a)
+        # このelseに引っかかった時にトランプのソートが必要
+        else:
+            L[bisect_left(L, a)] = a
+    return len(L)
+
+# print(lis(ans), lis(list(reversed(ans))))
+print(*ans)
