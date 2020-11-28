b3c1a1d09371f245d51c790ebc3f111098043644
customaddone <custom1485@docomo.ne.jp>
2020-11-28 14:06:01 +0900

    行列

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -51,206 +51,110 @@ dy = [0, 1, 0, -1]
 # Main Code #
 #############
 
-N, K = 7, 6
-S = [4, 3, 1, 1, 2, 10, 2]
-
-# 左を伸ばしていく
-# その部分列に含まれる全ての要素の値の積は「K以下」である。
-# lはrをオーバーすることもある
-
-if 0 in S:
-    print(N)
-    exit()
-else:
-    l, ans, total = 0, 0, 1
-    for r in range(N):
-        total *= S[r]
-        while total > K and l <= r:
-            total //= S[l]
-            l += 1
-        ans = max(ans, r - l + 1)
-print(ans)
-
-# (条件) 連続部分列に含まれる全ての要素の値の和は、「K以上」である。
-N, K = 4, 10
-A = [6, 1, 2, 7]
-
-left = 0
-total = 0
-ans = 0
-
-for right in range(0, N):
-    total += A[right]
-    while total >= K:
-        ans += N - right
-        total -= A[left]
-        left += 1
-print(ans)
-
 N = 10
-S = 15
-A = [5, 1, 3, 5, 10, 7, 4, 9, 2, 8]
-right = 0
-total = 0
-ans = 0
-# S以上を求める場合にはこの形で
-for left in range(N):
-    while right < N and total < S:
-        total += A[right]
-        right += 1
-    if total < S:
-        break
-    if left == right:
-        right += 1
-    total -= A[left]
+logk = N.bit_length()
 
-# 要素の種類についての問題
-# 全ての要素を含む
-P = 5
-A = [1, 8, 8, 8, 1]
-dict = {}
-for i in A:
-    dict[i] = 0
-# 要素の種類数
-V = len(dict.items())
+# [Fi+2, Fi+1] = [[1, 1], [1, 0]][Fi+1, Fi]
+# 一般項が出ない漸化式は行列の形に落とし込める
+dp = [[[0, 0] for i in range(2)] for i in range(logk)]
+dp[0] = [[1, 1], [1, 0]]
 
-# 事象の数をカウント
-cnt = 0
-right = 0
-# １つ目から全ての事象をカバーするまでrightを進める
-while right < P:
-    if dict[A[right]] == 0:
-        cnt += 1
-    dict[A[right]] += 1
+# 行列掛け算 O(n3)かかる
+def array_cnt(ar1, ar2):
+    h = len(ar1)
+    w = len(ar2[0])
+    row = ar1
+    col = []
+    for j in range(w):
+        opt = []
+        for i in range(len(ar2)):
+            opt.append(ar2[i][j])
+        col.append(opt)
 
-    if cnt == len(dict.items()):
-        break
+    res = [[[0, 0] for i in range(w)] for i in range(h)]
+    for i in range(h):
+        for j in range(w):
+            cnt = 0
+            for x, y in zip(row[i], col[j]):
+                cnt += x * y
+            res[i][j] = cnt
+    return res
 
-    right += 1
-print(l, r)
+for i in range(1, logk):
+    dp[i] = array_cnt(dp[i - 1], dp[i - 1])
 
-l = 0
-# 右を一つ進めて左をできる限り進める
-for r in range(right + 1, P):
-    # 新しく一つ加える
-    dict[A[r]] += 1
-    while True:
-        # もし要素が一つしか無かったら削れない
-        if dict[A[l]] == 1:
+# 行列の単位元
+ans = [[1, 0], [0, 1]]
+for i in range(logk):
+    if N & (1 << i):
+        ans = array_cnt(ans, dp[i])
+
+# [Fi+2, Fi+1] = [[1, 1], [1, 0]][Fi+1, Fi]より
+# [Fn+1, Fn] = A ** n[F1, F0] = A ** n[1, 0]
+# Fn = ans[1][0] * 1 + ans[1][1] * 0
+print(array_cnt(ans, [[1], [0]])[1][0])
+
+# ABC113 D - Number of Amidakuji
+
+H, W, K = getNM()
+logk = H.bit_length()
+
+if W == 1:
+    print(1)
+    exit()
+
+mat = [[0] * W for i in range(W)]
+# 列の数はH個
+# うまく行列に
+for bit in range(1 << (W - 1)):
+    for i in range(1, (W - 1)):
+        # 両方にフラグがあればとばす
+        if bit & (1 << i) and bit & (1 << (i - 1)):
             break
-        dict[A[l]] -= 1
-        l += 1
-    print(l, r)
+    else:
+        for i in range(W):
+            if bit & (1 << i):
+                # 目的地、出発地
+                mat[i + 1][i] += 1
+            # negative shiftしないよう
+            elif i > 0 and bit & (1 << (i - 1)):
+                mat[i - 1][i] += 1
+            else:
+                mat[i][i] += 1
 
-# 各要素にダブりがない範囲
-N = 6
-A = [1, 2, 2, 3, 4, 4]
+dp = [[[0] * W for i in range(W)] for i in range(logk)]
+dp[0] = mat
 
-dict = defaultdict(int)
-l = 0
-for r in range(N):
-    while dict[A[r]] == 1:
-        dict[A[l]] -= 1
-        l += 1
-    print(l, r)
-    dict[A[r]] += 1
+# 行列掛け算 O(n3)かかる
+def array_cnt(ar1, ar2):
+    h = len(ar1)
+    w = len(ar2[0])
+    row = ar1
+    col = []
+    for j in range(w):
+        opt = []
+        for i in range(len(ar2)):
+            opt.append(ar2[i][j])
+        col.append(opt)
 
-N = 4
-A = [2, 5, 4, 6]
+    res = [[[0, 0] for i in range(w)] for i in range(h)]
+    for i in range(h):
+        for j in range(w):
+            cnt = 0
+            for x, y in zip(row[i], col[j]):
+                cnt += x * y
+            res[i][j] = cnt
+    return res
 
-l, ans, xo, total = 0, 0, 0, 0
+for i in range(1, logk):
+    dp[i] = array_cnt(dp[i - 1], dp[i - 1])
 
-for r in range(N):
-    xo ^= A[r]
-    total += A[r]
+# スタート地点
+ans = [[0] * W]
+ans[0][0] = 1
+# 計算
+for i in range(logk):
+    if H & (1 << i):
+        ans = array_cnt(ans, dp[i])
 
-    # xo == totalになるまでA[l]で引き続ける
-    while xo < total:
-        xo ^= A[l]
-        total -= A[l]
-        l += 1
-
-    ans += r - l + 1
-
-print(ans)
-
-
-N, K = 10, 4
-A = [100, 300, 600, 700, 800, 400, 500, 800, 900, 900]
-
-right, ans = 0, 0
-for left in range(N):
-    # 単調増加するとこまでもしくは長さKになるまで
-    while right < N - 1 and A[right] < A[right + 1] and right - left < K - 1:
-        right += 1
-    # もし長さKまで伸ばせたらans += 1
-    if right - left == K - 1:
-        ans += 1
-    # 前に進めないならright += 1
-    if left == right and right < N:
-        right += 1
-print(ans)
-
-# 第5回 ドワンゴからの挑戦状 予選 C - k-DMC
-
-"""
-Q <= 75?
-文字列 dpか
-k-DMCを求めよ
-整数の組の個数 dp or combo
-D, M, Cの順で並んでおり、文字の長さがk以下である
-文字の長さを伸ばしていく
-k = 1, 2... Qについて k - DMC数の数は？
-累積の数を求める
-
-長さがiのものをレコードしていく
-文字の長さはaとcのみに依存する
-
-A = []
-B = []
-C = []
-for i in range(N):
-    if S[i] == 'D':
-        A.append(i)
-    if S[i] == 'M':
-        B.append(i)
-    if S[i] == 'C':
-        C.append(i)
-これだとO(N ** 2)
-
-a ~ c間にあるbの個数を累積和で求める
-あるaについて対応するcが何個あるかは二分探索で求められる
-間のbについては0 ~ cまで - 0 ~ aまで
-
-二重累積和 + 計算量logNの改善
-累積二分探索は尺取り法使える
-"""
-
-N = getN()
-S = list(input())
-Q = getN()
-K = getList()
-
-for k in K:
-    ans = 0
-    d_cnt = 0 # dの数
-    m_cnt = 0 # mの数
-    dm_cnt = 0 # dとmの組み合わせの数
-    for i in range(N):
-        # 上限超えたので捨てる
-        if i - k >= 0:
-            if S[i - k] == "D":
-                d_cnt -= 1 # dを一つ捨て
-                dm_cnt -= m_cnt # 捨てたdはmを現在ホールドしてる個数持っているので（dが左端にあるから）
-            elif S[i - k] == "M":
-                m_cnt -= 1 # mを捨てる
-
-        if S[i] == "D":
-            d_cnt += 1
-        elif S[i] == "M":
-            m_cnt += 1
-            dm_cnt += d_cnt # 現在のd * 新しく入ったm(1つ)
-        elif S[i] == "C":
-            ans += dm_cnt # dm_cntの数 * 新しく入ったc(1つ)
-
-    print(ans)
+print(ans[0][K - 1] % mod)
