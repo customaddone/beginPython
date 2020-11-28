5f1d7ba13702d520559105d56a4150b8e42b68f4
customaddone <custom1485@docomo.ne.jp>
2020-11-28 14:52:19 +0900

    xor

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -51,110 +51,286 @@ dy = [0, 1, 0, -1]
 # Main Code #
 #############
 
-N = 10
-logk = N.bit_length()
+# ARC021 B - Your Numbers are XORed...
 
-# [Fi+2, Fi+1] = [[1, 1], [1, 0]][Fi+1, Fi]
-# 一般項が出ない漸化式は行列の形に落とし込める
-dp = [[[0, 0] for i in range(2)] for i in range(logk)]
-dp[0] = [[1, 1], [1, 0]]
+"""
+2 010 + 4 100 = 6 110
+3 0011 + 12 1100 = 15 1111
 
-# 行列掛け算 O(n3)かかる
-def array_cnt(ar1, ar2):
-    h = len(ar1)
-    w = len(ar2[0])
-    row = ar1
-    col = []
-    for j in range(w):
-        opt = []
-        for i in range(len(ar2)):
-            opt.append(ar2[i][j])
-        col.append(opt)
+元のA:{A1, A2...}を知りたい
+Bi = Ai xor Ai+1
+Bi+1 = Ai+1 xor Ai+2
+Bi xor Bi+1 = Ai xor Ai+1 xor Ai+1 xor Ai+2
+B1 xor B2 xor...Bn-1 = Ai xor An ?
+Bn = An xor A1
 
-    res = [[[0, 0] for i in range(w)] for i in range(h)]
-    for i in range(h):
-        for j in range(w):
-            cnt = 0
-            for x, y in zip(row[i], col[j]):
-                cnt += x * y
-            res[i][j] = cnt
-    return res
+B1 xor B2 xor...Bn = A1 xor A1 = 0 ?
+該当する数列が存在するならこれは成立つ
+B1 ^ A1 = A2
+B2 ^ A2 = B2 ^ (B1 ^ A1) = A3
+"""
 
-for i in range(1, logk):
-    dp[i] = array_cnt(dp[i - 1], dp[i - 1])
+L = getN()
+B = getArray(L)
 
-# 行列の単位元
-ans = [[1, 0], [0, 1]]
-for i in range(logk):
-    if N & (1 << i):
-        ans = array_cnt(ans, dp[i])
+now = B[0]
+for i in range(1, L):
+    now ^= B[i]
 
-# [Fi+2, Fi+1] = [[1, 1], [1, 0]][Fi+1, Fi]より
-# [Fn+1, Fn] = A ** n[F1, F0] = A ** n[1, 0]
-# Fn = ans[1][0] * 1 + ans[1][1] * 0
-print(array_cnt(ans, [[1], [0]])[1][0])
+if not now:
+    # A1は辞書順最小なので0に
+    a = 0
+    for i in range(L):
+        print(a)
+        a ^= B[i]
+else:
+    print(-1)
 
-# ABC113 D - Number of Amidakuji
+# ABC098 D - Xor Sum 2
+# 連続する区間の長さを答える　尺取り
 
-H, W, K = getNM()
-logk = H.bit_length()
+N = getN()
+A = getList()
 
-if W == 1:
-    print(1)
-    exit()
+l, ans, xo, total = 0, 0, 0, 0
 
-mat = [[0] * W for i in range(W)]
-# 列の数はH個
-# うまく行列に
-for bit in range(1 << (W - 1)):
-    for i in range(1, (W - 1)):
-        # 両方にフラグがあればとばす
-        if bit & (1 << i) and bit & (1 << (i - 1)):
-            break
+for r in range(N):
+    xo ^= A[r]
+    total += A[r]
+
+    # xo == totalになるまでA[l]で引き続ける
+    while xo < total:
+        xo ^= A[l]
+        total -= A[l]
+        l += 1
+
+    ans += r - l + 1
+
+print(ans)
+
+# 第五回ドワンゴからの挑戦状 B - Sum AND Subarrays
+
+"""
+N <= 1000
+連続部分列は全て取り出せる
+どうやってK個を選ぶか
+
+optを達成できるか
+bit数が大きい方が絶対正義
+とにかく大きいbitを立てる
+"""
+
+N, K = getNM()
+A = getList()
+
+l = []
+for i in range(N):
+    now = 0
+    for j in range(i, N):
+        now += A[j]
+        l.append(now)
+
+flag = 0 # フラグを強化していく
+
+# フラグiが建っているものを集める
+for i in range(60, -1, -1):
+    opt = flag | (1 << i)
+    cnt = 0
+    for j in range(len(l)):
+        if l[j] & opt == opt:
+            cnt += 1
+    if cnt >= K:
+        flag |= (1 << i)
+
+print(flag)
+
+
+# ABC117 D - XXOR
+N, K = getNM()
+A = getList()
+
+# 各X xor Aiについて
+# 各桁について
+# Xにフラグ立つ + Aiにフラグ立たない
+# Xにフラグ立たない + Aiにフラグ立つ　の時 2 ** iだけxorの値が増える
+# Aの各要素の2 ** iのフラグの合計がn本の時
+# Xの2 ** iのフラグを立てるとN - n * 2 ** i、立てないとn * 2 ** i　f(x)の値が増える
+
+# 各桁のフラグが合計何本あるか
+flag = [0] * 61
+def splitbit(n):
+    for i in range(61):
+        if n & (1 << i):
+            flag[i] += 1
+for i in range(N):
+    splitbit(A[i])
+
+x = 0
+ans = 0
+for i in range(60, -1, -1):
+    # flag[i] < N - flag[i]ならフラグを立てるほうがお得
+    # だがKの制限があり立てたくても立てられないことがある
+    # Xの2 ** iのフラグを立ててもXがKを超えないか
+    if flag[i] < N - flag[i] and x + 2 ** i <= K:
+        # Xにフラグを立てる
+        x += 2 ** i
+        # f(x)の値が増える
+        ans += 2 ** i * (N - flag[i])
+    # flag[i] < N - flag[i]だがフラグを立てられない場合 +
+    # flag[i] >= N - flag[i]の時
     else:
-        for i in range(W):
-            if bit & (1 << i):
-                # 目的地、出発地
-                mat[i + 1][i] += 1
-            # negative shiftしないよう
-            elif i > 0 and bit & (1 << (i - 1)):
-                mat[i - 1][i] += 1
-            else:
-                mat[i][i] += 1
+        ans += 2 ** i * flag[i]
 
-dp = [[[0] * W for i in range(W)] for i in range(logk)]
-dp[0] = mat
+print(ans)
 
-# 行列掛け算 O(n3)かかる
-def array_cnt(ar1, ar2):
-    h = len(ar1)
-    w = len(ar2[0])
-    row = ar1
-    col = []
-    for j in range(w):
-        opt = []
-        for i in range(len(ar2)):
-            opt.append(ar2[i][j])
-        col.append(opt)
+# ABC121 D - XOR World
+A, B = getNM()
+# bit1桁目のフラグの個数
+# 周期は2 ** 1
+# 0と1が交互に
+# bit2桁目のフラグの個数
+# 周期は2 ** 2
+flags1 = [0] * 61
+flags2 = [0] * 61
+# 1 ~ nまでに各桁のフラグが何本立つか計算する関数
+def bitflag(n, flaglist):
+    if n > 0:
+        for i in range(1, 61):
+            split = 2 ** i
+            flag1 = (n // split) * (split // 2)
+            flag2 = max(n % split + 1 - (split // 2), 0)
+            flaglist[i] += flag1 + flag2
+# 1 ~ A - 1について（Aは範囲に入っているため）
+bitflag(A - 1, flags1)
+bitflag(B, flags2)
+for i in range(61):
+    flags2[i] -= flags1[i]
+ans = 0
+# 奇数ならフラグが立つ
+for i in range(61):
+    if flags2[i] % 2 != 0:
+        ans += 2 ** (i - 1)
+print(ans)
 
-    res = [[[0, 0] for i in range(w)] for i in range(h)]
-    for i in range(h):
-        for j in range(w):
-            cnt = 0
-            for x, y in zip(row[i], col[j]):
-                cnt += x * y
-            res[i][j] = cnt
-    return res
+# ABC147 D - Xor Sum 4
 
-for i in range(1, logk):
-    dp[i] = array_cnt(dp[i - 1], dp[i - 1])
+N = getN()
+A = getList()
+# Aの各数字の（２進数における）各桁ごとに分解して排他的論理和を求める
+# 例
+# 3
+# 1 2 3 →
+# 1, 10, 11
+# 2 ** 0の桁について(1 ^ 2) 1 ^ 0 = 1,(1 ^ 3) 1 ^ 1 = 0,(2 ^ 3) 0 ^ 1 = 1
+# 2 ** 1の桁について 0(1の2 ** 1の桁は0) ^ 1 = 1, 0 ^ 1 = 1, 1 ^ 1 = 0
+# 各桁について2 ** iの桁が1の数字の選び方 * 2 ** iの桁が0の数字の選び方 * 2 ** iを
+# 足し合わせる
+lista = [[0, 0] for i in range(61)]
+# bitの各桁が１か０かをlistaに収納
+def splitbit(n):
+    for i in range(61):
+        if n & (1 << i):
+            lista[i][1] += 1
+        else:
+            lista[i][0] += 1
+for i in A:
+    splitbit(i)
+ans = 0
+for i in range(61):
+    ans += ((lista[i][0] * lista[i][1]) * (2 ** i)) % mod
+print(ans % mod)
 
-# スタート地点
-ans = [[0] * W]
-ans[0][0] = 1
-# 計算
-for i in range(logk):
-    if H & (1 << i):
-        ans = array_cnt(ans, dp[i])
+# ARC021 B - Your Numbers are XORed...
+L = getN()
+B = getArray(L)
+B_xor = B[0]
+for i in range(1, L - 1):
+    B_xor ^= B[i]
 
-print(ans[0][K - 1] % mod)
+# B_xor ^ a1(aの最後) ^ a1 == Bの最後なら成立
+# この時aがどんな値であろうと条件が成立する
+if B_xor == B[-1]:
+    now = 0
+    print(now)
+    # a2 = B1 ^ a1
+    # a3 = B2 ^ a2
+    for j in range(L - 1):
+        now = B[j] ^ now
+        print(now)
+else:
+    print(-1)
+
+# Tenka1 Programmer Contest D - IntegerotS
+
+"""
+Aのbitの総和がK以下になるように整数を集める
+ナップサック問題っぽいがAもBもおおきい
+貪欲に行く
+Kを超えた場合でも、他のでフラグを消せればいい
+
+まず全部足し、フラグを抜いていく？
+dpっぽいが2 ** 30の30を使う？
+フラグの数が奇数か偶数か
+
+bitwise or どちらかのビットが1なら1、そうでなければ0
+一度立てたフラグは消えない
+
+Kの各bitについて
+フラグを立てるか立てないか
+0 のbitlengthは0
+1 のbitlengthは1 (右から1番目に最初のフラグが立っている)
+2 のbitlengthは2
+3 のbitlengthは2
+
+[0, 0, 8, 4, 0, 0, 0,
+むやみにフラグを立てないように
+
+K = 101にどう従っていくか
+1本目を1にするなら それ以降の条件にも従ってもらう
+
+Kと比べた時に何本目まで従っているか
+K = 101
+a = 10の場合左から2番目が従えてない
+[[3, 3, 1], [4, 4, -1], [2, 5, 1]]
+
+別に30回回していい
+一本目のKについて
+立てるなら
+Kにフラグが立ってなかったら
+立ててはいけない
+
+K以下の値の2進数換算は
+Kのi番目の1のフラグを1 → 0にし、それ以下を自由にしたもの　である
+フラグの数が多い方がいいので
+整数を集めてできる数をXとすると
+K = 11010の場合は
+X = 01111, 10111, 11001 を見ていけばいい
+足してXになる数を求めればいい
+
+K以下の〜について
+最も有利な条件を並べてそれぞれで計算
+"""
+
+N, K = getNM()
+que = [getList() for i in range(N)]
+
+ans = 0
+# 足すとKのフラグになる
+opt = 0
+for a, b in que:
+    if K | a == K:
+        opt += b
+ans = max(ans, opt)
+
+# 足すとXのフラグになる
+for i in range(31, 0, -1):
+    opt = 0
+    if K & (1 << i): # フラグが立っていれば
+        X = (K ^ (1 << i)) | ((2 ** i) - 1) # 1 << iのフラグを消す + それ以下を全て1に
+        for a, b in que:
+            if X | a == X: # フラグの本数が変わらなければ
+                opt += b
+
+        ans = max(ans, opt)
+
+print(ans)
