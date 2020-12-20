6b2b0e4bd618a4ca4b5e3739948c1c0602b793f6
customaddone <custom1485@docomo.ne.jp>
2020-12-20 12:08:22 +0900

    確率dp

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -51,286 +51,101 @@ dy = [0, 1, 0, -1]
 # Main Code #
 #############
 
-# AGC005 C - Tree Restoring
-
 """
-頂点数N Nは小さいが...
-木です　
-頂点1, 2... について最も遠い頂点がAi
-距離iがいくつあるかcntする
+y = x + (1 - p)y
+全てのアイドルを集めたいです
+N, M は非常に小さい
+bit　dpだろ
+5 2
 
-最遠N - 1の距離が作れるのはパスグラフ
-1 - 2 - 3 - 4 距離3が作れる
-この時距離3は2つ、距離2は2つ
-1 - 2 - 3
-      - 4 の場合
-距離2が3つ、距離1が1つ
-まずパスグラフから考える
-1 - 2 - 3 - 4 距離3が2つできる
-              距離2は2つ
-1 - 2 - 3 - 4
-    　　　 - 5 距離3が3つできる
+2 300　アイドルの人数2, 500円
+3 5 アイドル3が5%で手に入る
+4 95 アイドル4が95%で手に入る
 
-距離n - 1に一つ頂点をつなぐと距離nの頂点ができる
-1 - 2 - 3
-最初のパスグラフを作った時、max - 1, max - 2...の頂点が2個ずつできる
-残った頂点を順番にパスグラフに刺していく
+3 500
+5 20
+1 30
+2 50
 
-距離n - 1に一つ頂点をつなぐと距離nの頂点ができる
-"""
+アイドル一種類の場合
+y = 1 + 0.95y 期待回数は1 / 0.05回
+アイドル複数種類、くじ1個の場合
+1 5
+2 95
+5パーセントの確率で(1を持っている状態)に移動し
+95パーセントの確率で(2を持っている確率)に移動する
+dp[1][0]: 0.0475の確率で何も持ってない
+dp[1][1]: 0.0025の確率で1のみ持っており、
+dp[1][2]: 0.9025の確率で2のみ持っている
+dp[1][3]: 0.0475の確率で2つとも持っている
 
-N = getN()
-A = getList()
+dp[2][0] = 0.0475 * dp[1][0]
+dp[2][1] = 0.0025 * dp[1][0] + 0.0475 * dp[1][1]
+dp[2][2] = 0.9025 * dp[1][0] + 0.0475 * dp[1][2]
+dp[2][3] = 0.0475 * dp[1][0] + 0.95 * dp[1][1] + 0.05 * dp[1][2] + dp[1][3]
+これを回せば答えは求まる
 
-distance = [0] * N
-for i in range(N):
-    distance[A[i]] += 1
-
-opt = [0] * N # 現在距離iの頂点はいくつあるか
-left = N # 残り頂点数
-for i in range(N - 1, -1, -1):
-    if distance[i] > 0:
-        if distance[i] == 1: # 最遠は必ず2つ以上ある
-            print('Impossible')
-            exit()
-        else:
-            # 最初のパスグラフを引く
-            for j in range(i + 1):
-                left -= 1
-                opt[max(i - j, j)] += 1
-            break
-
-# distance（クエリ）とopt(パスグラフ)を見比べる
-for i in range(N - 1, 0, -1):
-    # optは基礎的に存在する頂点
-    # それを下回るようであればout
-    if distance[i] < opt[i]:
-        print('Impossible')
-        exit()
-    else:
-        diff = distance[i] - opt[i]
-        if diff: # 足さないといけない場合、距離がd - 1の頂点がなければいけない
-            if opt[i - 1] == 0:
-                print('Impossible')
-                exit()
-            else:
-                left -= diff
-                opt[i] += diff
-
-if left == 0: # 頂点をちょうど使い切ったら
-    print('Possible')
-else:
-    print('Impossible')
-
-# ABC173 F - Intervals on Tree
-# ある意味Unionfindか
-# 新しい頂点を加えると、連結成分が1 - (既にある頂点へのエッジ)の分増える
-
-"""
-木の問題
-N <= 10 ** 5
-辺があると連結するので連結成分の個数が減る
-L,RをnC2通り求める
-bitは普通に間に合わない
-
-やっぱり分解して考える
-小さいのから
-L = 1の時
-R = 1 ~ N これにO(1)で答える
-R = 1
-1
-R = 2 2は1と繋がってない
-1
+くじが複数個ある場合
+最適な行動は
+どのクジを使うかは全探索
+全てのアイドルがもともとなかったらcontinue
+アイドル1はそのクジでしか引けない　それは絶対アイドルが出るまで回す
+いくつかのクジで引ける場合　最も有利なものを
+クジを融合する？
 2 2
-R = 3 3は1, 2と繋がっている
-1 2 3 1
+2 1000
+1 30
+2 70
+2 800
+1 80
+2 20
 
-頂点1は何回数えられるか
-頂点1が含まれるのは3回
-頂点2が含まれるのは4回
-頂点3が含まれるのは3回
-
-木dpとか
-前から順にやる？
-
-Rで新しい数を入れると急に結合したりする
-数え上げでしょう
-
-ダブった部分を引く
-全ての部分木の大きさを求めるのは無理そう
-新しい要素は高々２つの要素を繋げるだけ
-
-for i in range(N - 1):
-    u, v = getNM()
-    if u > v:
-        u, v = v, u
-    # 前に戻るエッジのみ
-    dist[v - 1].append(u - 1)
-for i in range(N):
-    dist[i].sort()
-
-ans = 0
-for i in range(N): # L
-    cnt = 0
-    for j in range(i, N): # R
-        # 範囲外に伸びるエッジは無視
-        cnt += 1 - (len(dist[j]) - bisect_left(dist[j], i))
-        ans += cnt
-
-これはO(N ** 2)かかる
-[[], [], [], [], [2], [], [1, 3, 4], [3], [0, 7], [5, 8]]
-累積されていく
-何もなければ1 ~ 10の累積で55のはず
-しかし控除が累積して1 + 1 + 4(len([2] + len([1, 3, 4]))) + 5 + 7 + 9 = 27あるのでこれを引く
-dist[4]の位置にある2はL = 1の時も引っかかる
-dist[8]にある0は一回のみ引っかかって被害はN - 8 = 2
-
-エッジが伸びることでいくら減るか
+dp[1] = 0.0025 * dp[0] + 0.0475 * dp[1] + M
+何も手に入らない確率は
 """
 
-N = getN()
-dist = [[] for i in range(N)]
-for i in range(N - 1):
-    u, v = getNM()
-    # 前に行くように
-    if u > v:
-        u, v = v, u
-    dist[v - 1].append(u - 1)
-
-ans = 0
-cnt = 0
-for i in range(N):
-    ans += (i + 1) * (i + 2) // 2
-    # distの探索
-    for j in dist[i]:
-        cnt += (j + 1) * (N - i)
-
-print(ans - cnt)
-
-# ARC028 C - 高橋王国の分割統治
-
-"""
-全方位木dpを使わない方法で
-頂点iについて
-iの子要素の部分木のサイズを調べる
-全方位木dpの要領でやっていく
-①葉から木dpしていく
-②遡る
-"""
-
-N = getN()
-E = [[] for i in range(N)]
-for i in range(N - 1):
-    p = getN()
-    E[i + 1].append(p)
-    E[p].append(i + 1)
-
-# dfsする
-ans = [0] * N
-
-def dfs(u, par):
-    res = 1 # 自分 + 子要素の部分木の大きさの合計
-    opt_c = 0 # 子要素の部分木の大きさの最大値
-    for v in E[u]:
-        if v != par:
-            c = dfs(v, u)
-            res += c
-            opt_c = max(opt_c, c)
-
-    # 親方向の部分木の大きさはN - res
-    ans[u] = max(opt_c, N - res)
-    return res
-
-dfs(0, -1)
-for i in ans:
-    print(i)
-
-"""
-木構造
-M枚のコイン　K回操作
-
-0にコインがない場合は、0に新しいコインを置く
-コインが置かれている頂点を1つ選んで、コインが置かれていない子のどれかに置く
-コインはどんどん降りていく
-Kが過剰であると-1
-
-Kの縛りがない場合は1に1枚目、2に2枚目...とやっていく
-M枚置けるか？ 1に置くと通せんぼしてしまう
-置くごとに部分木の子要素以下を全て使用不可に　それでもM枚置けるか　を判定
-有向木の方が良さそう
-5 2 4
-5 1 1 5 0
-[[1, 2], [], [], [], [0, 3]] edge
-[{0, 1, 2}, {1}, {2}, {3}, {0, 1, 2, 3, 4}] size
-置く順番はトポロジカルになっている
-[1, 2, 2, 1, 0] distance
-1に置きます
-1はmosの中にありますか？
-mos - size[0]は現在のM - 1以上ありますか？
-あれば置く
-for i in range(M): で置けないと-1
-
-Kが入ってくると
-上の条件 + dis[0]だけ消費しても間に合いますか？
-コインを置くのにM回消費　
-移動に使える分はK - M回
-1枚目を1においても後のものを置けますか
-距離の合計がK - Mになるように頂点を選ぶ
-rootにM枚のコインを置きます
-置く場所が決まればあとはトポロジカルに
-
-1に置きたい
-1はmosの中にありますか？
-1の部分木を取り除いた集合で、距離の総和が条件を満たすか
-"""
-
-N, M, K = getNM()
-P = getList()
-E = [[] for i in range(N)]
-for i, v in enumerate(P):
-    if v:
-        E[v - 1].append(i)
-    else:
-        root = i
-
-dis = [1] * N
-size = [set() for i in range(N)]
-
-def dfs(u):
-    global dis
-    size[u].add(u)
-    for v in E[u]:
-        dis[v] = dis[u] + 1
-        size[u] |= dfs(v)
-    return size[u]
-dfs(root)
-
-remain = set(range(N))
-ans = []
-# M個のコインを置きます
+# 何これ？
+N, M = getNM()
+Q = []
+C = [0] * M
 for i in range(M):
-    # 小さい順に探索する
-    for j in range(N):
-        # jはremainに残ってますか
-        if j not in remain:
-            continue
-        can = remain - size[j]
-        l = len(can)
-        # あとM - i - 1個置けますか
-        if l < M - i - 1:
-            continue
-        L = [dis[i] for i in can]
-        L.sort()
-        # 浅い順にM - i - 1個 ~ 深い順にM - i - 1個の中に含まれますか
-        # 1とかあるのでこの範囲内の全ての距離を作れる
-        if sum(L[:M - i - 1]) <= K - dis[j] <= sum(L[l - M + i + 1:]):
-            # 全て通過したので
-            K -= dis[j]
-            ans.append(j + 1)
-            remain -= size[j]
-            break
-    else:
-        ans = [-1]
-        break
+    ido, c = getNM()
+    C[i] = c
+    m1 = []
+    m2 = []
+    for j in range(ido):
+        ind, prob = getNM()
+        m1.append(ind - 1)
+        m2.append(prob / 100)
+    Q.append([m1, m2])
 
-print(*ans)
+prev = [float('inf')] * (1 << N)
+prev[0] = 0
+# クジ
+for _ in range(10):
+    for i, (id, prob) in enumerate(Q):
+        next = [float('inf')] * (1 << N)
+        next[0] = 0
+        # 配るdp
+        d = {}
+        for bit in range(1 << len(id)):
+            tar = 0
+            opt = 1
+            for j in range(len(id)):
+                if bit & (1 << j):
+                    tar |= (1 << id[j])
+                    opt *= prob[j]
+                else:
+                    opt *= (1 - prob[j])
+            d[tar] = opt
+
+        for bit in range(1 << N):
+            opt = C[i]
+            for key, value in d.items():
+                if key and bit & key == key:
+                    opt += prev[bit - key] * value
+            next[bit] = min(next[bit], opt / (1 - d[0]))
+
+        prev = next
+
+    print(prev)
