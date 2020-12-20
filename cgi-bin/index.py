a3a188ef6e164990b9ed273169448106521fa368
customaddone <custom1485@docomo.ne.jp>
2020-12-20 10:24:39 +0900

    木

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -51,288 +51,286 @@ dy = [0, 1, 0, -1]
 # Main Code #
 #############
 
-# JOI本戦 B - IOI饅頭（IOI Manju）
-M, N = getNM() # M:饅頭 N:箱
-sweets = getArray(M)
-box = [getList() for i in range(N)] # 最大C個, E円　1個ずつ
-# 菓子の価格 - 箱の価格 の最大値
-# M <= 10000, N <= 500
-# dp?
-
-sweets.sort(reverse = True)
-imos = [0]
-for i in range(M):
-    imos.append(imos[i] + sweets[i])
-
-# 箱に詰める饅頭の価格を大きく、箱の価格を小さくする
-# 合計でi個詰められる箱を用意した時の箱の価格の最小値
-# 貪欲に前から?
-# grid dp無理め? M <= 10000なのでそこまで求めるだけでいい
-# 500 * 10000
-prev = [float('inf')] * 10001
-prev[0] = 0
-
-for c, e in box:
-    for j in range(10000, -1, -1): # 逆順に
-        if j - c >= 0:
-            prev[j] = min(prev[j], prev[j - c] + e)
-        else:
-            prev[j] = min(prev[j], prev[0] + e)
-
-ans = 0
-for i in range(1, M + 1):
-    ans = max(ans, imos[i] - prev[i])
-print(ans)
-
-# JOI11予選 D - パスタ (Pasta)
-N, K = getNM()
-pasta = [[] for i in range(N)]
-for i in range(K):
-    a, b = getNM()
-    pasta[a - 1].append(b - 1)
-
-# 通りの数を求める: dp, 数え上げ組み合わせ
-# n日前のことが関係する: n次元のdpを作れる
-
-# modが10000
-# パスタは3種類
-# dp[j][k]: 本日jのパスタで、前日kのパスタの通り
-
-prev = [[0] * 3 for i in range(3)]
-# 1日目
-if pasta[0]:
-    prev[pasta[0][0]][(pasta[0][0] + 1) % 3] = 1
-else:
-    for i in range(3):
-        prev[i][(i + 1) % 3] = 1
-
-# 2日目以降
-for p in pasta[1:]:
-    next = [[0] * 3 for i in range(3)]
-    # すでに決められているなら
-    if p:
-        j = p[0]
-        # 本日のパスタjは確定
-        # その前の日のパスタnext[]j[k]のk, prev[j][k]のjは3通り
-        # そのまた前日のパスタprev[j][k]のkは3通り
-        for k in range(3):
-            for p_k in range(3):
-                if j == k and k == p_k:
-                    continue
-                next[j][k] += prev[k][p_k]
-                next[j][k] %= 10000
-    else:
-        for j in range(3):
-            for k in range(3):
-                for p_k in range(3):
-                    if j == k and k == p_k:
-                        continue
-                    next[j][k] += prev[k][p_k]
-                    next[j][k] %= 10000
-    prev = next
-
-ans = 0
-for i in range(3):
-    for j in range(3):
-        ans += prev[i][j]
-        ans %= 10000
-print(ans)
-
-# Code Formula 2014 本選 D - 映画の連続視聴
+# AGC005 C - Tree Restoring
 
 """
-N <= 3000 O(N ** 2)まで
-Mの映画が[S ~ E]までの間上映されている
-同じ映画を見ることでより多くの幸福感
-違う映画を見るとリセットされる
-上映時刻がダブってなければ連続して視聴可能
+頂点数N Nは小さいが...
+木です　
+頂点1, 2... について最も遠い頂点がAi
+距離iがいくつあるかcntする
 
-まず貪欲を考える
-区間スケジューリング
-終わりの時刻でソート
-[[2, 10, 40], [1, 0, 120], [1, 15, 135], [1, 240, 330]]
-どの映画を１回見ても幸福度は同じ　出来るだけ連続させた方がいい？
-連続してみると幸福度は絶対上がる
-全探索？
-同じ映画を見た場合、違う映画を見た場合
-S, Eが小さいのでDPできそうO(N ** 2)できるので
-同じ種類のものは連結して考えられる
-1: [1, 0, 120], [1, 15, 135], [1, 240, 330]
-2:    [2, 10, 40]
-[2, 10, 40]をとる　次取れるのは[1, 240, 330]
-[2, 10, 40]をとらない
-2 → 3と繋いで100しか上がらなくでも、3 → 4とつなぐと100000上がるかもしれない
-streak1: 何個
-streak2: 何個..という風に数える
-合成して区間スケジューリングかも
-3 + 2がバッティングしても2 + 2なら通るかもしれない
-2 + 2より3 + 1の方が強い
-MAX連続を想定する
+最遠N - 1の距離が作れるのはパスグラフ
+1 - 2 - 3 - 4 距離3が作れる
+この時距離3は2つ、距離2は2つ
+1 - 2 - 3
+      - 4 の場合
+距離2が3つ、距離1が1つ
+まずパスグラフから考える
+1 - 2 - 3 - 4 距離3が2つできる
+              距離2は2つ
+1 - 2 - 3 - 4
+    　　　 - 5 距離3が3つできる
 
-### 今いくつ連続してるかを考えなくていいようにする ###
-1つ連続させたもの、2つ連続させたもの...をmovieに混ぜ込む
-セグ木を使う
-合成したものを混ぜ込む
+距離n - 1に一つ頂点をつなぐと距離nの頂点ができる
+1 - 2 - 3
+最初のパスグラフを作った時、max - 1, max - 2...の頂点が2個ずつできる
+残った頂点を順番にパスグラフに刺していく
+
+距離n - 1に一つ頂点をつなぐと距離nの頂点ができる
 """
 
 N = getN()
-H = getList()
-for i in  range(N - 1):
-    H[i + 1] += H[i]
-movie = [getList() for i in range(N)]
-movie.sort(key = lambda i: i[2])
+A = getList()
 
-# セグ木を使わないver
-dp = [0] * (10 ** 5 + 7)
-prev = 0
+distance = [0] * N
 for i in range(N):
-    m, s, e = movie[i]
-    # 初期化
-    for j in range(prev + 1, e + 1):
-        dp[j] = max(dp[j], dp[j - 1])
-    prev = e
+    distance[A[i]] += 1
 
-    dp[e] = max(dp[e], dp[s] + H[0])
-    cnt = 0
+opt = [0] * N # 現在距離iの頂点はいくつあるか
+left = N # 残り頂点数
+for i in range(N - 1, -1, -1):
+    if distance[i] > 0:
+        if distance[i] == 1: # 最遠は必ず2つ以上ある
+            print('Impossible')
+            exit()
+        else:
+            # 最初のパスグラフを引く
+            for j in range(i + 1):
+                left -= 1
+                opt[max(i - j, j)] += 1
+            break
 
-    # 現在のtargetから区間スケジューリング
-    for j in range(i + 1, N):
-        nm, ns, ne = movie[j]
-        if nm != m:
-            continue
-        if e <= ns:
-            cnt += 1
-            e = ne
-            dp[e] = max(dp[e], dp[s] + H[cnt])
-
-print(max(dp))
-
-# みんなのプロコン2019 D - Ears
-
-"""
-必要な回数の最小値　効率的な方法を考える
-散歩開始と終了地点は任意
-
-0 ○ 1 ○ 2 ○ 3 ○ 4
-地点iを
-前方向に i += 1
-後方向に i-1 += 1
-
-出発の回数はsum(A)
-diffの最小値を目指す　二分探索したい
-
-1 - 2の周回で無限に地点2の石を増やせる
-ただし偶奇によって状況は変わる
-0が多い場合は避けるといいぞ
-範囲内は確実に1は増える
-L <= 10 ** 5
-偶奇または0の累積
-セグ木も使える
-
-(ゼロゾーン)(偶数ゾーン)(奇数ゾーン)(偶数ゾーン)(ゼロゾーン)
-両端の偶数ゾーンはなくてもいい
-0の処理は
-LC2でもダメ
-区切りは1つだけ 累積してみる
-任意のゼロゾーンを
-
-独立してゾーンを考えられるはず
-まず奇数ゾーンを置く
-
-偶数ゾーン2つがそれぞれ存在する場合、しない場合を考える
-ゼロも偶数として考えるか
-
-奇数の方がいい区間
-中央で分ける
-それぞれ
-(ゼロゾーン)(偶数ゾーン)
-左側の最小コスト + 右側の最小コスト
-セグ木使えば
-
-マーブルと同じようにDP
-"""
-
-L = getN()
-A = getArray(L)
-
-dp = [[0] * (L + 1) for _ in range(5)]
-
-for i in range(L):
-    if A[i] == 0: # 0のとこに突入するなら
-        t = 2
+# distance（クエリ）とopt(パスグラフ)を見比べる
+for i in range(N - 1, 0, -1):
+    # optは基礎的に存在する頂点
+    # それを下回るようであればout
+    if distance[i] < opt[i]:
+        print('Impossible')
+        exit()
     else:
-        t = A[i] % 2
-    # それぞれ前の状態から遷移できる
-    dp[0][i + 1] = dp[0][i] + A[i]
-    dp[1][i + 1] = min(dp[0][i + 1], dp[1][i] + t)
-    dp[2][i + 1] = min(dp[1][i + 1], dp[2][i] + (A[i] + 1) % 2)
-    dp[3][i + 1] = min(dp[2][i + 1], dp[3][i] + t)
-    dp[4][i + 1] = min(dp[3][i + 1], dp[4][i] + A[i])
+        diff = distance[i] - opt[i]
+        if diff: # 足さないといけない場合、距離がd - 1の頂点がなければいけない
+            if opt[i - 1] == 0:
+                print('Impossible')
+                exit()
+            else:
+                left -= diff
+                opt[i] += diff
 
-print(dp[4][L])
+if left == 0: # 頂点をちょうど使い切ったら
+    print('Possible')
+else:
+    print('Impossible')
 
-# EDPC j - shshi
+# ABC173 F - Intervals on Tree
+# ある意味Unionfindか
+# 新しい頂点を加えると、連結成分が1 - (既にある頂点へのエッジ)の分増える
 
 """
-dp[i][j][k]: i個、j個、k個ある時全部なくすための期待値
-N <= 300 O(N ** 3)まで
-dp[i][j][k] = i / N * dp[i - 1][j][k] + j / N * dp[i][j - 1][k]
-+ k / N * dp[i][j][k - 1] + (N - i - j - k) / N * dp[i][j][k]
-dp[i][j][k]をまとめると
-dp[i][j][k] = (i * dp[i - 1][j][k] + j * dp[i][j - 1][k] + k * dp[i][j][k - 1] + N) / (i + j + k)
+木の問題
+N <= 10 ** 5
+辺があると連結するので連結成分の個数が減る
+L,RをnC2通り求める
+bitは普通に間に合わない
+
+やっぱり分解して考える
+小さいのから
+L = 1の時
+R = 1 ~ N これにO(1)で答える
+R = 1
+1
+R = 2 2は1と繋がってない
+1
+2 2
+R = 3 3は1, 2と繋がっている
+1 2 3 1
+
+頂点1は何回数えられるか
+頂点1が含まれるのは3回
+頂点2が含まれるのは4回
+頂点3が含まれるのは3回
+
+木dpとか
+前から順にやる？
+
+Rで新しい数を入れると急に結合したりする
+数え上げでしょう
+
+ダブった部分を引く
+全ての部分木の大きさを求めるのは無理そう
+新しい要素は高々２つの要素を繋げるだけ
+
+for i in range(N - 1):
+    u, v = getNM()
+    if u > v:
+        u, v = v, u
+    # 前に戻るエッジのみ
+    dist[v - 1].append(u - 1)
+for i in range(N):
+    dist[i].sort()
+
+ans = 0
+for i in range(N): # L
+    cnt = 0
+    for j in range(i, N): # R
+        # 範囲外に伸びるエッジは無視
+        cnt += 1 - (len(dist[j]) - bisect_left(dist[j], i))
+        ans += cnt
+
+これはO(N ** 2)かかる
+[[], [], [], [], [2], [], [1, 3, 4], [3], [0, 7], [5, 8]]
+累積されていく
+何もなければ1 ~ 10の累積で55のはず
+しかし控除が累積して1 + 1 + 4(len([2] + len([1, 3, 4]))) + 5 + 7 + 9 = 27あるのでこれを引く
+dist[4]の位置にある2はL = 1の時も引っかかる
+dist[8]にある0は一回のみ引っかかって被害はN - 8 = 2
+
+エッジが伸びることでいくら減るか
 """
 
 N = getN()
-A = getList()
-c = Counter(A)
-a, b, c = c[1], c[2], c[3]
-dp = [[[0] * (N + 2) for i in range(N + 2)] for j in range(N + 2)]
+dist = [[] for i in range(N)]
+for i in range(N - 1):
+    u, v = getNM()
+    # 前に行くように
+    if u > v:
+        u, v = v, u
+    dist[v - 1].append(u - 1)
 
-for i in range(c + 1): # 上限はc
-    for j in range(N - a - i + 1): # 上限はb + c
-        for k in range(N - i - j + 1):
-            if i + j + k:
-                dp[i][j][k] = (i * dp[i - 1][j + 1][k] + j * dp[i][j - 1][k + 1] + k * dp[i][j][k - 1] + N) / (i + j + k)
+ans = 0
+cnt = 0
+for i in range(N):
+    ans += (i + 1) * (i + 2) // 2
+    # distの探索
+    for j in dist[i]:
+        cnt += (j + 1) * (N - i)
 
-print(dp[c][b][a])
+print(ans - cnt)
 
-# CODE THANKS FESTIVAL 2018(Parallel) E-union
-
-"""
-指数関数的なdp遷移
-ただ一つだけの整数が黒板にある状態
-数字iが0 ~ i個置ける A1 * A2 * ... に見える
-条件:同じ数字を２つ消してX+1を一個書くを繰り返すと最終的に整数1つになるように
-整数の置き方は何通りあるか　遷移の方法を考える
-
-3
-2 1 1の場合
-
-例えば1: 1個とか2: 1個とか
-1: 2個の場合は2: 1個にできるのでok
-1: 2個, 2: 1個の場合は3: 1個にできるのでok
-制約的にdpできそう　dp[i][j]:
-3が1個, 3が2個, 3が4個　これらは条件を満たす
+# ARC028 C - 高橋王国の分割統治
 
 """
+全方位木dpを使わない方法で
+頂点iについて
+iの子要素の部分木のサイズを調べる
+全方位木dpの要領でやっていく
+①葉から木dpしていく
+②遡る
+"""
 
-T = getN()
-A = getList()
-dp = [[0] * 1000 for i in range(T)]
-for i in range(A[0] + 1):
-    dp[0][i] += 1
+N = getN()
+E = [[] for i in range(N)]
+for i in range(N - 1):
+    p = getN()
+    E[i + 1].append(p)
+    E[p].append(i + 1)
 
-ans = dp[0][1]
-for i in range(1, T):
-    # 前回の0のもの、2のもの、4のもの...
-    for j in range(0, 1000, 2): # i - 1から持ってくるもの
-        for k in range(A[i] + 1): # iで生成するもの
-            dp[i][k + (j // 2)] += dp[i - 1][j]
-            dp[i][k + (j // 2)] %= mod
-    ans += dp[i][1]
-    ans %= mod
+# dfsする
+ans = [0] * N
 
-for i in range(1, 10):
-    ans += dp[-1][1 << i]
-    ans %= mod
-print(ans % mod)
+def dfs(u, par):
+    res = 1 # 自分 + 子要素の部分木の大きさの合計
+    opt_c = 0 # 子要素の部分木の大きさの最大値
+    for v in E[u]:
+        if v != par:
+            c = dfs(v, u)
+            res += c
+            opt_c = max(opt_c, c)
+
+    # 親方向の部分木の大きさはN - res
+    ans[u] = max(opt_c, N - res)
+    return res
+
+dfs(0, -1)
+for i in ans:
+    print(i)
+
+"""
+木構造
+M枚のコイン　K回操作
+
+0にコインがない場合は、0に新しいコインを置く
+コインが置かれている頂点を1つ選んで、コインが置かれていない子のどれかに置く
+コインはどんどん降りていく
+Kが過剰であると-1
+
+Kの縛りがない場合は1に1枚目、2に2枚目...とやっていく
+M枚置けるか？ 1に置くと通せんぼしてしまう
+置くごとに部分木の子要素以下を全て使用不可に　それでもM枚置けるか　を判定
+有向木の方が良さそう
+5 2 4
+5 1 1 5 0
+[[1, 2], [], [], [], [0, 3]] edge
+[{0, 1, 2}, {1}, {2}, {3}, {0, 1, 2, 3, 4}] size
+置く順番はトポロジカルになっている
+[1, 2, 2, 1, 0] distance
+1に置きます
+1はmosの中にありますか？
+mos - size[0]は現在のM - 1以上ありますか？
+あれば置く
+for i in range(M): で置けないと-1
+
+Kが入ってくると
+上の条件 + dis[0]だけ消費しても間に合いますか？
+コインを置くのにM回消費　
+移動に使える分はK - M回
+1枚目を1においても後のものを置けますか
+距離の合計がK - Mになるように頂点を選ぶ
+rootにM枚のコインを置きます
+置く場所が決まればあとはトポロジカルに
+
+1に置きたい
+1はmosの中にありますか？
+1の部分木を取り除いた集合で、距離の総和が条件を満たすか
+"""
+
+N, M, K = getNM()
+P = getList()
+E = [[] for i in range(N)]
+for i, v in enumerate(P):
+    if v:
+        E[v - 1].append(i)
+    else:
+        root = i
+
+dis = [1] * N
+size = [set() for i in range(N)]
+
+def dfs(u):
+    global dis
+    size[u].add(u)
+    for v in E[u]:
+        dis[v] = dis[u] + 1
+        size[u] |= dfs(v)
+    return size[u]
+dfs(root)
+
+remain = set(range(N))
+ans = []
+# M個のコインを置きます
+for i in range(M):
+    # 小さい順に探索する
+    for j in range(N):
+        # jはremainに残ってますか
+        if j not in remain:
+            continue
+        can = remain - size[j]
+        l = len(can)
+        # あとM - i - 1個置けますか
+        if l < M - i - 1:
+            continue
+        L = [dis[i] for i in can]
+        L.sort()
+        # 浅い順にM - i - 1個 ~ 深い順にM - i - 1個の中に含まれますか
+        # 1とかあるのでこの範囲内の全ての距離を作れる
+        if sum(L[:M - i - 1]) <= K - dis[j] <= sum(L[l - M + i + 1:]):
+            # 全て通過したので
+            K -= dis[j]
+            ans.append(j + 1)
+            remain -= size[j]
+            break
+    else:
+        ans = [-1]
+        break
+
+print(*ans)
