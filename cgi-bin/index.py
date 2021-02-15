723c8956425fcb27e54917ef75293850cae4e7c3
customaddone <custom1485@docomo.ne.jp>
2021-02-15 19:20:41 +0900

    tree

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -32,80 +32,13 @@ dy = [0, 1, 0, -1]
 # Main Code #
 #############
 
-# ☦️全方位木dp☦️
-# https://qiita.com/Kiri8128/items/a011c90d25911bdb3ed3
+# Kからの最短距離をbfsで測る
+def distance(sta):
+    pos = deque([sta])
 
-# unit(単位元)
-# merge(親pの子要素iについてどういう基準でdpするか最大値、最小値、合計など)
-# adj_bu, adj_td(基本的に同じものを書き込む　根の方向に遡る時に子の要素に何をして親要素に渡すか)
-
-class Reroot():
-    def __init__(self, query):
-        graph = [[] for i in range(N)]
-        dist = [{} for i in range(N)]
-        # 0-index
-        for x, y, z in query:
-            graph[x].append(y)
-            graph[y].append(x)
-            dist[x][y] = z
-            dist[y][x] = z
-
-        self.graph = graph
-        self.dist = dist
-        # トポロジカルソート Rに収納する
-        P = [-1] * N
-        Q = deque([0])
-        R = []
-        while Q:
-            i = deque.popleft(Q)
-            R.append(i)
-            for a in self.graph[i]:
-                if a != P[i]:
-                    P[a] = i
-                    self.graph[a].remove(i)
-                    deque.append(Q, a)
-
-        ##### Settings #####
-        self.unit = 0
-        self.merge = lambda a, b: max(a, b)
-        self.adj_bu = lambda a, i: a + dist[i][P[i]]
-        self.adj_td = lambda a, i, p: a + dist[i][P[i]]
-        self.adj_fin = lambda a, i: a
-        ####################
-
-        ME = [self.unit] * N # mergeを使う
-        XX = [0] * N # dpする
-        for i in R[1:][::-1]: # 巡回を逆順に辿る bottom-up
-            XX[i] = self.adj_bu(ME[i], i) # adj_buで子要素が白である通りを追加する
-            p = P[i] # 親pを取り出す
-            # 子要素jの持つ状態の個数 + 子要素が白である(1通り)を親要素に掛ける
-            ME[p] = self.merge(ME[p], XX[i]) # iの親要素とiの値をマージする
-        XX[R[0]] = self.adj_fin(ME[R[0]], R[0]) # 先頭については答えが求められるのでXXにレコード
-        print(ME)
-
-        TD = [self.unit] * N
-        for i in R: # 巡回した順番に
-            ac = TD[i] # 左からDP（結果はTDに入れている）
-            for j in self.graph[i]: # 各子要素について順番に更新を試みる
-                TD[j] = ac # TDにはjまで累積した分が入っている
-                ac = self.merge(ac, XX[j])  # マージする
-            ac = self.unit # リセット 右からDP（結果はacに入れている）
-            for j in self.graph[i][::-1]: # 各子要素について逆順に
-                # TDに残っている左から累積した分とacにある右から累積した分をTDにマージ
-                TD[j] = self.adj_td(self.merge(TD[j], ac), j, i)
-                ac = self.merge(ac, XX[j]) # acの累積更新 # 逆向きにマージしていく
-                # レコード 順向きのものと逆向きのものをマージする
-                XX[j] = self.adj_fin(self.merge(ME[j], TD[j]), j)
-        self.res = XX
-
-# AOJ Diameter of a Tree
-# 各頂点からもっとも遠い点
-N = 4
-query = [
-[0, 1, 1],
-[1, 2, 2],
-[2, 3, 4]
-]
-
-tree = Reroot(query)
-print(tree.res) # [7, 6, 4, 7]
+    while pos:
+        u = pos.popleft()
+        for i in dist[u]:
+            if ignore[i[0]] == -1:
+                ignore[i[0]] = ignore[u] + i[1]
+                pos.append(i[0])
---
+++ cgi-bin/library/tree/tree.py
@@ -0,0 +1,103 @@
+from collections import defaultdict, deque, Counter
+import sys
+from decimal import *
+from heapq import heapify, heappop, heappush
+import math
+import random
+import string
+from copy import deepcopy
+from itertools import combinations, permutations, product
+from operator import mul, itemgetter
+from functools import reduce, lru_cache
+from bisect import bisect_left, bisect_right
+
+def input():
+    return sys.stdin.readline().rstrip()
+def getN():
+    return int(input())
+def getNM():
+    return map(int, input().split())
+def getList():
+    return list(map(int, input().split()))
+def getArray(intn):
+    return [int(input()) for i in range(intn)]
+
+sys.setrecursionlimit(1000000000)
+mod = 10 ** 9 + 7
+INF = float('inf')
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
+#############
+# Main Code #
+#############
+
+N = 5
+# 木グラフ
+que = [
+[1, 2],
+[1, 4],
+[2, 3],
+[2, 5]
+]
+# 重みつき
+que_dis = [
+[1, 2, 2],
+[1, 4, 1],
+[2, 3, 2],
+[2, 5, 1]
+]
+
+# 重みなし
+def build_tree(n, edge_list):
+    G = [[] for i in range(n)]
+    for a, b in edge_list:
+        G[a - 1].append(b - 1)
+        G[b - 1].append(a - 1)
+
+    return G
+
+# 重みつき
+def build_tree_dis(n, edge_list):
+    G = [[] for i in range(n)]
+    for a, b, c in edge_list:
+        G[a - 1].append([b - 1, c])
+        G[b - 1].append([a - 1, c])
+
+    return G
+
+# 木の建設
+G1 = build_tree(N, que)
+G2 = build_tree_dis(N, que_dis)
+
+# 木を探索
+def search(n, edges, sta):
+    ignore = [0] * N
+    ignore[sta] = 1
+    pos = deque([sta])
+    # 探索
+    while len(pos) > 0:
+        u = pos.popleft()
+        for i in edges[u]:
+            if ignore[i] == 0:
+                ignore[i] = 1
+                pos.append(i)
+# [0, 1, 3, 2, 4]
+search(N, G1, 0)
+
+# staからの距離
+# staからの距離を求めてそのもっとも遠い店からまた距離を求めた時のもっとも遠い店が木の直径
+def distance(n, edges, sta):
+    ignore = [-1] * N
+    ignore[sta] = 0
+    pos = deque([sta])
+
+    while len(pos) > 0:
+        u = pos.popleft()
+        for i in edges[u]:
+            if ignore[i[0]] == -1:
+                ignore[i[0]] = ignore[u] + i[1]
+                pos.append(i[0])
+    return ignore
+# [0, 2, 4, 1, 3]
+print(distance(N, G2, 0))
