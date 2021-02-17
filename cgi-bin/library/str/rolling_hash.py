38202d45d07add58cccb34aac3a17c1355b238e9
customaddone <custom1485@docomo.ne.jp>
2021-02-17 20:09:53 +0900

    rolling hash

--- cgi-bin/library/str/rolling_hash.py
+++ cgi-bin/library/str/rolling_hash.py
@@ -51,103 +51,18 @@ dy = [0, 1, 0, -1]
 # Main Code #
 #############
 
-# Rolling hash
-N = getN()
-S = list(map(ord, list(input())))
-# 適当
-base = 1007
-# base = 1009
-power = [1] * (N + 1)
-# 部分文字列を数字に
-# ハッシュ生成
-for i in range(1, N + 1):
-    power[i] = power[i - 1] * base % mod
-print(power)
-
-# 長さmの文字列がダブらずに存在するか
-def check(m):
-    if N - m < m:
-        return False
-    res = 0
-    # 頭m文字のハッシュ生成
-    for i in range(m):
-        res += S[i] * power[m - i - 1]
-        res %= mod
-    dic = {res: 0}
-    for i in range(N - m):
-        # ハッシュをローリングしていって次々m文字のハッシュを生成していく
-        res = ((res - S[i] * power[m - 1]) * base + S[i + m]) % mod
-        # もし既出なら
-        if res in dic.keys():
-            index = dic[res]
-            if index + m <= i + 1: # 重ならないか
-                return True
-        else:
-            dic[res] = i + 1 # i + 1:頭の位置を記録する
-        print(dic)
-    return False
-
-ok = 0
-ng = N + 1
-while ng - ok > 1:
-    mid = (ok + ng) // 2
-
-    if check(mid):
-        ok = mid
-    else:
-        ng = mid
-print(ok)
-
-N, K = getNM()
-S = input()
-if(K * 2 > N):
-    print('NO')
-    exit()
-def check(n, s, k, mod, alp):
-    # 各アルファベットをエンコード
-    encode = {}
-    for i, ch in enumerate(alp, 10001):
-        encode[ch] = pow(172603, i, mod)
-    # 文字列の先頭からm文字、k文字目からm文字をハッシュしていく
-    left = 0
-    right = 0
-    for i in range(k):
-        left += encode[s[i]]
-        left %= mod
-        right += encode[s[i + k]]
-        right %= mod
-    words = set()
-    # ローリングする
-    for i in range(n):
-        words.add(left)
-        if (right in words):
-            return True
-        if (i + 2 * k >= n):
-            break
-        left += encode[s[i + k]] - encode[s[i]]
-        left %= mod
-        right += encode[s[i + k * 2]] - encode[s[i + k]]
-        right %= mod
-    return False
-ans = True
-ps = '9999217 9999221 9999233 9999271 9999277 9999289 9999299 9999317 9999337 9999347 9999397 9999401 9999419 9999433 9999463 9999469 9999481 9999511 9999533 9999593 9999601 9999637 9999653 9999659 9999667 9999677 9999713 9999739 9999749 9999761 9999823 9999863 9999877 9999883 9999889 9999901 9999907 9999929 9999931 9999937 9999943 9999971 9999973'
-ps = list(map(int, ps.split(' ')))
-ps.append(10 ** 9 + 7)
-ps.append(3040409)
-ps.append(10 ** 20 + 9)
-# 各modについて
-for p in ps:
-    # アルファベットをランダムに並べた表でエンコード表を作る
-    for alp in ['qwertyuioplkmnjhbvgfcdxsaz','qazxcsdwertfgvbnmhjyuioklp']:
-        ans = (ans & check(N, S, K, p, alp))
-if(ans):
-    print('YES')
-else:
-    print('NO')
+# ローリングハッシュ
+# 文字Sのl ~ r文字目をハッシュ化する
+# 順番を考慮するものとしないものがある
 
 # ARC024 だれじゃ
-N, K = getNM()
-S = input()
+# 順番を考慮するもの
+# abc, bcd, cdb...を次々ハッシュ化していく
+# 共通するものがあるか
+# psが一つの場合はnoのものがyesになる場合があるのでたくさん用意する
+
+N, K = 8, 3
+S = 'abcdbcae'
 
 if(K * 2 > N):
     print('NO')
@@ -171,7 +86,9 @@ def check(n, s, k, mod, alp):
     for i in range(n - k):
         res += encode[s[i + k]] - encode[s[i]]
         res %= mod
+        # 既出なら
         if res in dic.keys():
+            # ダブってもいいなら以下はいらん
             index = dic[res]
             if index + k <= i + 1:# ダブって無いか
                 return True
@@ -183,21 +100,20 @@ def check(n, s, k, mod, alp):
 ans = True
 ps = '9999217 9999221 9999233 9999271 9999277 9999289 9999299 9999317 9999337 9999347 9999397 9999401 9999419 9999433 9999463 9999469 9999481 9999511 9999533 9999593 9999601 9999637 9999653 9999659 9999667 9999677 9999713 9999739 9999749 9999761 9999823 9999863 9999877 9999883 9999889 9999901 9999907 9999929 9999931 9999937 9999943 9999971 9999973'
 ps = list(map(int, ps.split(' ')))
-ps.append(10 ** 9 + 7)
-ps.append(3040409)
-ps.append(10 ** 20 + 9)
+
 # 各modについて
 for p in ps:
     # アルファベットをランダムに並べた表でエンコード表を作る
     for alp in ['qwertyuioplkmnjhbvgfcdxsaz','qazxcsdwertfgvbnmhjyuioklp']:
         ans = (ans & check(N, S, K, p, alp))
 
-if(ans):
+if ans:
     print('YES')
 else:
     print('NO')
 
 # 天下一プログラマーコンテスト2014予選B B - エターナルスタティックファイナル
+# 順番を考慮しない
 
 # ローリングハッシュ
 # sの連続部分列とkがマッチした部分s[i:j]についてのi, jを求める
@@ -278,3 +194,50 @@ for i in range(len(S) + 1): # iが右端
     dp[i] %= mod
 
 print(dp[-1] % mod)
+
+# Rolling hash
+N = getN()
+S = list(map(ord, list(input())))
+# 適当
+base = 1007
+# base = 1009
+power = [1] * (N + 1)
+# 部分文字列を数字に
+# ハッシュ生成
+for i in range(1, N + 1):
+    power[i] = power[i - 1] * base % mod
+print(power)
+
+# 長さmの文字列がダブらずに存在するか
+def check(m):
+    if N - m < m:
+        return False
+    res = 0
+    # 頭m文字のハッシュ生成
+    for i in range(m):
+        res += S[i] * power[m - i - 1]
+        res %= mod
+    dic = {res: 0}
+    for i in range(N - m):
+        # ハッシュをローリングしていって次々m文字のハッシュを生成していく
+        res = ((res - S[i] * power[m - 1]) * base + S[i + m]) % mod
+        # もし既出なら
+        if res in dic.keys():
+            index = dic[res]
+            if index + m <= i + 1: # 重ならないか
+                return True
+        else:
+            dic[res] = i + 1 # i + 1:頭の位置を記録する
+        print(dic)
+    return False
+
+ok = 0
+ng = N + 1
+while ng - ok > 1:
+    mid = (ok + ng) // 2
+
+    if check(mid):
+        ok = mid
+    else:
+        ng = mid
+print(ok)
