30d8739c75556b5747071491fbe739bbd939858d
customaddone <custom1485@docomo.ne.jp>
2021-02-17 18:15:58 +0900

    lca

---
+++ cgi-bin/library/tree/lcd.py
@@ -0,0 +1,125 @@
+def getN():
+    return int(input())
+def getNM():
+    return map(int, input().split())
+def getList():
+    return list(map(int, input().split()))
+def getArray(intn):
+    return [int(input()) for i in range(intn)]
+def input():
+    return sys.stdin.readline().rstrip()
+def rand_N(ran1, ran2):
+    return random.randint(ran1, ran2)
+def rand_List(ran1, ran2, rantime):
+    return [random.randint(ran1, ran2) for i in range(rantime)]
+def rand_ints_nodup(ran1, ran2, rantime):
+  ns = []
+  while len(ns) < rantime:
+    n = random.randint(ran1, ran2)
+    if not n in ns:
+      ns.append(n)
+  return sorted(ns)
+
+def rand_query(ran1, ran2, rantime):
+  r_query = []
+  while len(r_query) < rantime:
+    n_q = rand_ints_nodup(ran1, ran2, 2)
+    if not n_q in r_query:
+      r_query.append(n_q)
+  return sorted(r_query)
+
+from collections import defaultdict, deque, Counter
+from sys import exit
+from decimal import *
+import heapq
+import math
+from fractions import gcd
+import random
+import string
+import copy
+from itertools import combinations, permutations, product
+from operator import mul
+from functools import reduce
+from bisect import bisect_left, bisect_right
+
+import sys
+sys.setrecursionlimit(1000000000)
+mod = 10 ** 9 + 7
+
+
+#############
+# Main Code #
+#############
+
+class LCA(object):
+    def __init__(self, G, root=0):
+        self.G = G
+        self.root = root
+        self.n = len(G)
+        self.logn = (self.n - 1).bit_length()
+        self.depth = [-1 if i != root else 0 for i in range(self.n)]
+        self.parent = [[-1] * self.n for _ in range(self.logn)]
+        self.dfs()
+        self.doubling()
+
+    def dfs(self):
+        que = [self.root]
+        while que:
+            u = que.pop()
+            for v in self.G[u]:
+                if self.depth[v] == -1:
+                    self.depth[v] = self.depth[u] + 1
+                    self.parent[0][v] = u
+                    que += [v]
+
+    def doubling(self):
+        for i in range(1, self.logn):
+            for v in range(self.n):
+                if self.parent[i - 1][v] != -1:
+                    self.parent[i][v] = self.parent[i - 1][self.parent[i - 1][v]]
+
+    def get(self, u, v):
+        if self.depth[v] < self.depth[u]:
+            u, v = v, u
+        du = self.depth[u]
+        dv = self.depth[v]
+
+        for i in range(self.logn):  # depthの差分だけuを遡らせる
+            if (dv - du) >> i & 1:
+                v = self.parent[i][v]
+        if u == v: return u  # 高さ揃えた時点で一致してたら終わり
+
+        for i in range(self.logn - 1, -1, -1):  # そうでなければ上から二分探索
+            pu, pv = self.parent[i][u], self.parent[i][v]
+            if pu != pv:
+                u, v = pu, pv
+        return self.parent[0][u]
+
+"""
+5
+1 2
+1 3
+1 4
+4 5
+3
+2 3
+2 5
+2 4
+"""
+
+n = getN()
+G = [[] for _ in range(n)]
+for x, y in [getNM() for i in range(n - 1)]:
+    G[x - 1] += [y - 1]
+    G[y - 1] += [x - 1]
+
+# 木の共通祖先を見つけてくれる
+lca = LCA(G)
+q = getN()
+ans = []
+for a, b in [getNM() for i in range(q)]:
+    # 根からのaの深さ + 根からのbの深さ - 2 * ダブった部分
+    # lca.get(a - 1, b - 1):aとbのlca
+    ans += [lca.depth[a - 1] + lca.depth[b - 1] - 2 * lca.depth[lca.get(a - 1, b - 1)] + 1]
+
+print(*ans, sep='\n')
