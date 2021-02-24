da6347d002c7f981b251aa4f24f27105c9944b6a
customaddone <custom1485@docomo.ne.jp>
2021-02-24 22:00:54 +0900

    unionfind

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -32,26 +32,80 @@ dy = [0, 1, 0, -1]
 # Main Code #
 #############
 
-N, M = getNM()
-S = input()
-P = []
-for i in range(N + 1):
-    if S[i] == '0':
-        P.append(i)
-now = P.pop()
-ans = []
+# ABC189 C - Mandarin Orange
 
-while now > 0:
-    # 行けない
-    if P and P[-1] + M < now:
-        print(-1)
-        exit()
-    next = float('inf')
-    # いけるとこまでいく
-    while next > 0 and P[-1] + M >= now:
-        next = P.pop()
+N = getN()
+A = getList()
+A = [[A[i], i] for i in range(N)]
+A.sort()
 
-    ans.append(now - next)
-    now = next
+ans = 0
+check = [0] * N
+U = UnionFind(N)
 
-print(*list(reversed(ans)))
+while A:
+    now = A[-1][0]
+    index = []
+    # 同じ数を引き終わるまで引き続ける
+    while A and A[-1][0] == now:
+        val, ind = A.pop()
+        index.append(ind)
+        check[ind] = 1
+
+    # uniteする
+    for ind in index:
+        # 左側と
+        if ind > 0 and check[ind - 1] == 1:
+            U.union(ind - 1, ind)
+        # 右側と
+        if ind < N - 1 and check[ind + 1] == 1:
+            U.union(ind, ind + 1)
+
+    # 計算
+    for ind in index:
+        ans = max(ans, now * U.size(ind))
+
+print(ans)
+
+# 技術室奥プログラミングコンテスト#4 Day2
+# E - 引きこもり
+
+N, M, Q = getNM()
+E = [getList() for i in range(M)]
+E.sort(reverse = True, key = lambda i:i[2])
+query = getArray(Q)
+
+U = UnionFind(N)
+d = [float('inf')] * (10 ** 5 + 7)
+d[1] = 0
+que = []
+for i in range(N):
+    heappush(que, [1, i])
+
+# 混ぜこぜでqueにぶち込んで取り出すときにそれが正しいか判定する
+while E:
+    now = E[-1][2]
+    al = []
+    # 抜き取る
+    while E and E[-1][2] == now:
+        a, b, c = E.pop()
+        U.union(a - 1, b - 1)
+        al.append([a - 1, b - 1])
+    # 判定
+    for a, b in al:
+        heappush(que, [U.size(a), a])
+        heappush(que, [U.size(b), b])
+
+    # 正規のやつを引くまで
+    while que[0][0] != U.size(U.find(que[0][1])):
+        heappop(que)
+    d[que[0][0]] = min(d[que[0][0]], now)
+
+for i in range(len(d) - 2, -1, -1):
+    d[i] = min(d[i], d[i + 1])
+
+for q in query:
+    if d[q] == float('inf'):
+        print('trumpet')
+    else:
+        print(d[q])
--- cgi-bin/myfile.py
+++ cgi-bin/myfile.py
@@ -31,3 +31,69 @@ dy = [0, 1, 0, -1]
 #############
 # Main Code #
 #############
+
+"""
+K = 3なら
+1 4 9(爆心地) 4 1という風にダメージを受ける　これは固定
+これは好きな回数できる
+
+まず最初の1つを落とせるか　2番目を落とせるか...をやっていく
+これは必ず1でやらないといけない
+1 4 10 9 14 13 5 1
+1 4  9 4  1
+0 0  1 5 13 13 5 1
+     1 4  9  4 1
+0 0  0 1  4  9 4 1
+K ~ N - K + 1番目までこの方法でやって全て0になるか
+
+足し合わせが面倒そう
+blastした位置を保存しておくと？　毎回ごとに計算できない？
+i番目のモンスターの体力を0にしたい
+残り体力はAi - (これまでのダメージ)
+与えるダメージが平均であれば簡単なんだけど
+二乗しなければ　これまでのblast位置の合計 - 現在の地点
+二乗すると
+0 0  1 5 13 13 5 1 blast位置は3
+i = 2について
+(K - (y - x))**2 = (K**2 - 2Ky + y**2) + (x**2 + 2Kx) - 2yx
+squに収納 K**2 - 2Ky + y**2
+minusに収納 -2y
+後で計算 x**2 + 2Kx
+2yをどこかに保持する　あとで
+squ = 0
+minus = 0
+3にblast
+squ = [9]
+minus = [-6]
+i = 2の計算
+通りすぎたら引く
+
+全部足せばOK
+爆心地がyの時xの相手に与えるダメージ
+def cal_squ(y):
+    return K ** 2 - (2 * K * y) + y ** 2
+def cal_x(x):
+    return x ** 2 + 2 * K * x
+def minus(y):
+    return -2 * y
+print(cal_squ(5), cal_x(5), minus(5) * 5)
+逆側はどうなっている？
+"""
+
+N, K = getNM()
+A = getList()
+def cal_squ(y):
+    return K ** 2 - (2 * K * y) + y ** 2
+def cal_x(x):
+    return x ** 2 + 2 * K * x
+def minus(y):
+    return -2 * y
+
+squ = 0
+minus = 0
+blast = deque([])
+for i in range(N - K + 1):
+    # A[i]回blast
+    blast.append([i + K - 1, A[i]])
+    squ += cal_squ(i + K - 1) * A[i]
+    minus += minus()
