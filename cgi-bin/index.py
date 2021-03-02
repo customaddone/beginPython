365f6f1e292368388452de5e1412fa93ec1c0f5a
customaddone <custom1485@docomo.ne.jp>
2021-03-02 18:34:06 +0900

    bit

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,6 +1,12 @@
-from collections import deque
+from collections import defaultdict, deque, Counter
+from heapq import heapify, heappop, heappush
 import sys
-
+import math
+import random
+import string
+from copy import deepcopy
+from itertools import combinations, permutations, product
+from bisect import bisect_left, bisect_right
 
 def input():
     return sys.stdin.readline().rstrip()
@@ -23,72 +29,101 @@ dy = [0, 1, 0, -1]
 # Main Code #
 #############
 
-"""
-bulletっぽさある
-一個以上含むやつを作れるか
-連結していれば作れる
+from collections import defaultdict, deque, Counter
+from heapq import heapify, heappop, heappush
+import sys
+import math
+import random
+import string
+from copy import deepcopy
+from itertools import combinations, permutations, product
+from bisect import bisect_left, bisect_right
 
-魔法石の最小値
-なんでK少ないの　Kから任意の２つを選んで直径にする
-巡回したいけど無理ですか？
-"""
+def input():
+    return sys.stdin.readline().rstrip()
+def getN():
+    return int(input())
+def getNM():
+    return map(int, input().split())
+def getList():
+    return list(map(int, input().split()))
+def getArray(intn):
+    return [int(input()) for i in range(intn)]
 
-N, M = getNM()
-E = [[] for i in range(N)]
-for i in range(M):
-    a, b = getNM()
-    E[a - 1].append([b - 1, 1])
-    E[b - 1].append([a - 1, 1])
-K = getN()
-C = [i - 1 for i in getList()]
-set_c = set(C)
+sys.setrecursionlimit(1000000000)
+mod = 10 ** 9 + 7
+INF = float('inf')
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
 
-def distance(n, edges, sta):
-    ignore = [float('inf')] * N
-    ignore[sta] = 0
-    pos = deque([sta])
+#############
+# Main Code #
+#############
 
-    while len(pos) > 0:
-        u = pos.popleft()
-        for i in edges[u]:
-            if ignore[i[0]] == float('inf'):
-                ignore[i[0]] = ignore[u] + i[1]
-                pos.append(i[0])
-    return ignore
+# 二分探索木
+# binary search tree
+class BST:
+    # arrayでどんな値が来るか読み込ませる
+    # arrayはソートした配列をいれてね
+    def __init__(self, N, array):
+        self.N = N
+        self.bit = [0] * (N + 1)
+        self.comp = {} # value → index
+        self.rev = {} # index → value
+        for i, a in enumerate(array):
+            self.comp[a] = i + 1
+            self.rev[i + 1] = a
+        self.b = 1 << N.bit_length() - 1
 
-G = []
-for i in range(K):
-    g = []
-    opt = distance(N, E, C[i])
-    for j in range(N):
-        if j in set_c:
-            g.append(opt[j])
-    G.append(g)
+    # 追加 1-indexで収納される
+    def add(self, a):
+        x = self.comp[a]
+        while(x <= self.N):
+            self.bit[x] += 1
+            x += x & -x
 
-if sum(G[0]) == float('inf'):
-    print(-1)
-    exit()
+    # 削除
+    def erase(self, a):
+        x = self.comp[a]
+        while(x <= self.N):
+            self.bit[x] += -1
+            x += x & -x
 
-def counter(sta, K, G):
-    # dp[bit][i]これまでに踏んだ場所がbitであり、現在の場所がiである
-    dp = [[float('inf')] * K for i in range(1 << K)]
-    for i in range(K):
-        dp[1 << i][i] = 0
+    def lowerbound(self, w):
+        if w <= 0:
+            return 0
+        x = 0
+        k = self.b
+        while k > 0:
+            if x + k <= self.N and self.bit[x + k] < w:
+                w -= self.bit[x + k]
+                x += k
+            k //= 2
+        return x + 1
 
-    for bit in range(1, 1 << K):
-        if not bit:
-            continue
-        # s:現在の場所
-        for s in range(K):
-            # sを踏んだことになっていなければ飛ばす
-            if not bit & (1 << s):
-                continue
-            # t:次の場所
-            for t in range(K):
-                # tを過去踏んでいない and s → tへのエッジがある
-                if (bit & (1 << t)) == 0:
-                    dp[bit|(1 << t)][t] = min(dp[bit|(1 << t)][t], dp[bit][s] + G[s][t])
+    # x番目の値を返す
+    def query(self, x):
+        ind = self.lowerbound(x)
+        return self.rev[ind]
 
-    return min(dp[-1])
+# 使い方
+Q = getN()
+# ①まずクエリ先読みで使用する値をBSTに読み込ませる
+read = []
+que = []
+for i in range(Q):
+    t, x = getNM()
+    if t == 1:
+        read.append(x)
+    que.append([t, x])
+bit = BST(len(read), sorted(read))
 
-print(counter(0, K, G) + 1)
+for t, x in que:
+    if t == 1:
+        # ②addで値を1つ追加、eraseで消去
+        bit.add(x)
+    else:
+        # ③queryで小さい方からx番目の値を返す
+        res = bit.query(x)
+        print(res)
+        bit.erase(res)
--- cgi-bin/library/segment_tree/binary_index_tree.py
+++ cgi-bin/library/segment_tree/binary_index_tree.py
@@ -85,3 +85,72 @@ class BIT:
                 x += k
             k //= 2
         return x + 1
+
+# 二分探索木
+# binary search tree
+class BST:
+    # arrayでどんな値が来るか読み込ませる
+    # arrayはソートした配列をいれてね
+    def __init__(self, N, array):
+        self.N = N
+        self.bit = [0] * (N + 1)
+        self.comp = {} # value → index
+        self.rev = {} # index → value
+        for i, a in enumerate(array):
+            self.comp[a] = i + 1
+            self.rev[i + 1] = a
+        self.b = 1 << N.bit_length() - 1
+
+    # 追加 1-indexで収納される
+    def add(self, a):
+        x = self.comp[a]
+        while(x <= self.N):
+            self.bit[x] += 1
+            x += x & -x
+
+    # 削除
+    def erase(self, a):
+        x = self.comp[a]
+        while(x <= self.N):
+            self.bit[x] += -1
+            x += x & -x
+
+    def lowerbound(self, w):
+        if w <= 0:
+            return 0
+        x = 0
+        k = self.b
+        while k > 0:
+            if x + k <= self.N and self.bit[x + k] < w:
+                w -= self.bit[x + k]
+                x += k
+            k //= 2
+        return x + 1
+
+    # x番目の値を返す
+    def query(self, x):
+        ind = self.lowerbound(x)
+        return self.rev[ind]
+
+# 使い方
+# 例: ARC033 C - データ構造
+Q = getN()
+# ①まずクエリ先読みで使用する値をBSTに読み込ませる
+read = []
+que = []
+for i in range(Q):
+    t, x = getNM()
+    if t == 1:
+        read.append(x)
+    que.append([t, x])
+bit = BST(len(read), sorted(read))
+
+for t, x in que:
+    if t == 1:
+        # ②addで値を1つ追加、eraseで消去
+        bit.add(x)
+    else:
+        # ③queryで小さい方からx番目の値を返す
+        res = bit.query(x)
+        print(res)
+        bit.erase(res)
