a2ad6f4341cba12c64a6d951d42b992ce6cc2e71
customaddone <custom1485@docomo.ne.jp>
2021-03-09 18:06:29 +0900

    スタック、キュー

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -29,337 +29,147 @@ dy = [0, 1, 0, -1]
 # Main Code #
 #############
 
-from collections import defaultdict, deque, Counter
-from heapq import heapify, heappop, heappush
-import sys
-import math
-import random
-import string
-from copy import deepcopy
-from itertools import combinations, permutations, product
-from bisect import bisect_left, bisect_right
+# 天下一プログラマーコンテスト2013予選B B - 天下一後入れ先出しデータ構造
 
-def input():
-    return sys.stdin.readline().rstrip()
-def getN():
-    return int(input())
-def getNM():
-    return map(int, input().split())
-def getList():
-    return list(map(int, input().split()))
-def getArray(intn):
-    return [int(input()) for i in range(intn)]
+Q, L = getNM()
+query = [input().split() for i in range(Q)]
+s = [] # 配列の中身
+l = 0 # 配列の大きさ
 
-sys.setrecursionlimit(1000000000)
-mod = 10 ** 9 + 7
-INF = float('inf')
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
+for q in query:
+    if q[0] == "Push":
+        s.append([int(q[1]), int(q[2])])
+        l += int(q[1])
+    if l > L:
+        print("FULL")
+        exit()
 
-#############
-# Main Code #
-#############
-
-N = 10
-logk = N.bit_length()
-
-# [Fi+2, Fi+1] = [[1, 1], [1, 0]][Fi+1, Fi]
-# 一般項が出ない漸化式は行列の形に落とし込める
-dp = [[[0, 0] for i in range(2)] for i in range(logk)]
-dp[0] = [[1, 1], [1, 0]]
-
-# 行列掛け算 O(n3)かかる
-def array_cnt(ar1, ar2):
-    h = len(ar1)
-    w = len(ar2[0])
-    row = ar1
-    col = []
-    for j in range(w):
-        opt = []
-        for i in range(len(ar2)):
-            opt.append(ar2[i][j])
-        col.append(opt)
-
-    res = [[[0, 0] for i in range(w)] for i in range(h)]
-    for i in range(h):
-        for j in range(w):
-            cnt = 0
-            for x, y in zip(row[i], col[j]):
-                cnt += x * y
-            res[i][j] = cnt
-    return res
-
-for i in range(1, logk):
-    dp[i] = array_cnt(dp[i - 1], dp[i - 1])
-
-# 行列の単位元
-ans = [[1, 0], [0, 1]]
-for i in range(logk):
-    if N & (1 << i):
-        ans = array_cnt(ans, dp[i])
-
-# [Fi+2, Fi+1] = [[1, 1], [1, 0]][Fi+1, Fi]より
-# [Fn+1, Fn] = A ** n[F1, F0] = A ** n[1, 0]
-# Fn = ans[1][0] * 1 + ans[1][1] * 0
-print(array_cnt(ans, [[1], [0]])[1][0])
-
-# ABC113 D - Number of Amidakuji
-
-H, W, K = getNM()
-logk = H.bit_length()
-
-if W == 1:
-    print(1)
-    exit()
-
-mat = [[0] * W for i in range(W)]
-# 列の数はH個
-# うまく行列に
-for bit in range(1 << (W - 1)):
-    for i in range(1, (W - 1)):
-        # 両方にフラグがあればとばす
-        if bit & (1 << i) and bit & (1 << (i - 1)):
-            break
-    else:
-        for i in range(W):
-            if bit & (1 << i):
-                # 目的地、出発地
-                mat[i + 1][i] += 1
-            # negative shiftしないよう
-            elif i > 0 and bit & (1 << (i - 1)):
-                mat[i - 1][i] += 1
+    elif q[0] == "Pop":
+        x = int(q[1])
+        l -= x
+        while s: # sが空になるまで xがゼロになるとbreakで抜ける
+            c, y = s[-1]
+            if x >= c:
+                x -= c
+                s.pop() # 抜く
             else:
-                mat[i][i] += 1
+                s[-1][0] -= x # 引くだけで抜かない
+                x = 0
+                break
+        if x:
+            print("EMPTY")
+            exit()
 
-dp = [[[0] * W for i in range(W)] for i in range(logk)]
-dp[0] = mat
+    elif q[0] == "Size":
+        print(l)
 
-# 行列掛け算 O(n3)かかる
-def array_cnt(ar1, ar2):
-    h = len(ar1)
-    w = len(ar2[0])
-    row = ar1
-    col = []
-    for j in range(w):
-        opt = []
-        for i in range(len(ar2)):
-            opt.append(ar2[i][j])
-        col.append(opt)
+    elif q[0] == "Top":
+        if s:
+            print(s[-1][1])
+        else:
+            print("EMPTY")
+            exit()
 
-    res = [[[0, 0] for i in range(w)] for i in range(h)]
-    for i in range(h):
-        for j in range(w):
-            cnt = 0
-            for x, y in zip(row[i], col[j]):
-                cnt += x * y
-            res[i][j] = cnt
-    return res
+print("SAFE")
 
-for i in range(1, logk):
-    dp[i] = array_cnt(dp[i - 1], dp[i - 1])
+# ヒストグラムの最大長方形
 
-# スタート地点
-ans = [[0] * W]
-ans[0][0] = 1
-# 計算
-for i in range(logk):
-    if H & (1 << i):
-        ans = array_cnt(ans, dp[i])
-
-print(ans[0][K - 1] % mod)
-
-# EDPC R - Walk
-
-"""
-通りの数を答える
-同じ辺を複数回通ってもいい
-長さKの有効パス　二分グラフとか？
-Kがクソでか
-ループで解けという
-N <= 50 巡回セールスか
-Kが小さいものを求める
-
-dp iターン目にjにいる時、その通りは？
-行列累乗する
-
-4 2
-0 1 0 0
-0 0 1 1
-0 0 0 1
-1 0 0 0
-
-0 1 2 3 縦に使う
-0 - 1, 1 - 2, 1 - 3...
-
-prev = [1, 1, 1, 1]
-next[0] = 1 * 0 + 1 * 0 + 1 * 0 + 1 * 1
-next[1] = 1 * 1 + 1 * 0 + 1 * 0 + 1 * 0
-next[2] = 1 * 0 + 1 * 1 + 1 * 0 + 1 * 0
-next[3] = 1 * 0 + 1 * 1 + 1 * 1 + 1 * 0
-"""
-
-N, K = getNM()
-logk = K.bit_length()
-
-mat = [getList() for i in range(N)]
-dp = [[[0] * N for i in range(N)] for i in range(logk)]
-dp[0] = mat
-
-for i in range(1, logk):
-    dp[i] = array_cnt(dp[i - 1], dp[i - 1])
-
-ans = [[1] * N]
-for i in range(logk):
-    if K & (1 << i):
-        ans = array_cnt(ans, dp[i])
-
-print(sum(ans[0]) % mod)
-
-"""
-A, Cの長さは100 O(N ** 3)までいける
-K + 1以降は自分で計算しろ　まともに1個ずつするのは無理　法則を見つけるかダブリングするか
-3 5
-10 20 30
-7 19 13 の場合
-A4 = 7 & 30 ^ 20 & 19 ^ 10 & 13
-10 20 30 30
- 7 19 13
-A5は
-10 [20 30 30]
- [7 19 13]
- 7 & 30 ^ 19 & 30 ^ 13 & 20
-
-for i in range(M - K):
-    res = C[0] & A[-1]
-    for j in range(1, K):
-        res ^= (C[j] & A[-j - 1])
-    A.append(res)
-
-A[3] = (C[0] & A[2]) ^ (C[1] & A[1]) ^ (C[2] & A[0]) これを簡単にできないか
-A[4] = (C[0] & A[3]) ^ (C[1] & A[2]) ^ (C[2] & A[1])
-
-(C[0] & A[3]) = (C[0] & ((C[0] & A[2]) ^ (C[1] & A[1]) ^ (C[2] & A[0])))
-論理積　全てにフラグが立っている場合に立つ
-
-A[99] = (C[0] & A[98]) ^ (C[1] & A[97]) ^ (C[2] & A[96]) ^ (C[3] & A[95]) ^ (C[4] & A[94])
-各数字が0か1なら
-0 1 1
-1 0 0 フラグが両方立っているときにしか反応しない　両方フラグが立っている本数は？
-3 100
-0 1 1
-1 0 1
-[0, 1, 1, 1,
- 0, 1, 0,
- 0, 1, 1, 1,
- 0, 1, 0,
- 0, 1, 1, 1,
- 0, 1, 0...　周期性あり
-
-[0, 1, 0, 1, 0,
- 0, 1, 0, 1, 0,
- 0, 1, 0, 1, 0,
- 0, 1, 0, 1, 0,
- 0, 1, 0, 1, 0, 0,
-
-スライドした時のフラグの構成が同じであれば同じ結果がループされる
-ローリングハッシュの要領
-
-AND XORの半環の性質を使う
-掛け合わせするCが固定なので行列累乗できるのん
-ANDを掛け算、XORを足し算だと思え
-[[0, 0, 7],
- [1, 0, 19],
- [0, 1, 13]] の行列を作る　最後の一項だけ計算す
-計算は逆になる
-"""
-
-K, M = getNM()
+N = getN()
 A = getList()
-C = getList()
 
-if M <= K:
-    print(A[M - 1])
-    exit()
+def largestRectangleArea(A):
+    ans = 0
+    A = [-1] + A # 最初に番兵
+    A.append(-1) # 最後に番兵
+    n = len(A)
 
-# 行列累乗
-k = M - K
-logk = k.bit_length()
-dp = [[[0] * K for j in range(K)] for i in range(logk)]
-# 初項を書き込む
-for i in range(K - 1):
-    dp[0][K - i - 2][i] = 2 ** 32 - 1
-for i in range(K):
-    dp[0][i][-1] = C[i]
+    stack = [0]  # store index
 
-def array_cnt(ar1, ar2):
-    h = len(ar1)
-    w = len(ar2[0])
-    row = ar1
-    col = []
-    for j in range(w):
-        opt = []
-        for i in range(len(ar2)):
-            opt.append(ar2[i][j])
-        col.append(opt)
+    for i in range(n):
+        # A[i]が一番大きくなるよう
+        while A[i] < A[stack[-1]]:
+            h = A[stack.pop()]
+            area = h * (i - stack[-1] - 1)
+            ans = max(ans, area)
+        stack.append(i)
+    return ans
 
-    res = [[[0, 0] for i in range(w)] for i in range(h)]
-    for i in range(h):
-        for j in range(w):
-            cnt = 0
-            for x, y in zip(row[i], col[j][::-1]): # 計算は逆になる
-                cnt ^= x & y # ここを変更
-            res[i][j] = cnt
-    return res
+print(largestRectangleArea(A))
 
-for i in range(1, logk):
-    dp[i] = array_cnt(dp[i - 1], dp[i - 1])
-
-ans = [A]
-for i in range(logk):
-    if k & (1 << i):
-        ans = array_cnt(ans, dp[i])
-
-print(ans[0][-1])
-
-# ABC189 E - Rotate and Flip
+# F - Many Slimes
+# pop-appendleft型でソートを保てる
+# 保てない場合はヒープキュー 
 
 """
-[x, y, 1]について変換する
-1: xとyを交換してyを反転
-[[0, 1, 0], [-1, 0, 0], [0, 0, 1]]
-2: xとyを交換してxを反転
-[[0, -1, 0], [1, 0, 0], [0, 0, 1]]
-3: xについて-x+2pに変換
-[[-1, 0, 2p], [0, 1, 0], [0, 0, 1]]
-4: yについて-y+2pに変換する
-[[1, 0, 0], [0, -1, 2p], [0, 0, 1]]
+スライムは1, 2, 4...と増える　計算量はせいぜいN**2 * 2 ** Nまでできる
+簡単な場合から考える
+
+1, 2, 3, 4
+一番最初のスライムは4(一番大きいの)
+3, 4 → 1, 2, 3, 4 Yesになる
+もし1, 1, 3, 4でも
+3, 4 → 1, 1, 3, 4 Yesになる
+1, 1, 1, 4なら
+1, 4 → 1, 1, 4できない
+
+最初のスライムにはできるだけ大きい数字を産ませた方がいい
+つまり一番いい状態は4, 3, 3, 3, 2, 2, 2...みたいな状態
+これとSを比べて全てについて大きかったらいい？
+No 5, 1, 1, 1とかの場合は5, 4, 3, 3だがYesにはならない
+
+生産過程を木として見ると、下るにつれ数字が単調減少していく
+支流を全て数え上げると判定できる？
+めんどそう
+
+最初の4, 1番目に出てきた3, 2番目に出てきた3...がゲーム終了までに何を産むか？
+or ゲームのiターン目にそれぞれのスライムが何を産むか
+1, 1, 1, 2, 3, 3, 3 と 4(最初)
+1ターン
+4は3を産む
+1, 1, 1, 2, 3, 3と 3, 4
+2ターン
+4は3を産む
+3は2を産む
+1, 1, 1, 3 と2, 3, 3, 4
+3ターン目　できる　
+これはできそう
+
+iターン目について手持ちのスライムがそれぞれ何を産むか(Sから何を削除するか)
+はSを舐めてやる
+手持ちのスライムについて、大きいものから考える　一つスケールが小さいものを選ぶ
+Sは26万ぐらいあるけど、精々18周なので計算量はそんなにない
+Sはpop appendleftすればソートされた順序を保てる
+手持ちのスライムは無理なのでヒープキュー
 """
 
 N = getN()
-P = [getList() for i in range(N)]
-M = getN()
-manu = [getList() for i in range(M)]
-Q = getN()
-que = [getList() for i in range(Q)]
+S = getList()
+S.sort()
 
-# M回の操作をまとめる
-# 3 * 3のマトリックスを作る
-dp = [[[0, 0, 0] * 3 for j in range(3)] for i in range(M + 1)]
-dp[0] = [[1, 0, 0], [0, 1, 0], [0, 0, 1]] # 単位行列
-for i in range(M):
-    if manu[i][0] == 1:
-        dp[i + 1] = [[0, 1, 0], [-1, 0, 0], [0, 0, 1]]
-    elif manu[i][0] == 2:
-        dp[i + 1] = [[0, -1, 0], [1, 0, 0], [0, 0, 1]]
-    elif manu[i][0] == 3:
-        index, p = manu[i]
-        dp[i + 1] = [[-1, 0, 2 * p], [0, 1, 0], [0, 0, 1]]
-    else:
-        index, p = manu[i]
-        dp[i + 1] = [[1, 0, 0], [0, -1, 2 * p], [0, 0, 1]]
-    dp[i + 1] = array_cnt(dp[i + 1], dp[i])
+S = deque(S)
+que = []
+heappush(que, -S.pop())
 
-for m, p in que:
-    x, y = P[p - 1]
-    a_x, a_y, one_ = array_cnt(dp[m], [[x], [y], [1]]) # 縦は[[x], [y], [1]]みたいに
-    print(a_x[0], a_y[0])
+# N回やる
+for i in range(N):
+    new_S = deque([])
+    new_que = []
+    while S and que:
+        # 大きい順から取っていく
+        # Sから取ったのはqueに入れる
+        if S[-1] < -que[0]:
+            heappush(new_que, -S.pop())
+            heappush(new_que, heappop(que))
+        else:
+            new_S.appendleft(S.pop())
+    # 整理
+    while S:
+        new_S.appendleft(S.pop())
+    S = new_S
+    que = new_que
+
+# Sに何も残ってないと
+if not S:
+    print('Yes')
+else:
+    print('No')
