c1531835b0fef940e4aea4af6596f39dd1e959d2
customaddone <custom1485@docomo.ne.jp>
2021-03-17 19:38:44 +0900

    確率

--- cgi-bin/myfile.py
+++ cgi-bin/myfile.py
@@ -32,27 +32,23 @@ dy = [0, 1, 0, -1]
 # Main Code #
 #############
 
-# cargo: 0 ~ N - 1, box: N ~ N + m - 1
-# 始点: N + m, 終点: N + m + 1
-mcf = MinCostFlow(N + M + 2)
-for i in range(N):
-    # start ~ 荷物
-    mcf.add_edge(N + M, i, 1, 0)
-    print(N + M, i)
-    for j in range(M):
-        if l <= j <= r:
-            continue
-        # 荷物 ~ box
-        if cargo[i][0] <= box[j]:
-            print(i, N + j)
-            mcf.add_edge(i, N + j, 1, -cargo[i][1])
-for j in range(M):
-    # box ~ end
-    print(N + j, N + M + 1)
-    mcf.add_edge(N + j, N + M + 1, 1, 0)
+E = [0] * (N + M + 1)
+cnt = [0] * (N + M + 1)
 
-ans = -mcf.flow(N + M, N + M + 1, N)
-if ans == -float('inf'):
-    print(0)
-else:
-    print(ans)
+
+# 0からマスiに到達する期待値を求める
+for i in range(N + M + 1):
+    if 1 < i:
+        E[i] += E[i - 1]
+        cnt[i] += cnt[i - 1]
+    E[i] = max(0, E[i]) # マイナスにはならない
+    if cnt[i]:
+        E[i] = E[i] * M / cnt[i] # 足したのはM回中cnt[i]回だけなので
+
+    if i < N and not i in A:
+        E[i + 1] += (E[i] + 1) / M
+        E[i + M + 1] -= (E[i] + 1) / M
+        cnt[i + 1] += 1
+        cnt[i - 1] -= 1
+
+    print(i, E, cnt, P)
