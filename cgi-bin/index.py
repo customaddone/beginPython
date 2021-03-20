a2ff63db52df37277ffed16d48c059c99e18fde1
customaddone <custom1485@docomo.ne.jp>
2021-03-20 12:56:03 +0900

    after

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -33,15 +33,22 @@ from collections import deque
 
 def bfs(s):
     q = deque()
-    q.append((s, 1))
+    q.append((s, 1, 1))
     visit = [0] * n0
     visit[s] = 1
     while q:
-        i, now = q.popleft()
+        i, now1, now2 = q.popleft()
         for j, m in G[i]:
             if not visit[j]:
-                dist[s][j] = now * m
-                q.append((j, now * m))
+                if m > 0:
+                    dist1[s][j] = now1 * m
+                    dist2[s][j] = now2
+                else:
+                    dist1[s][j] = now1
+                    dist2[s][j] = now2 * -m
+                if dist1[s][j] % dist2[s][j] == 0:
+                    dist1[s][j], dist2[s][j] = dist1[s][j] // dist2[s][j], 1
+                q.append((j, dist1[s][j], dist2[s][j]))
                 visit[j] = 1
     return
 
@@ -61,16 +68,20 @@ for _ in range(n):
         w.append(l)
         n0 += 1
     G[d[l]].append([d[s], int(m)])
-    G[d[s]].append([d[l], 1 / int(m)])
-dist = [[1] * n0 for _ in range(n0)]
+    G[d[s]].append([d[l], -int(m)])
+dist1 = [[1] * n0 for _ in range(n0)]
+dist2 = [[1] * n0 for _ in range(n0)]
 for i in range(n0):
     bfs(i)
 maxdist = 0
 x, y = 0, 0
 for i in range(n0):
     for j in range(n0):
-        if maxdist < dist[i][j]:
-            maxdist = dist[i][j]
+        if not dist1[i][j] % dist2[i][j] == 0:
+            continue
+        nowdist = dist1[i][j] // dist2[i][j]
+        if maxdist < nowdist:
+            maxdist = nowdist
             x, y = i, j
 ans = "".join(map(str, [1, w[y], "=", maxdist, w[x]]))
 print(ans)
