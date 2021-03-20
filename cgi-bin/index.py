11aa4343cdd901e353f6d78d9d511244beda5432
customaddone <custom1485@docomo.ne.jp>
2021-03-20 20:28:26 +0900

    F - Minimum Bounding Box

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -29,59 +29,31 @@ dy = [0, 1, 0, -1]
 # Main Code #
 #############
 
-from collections import deque
+"""
+XとYを分割するんだろ
+X方向のみ　Yは変動なし
+もともとのdiffの分だけ
+LとRのみの場合は
+頂点が2つの場合を考える
+互いに近づいて行くなら最も近づいた時
+遠ざかるなら0秒地点で
+頂点3つR L L の場合
+Rが一番右にある場合は0秒地点で止める
+左、中にある場合は二つのLの差になる
+R R L L の場合は
+Rが一番右にいる場合は0秒地点
+Lが一番左にいる場合も0秒地点
+R L R Lとかの場合は互いが近づき合う
+Rで挟む場合0秒地点
+R L
+0 1　とかの場合は一秒後は　1 0になる 0.5秒とかでもいい
 
-def bfs(s):
-    q = deque()
-    q.append((s, 1, 1))
-    visit = [0] * n0
-    visit[s] = 1
-    while q:
-        i, now1, now2 = q.popleft()
-        for j, m in G[i]:
-            if not visit[j]:
-                if m > 0:
-                    dist1[s][j] = now1 * m
-                    dist2[s][j] = now2
-                else:
-                    dist1[s][j] = now1
-                    dist2[s][j] = now2 * -m
-                if dist1[s][j] % dist2[s][j] == 0:
-                    dist1[s][j], dist2[s][j] = dist1[s][j] // dist2[s][j], 1
-                q.append((j, dist1[s][j], dist2[s][j]))
-                visit[j] = 1
-    return
+動くものの下限と上限を持っておけばいい
+steadyなラインを考えて
+３分探索にならなそう
+A * Bの掛け算を小さくすることを考える
 
-n = int(input())
-d = dict()
-w = []
-n0 = 0
-G = [[] for _ in range(405)]
-for _ in range(n):
-    s, m, l = input().split()
-    if not s in d:
-        d[s] = n0
-        w.append(s)
-        n0 += 1
-    if not l in d:
-        d[l] = n0
-        w.append(l)
-        n0 += 1
-    G[d[l]].append([d[s], int(m)])
-    G[d[s]].append([d[l], -int(m)])
-dist1 = [[1] * n0 for _ in range(n0)]
-dist2 = [[1] * n0 for _ in range(n0)]
-for i in range(n0):
-    bfs(i)
-maxdist = 0
-x, y = 0, 0
-for i in range(n0):
-    for j in range(n0):
-        if not dist1[i][j] % dist2[i][j] == 0:
-            continue
-        nowdist = dist1[i][j] // dist2[i][j]
-        if maxdist < nowdist:
-            maxdist = nowdist
-            x, y = i, j
-ans = "".join(map(str, [1, w[y], "=", maxdist, w[x]]))
-print(ans)
+最小値の候補
+0秒地点
+地点がいくつかあるので地点ごとに計算して求める
+"""
