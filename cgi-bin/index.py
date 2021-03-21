31b75ae909b274390acd29bcc88820f4ea10ca3c
customaddone <custom1485@docomo.ne.jp>
2021-03-21 23:36:44 +0900

    E - Two Currencies

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -29,51 +29,65 @@ dy = [0, 1, 0, -1]
 # Main Code #
 #############
 
-N = getN()
-manu = [getList() for i in range(N)]
-Q = getN()
-que = getList()
+"""
+遠方に行くには金貨をどこかで補充しないと
+変形ダイクストラ　銀貨をA枚持ってた時の最短距離
+銀貨何枚を持っていれば何分でどこにいけるかはわかる
+ルート上のどこかで金を交換すればいい　交換分数のみが重要
+"""
 
-# 上界と下界を求める
-def f(x):
-    res = x
-    for a, t in manu:
-        if t == 1:
-            res += a
-        elif t == 2:
-            res = max(res, a)
-        else:
-            res = min(res, a)
+# 最短経路へのパス付きダイクストラ
+# 運賃s以内でいけるか
+def dij(start, edges, s):
+    dist = [float('inf') for i in range(N)]
+    dist[start] = 0
+    pq = [(0, start, s)] # 銀S枚を持ってスタート
+    parent = [-1] * N
+    cost = [float('inf')] * N
+    cost[0] = 0
 
-    return res
+    # pqの先頭がgoal行きのものなら最短距離を返す
+    while len(pq) > 0:
+        di, now, sil = heappop(pq)
+        if (di > dist[now]):
+            continue
+        for v, c, d in edges[now]:
+            if dist[v] > dist[now] + d and sil >= c:
+                dist[v] = dist[now] + d
+                parent[v] = now
+                cost[v] = s - sil + c
+                heappush(pq, (dist[v], v, sil - c))
 
-### マイナス付き二分探索するときの方法 ###
+    return dist, cost
 
-# 幅は2 * 10 ** 9
-# 2 ** 32 - 1あれば十分 4294967296
-# ある程度upperの値が大きいと数値を少し下げてもresの値は変わらない
-upper = (2 ** 32) - 1 # 1111111111...
-diff = 2 ** 31 # 調整用　十分2 * 10 ** 9より大きい
-for i in range(31, -1, -1):
-    if f(upper - diff) == f(upper - (2 ** i) - diff):
-        upper -= 2 ** i
+N, M, S = getNM()
+road = [[] for i in range(N)]
+for i in range(M):
+    u, v, a, b = getNM()
+    road[u - 1].append([v - 1, a, b])
+    road[v - 1].append([u - 1, a, b])
+exc = [getList() for i in range(N)]
 
-under = 0
-for i in range(31, -1, -1):
-    if f(under - diff) == f(under + (2 ** i) - diff):
-        under += 2 ** i
+# iスタート、銀貨がj枚でどこにいけるか
+table = [[] for i in range(N)]
+for i in range(N):
+    for j in range(5001):
+        table[i].append(dij(i, road, j)[0])
 
-upper -= diff
-under -= diff
-ans_under = f(under)
-ans_upper = f(upper)
-su = sum([i[0] for i in manu if i[1] == 1])
-
-for q in que:
-    # under以下であればf(under)
-    if q <= under:
-        print(ans_under)
-    elif upper <= q:
-        print(ans_upper)
-    else:
-        print(q + su)
+ans = [float('inf')] * N
+cost = dij(0, road, S)[1]
+print(cost)
+# 点iで金を補充して点jまで行くことを考える
+# 中継地点
+for i in range(N):
+    c, d = exc[i]
+    # 中継地点スタートの時銀貨s枚
+    for s in range(5001):
+        # s - S + cost[i]枚足らないのだから
+        add = ((max(s + cost[i] - S, 0) + c - 1) // c) * d
+        # ゴール地点
+        for j in range(N):
+            if ans[j] > table[0][S][i] + add + table[i][s][j]:
+                print(table[0][S][i], add, table[i][s][j], i, j, s)
+            ans[j] = min(ans[j], table[0][S][i] + add + table[i][s][j])
+print(ans)
