e2ac0c2bfb9a309fd348b51608d5279d3a253174
customaddone <custom1485@docomo.ne.jp>
2021-03-26 19:37:08 +0900

    確率

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -29,311 +29,176 @@ dy = [0, 1, 0, -1]
 # Main Code #
 #############
 
-num = [i for i in range(0, 10, 2)]
-A = [2, 4, 5]
-B = [2, 3]
+# ABC144 F - Fork in the Road
 
-for i in A:
-    index = bisect_right(num, i)
-    print(num[index - 1])
+"""
+塞いだあと高橋くんがどこかで詰まったらだめ
+辺を一つ消す　判定するをN回繰り返す
+dpする？
+dp[i][j]: 頂点iにj回の移動で到達する確率
 
-# numの中でのi未満の数字の最大値を求める
-for i in A:
-    index = bisect_left(num, i)
-    print(num[index - 1])
+# dp[i][j]: 頂点iにj回の移動で到達する確率
+dp = [[0] * (N + 1) for i in range(N)]
+dp[0][0] = 1
 
-# numの中でのiより大きい数字の最小値を求める
-for i in B:
-    index = bisect_right(num, i)
-    print(num[index])
-
-# numの中でのi以上の数字の最小値を求める
-for i in B:
-    index = bisect_left(num, i)
-    print(num[index])
-
-A = [1, 2, 4, 8, 16, 32]
-
-def or_less(array, x):
-    # arrayの中のx以下のものの個数
-    # arrayの中のx以下のもののうちの最大値
-    index = bisect_right(array, x)
-    if index == 0:
-        or_less_int = -float('inf')
-    else:
-        or_less_int = array[index - 1]
-    return [index, or_less_int]
-
-def less_than(array, x):
-    # arrayの中のx未満のものの個数
-    # arrayの中のx未満のもののうちの最大値
-    index = bisect_left(array, x)
-    if index == 0:
-        less_than_int = -float('inf')
-    else:
-        less_than_int = array[index - 1]
-    return [index, less_than_int]
-
-print(or_less(A, 8))
-print(less_than(A, 1))
-
-def or_more(array, x):
-    # arrayの中のx以上のものの個数
-    # arrayの中のx以上のもののうちの最小値
-    n = len(array)
-    index = bisect_left(array, x)
-    if index == n:
-        or_more_int = float('inf')
-    else:
-        or_more_int = array[index]
-    return [n - index, or_more_int]
-
-def more_than(array, x):
-    # arrayの中のxより大きいものの個数
-    # arrayの中のxより大きいのもののうちの最小値
-    n = len(array)
-    index = bisect_right(array, x)
-    if index == n:
-        more_than_int = float('inf')
-    else:
-        more_than_int = array[index]
-    return [n - index, more_than_int]
-
-print(or_more(A, 32))
-print(more_than(A, 1))
-
-# ABC122 D - We Like AGC
-X, Y, Z, K = getNM()
-A = sorted([-i for i in getList()])
-B = sorted([-i for i in getList()])
-C = sorted([-i for i in getList()])
-
-pos = []
-heapify(pos)
-dict = defaultdict(int)
-u = (A[0] + B[0] + C[0], 0, 0, 0)
-heappush(pos, u)
-dict[u] = 1
-
-for i in range(K):
-    p, i, j, l = heappop(pos)
-    print(-p)
-    # 取り出すごとにA, B, Cについての次の値をpush
-    if i + 1 < X:
-        opt_a = (A[i + 1] + B[j] + C[l], i + 1, j, l)
-        if dict[opt_a] == 0:
-            heappush(pos, opt_a)
-            dict[opt_a] = 1
-    if j + 1 < Y:
-        opt_b = (A[i] + B[j + 1] + C[l], i, j + 1, l)
-        if dict[opt_b] == 0:
-            heappush(pos, opt_b)
-            dict[opt_b] = 1
-    if l + 1 < Z:
-        opt_c = (A[i] + B[j] + C[l + 1], i, j, l + 1)
-        if dict[opt_c] == 0:
-            heappush(pos, opt_c)
-            dict[opt_c] = 1
-
-# ABC149 E - Handshake
-N, K = 9, 14
-A = [1, 3, 5, 110, 24, 21, 34, 5, 3]
-
-A.sort(reverse = True)
-
-# 各ペアを大きい順に並べるのはムリ
-# ただし、合計がx以上/以下になるペアが何個あるかは求められる
-# x以上になるペアが何個あるか
-def cnt(x):
-    r = 0
-    ans = 0
-    list_num = [0] * N
-    for i in range(N - 1, -1, -1):
-        while r < N and A[r] + A[i] >= x:
-            r += 1
-        list_num[i] = r
-        ans += r
-
-    return [ans, list_num]
-
-ng = 0
-ok = 2 * (10 ** 6)
-
-while ok - ng > 1:
-    mid = (ok + ng) // 2
-    if cnt(mid)[0] <= K:
-        ok = mid
-    else:
-        ng = mid
-
-# 境界がわかったら
-imos = [0] + copy.deepcopy(A)
+# 頂点
 for i in range(N):
-    imos[i + 1] += imos[i]
+    # j回の移動で
+    for j in range(i + 1):
+        for v in E[i]:
+            dp[v][j + 1] += dp[i][j] / len(E[i])
+print(dp)
+
+一回の判定でO(M)ぐらいかかる
+エッジにウェートをかけると？
+エッジを見るだけでなんとかなるように
+dpの全てについて調べる必要はないんでは
+P[s] * (1 / M)の大きさが最も大きいものが一番Nの期待値への寄与度が大きい
+"""
+
+N, M = getNM()
+dist = [[] for i in range(N)]
+for i in range(M):
+    s, t = getNM()
+    dist[s - 1].append(t - 1)
+for i in range(N):
+    dist[i].sort()
+
+# トポロジカルソートにすればs < tの条件がなくても使える
+def calc(edges):
+    # 確率を計算
+    P = [0] * N
+    P[0] = 1
+    for u in range(N):
+        for v in edges[u]:
+            P[v] += P[u] / len(edges[u])
+
+    # 期待値を計算　ゴールから逆向きで期待値を求める
+    # あと何回進めばゴールまで行けるか
+    E = [0] * N
+    for u in range(N - 1, -1, -1):
+        for v in edges[u]:
+            # この(E[v] + 1)が大きくなるものを削ればいい
+            E[u] += (E[v] + 1) / len(edges[u])
+
+    return P, E
+
+P, E = calc(dist)
+ans = E[0]
+diff = 0
+
+for u in range(N):
+    for v in dist[u]:
+        if len(dist[u]) > 1:
+            # u ~ vの辺を削ると(E[v] + 1) / len(dist[u])の分だけ軽くなるが
+            # 残った分が len(dist[u]) / (len(dist[u]) - 1)でかけられる
+            ban = (E[v] + 1) / len(dist[u]) # これが消える予定
+            new = (E[u] - ban) * len(dist[u]) / (len(dist[u]) - 1)
+            diff = max(diff, (E[u] - new) * P[u])
+
+print(ans - diff)
+
+# M-SOLUTION プロコン　C - Best-of-(2n-1)
+
+"""
+数学問題でーす
+どちらかが累計でN回勝てば良い　遷移を考える N^2解から考える
+高橋くんが1回勝って終わる確率
+答えは整数 / 整数になるらしい
+
+高橋くんがN回勝って終わる時
+青木くんはN - 1回以下の任意の整数回勝って終わっている
+青木くんが勝つ確率をb回で固定して足し合わせ
+引き分けは無限回あって良い
+青木くんがN回勝って終わるのは逆になる
+２つは独立事象か？
+
+N+b+cCc * N+bCb * (A / 100)^N * (B / 100)^b * (C / 100)^c
+
+期待値を計算
+dp[1][0] = A / 100 * (dp[0][0] + 1) + C / 100 * (dp[1][0] + 1)
+dp[i][j] = A / 100 * (dp[i - 1][j] + 1) + B / 100 * (dp[i][j - 1] + 1) + C / 100 * (dp[i][j])
+(100 - C)dp[i][j] = A * (dp[i - 1][j] + 1) + B * (dp[i][j - 1] + 1)
+
+合計でN回勝つ期待値でdpとるか
+dp[i]: どちらかがN回勝って終わっている
+i+1だと？
+
+一発では無理
+ans += 期待値 / 確率を全ての場合で求めるやり方
+
+確率の求め方
+高橋くんがa回、青木くんがb回出すとすれば
+a+bCa * (A / A + B)^a * (B / A + B)^b
+a+bCa * A^a * B^b * (A + B)^a+b
+高橋くんがN回、青木くんが0回、1回...その逆を求めるなら
+
+高橋くんがN-1回、青木くんがi回勝ち、さらに高橋くんが勝てば良い
+
+高橋くんがN回、青木くんがi回勝つのにかかる期待値
+マスを一つ移動するのにかかる期待値は？
+C = 0なら自明にa + bになる
+100 * (N + i) / 100 - C
+100 / 100 - Cは空回り分
+
+A == B == 0はない
+"""
+
+# 逆元事前処理ver
+# nが小さい場合に
+lim = 10 ** 6 + 1
+fact = [1, 1]
+factinv = [1, 1]
+inv = [0, 1]
+
+for i in range(2, lim + 1):
+    fact.append((fact[-1] * i) % mod)
+    inv.append((-inv[mod % i] * (mod // i)) % mod)
+    # 累計
+    factinv.append((factinv[-1] * inv[-1]) % mod)
+
+def cmb(n, r):
+    if (r < 0) or (n < r):
+        return 0
+    r = min(r, n - r)
+    return fact[n] * factinv[r] * factinv[n - r] % mod
+
+N, A, B, C = getNM()
+a_pow = [1] * (10 ** 6 + 1)
+b_pow = [1] * (10 ** 6 + 1)
+for i in range(1, 10 ** 6 + 1):
+    a_pow[i] = (a_pow[i - 1] * A) % mod
+    b_pow[i] = (b_pow[i - 1] * B) % mod
 
 ans = 0
-plus = cnt(ok)[1]
+# 高橋くんがN-1回、青木くんが0 ~ N - 1回勝ったあと、高橋くんが勝つ
 
-# cntから得た情報を元に上から順番に足していく
 for i in range(N):
-    ans += (A[i] * plus[i]) + imos[plus[i]]
-# 合計がngになるペアを足りない分K - sum(plus)個足す
-ans += ng * (K - sum(plus))
-print(ans)
+    # 分子はpowして良い
+    prob = ((cmb(N + i - 1, i) * a_pow[N - 1] * b_pow[i]) * A) % mod
+    # 分母は(A + B) ** (2 * N - 1)に揃える
+    prob *= pow(A + B, N - i - 1, mod)
+    prob %= mod
+    # deno = (A + B) ** (2 * N - 1)
 
-# ABC155 D - Pairs
-N, K = 10, 40
-A = [5, 4, 3, 2, -1, 0, 0, 0, 0, 0]
-minus = [-x for x in A if x < 0]
-plus = [x for x in A if x >= 0]
+    # 期待値　分子部分
+    exp = 100 * (N + i)
+    # 分母はA + B
+    ans += prob * exp
+    ans %= mod
 
-minus.sort()
-plus.sort()
+# 青木くんサイド
+for i in range(N):
+    prob = ((cmb(N + i - 1, i) * a_pow[i] * b_pow[N - 1]) * B) % mod
+    prob *= pow(A + B, N - i - 1, mod)
+    prob %= mod
 
-def cnt(x):
-    ans = 0
-    if x < 0:
-        x = -x
-        r = 0
-        # - * +
-        for num in minus[::-1]:
-            while r < len(plus) and plus[r] * num < x:
-                r += 1
-            ans += len(plus) - r
-        return ans
+    exp = 100 * (N + i)
+    ans += prob * exp
+    ans %= mod
 
-    r = 0
-    for num in minus[::-1]:
-        if num * num <= x:
-            ans -= 1
-        while r < len(minus) and minus[r] * num <= x:
-            r += 1
-        ans += r
-    r = 0
-    for num in plus[::-1]:
-        if num * num <= x:
-            ans -= 1
-        while r < len(plus) and plus[r] * num <= x:
-            r += 1
-        ans += r
-    ans //= 2
-    # -になるものはまとめて計算
-    ans += len(minus) * len(plus)
-    return ans
-
-bottom = 0
-top = 2 * (10 ** 18) + 2
-
-
-while top - bottom > 1:
-    mid = (top + bottom) // 2
-    if cnt(mid - 10 ** 18 - 1) < K:
-        bottom = mid
-    else:
-        top = mid
-
-print(int(top - 10 ** 18 - 1))
-
-# ARC037 C - 億マス計算
-N, K = getNM()
-A = sorted(getList())
-B = sorted(getList())
-
-# かけ合わせるとx以下になるものが何個あるか
-def cnt(x):
-    r = 0
-    ans = 0
-    list_num = [0] * N
-    # BiについてAの各要素とペアになれるのがいくつあるか
-    for i in range(N - 1, -1, -1):
-        while r < N and A[r] * B[i] <= x:
-            r += 1
-        list_num[i] = r
-        ans += r
-
-    return [ans, list_num]
-
-ng = 0
-ok = 2 * (10 ** 18)
-
-while ok - ng > 1:
-    mid = (ok + ng) // 2
-    if cnt(mid)[0] >= K:
-        ok = mid
-    else:
-        ng = mid
-
-print(ok)
-
-# ABC107 D - Median of Medians
-
-"""
-ソートしても問題はない
-連続部分列はN(N + 1)//2個ある
-ヒープキューによるN^2解は思いつく
-端っこがlのものについてO(logN)ぐらいで求められないか
-BITを立てていこうか A[i]が中央値になるか
-左右にどれだけ広がればいいか
-左右に広げた時にちょうど濃度が1/2になればいい
-結局累積が必要になる？
-
-中央値の列挙は必要ない　二分探索？
-前から数えてN(N + 1)//4 + 1個目は何だろう
-
-全部列挙するとN(N + 1) // 2個ある
-列挙できたとしてもその中の中央値は求められない
-二分探索で求める？
-結局のところ濃度を考える
-平均値がx以下である連続区間の個数は
-"""
-
-N = getN()
-A = getList()
-
-# 一回あたり計算量はO(NlogN)
-def f(x):
-    imos = [0] * (N + 1) # 1-index
-    for i in range(N):
-        if A[i] <= x:
-            imos[i + 1] = 1
-        imos[i + 1] += imos[i]
-
-    # 前から濃度を計算する
-    # x以下の値の個数 - xより大きい値の個数
-    # 0でeven
-    for i in range(N + 1):
-        under, upper = imos[i], i - imos[i]
-        imos[i] = [under - upper, i + 1] # 2-indexに
-
-    # imos[j] - imos[i] > 0になるi, jを選ぶ
-    # 小さい順に置いていって左のものの個数を数えればいい
-    imos.sort(reverse = True)
-    bit = BIT(N + 2) # 2-index
-
-    res = 0
-    while imos:
-        now = imos[-1][0]
-        opt = []
-        while imos and imos[-1][0] <= now:
-            _, index = imos.pop()
-            opt.append(index)
-            # 左にあるものの個数を数える
-            res += bit.get(index)
-        while opt:
-            bit.add(opt.pop(), 1)
-
-    return res
-
-# N(N + 1) // 2個あるけど、その半分 (N(N + 1) // 2) // 2以下の個数になるように
-lim = (N * (N + 1) // 2) // 2
-alt = [0] + sorted(A)
-
-# NlogmaxAlogNだと間に合わない
-# NloglogNでやる
-index = 0
-for i in range(32, -1, -1):
-    opt = index + 2 ** i
-    if opt < N + 1 and f(alt[opt]) <= lim:
-        index += 2 ** i
-
-print(alt[index + 1])
+# 最後に確率計算(A + B) ** (2 * N - 1)、期待値(A + B)
+# 計((A + B) ** (2 * N))で割ることにする
+# 分母の逆元の計算はこうやる
+deno = (pow(A + B, 2 * N, mod)) % mod
+print((ans * pow(deno, mod - 2, mod)) % mod)
--- cgi-bin/myfile.py
+++ cgi-bin/myfile.py
@@ -32,25 +32,36 @@ dy = [0, 1, 0, -1]
 # Main Code #
 #############
 
-N, M = getNM()
-men = [getList() for i in range(N)] + [[float('inf'), float('inf')]]
-women = [getList() for i in range(M)]
+"""
+数学問題でーす
+どちらかが累計でN回勝てば良い　遷移を考える N^2解から考える
+高橋くんが1回勝って終わる確率
+答えは整数 / 整数になるらしい
 
-men.sort(key = lambda i:i[1])
-bef_men = []
-women.sort(reverse = True)
+高橋くんがN回勝って終わる時
+青木くんはN - 1回以下の任意の整数回勝って終わっている
+青木くんが勝つ確率をb回で固定して足し合わせ
+引き分けは無限回あって良い
+青木くんがN回勝って終わるのは逆になる
+２つは独立事象か？
 
-for a, b in men:
-    # ここで脱落する女を選ぶ
-    gone_women = []
-    while women and women[-1][0] < b:
-        c, d = women.pop()
-        gone_women.append(d)
-    # ここで男を選ぶ d <= aになるように
-    # 条件が厳しいもの（dが大きい順)に結んでいく
-    gone_women.sort(reverse = True)
-    for g in gone_women:
-        print(g, bef_men)
+N+b+cCc * N+bCb * (A / 100)^N * (B / 100)^b * (C / 100)^c
 
-    # 男収納
-    heappush(bef_men, -a)
+期待値を計算
+dp[1][0] = A / 100 * (dp[0][0] + 1) + C / 100 * (dp[1][0] + 1)
+dp[i][j] = A / 100 * (dp[i - 1][j] + 1) + B / 100 * (dp[i][j - 1] + 1) + C / 100 * (dp[i][j])
+(100 - C)dp[i][j] = A * (dp[i - 1][j] + 1) + B * (dp[i][j - 1] + 1)
+
+合計でN回勝つ期待値でdpとるか
+dp[i]: どちらかがN回勝って終わっている
+i+1だと？
+
+"""
+
+N, A, B, C = getNM()
+dp = [[0] * (N + 1) for i in range(N + 1)]
+
+for i in range(N):
+    for j in range(N):
+        if i == j == 0:
+            continue
