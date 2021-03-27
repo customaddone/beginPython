20616deae402207d7669000fd48b4ca1edc5db6c
customaddone <custom1485@docomo.ne.jp>
2021-03-27 11:02:32 +0900

    a

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,34 +1,3 @@
-from collections import defaultdict, deque, Counter
-from heapq import heapify, heappop, heappush
-import sys
-import math
-import random
-import string
-from copy import deepcopy
-from itertools import combinations, permutations, product
-from bisect import bisect_left, bisect_right
-
-def input():
-    return sys.stdin.readline().rstrip()
-def getN():
-    return int(input())
-def getNM():
-    return map(int, input().split())
-def getList():
-    return list(map(int, input().split()))
-def getArray(intn):
-    return [int(input()) for i in range(intn)]
-
-sys.setrecursionlimit(1000000000)
-mod = 10 ** 9 + 7
-INF = float('inf')
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
-
-#############
-# Main Code #
-#############
-
 def getN():
     return int(input())
 def getNM():
