e7295761b89ecffe9f9be5d914b9c2a9796b9156
customaddone <custom1485@docomo.ne.jp>
2021-04-06 21:18:36 +0900

    arrat

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,26 +1,60 @@
-S = 'atcoder'
-K = 3
+from collections import defaultdict, deque, Counter
+from heapq import heapify, heappop, heappush
+import math
+from copy import deepcopy
+from itertools import combinations, permutations, product
+from bisect import bisect_left, bisect_right
 
-N = len(S)
-L = [[] for i in range(26)]
-for i in range(N - 1, -1, -1):
-    L[ord(S[i]) - ord('a')].append(i)
+import sys
+def input():
+    return sys.stdin.readline().rstrip()
+def getN():
+    return int(input())
+def getNM():
+    return map(int, input().split())
+def getList():
+    return list(map(int, input().split()))
+def getArray(intn):
+    return [int(input()) for i in range(intn)]
 
-#  現在選択し終えた文字列のうち最新の文字
-now = -1
-ans = ''
-# k文字目に何を選ぶか
-for k in range(K):
-    # a ~ zについて探索する
-    for j in range(26):
-        # 現在の文字より前にあるものは全て取り除く
-        while L[j] and L[j][-1] <= now:
-            L[j].pop()
-        # N - L[j][-1]: この文字を含めたSの残りの文字数
-        # K - k: 指定する必要のある残りの文字数
-        if L[j] and N - L[j][-1] >= K - k:
-            now = L[j].pop()
-            ans += S[now]
-            break
+sys.setrecursionlimit(1000000000)
+mod = 10 ** 9 + 7
+INF = float('inf')
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
 
-print(ans)
+#############
+# Main Code #
+#############
+
+def array_cnt(ar1, ar2):
+    h = len(ar1)
+    w = len(ar2[0])
+    row = ar1
+    col = []
+    for j in range(w):
+        opt = []
+        for i in range(len(ar2)):
+            opt.append(ar2[i][j])
+        col.append(opt)
+
+    res = [[[0, 0] for i in range(w)] for i in range(h)]
+    for i in range(h):
+        for j in range(w):
+            cnt = 0
+            for x, y in zip(row[i], col[j]):
+                cnt += x * y
+            res[i][j] = cnt
+    return res
+
+a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+o1 = [[1, 0, 0], [0, 1, 0], [-7, 0, 1]]
+o2 = [[1, 0, 0], [-4, 1, 0], [0, 0, 1]]
+o3 = [[1, -2, 0], [0, 1, 0], [0, 0, 1]]
+o4 = [[1, 0, -3], [0, 1, 0], [0, 0, 1]]
+a = array_cnt(o1, a) # 右
+a = array_cnt(o2, a)
+a = array_cnt(a, o3) # 左
+a = array_cnt(a, o4)
+# [[1, 0, 0], [0, -3, -6], [0, -6, -12]]
+print(a)
