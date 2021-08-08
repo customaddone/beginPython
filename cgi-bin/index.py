841aadb5718c729c2a05721cc8627bb7b8d65af1
customaddone <custom1485@gmail.com>
2021-08-08 14:58:14 +0900

    閉路

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -31,171 +31,258 @@ dx = [1, 0, -1, 0]
 # Main Code #
 #############
 
-# パ研合宿2020　第1日「SpeedRun」
-# 同じgcdを持つ区間は結合しても同じまま
+# ABC021 C - 正直者の高橋くん
+# 経路の通りを求める問題
+N = 7
+a, b = 1, 7
+M = 8
+que = [
+[1, 2],
+[1, 3],
+[4, 2],
+[4, 3],
+[4, 5],
+[4, 6],
+[7, 5],
+[7, 6]
+]
+dist = [[] for i in range(N)]
+for x, y in que:
+    dist[x - 1].append(y - 1)
+    dist[y - 1].append(x - 1)
 
-"""
-K個に切り分ける
-それぞれの部分の総和のgcdが大きいほどいい
-最大公約数はいくつになるか
-二分探索したいが dpもしたい
+# スタートからの最短距離測定
+def distance(sta):
+    # 木をstaから順にたどる（戻るの禁止）
+    pos = deque([sta])
+    ignore = [-1] * N
+    ignore[sta] = 0
 
-300C150とかは無理
+    while len(pos) > 0:
+        u = pos.popleft()
+        for i in dist[u]:
+            if ignore[i] == -1:
+                ignore[i] = ignore[u] + 1
+                pos.append(i)
 
-K = 1から考える
-答えはAの総和
+    return ignore
 
-A が最大300しかない
-エッジを貼る
-k回辺を移動して0 ~ Nにいけるか
+d = distance(a - 1)
 
-調和級数
-"""
+# スタートから特定の点まで最短距離で行く通りの数
+def counter(sta):
+    pos = deque([sta])
+    ignore = [0] * N
+    cnt = [0] * N
+    cnt[sta] = 1
 
+    while len(pos) > 0:
+        u = pos.popleft()
+        if ignore[u] == 0:
+            ignore[u] = 1
+            # d[i] == d[u] + 1を満たすuの子ノード全てに
+            # 「スタートからuまでの通りの数」をプラス（他のルートからも来る）
+            for i in dist[u]:
+                if d[i] == d[u] + 1:
+                    cnt[i] += cnt[u]
+                    pos.append(i)
+    return cnt
+
+print(counter(a - 1)[b - 1] % mod)
+
+# ARC044 B - 最短路問題
+# 通りの数を求める問題
+# 深さ1のものは,深さ2のものは
 N = getN()
-A = [0] + getList()
-su = sum(A)
-for i in range(1, N + 1):
-    A[i] += A[i - 1]
-
-ans = [0] * (N + 1)
-for i in range(su + 1, 0, -1):
-    # これを通過すると必ず条件を満たす区間を作れる
-    if su % i != 0:
-        continue
-
-    cnt = 0
-    last = 0
-    for j in range(1, N + 1):
-        if (A[j] - A[last]) % i == 0:
-            last = j
-            cnt += 1
-
-    # cnt以下の区間は結合することで簡単に作れる
-    for a in range(cnt + 1):
-        ans[a] = max(ans[a], i)
-
-for a in ans[1:]:
-    print(a)
-
-# Chokudai SpeedRun 002 J - GCD β
-
-"""
-N <= 50000 これはなに？
-NlogNまでならいける
-最大公約数を最大にするには　同じ倍数でまとめればいい
-ただしAiは大きい
-どちらの数字を使うか
-targetを選択するか
-全部因数分解して候補を探る Aiがでかいので間に合わない
-まずAi, Bi 10 ** 5個が候補としてある
-1個下の奴とのgcdで結ぶ　エッジは高々20万本
-AiスタートとBiスタートがある
-残っているものとgcdする
-Aiがでかいので...
-A0, B0の約数しか候補にならない
-"""
-
-def make_divisors(n):
-    divisors = []
-    for i in range(1, int(math.sqrt(n)) + 1):
-        if n % i == 0:
-            divisors.append(i)
-            # √nで無い数についてもう一個プラス
-            if i != n // i:
-                divisors.append(n // i)
-    return sorted(divisors)
-
-N = getN()
-A, B = [], []
-for i in range(N):
-    a, b = getNM()
-    A.append(a)
-    B.append(b)
-
-ans = set()
-opt_l = make_divisors(A[0]) + make_divisors(B[0])
-
-for opt in opt_l:
-    for i in range(N):
-        if (A[i] % opt != 0) and (B[i] % opt != 0):
-            break
-    else:
-        ans.add(opt)
-
-ans = sorted(list(ans))
-print(ans[-1])
-
-# ABC136 Max GCD
-
-"""
-0回以上K回以下行う
-Aの全ての要素を割り切る: 操作後をAの全てがansの倍数になる
-二分探索したい　単調性はあるのか
-K = 無限の時
-8 20
-0 28
--A 28 + A 結局最初のAの値に拘束される
-0 28で28が最大か
-K = 無限の場合の最大値はsum(A)
-どれだけこれに近づけるか
-sum(A)の倍数にしか到達できない　倍数の数は大体logN にぶたんしなくても十分早い
-K回操作することでtargetの倍数に揃えられるか
-4 5
-10 1 2 22　の時
-35に揃えられるか
--10, -1, -2, +13 無理
-7に揃えられるか
--3, -1, -2, -1 total: -7, abs: 7
-前からtotal // 7個を反転させる　反転させた場合現在のabsから7 - 2abs(Ai)増える
-それぞれの要素についてプラスするかマイナスするか
-プラスするものとマイナスするものはイーブンでないといけない
-"""
-
-N, K = getNM()
 A = getList()
-su = sum(A)
+M = max(A)
 
-for p in make_divisors(su)[::-1]:
-    ar = sorted([a % p for a in A], reverse = True)
-    total = sum(ar) # total値は反転させるごとに7ずつ減っていく
-    ab = total # abは反転させるごとにa * 2 - pずつ減る ならaの大きい順に反転させた方がお得
+if A[0] != 0:
+    print(0)
+    exit()
 
-    for i in range(N):
-        if total == 0:
-            break
-        total -= p
-        ab -= ar[i] * 2 - p
-    if ab <= K * 2:
-        print(p)
+lista = [0] * (M + 1)
+lista[0] = 1
+for i in range(1, N):
+    if A[i] == 0:
+        print(0)
         exit()
+    lista[A[i]] += 1
 
-# codeforces round691
-# C - Row GCD
+ans = 1
+for i in range(1, M + 1):
+    if lista[i] == 0:
+        print(0)
+        exit()
+    # 全ての距離i - 1の点とある距離iの点との辺について
+    # 繋いだ場合辺はi - 1の点の数だけあるが、これらのうち１つ以上と繋ぐ
+    opt1 = (pow(2, lista[i - 1], mod) - 1)
+    # それが距離iの点の数分ある
+    depth = pow(opt1, lista[i], mod)
+    # 距離i間の辺について
+    # 辺はlista[i] * (lista[i] - 1) // 2だけあるが、そのうち０本以上と繋ぐ
+    # これによって頂点の最短距離が変わることはない
+    width = pow(2, lista[i] * (lista[i] - 1) // 2, mod)
+    ans *=  depth * width
+    ans %= mod
+print(ans)
 
-# commmon divisor
-# Ai + nのgcdについて　1 ~ Mまで
-# 最悪セグ木でなんとか
+# F - Pure
 
-# 求めるdがあったとすると　現在の時点でもmodが全て同じでないといけない
-# 候補は各数字同士の差分のgcdの約数　因数ごとにやれば
-# A[0] + B[i]とgのgcdを取ればいい
+"""
+強連結成分分解とかの話に繋がってくる
+つまりループを作ればいい
+1 - 2 - 3 - 4 - 1みたいな
+1 が2以外の例えば1 - 3みたいなパスがあれば
+1 - 3 - 4 - 1で作ればいい
 
-# A[0] + B[i]とgが共通してもつ因数をmとすると
-# g = nmと表せる
-# A[0] + B[i]よりknm大きいA[1] + B[i]も当然mを約数にもつ
+最小のループが答え
+bfsなら早々に最小のものが見つかる
+
+強連結
+有向グラフにおいて、すべての頂点間で互いに行き来できる
+強連結成分を一つの頂点に潰すと、DAGになる　トポソできる
+
+まずbfsする　その後、戻れるエッジがあるか
+あればその最小値が答え
+
+まずbfsしてDAGで考えるともう処理したものを考えなくていいのでいろいろ便利
+"""
 
 N, M = getNM()
-A = getList()
-B = getList()
+dist = [set() for i in range(N)]
+for i in range(M):
+    a, b = getNM()
+    dist[a - 1].add(b - 1)
 
-target = A[0]
-if N > 1:
-    A = [abs(A[i] - A[i - 1]) for i in range(N - 1)]
-    g = A[0]
-    for i in range(2, N - 1):
-        g = math.gcd(g, A[i])
+ignore = [-1] * N
+path = [set() for i in range(N)] # 始点からのパス
+parents = [-1] * N
+roop = [-1] * N #  ループの始点と終点
+roop_len = [-1] * N # ループの長さ
+
+for i in range(N):
+    if ignore[i] >= 0:
+        continue
+
+    ignore[i] = i
+    pos = deque([[i, 0]])
+    path[i].add(i)
+
+    while pos:
+        u, dis = pos.popleft()
+        for j in list(dist[u]):
+            if ignore[j] == -1:
+                ignore[j] = i
+                parents[j] = u
+                # ループ判定
+                path[j] = deepcopy(path[u])
+                path[j].add(j)
+                for i, e in enumerate(list(path[j])[::-1]): # 後ろから一つずつ
+                    if e in dist[j]: # もし戻るパスがあれば
+                        roop[j] = e # 終点j, 始点eのループがある
+                        roop_len[j] = i + 1 # ループの長さ
+                        break # 一番小さいのしかいらない
+
+                pos.append([j, dis + 1])
+
+# 最小のループを探す
+l = float('inf')
+index = -1
+for i in range(N):
+    if roop_len[i] >= 0 and roop_len[i] < l:
+        l = min(l, roop_len[i])
+        index = i
+
+if index == -1:
+    print(-1)
+    exit()
+
+# 構築
+ans = [index + 1]
+now = index
+while now != roop[index]: # 始点に戻るまで
+    now = parents[now]
+    ans.append(now + 1)
+
+print(l)
+for i in ans[::-1]:
+    print(i)
+
+# AtCoder Beginner Contest 197（Sponsored by Panasonic）
+# F Construct a Palindrome
+
+N, M = getNM()
+E = [[[] for i in range(26)] for i in range(N)]
+adj = [set() for i in range(N)] # 隣接する頂点について
+for _ in range(M):
+    a, b, c = input().split()
+    a = int(a) - 1
+    b = int(b) - 1
+    E[a][ord(c) - ord('a')].append(b)
+    E[b][ord(c) - ord('a')].append(a)
+    adj[a].add(b)
+    adj[b].add(a)
+
+ans = float('inf')
+que = deque([(0, N - 1, 0)])
+used = set()
+
+while que:
+    s, e, d = que.popleft()
+    # 探索が一回目か
+    if (s, e) in used:
+        continue
+    used.add((s, e))
+    if s == e:
+        ans = min(ans, d * 2)
+    if e in adj[s]:
+        ans = min(ans, d * 2 + 1)
+    # a ~ zまでについて同じ文字の辺はあるか
+    for i in range(26):
+        for n_s in E[s][i]:
+            for n_e in E[e][i]:
+                que.append((n_s, n_e, d + 1))
+
+if ans == float('inf'):
+    print(-1)
 else:
-    g = 0
+    print(ans)
 
-print(*[math.gcd(target + B[i], g) for i in range(M)])
+# codeforce round686
+# E - Number of Simple Paths
+# 一筆書きの本数を求める
+
+T = getN()
+for _ in range(T):
+    N = getN()
+    G = [[] for i in range(N)]
+    for __ in range(N):
+        u, v = getNM()
+        G[u - 1].append(v - 1)
+        G[v - 1].append(u - 1)
+
+    val = [1] * N
+    # 葉を探索する
+    leaf = []
+    for i in range(N):
+        if len(G[i]) == 1:
+            leaf.append(i)
+
+    leaf = deque(leaf)
+    while leaf:
+        v = leaf.popleft()
+        to = G[v][0]
+        val[to] += val[v] # 運送する
+        val[v] = 0 # 運送し終えたものはカウントしない
+        G[v].clear() # 葉を刈る
+        G[to].remove(v)　# 葉を刈る
+        if len(G[to]) == 1:
+            leaf.append(to)
+
+    ans = 0
+    for i in range(N):
+        # val[i] * (val[i] - 1) // 2: ここまで * ここまで
+        # val[i] * (N - val[i]): ここまで * これから
+        ans += val[i] * (val[i] - 1) // 2 + val[i] * (N - val[i])
+    print(ans)
