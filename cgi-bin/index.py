a9766e9ccdd02db79f4fb6106ef69cc5f061c068
customaddone <custom1485@gmail.com>
2021-09-05 15:10:21 +0900

    ABC217

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -31,38 +31,23 @@ dx = [1, 0, -1, 0]
 # Main Code #
 #############
 
-T = getN()
-for _ in range(T):
-    _ = input()
-    N, M = getNM()
-    dis1 = [float('inf')] * N
-    dis1[0] = 0
-    E = [[] for i in range(N)]
-    for _ in range(M):
-        u, v = getNM()
-        E[u - 1].append(v - 1)
+"""
+N <= 5000
+1 ~ Nを空でない1 ~ K個のグループに分ける
+O(NK)でやる
+K個のグループに分ける場合、やり方はN^K通りから空のやつを引く　でできそう
+N^K // fact(K)(グループを区別しない)
+つまりN^K - N^(K - 1)(K - 1個のグループの作り方を引く)
 
-    # 距離の計測
-    q = deque([0])
-    # トポソ順
-    order = []
-    while q:
-        u = q.popleft()
-        order.append(u)
-        for v in E[u]:
-            if dis1[v] > dis1[u] + 1:
-                dis1[v] = dis1[u] + 1
-                q.append(v)
+mod M これとこれが同じグループに入っている　を引く
+iとjが同じグループに入っている通り　(N - 2)^K通り
+全てのcond1 & cond2... を求められればいける
 
-    dis2 = [float('inf')] * N
-    # 葉から探索
-    while order:
-        u = order.pop()
-        for v in E[u]:
-            # 逆行
-            if dis1[v] <= dis1[u]:
-                dis2[u] = min(dis2[u], dis1[v])
-            else:
-                dis2[u] = min(dis2[u], dis2[v])
+n人を別々にk個のグループに分ける kPn
+これを全てのグループ分やる
 
-    print(*[min(dis1[i], dis2[i]) for i in range(N)])
+集合1が空でない & 集合2が空でない... =
+not 集合1が空 or 集合2が空...
+k - 空集合の個数 P n1 * k - 空 P n2...
+条件をi個（空集合をi個選ぶ）選ぶ通りはkCi通り
+"""
