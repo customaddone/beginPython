c8c5be39c1d101cb3ef3287819d8a0857a76b873
customaddone <custom1485@gmail.com>
2021-09-16 23:25:55 +0900

    dp

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -31,231 +31,200 @@ dx = [1, 0, -1, 0]
 # Main Code #
 #############
 
-# パ研合宿2020　第1日「SpeedRun」
-# 同じgcdを持つ区間は結合しても同じまま
+# ABC195 F. Coprime Present
 
 """
-K個に切り分ける
-それぞれの部分の総和のgcdが大きいほどいい
-最大公約数はいくつになるか
-二分探索したいが dpもしたい
+A以上B以下　幅は72以下
+A, Bがでかい
+連続する数　ここポイント
+連続する数は互いに素
 
-300C150とかは無理
-
-K = 1から考える
-答えはAの総和
-
-A が最大300しかない
-エッジを貼る
-k回辺を移動して0 ~ Nにいけるか
-
-調和級数
+Q:どの数とどの数をペアにしてはいけないか
+偶数は一緒にしてはいけない　偶数のどれか1つ or 全くない
+奇数はどんな感じ
+3の倍数の場合は6つ前、6つ後ろと一緒になってはいけない
+5の倍数は10こ前、10こ後ろと一緒になってはいけない...
+2の倍数が入る通り、3の倍数が入る通り...をdp
+bit dpすれば
+2 4
+0b1 2は3の倍数
+0b10 3は3の倍数
+0b1 4は2の倍数
 """
 
-N = getN()
-A = [0] + getList()
-su = sum(A)
-for i in range(1, N + 1):
-    A[i] += A[i - 1]
+A, B = getNM()
+# 72までに素数が20個あります
+prime = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]
+N = [i for i in range(A, B + 1)]
+L = len(N)
 
-ans = [0] * (N + 1)
-for i in range(su + 1, 0, -1):
-    # これを通過すると必ず条件を満たす区間を作れる
-    if su % i != 0:
-        continue
+# 各数字をbit棒に変換する
+for i in range(L):
+    # なんの約数かをbit形式で記録
+    opt = 0
+    for j in range(20):
+        if N[i] % prime[j] == 0:
+            opt += (1 << j)
+    N[i] = opt
 
-    cnt = 0
-    last = 0
-    for j in range(1, N + 1):
-        if (A[j] - A[last]) % i == 0:
-            last = j
-            cnt += 1
+prev = [0] * (1 << 20)
+prev[0] = 1 # 空集合が1
+for i in range(L):
+    next = [0] * (1 << 20)
+    # 配るdpで
+    for bit in range(1 << 20):
+        next[bit] += prev[bit] # 何も足さない場合
+        if not bit & N[i]: # 共通の約数がなければ
+            next[bit | N[i]] += prev[bit]
+    prev = next
 
-    # cnt以下の区間は結合することで簡単に作れる
-    for a in range(cnt + 1):
-        ans[a] = max(ans[a], i)
+print(sum(prev))
 
-for a in ans[1:]:
-    print(a)
+# 典型90 042 - Multiple of 9（★4）
+# 配るのは0から、累積するのは2から
 
-# Chokudai SpeedRun 002 J - GCD β
+K = getN()
+if K % 9 != 0:
+    print(0)
+    exit()
 
-"""
-N <= 50000 これはなに？
-NlogNまでならいける
-最大公約数を最大にするには　同じ倍数でまとめればいい
-ただしAiは大きい
-どちらの数字を使うか
-targetを選択するか
-全部因数分解して候補を探る Aiがでかいので間に合わない
-まずAi, Bi 10 ** 5個が候補としてある
-1個下の奴とのgcdで結ぶ　エッジは高々20万本
-AiスタートとBiスタートがある
-残っているものとgcdする
-Aiがでかいので...
-A0, B0の約数しか候補にならない
-"""
+dp = [0] * (K + 2)
+dp[0] = 1
+dp[1] = 1
+for i in range(K + 1):
+    if i > 1:
+        dp[i] += dp[i - 1]
+    dp[i] %= mod
+    dp[i + 1] += dp[i]
+    dp[min(i + 10, K + 1)] -= dp[i]
 
-def make_divisors(n):
-    divisors = []
-    for i in range(1, int(math.sqrt(n)) + 1):
-        if n % i == 0:
-            divisors.append(i)
-            # √nで無い数についてもう一個プラス
-            if i != n // i:
-                divisors.append(n // i)
-    return sorted(divisors)
+print(dp[-2])
 
-N = getN()
-A, B = [], []
-for i in range(N):
-    a, b = getNM()
-    A.append(a)
-    B.append(b)
+# codeforces round667
+# F. Subsequences of Length Two
 
-ans = set()
-opt_l = make_divisors(A[0]) + make_divisors(B[0])
-
-for opt in opt_l:
-    for i in range(N):
-        if (A[i] % opt != 0) and (B[i] % opt != 0):
-            break
-    else:
-        ans.add(opt)
-
-ans = sorted(list(ans))
-print(ans[-1])
-
-# ABC136 Max GCD
-
-"""
-0回以上K回以下行う
-Aの全ての要素を割り切る: 操作後をAの全てがansの倍数になる
-二分探索したい　単調性はあるのか
-K = 無限の時
-8 20
-0 28
--A 28 + A 結局最初のAの値に拘束される
-0 28で28が最大か
-K = 無限の場合の最大値はsum(A)
-どれだけこれに近づけるか
-sum(A)の倍数にしか到達できない　倍数の数は大体logN にぶたんしなくても十分早い
-K回操作することでtargetの倍数に揃えられるか
-4 5
-10 1 2 22　の時
-35に揃えられるか
--10, -1, -2, +13 無理
-7に揃えられるか
--3, -1, -2, -1 total: -7, abs: 7
-前からtotal // 7個を反転させる　反転させた場合現在のabsから7 - 2abs(Ai)増える
-それぞれの要素についてプラスするかマイナスするか
-プラスするものとマイナスするものはイーブンでないといけない
-"""
+# 耳dpの要領　tが文字数2なので
+# t1, t2にそれぞれ何個振り分けるか
+# 今までi個変換してこれより前にあるt1の個数がj個
 
 N, K = getNM()
-A = getList()
-su = sum(A)
+S = list(input())
+T = input()
+t1, t2 = T[0], T[1]
+if t1 == t2:
+    for i in range(N - 1, -1, -1):
+        if K and S[i] != t1:
+            S[i] = t1
+            K -= 1
+    ans = 0
+    cnt = 0
+    for i in range(N):
+        if S[i] == t1:
+            ans += cnt
+            cnt += 1
+    print(ans)
+    exit()
 
-for p in make_divisors(su)[::-1]:
-    ar = sorted([a % p for a in A], reverse = True)
-    total = sum(ar) # total値は反転させるごとに7ずつ減っていく
-    ab = total # abは反転させるごとにa * 2 - pずつ減る ならaの大きい順に反転させた方がお得
+# 今までi個変換してこれより前にあるt1の個数がj個の時の連続部分列の個数
+prev = [[-float('inf')] * (N + 1) for i in range(K + 1)]
+prev[0][0] = 0
+for n in range(N):
+    next = [[-float('inf')] * (N + 1) for i in range(K + 1)]
+    # 現在までに変換している個数
+    for i in range(K + 1):
+        # ここより前にあるt1の個数
+        for j in range(N):
+            # 変換しない
+            next[i][j] = max(next[i][j], prev[i][j])
+            # その文字がt1だった
+            if S[n] == t1:
+                next[i][j + 1] = max(next[i][j + 1], prev[i][j])
+            # その文字がt2だった
+            if S[n] == t2:
+                # これより前のt1の個数だけカウント数が増える
+                next[i][j] = max(next[i][j], prev[i][j] + j)
+
+            # 変換する
+            if i < K:
+                # t1に変換
+                next[i + 1][j + 1] = max(next[i + 1][j + 1], prev[i][j])
+                # t2に変換
+                next[i + 1][j] = max(next[i + 1][j], prev[i][j] + j)
+
+    prev = next
+
+print(max(prev[-1]))
+
+# edufo #1 E. Chocolate Bar
+
+"""
+O(NM)ぐらいで考えよう 50回やる
+面積Kのチョコが欲しい　切り方
+
+端っこを使うと？　N, Mのうち短い方を切るといい
+kが小さい　最高50
+縦 + 横、　縦 + 縦 + 横
+cutして減らすのもある
+"""
+
+dp = [[[0 for k in range (51)] for j in range (31)] for i in range (31)]
+
+def calc (n, m, k) :
+    #print(n,m,k)
+    if (dp[n][m][k] != 0) or (n*m == k) or (k == 0) :
+        return dp[n][m][k]
+
+    ans = 10**9
+
+    for i in range (1,n//2 + 1) :
+        for j in range (k+1) :
+            #print(i,j,'a')
+            if (i*m >= (k-j)) and ((n-i)*m >= j) :
+                ans = min(ans, m*m + calc(i,m,k-j) + calc(n-i,m,j))
+
+    for i in range (1,m//2 + 1) :
+        for j in range (k+1) :
+            #print(i,j,'b')
+            if (i*n >= (k-j)) and ((m-i)*n >= j) :
+                ans = min(ans, n*n + calc(n,i,k-j) + calc(n,m-i,j))
+
+    dp[n][m][k] = ans
+
+    return ans
+
+for _ in range (getN()):
+    N, M, K = getNM()
+
+    print(calc(N, M, K))
+
+# edufo73 D. Make The Fence Great Again
+# 状態を減らす系dp
+# 前と後ろしか関係ないのだから状態は3つずつでいい
+
+"""
+コスト1回で1伸ばせる　隣接するやつとの差をつけろ
+O(N^2)のdpなら...
+隣のやつが過度に伸ばされててもあんまり...
+1伸ばすか、伸ばさないか
+伸ばしても精々2伸ばすだけ
+"""
+
+T = getN()
+for _ in range(T):
+    N = getN()
+    F = [getList() for i in range(N)]
+    last = -1
+    # 伸ばさない時と伸ばした時と
+    prev = [0, inf, inf]
 
     for i in range(N):
-        if total == 0:
-            break
-        total -= p
-        ab -= ar[i] * 2 - p
-    if ab <= K * 2:
-        print(p)
-        exit()
+        next = [inf, inf, inf]
+        for j in range(3):
+            for k in range(3):
+                if last + j != F[i][0] + k:
+                    next[k] = min(next[k], prev[j] + F[i][1] * k)
 
-# codeforces round691
-# C - Row GCD
+        prev = next
+        last = F[i][0]
 
-# commmon divisor
-# Ai + nのgcdについて　1 ~ Mまで
-# 最悪セグ木でなんとか
-
-# 求めるdがあったとすると　現在の時点でもmodが全て同じでないといけない
-# 候補は各数字同士の差分のgcdの約数　因数ごとにやれば
-# A[0] + B[i]とgのgcdを取ればいい
-
-# A[0] + B[i]とgが共通してもつ因数をmとすると
-# g = nmと表せる
-# A[0] + B[i]よりknm大きいA[1] + B[i]も当然mを約数にもつ
-
-N, M = getNM()
-A = getList()
-B = getList()
-
-target = A[0]
-if N > 1:
-    A = [abs(A[i] - A[i - 1]) for i in range(N - 1)]
-    g = A[0]
-    for i in range(2, N - 1):
-        g = math.gcd(g, A[i])
-else:
-    g = 0
-
-print(*[math.gcd(target + B[i], g) for i in range(M)])
-
-# edufo 89 D - Two Divisors
-# 約数同士の足し算について
-
-"""
-2以上の数を2つ
-約数の中から探そう
-aiはギリギリエラストテネスはできる　全てについて約数列挙はできない
-√N = 3000ぐらい
-列挙する個数を減らすかそもそも約数列挙しないか
-素数はやらなくていい 存在しない証明が難しいか
-d1とd2は共通因数を持っていたらだめ　互いに素でないといけない
-因数についてどちらかにだけ置く　
-最大因数を足すか？
-
-a1が偶数の場合は？ 偶数 + 奇数にしないと
-2^k + 他の全ての因数の積 = 奇数になる　これは合成数か？
-これは合成数になりうるが、奇数の因数はない！！
-つまり逆にない条件を考える　素数
-2^kがない or 他の全ての因数の積がない
-他の全ての因数の積がない場合はどうあがいても偶数 + 偶数になるのでダメ
-例えば105(3 * 5 * 7)について　3 + 5 = 2 * (3 + 5 の平均　これは偶数　これをさらに２で割ると3は下回る)
-奇数の因数が２つあればいい
-3200以下の素数の個数は452個　これを回せば
-
-ない条件　2の累乗 or 奇数の因数が1種類しかない
-他は2^k + 奇数の因数の積でok
-結局のところ最小の因数 + それ以外の積　をすればいい
-因数が２つあればいい
-"""
-
-# osa_k法
-max = 10 ** 7 + 7
-ar = [i for i in range(max)]
-# 最大の約数を拾う
-for i in range(2, max):
-    if ar[i] == i:
-        for j in range(i * i, max, i):
-            ar[j] = i
-
-N = getN()
-A = getList()
-ans1, ans2 = [], []
-
-for a in A:
-    z = ar[a]
-    # zの累乗ではないか
-    while a % z == 0:
-        a //= z
-    # 累乗だった
-    if a == 1:
-        ans1.append(-1)
-        ans2.append(-1)
-    # 因数が2種類以上あった
-    else:
-        ans1.append(z)
-        ans2.append(a)
-
-print(*ans1)
-print(*ans2)
+    print(min(prev))
