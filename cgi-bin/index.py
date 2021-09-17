bd5707e0d109d9671178f8b5aadc98e1bd66beea
customaddone <custom1485@gmail.com>
2021-09-17 23:12:02 +0900

    dp7

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -31,132 +31,245 @@ dx = [1, 0, -1, 0]
 # Main Code #
 #############
 
-# ARC068 E - Snuke Line
+# ABC195 F. Coprime Present
 
 """
-約数により単調減少していきそう
-愚直にやると
-各Mにつき倍数のとこに置いていく MlogM
-各MにつきNでいくつ取れるかを見る NM
+A以上B以下　幅は72以下
+A, Bがでかい
+連続する数　ここポイント
+連続する数は互いに素
 
-二分探索とかしたいが
-各おみあげについてMが幾つの時に獲得できるか　セグメントツリーでできるか
-この区間に幾つの因数が入っているか
-l, rが全区間　これがM個ある時対応できない
-個数だけを見ていく方針で　包除原理とか
+Q:どの数とどの数をペアにしてはいけないか
+偶数は一緒にしてはいけない　偶数のどれか1つ or 全くない
+奇数はどんな感じ
+3の倍数の場合は6つ前、6つ後ろと一緒になってはいけない
+5の倍数は10こ前、10こ後ろと一緒になってはいけない...
+2の倍数が入る通り、3の倍数が入る通り...をdp
+bit dpすれば
+2 4
+0b1 2は3の倍数
+0b10 3は3の倍数
+0b1 4は2の倍数
+"""
 
-素数のところだけを数えることはできる　合成数のはどうする？
+A, B = getNM()
+# 72までに素数が20個あります
+prime = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]
+N = [i for i in range(A, B + 1)]
+L = len(N)
 
-一定間隔dで移動する時
-土産の幅がdより大きい場合は一回以上訪れる
-土産の幅がd以下の場合は最大でも1回だけ訪れる
+# 各数字をbit棒に変換する
+for i in range(L):
+    # なんの約数かをbit形式で記録
+    opt = 0
+    for j in range(20):
+        if N[i] % prime[j] == 0:
+            opt += (1 << j)
+    N[i] = opt
 
-dより大きい幅の場合は単純に計算できる
-dより大きい幅について
-各点でいくつ土産の種類があるかを数え、dの倍数について数える
-最大でも1回だけ訪れる土産しかないのでダブることはない
+prev = [0] * (1 << 20)
+prev[0] = 1 # 空集合が1
+for i in range(L):
+    next = [0] * (1 << 20)
+    # 配るdpで
+    for bit in range(1 << 20):
+        next[bit] += prev[bit] # 何も足さない場合
+        if not bit & N[i]: # 共通の約数がなければ
+            next[bit | N[i]] += prev[bit]
+    prev = next
 
-ダブらないように数えるのがミソ
+print(sum(prev))
 
-N, M = getNM()
-P = [getList() for i in range(N)]
-P.sort(reverse = True)
+# 典型90 042 - Multiple of 9（★4）
+# 配るのは0から、累積するのは2から
 
-for m in range(1, M + 1):
-    p = [i for i in P if i[1] - i[0] + 1 <= m]
-    l = [0] * (M + 1)
-    q = []
-    ans = N - len(p) # 一回以上訪れる
-    # 各倍数について調べる
-    for i in range(m, M + 1, m):
-        while p and p[-1][0] <= i:
-            heappush(q, p.pop()[1])
-        while q and q[0] < i:
-            heappop(q)
-        ans += len(q)
+K = getN()
+if K % 9 != 0:
+    print(0)
+    exit()
+
+dp = [0] * (K + 2)
+dp[0] = 1
+dp[1] = 1
+for i in range(K + 1):
+    if i > 1:
+        dp[i] += dp[i - 1]
+    dp[i] %= mod
+    dp[i + 1] += dp[i]
+    dp[min(i + 10, K + 1)] -= dp[i]
+
+print(dp[-2])
+
+# codeforces round667
+# F. Subsequences of Length Two
+
+# 耳dpの要領　tが文字数2なので
+# t1, t2にそれぞれ何個振り分けるか
+# 今までi個変換してこれより前にあるt1の個数がj個
+
+N, K = getNM()
+S = list(input())
+T = input()
+t1, t2 = T[0], T[1]
+if t1 == t2:
+    for i in range(N - 1, -1, -1):
+        if K and S[i] != t1:
+            S[i] = t1
+            K -= 1
+    ans = 0
+    cnt = 0
+    for i in range(N):
+        if S[i] == t1:
+            ans += cnt
+            cnt += 1
     print(ans)
+    exit()
 
-これで答えが出るがこのままだとTLE
-pに集計されるPの要素の数はだんだんと増えていく
-mについて調べる
-区間の長さがmのものを取り出し、区間[l, r]に区間加算
-mの倍数について調べる
-"""
+# 今までi個変換してこれより前にあるt1の個数がj個の時の連続部分列の個数
+prev = [[-float('inf')] * (N + 1) for i in range(K + 1)]
+prev[0][0] = 0
+for n in range(N):
+    next = [[-float('inf')] * (N + 1) for i in range(K + 1)]
+    # 現在までに変換している個数
+    for i in range(K + 1):
+        # ここより前にあるt1の個数
+        for j in range(N):
+            # 変換しない
+            next[i][j] = max(next[i][j], prev[i][j])
+            # その文字がt1だった
+            if S[n] == t1:
+                next[i][j + 1] = max(next[i][j + 1], prev[i][j])
+            # その文字がt2だった
+            if S[n] == t2:
+                # これより前のt1の個数だけカウント数が増える
+                next[i][j] = max(next[i][j], prev[i][j] + j)
 
-N, M = getNM()
-P = []
-for _ in range(N):
-    l, r = getNM()
-    P.append([l, r, r - l + 1])
-P.sort(key = lambda i:i[2], reverse = True)
+            # 変換する
+            if i < K:
+                # t1に変換
+                next[i + 1][j + 1] = max(next[i + 1][j + 1], prev[i][j])
+                # t2に変換
+                next[i + 1][j] = max(next[i + 1][j], prev[i][j] + j)
 
-#### 区間加算bit ################################
-bit1 = BIT(M)
-bit2 = BIT(M)
-# [l, r)にxを加算する
-def range_add(l, r, x):
-    bit1.add(l, x)
-    bit1.add(r, -x)
-    bit2.add(l, (-1) * x * (l - 1))
-    bit2.add(r, x * (r - 1))
-# [1 ~ a)までの値を集計する
-def range_get(a):
-    return bit2.get(a) + (a - 1) * bit1.get(a)
-################################################
+    prev = next
 
-# 1-indexにしてある
-for m in range(1, M + 1):
-    # 区間の長さがmより小さいものについて取り出す
-    while P and P[-1][2] <= m:
-        l, r, _ = P.pop()
-        range_add(l, r + 1, 1) # [l, r + 1)について区間加算
-    ans = len(P)
-    # 各倍数について土産の数を調べる　互いにダブらない
-    for i in range(m, M + 1, m):
-        ans += range_get(i + 1) - range_get(i)
-    print(ans)
+print(max(prev[-1]))
 
-# codeforces # 635 D-MEX maximizing
-# １個ずつ足していく場合のAの中の最小値とその最小値の場所を探す
+# edufo #1 E. Chocolate Bar
 
 """
-yi % x + n * xの範囲で自由に数を決めれる　mexの最大を目指せ
-まずmodした時に0 ~ x - 1まで揃うか　揃わなければそこが穴
-セグ木　は使う？
-一番小さい穴はどこか
-そこに数字があるかないかでBITを組む
-揃ったら消す
+O(NM)ぐらいで考えよう 50回やる
+面積Kのチョコが欲しい　切り方
+
+端っこを使うと？　N, Mのうち短い方を切るといい
+kが小さい　最高50
+縦 + 横、　縦 + 縦 + 横
+cutして減らすのもある
 """
 
-N, X = getNM()
-cnt = [0] * (X + 1) # カウント数
-layer = 0
-bit = BIT(X) # 0かそれ以外か
+dp = [[[0 for k in range (51)] for j in range (31)] for i in range (31)]
 
+def calc (n, m, k) :
+    #print(n,m,k)
+    if (dp[n][m][k] != 0) or (n*m == k) or (k == 0) :
+        return dp[n][m][k]
+
+    ans = 10**9
+
+    for i in range (1,n//2 + 1) :
+        for j in range (k+1) :
+            #print(i,j,'a')
+            if (i*m >= (k-j)) and ((n-i)*m >= j) :
+                ans = min(ans, m*m + calc(i,m,k-j) + calc(n-i,m,j))
+
+    for i in range (1,m//2 + 1) :
+        for j in range (k+1) :
+            #print(i,j,'b')
+            if (i*n >= (k-j)) and ((m-i)*n >= j) :
+                ans = min(ans, n*n + calc(n,i,k-j) + calc(n,m-i,j))
+
+    dp[n][m][k] = ans
+
+    return ans
+
+for _ in range (getN()):
+    N, M, K = getNM()
+
+    print(calc(N, M, K))
+
+# edufo73 D. Make The Fence Great Again
+# 状態を減らす系dp
+# 前と後ろしか関係ないのだから状態は3つずつでいい
+
+"""
+コスト1回で1伸ばせる　隣接するやつとの差をつけろ
+O(N^2)のdpなら...
+隣のやつが過度に伸ばされててもあんまり...
+1伸ばすか、伸ばさないか
+伸ばしても精々2伸ばすだけ
+"""
+
+T = getN()
+for _ in range(T):
+    N = getN()
+    F = [getList() for i in range(N)]
+    last = -1
+    # 伸ばさない時と伸ばした時と
+    prev = [0, inf, inf]
+
+    for i in range(N):
+        next = [inf, inf, inf]
+        for j in range(3):
+            for k in range(3):
+                if last + j != F[i][0] + k:
+                    next[k] = min(next[k], prev[j] + F[i][1] * k)
+
+        prev = next
+        last = F[i][0]
+
+    print(min(prev))
+
+# codeforces #544 E. K Balanced Teams
+
+"""
+チームをK個呼びたい
+強さの差が5以内でないといけない
+kが多ければ多いほど楽　近い数同士を含む
+ソートして　現在Kチームまで作った時、
+前からi個目まで進んだ時、kチームを作る　
+その時のtargetの値は出来るだけ大きい値　jointしやすいように
+二分探索も欲しいな j人ぶち込む場合それは可能か？
+k個の区間の中にj人入れることは可能か？
+
+端っこを一つ決めると何個入るかは自動的に決まる
+skipをi回した時の1からの距離の最小
+"""
+
+N, K = getNM()
+A = getList()
+A.sort()
+
+r = 0
+E = [0] * N
 for i in range(N):
-    y = (getN() % X) + 1 # 1-index
-    cnt[y] += 1
-    # 数字を置いた場所に1を立てる
-    if bit.cum(y, y + 1) == 0:
-        bit.add(y, 1)
-    # 揃ったので消す
-    if bit.get(X + 1) == X:
-        layer += 1 # あとで *= Xする
-        # cntの全てから1を引く
-        for i in range(1, X + 1):
-            cnt[i] -= 1
-            # もし1→0になったなら
-            if cnt[i] == 0:
-                bit.add(i, -1)
+    while r < N and A[r] <= A[i] + 5:
+        r += 1
+    E[i] = r
 
-    # 一番小さい穴を探す
-    ok, ng = 0, X + 1
-    while abs(ng - ok) > 1:
-        mid = (ok + ng) // 2
-        # これ以前の全ての場所にフラグが立っているか
-        if bit.get(mid + 1) == mid:
-            ok = mid
-        else:
-            ng = mid
+# i回スキップした時のエッジの本数
+dp = [[inf] * (N + 1) for i in range(N + 1)]
+dp[0][0] = 0
+for i in range(N):
+    for j in range(N + 1):
+        # スキップしない　配るdp
+        dp[E[i]][j] = min(dp[E[i]][j], dp[i][j] + 1)
+        # スキップする
+        if j < N:
+            dp[i + 1][j + 1] = min(dp[i + 1][j + 1], dp[i][j])
 
-    print(layer * X + ok)
+ans = 0
+# skipが少ない方から順に
+for j in range(N + 1):
+    if dp[N][j] <= K:
+        print(N - j)
+        break
