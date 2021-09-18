e4d3e50709d321a6c7a6548b47cdc952b4b38865
customaddone <custom1485@gmail.com>
2021-09-18 19:32:17 +0900

    imos

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -6,7 +6,7 @@ from itertools import combinations, permutations, product, combinations_with_rep
 from bisect import bisect_left, bisect_right
 
 import sys
-f = open(filename, encoding='CP932')
+
 def input():
     return sys.stdin.readline().rstrip()
 def getN():
@@ -32,102 +32,78 @@ dx = [1, 0, -1, 0]
 # Main Code #
 #############
 
-# edufo 109 C - Robot Collisions
+# ABC153 F - Silver Fox vs Monster
+
+# 遅延セグは使わない
+# 二分探索したい
+# imosする
+# どこに爆弾を投下するか
+# 効率のいい方法
+# 現在地点に爆弾を投下してモンスターを倒すまで
+# -D ~ Dまで = 前に2 * Dの範囲に効くと
+# bisect_right
+
+N, D, A = getNM()
+# 座標Xにいる 体力はH
+mons = [getList() for i in range(N)]
+mons.sort()
+X = []
+H = []
+for x, h in mons:
+    X.append(x)
+    H.append(h)
+
+# 累積ダメージ
+cnt = 0
+r = 0
+imos = [0] * (N + 1)
+for i in range(N):
+    # 爆風範囲
+    while r < N and X[i] + 2 * D >= X[r]:
+        r += 1
+    if i > 0:
+        imos[i] += imos[i - 1]
+    # ここで爆弾を投下する回数
+    hp = max(H[i] - imos[i], 0)
+    t = (hp + A - 1) // A # 爆風のぶんを削る
+    cnt += t
+    # imos
+    imos[i] += t * A
+    imos[r] -= t * A
+
+print(cnt)
+
+# edufo #102 D. Program
 
 """
-スピード1で右か左かに移動
-整数上で出会わなければ何も起こらない
+l~r間のinstructionを実行して値が何種類出るか
+値は連続するので最大値と最小値を求めればいい
+imosするだけでは...
+[0, -1, 0, -1, -2, -1, -2, -3, -2]
+I[r] - I[l - 1]するとどれだけ変化したかがわかる
+これを全区間でやって最大値、最小値をだす
 
-まずロボット2つで考える
-ロボット2つの差が偶数で、互いに近づくなら消滅する
-差が偶数であればいずれぶつかる
-なので場所の偶奇でグループ分け
-
-グループ内だと全てのロボットは互いにぶつかりあう　ロボットが奇数個だと1つ残るが
-ペアを組んで行ってどのペアが一番衝突が早いか　それを取り除く
-・互いに向かい合う
-・互いに背を向ける
-・同じ方向
-
-互いに向かい合う　一番近いのと一番最初にぶつかる　距離 // 2
-互いに背を向ける　一番遠いのと一番最初にぶつかる　(2N - 距離) // 2
-同じ方向　端までの距離が短い方 + 距離 // 2　= 端までの距離の平均
-
-互いに背を向ける　が一番遅い
-・互いに向かい合う R と L
-・同じ方向 R同士L同士
-の2つをまず考える
-同じ方向　の一番距離が短いのはすぐわかる　一番右にある2つと一番左にある2つ
-互いに向かい合う　はO(N)かければ何とか... Rについて一番近いLを探す
-互いに独立となる消し方を考える　順番は関係内容な
-それぞれのRについて互いに向かい合う　の最小値を求めておく　heapに入れる
-最小のものから取り出す　それがRR,LLより小さければ消す
-RRが小さい, LLが小さい　は消して記録しておく
-RRが消えている　探索は行わない
-LLが消えている　
-
-まず・互いに向かい合う R と Lから消えていく
-互いに向かい合う　一番近いのと一番最初にぶつかる　距離 // 2
-互いに背を向ける　一番遠いのと一番最初にぶつかる　(2N - 距離) // 2
-同じ方向　端までの距離が短い方 + 距離 // 2　= 端までの距離の平均
-1 < 3 < 2の順になる
+選ばれた区間は除外して
+つまり前と後ろからimosする　区間の最大値、最小値は保持しておく
 """
 
 T = getN()
 for _ in range(T):
-    N, M = getNM()
-    odd_r, even_r = [], []
-    odd_l, even_l = [], []
-    R = []
-    P = getList()
-    D = input().split()
-    for i in range(N):
-        R.append([P[i], D[i], i])
+    N, Q = getNM()
+    # 累積和
+    Ins = [0] + [1 if s == '+' else -1 for s in list(input())]
+    for i in range(1, N + 1):
+        Ins[i] += Ins[i - 1]
 
-    for p, d, ind in R:
-        if p % 2 == 0:
-            if d == 'R':
-                even_r.append([p, ind, 0])
-            else:
-                even_l.append([p, ind, 1])
-        else:
-            if d == 'R':
-                odd_r.append([p, ind, 0])
-            else:
-                odd_l.append([p, ind, 1])
+    # ラムダ式でseg木を立てられる
+    seg_min = SegTree(Ins, lambda x, y: min(x, y), inf)
+    seg_max = SegTree(Ins, lambda x, y: max(x, y), -inf)
+    for _ in range(Q):
+        l, r = getNM()
+        mi, ma = seg_min.query(0, l), seg_max.query(0, l)
+        if r < N:
+            # 中間の区間で増えた分を差し引く
+            mi = min(mi, seg_min.query(r + 1, N + 1) - (Ins[r] - Ins[l - 1]))
+            ma = max(ma, seg_max.query(r + 1, N + 1) - (Ins[r] - Ins[l - 1]))
 
-    ans = [-1] * N
-    def calc(R, L):
-        all = sorted(R + L)
-        r_o, l_o = [], deque([])
-        for p, ind, d in all:
-            # right
-            if d == 0:
-                r_o.append([p, ind])
-            else:
-                if r_o:
-                    p_e, ind_e = r_o.pop()
-                    ans[ind] = ans[ind_e] = abs(p - p_e) // 2
-                else:
-                    l_o.append([p, ind])
-
-        # 同じ向き
-        while len(r_o) > 1:
-            p1, i1 = r_o.pop()
-            p2, i2 = r_o.pop()
-            ans[i1] = ans[i2] = M - (p1 + p2) // 2
-
-        while len(l_o) > 1:
-            p1, i1 = l_o.popleft()
-            p2, i2 = l_o.popleft()
-            ans[i1] = ans[i2] = (p1 + p2) // 2
-
-        # 背を向け
-        if r_o and l_o:
-            p1, i1 = r_o.pop()
-            p2, i2 = l_o.popleft()
-            ans[i1] = ans[i2] = M - abs(p1 - p2) // 2
-
-    calc(even_r, even_l)
-    calc(odd_r, odd_l)
-    print(*ans)
+        print(ma - mi + 1)
