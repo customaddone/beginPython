1d2eb893a844e9208e96964811527f7320970d37
customaddone <custom1485@gmail.com>
2021-09-26 10:17:28 +0900

    imos

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -22,7 +22,7 @@ def getArray(intn):
 
 mod = 10 ** 9 + 7
 MOD = 998244353
-sys.setrecursionlimit(1000000)
+# sys.setrecursionlimit(1000000)
 inf = float('inf')
 eps = 10 ** (-10)
 dy = [0, 1, 0, -1]
@@ -32,282 +32,143 @@ dx = [1, 0, -1, 0]
 # Main Code #
 #############
 
-class Multiset:
-    def __init__(self):
-        self.h = []
-        self.d = dict()
+# ABC153 F - Silver Fox vs Monster
 
-    def insert(self,x):
-        heappush(self.h,x)
-        if x not in self.d:
-            self.d[x] = 1
-        else:
-            self.d[x] += 1
+# 遅延セグは使わない
+# 二分探索したい
+# imosする
+# どこに爆弾を投下するか
+# 効率のいい方法
+# 現在地点に爆弾を投下してモンスターを倒すまで
+# -D ~ Dまで = 前に2 * Dの範囲に効くと
+# bisect_right
 
-    def erase(self,x):
-        if x not in self.d or self.d[x] == 0:
-            return 'not found'
-        else:
-            self.d[x] -= 1
+N, D, A = getNM()
+# 座標Xにいる 体力はH
+mons = [getList() for i in range(N)]
+mons.sort()
+X = []
+H = []
+for x, h in mons:
+    X.append(x)
+    H.append(h)
 
-        while len(self.h) != 0:
-            if self.d[self.h[0]] == 0:
-                heappop(self.h)
-            else:
-                break
-
-    def erase_all(self,x):
-        if x not in self.d or self.d[x] == 0:
-            return 'not found'
-        else:
-            self.d[x] = 0
-
-        while len(self.h) != 0:
-            if self.d[self.h[0]] == 0:
-                heappop(self.h)
-            else:
-                break
-
-    def is_exist(self,x):
-        if x in self.d and self.d[x] != 0:
-            return True
-        else:
-            return False
-
-    def get_min(self):
-        if len(self.h) == 0:
-            return 'enpty'
-        return self.h[0]
-
-
-N, Q = getNM()
-limit = 2 * (10 ** 5) + 1
-
-infants = [getList() for i in range(N)]
-trans = [getList() for i in range(Q)]
-belong = [0] * N
-rate = [0] * N
-
-school = [Multiset() for i in range(limit)]
-purity = Multiset()
-
-# 各学校にいる生徒を記録する
+# 累積ダメージ
+cnt = 0
+r = 0
+imos = [0] * (N + 1)
 for i in range(N):
-    a, b = infants[i]
-    b -= 1
-    belong[i] = b
-    rate[i] = -a
-    school[b].insert(-a)
+    # 爆風範囲
+    while r < N and X[i] + 2 * D >= X[r]:
+        r += 1
+    if i > 0:
+        imos[i] += imos[i - 1]
+    # ここで爆弾を投下する回数
+    hp = max(H[i] - imos[i], 0)
+    t = (hp + A - 1) // A # 爆風のぶんを削る
+    cnt += t
+    # imos
+    imos[i] += t * A
+    imos[r] -= t * A
 
-# 各学校の最強園児を求める
-for i in range(limit):
-    if len(school[i].d) > 0:
-        purity.insert(-school[i].get_min())
+print(cnt)
 
-# 転園させる
-for c, d in trans:
-    c -= 1
-    d -= 1
-    ### 転園前処理 ###
-    prev = belong[c] # 所属変更
-    purity.erase(-1 * school[prev].get_min()) # 最強リストから削除
-    school[prev].erase(rate[c]) # 前の学校から削除
-    if len(school[prev].h) > 0:
-        purity.insert(-1 * school[prev].get_min()) # 最強リストを更新
-    ################
-
-    ### 転園後処理 ###
-    belong[c] = d
-    after = belong[c] # 所属変更
-    if len(school[after].h) > 0:
-        purity.erase(-1 * school[after].get_min()) # 最強リストから削除
-    school[after].insert(rate[c]) # 次の学校に追加
-    purity.insert(-1 * school[after].get_min()) # 最強リストを更新
-    #################
-
-    print(purity.get_min())
-
-# PAST3 L - スーパーマーケット
-# 配列ver
-class Multiset:
-    def __init__(self):
-        # x: [value, index]
-        self.h = [] # 配列を入れる
-        self.d = dict() # 値を入れる
-
-    def insert(self, x):
-        heappush(self.h, x)
-        if x[0] not in self.d:
-            self.d[x[0]] = 1
-        else:
-            self.d[x[0]] += 1
-
-    def erase(self, x): # x: [value, index]で
-        if x[0] not in self.d or self.d[x[0]] == 0:
-            return 'not found'
-        else:
-            self.d[x[0]] -= 1
-
-        while len(self.h) != 0:
-            if self.d[self.h[0][0]] == 0:
-                heappop(self.h)
-            else:
-                break
-
-    def erase_all(self,x):
-        if x[0] not in self.d or self.d[x[0]] == 0:
-            return 'not found'
-        else:
-            self.d[x[0]] = 0
-
-        while len(self.h) != 0:
-            if self.d[self.h[0][0]] == 0:
-                heappop(self.h)
-            else:
-                break
-
-    def is_exist(self,x): # x: [value, index]で
-        if x[0] in self.d and self.d[x[0]] != 0:
-            return True
-        else:
-            return False
-
-    def get_min(self):
-        if len(self.h) == 0:
-            return 'enpty'
-        return self.h[0]
-
-N = getN()
-K = [deque(getList()[1:]) for i in range(N)] # 商品の棚
-M = getN()
-A = getList() # 客 全ての列についてAi番目まで見て値の大きいものを選ぶ
-# それぞれの客について購入した商品の値を求める
+# edufo #102 D. Program
 
 """
-ヒープキュー？　aが小さいのでdequeでできる
-列が一個しかない場合は？
-1 ~ iのうち最大　をどのように求める？
-一個取る　一個appendする
-セグ木をK個立てるの無理なので、最大値をholdする
-aが1か2
-1 1の時
-取る、取る
-1 2の時
-取る、（取る + 見る）
-2 1の時
-（取る + 見る）、見る
-１番目の商品についてヒープキュー + ２番目の商品についてヒープキュー
+l~r間のinstructionを実行して値が何種類出るか
+値は連続するので最大値と最小値を求めればいい
+imosするだけでは...
+[0, -1, 0, -1, -2, -1, -2, -3, -2]
+I[r] - I[l - 1]するとどれだけ変化したかがわかる
+これを全区間でやって最大値、最小値をだす
 
-1番目のものを取った　
-2番目のものを1番目にappendする
-2番目のものについてどう処理する
-
-firについてheappop
-Kについてpopleft
-
-2つ目取ったらpop pop appendleft
-
-multiset2つとis_number2があればOK
-できる
+選ばれた区間は除外して
+つまり前と後ろからimosする　区間の最大値、最小値は保持しておく
 """
 
-S1, S2 = Multiset(), Multiset()
-# 番兵
-for i in range(N):
-    K[i].append(-mod)
-# 2番目の数字は？
-is_number2 = {}
-for i in range(N):
-    s1 = -K[i].popleft()
-    # is_number1[i] = s1
-    S1.insert([s1, i])
+T = getN()
+for _ in range(T):
+    N, Q = getNM()
+    # 累積和
+    Ins = [0] + [1 if s == '+' else -1 for s in list(input())]
+    for i in range(1, N + 1):
+        Ins[i] += Ins[i - 1]
 
-    if K[i]:
-        s2 = -K[i].popleft()
-        is_number2[i] = s2
-        S2.insert([s2, i])
+    # ラムダ式でseg木を立てられる
+    seg_min = SegTree(Ins, lambda x, y: min(x, y), inf)
+    seg_max = SegTree(Ins, lambda x, y: max(x, y), -inf)
+    for _ in range(Q):
+        l, r = getNM()
+        mi, ma = seg_min.query(0, l), seg_max.query(0, l)
+        if r < N:
+            # 中間の区間で増えた分を差し引く
+            mi = min(mi, seg_min.query(r + 1, N + 1) - (Ins[r] - Ins[l - 1]))
+            ma = max(ma, seg_max.query(r + 1, N + 1) - (Ins[r] - Ins[l - 1]))
 
-def pop_first():
-    r1, index = S1.get_min()
-    print(-r1)
-    # S1から値を消す
-    S1.erase([r1, index])
-    # S2のものをS1に移動させる
-    # S1にfloat('inf')が滞留するけど選ばないのでOK
-    S1.insert([is_number2[index], index])
-    S2.erase([is_number2[index], index])
-    # S2に数字を入れる
-    if K[index]:
-        r2 = K[index].popleft()
-        S2.insert([-r2, index])
-        is_number2[index] = -r2
+        print(ma - mi + 1)
 
-def pop_second():
-    r2, index = S2.get_min()
-    print(-r2)
-    # S2の値を消す
-    S2.erase([r2, index])
-    # S2に数字を入れる
-    # !K[index]だとis_numberにfloat('inf')が滞留するけど選ばないのでOK
-    if K[index]:
-        r2 = K[index].popleft()
-        S2.insert([-r2, index])
-        is_number2[index] = -r2
-
-for i in A:
-    if i == 1:
-        # 値をprint
-        pop_first()
-    else:
-        # -opt1の方が値が大きかったら
-        if S1.get_min()[0] < S2.get_min()[0]:
-            pop_first()
-        else:
-            pop_second()
-
-# Edufo 104 Cheap Dinner
+# edufo 94 D-Zigzags
 
 """
-4種類から料理を選ぶ
-i+1番目の料理がjの時の最小値
+同じ値のものを2つ　それを2組
+1種類の場合は連続する4つを選ぶ
+区間1を1つ決める(N^2)
+中の数字について　外側に点がいくつあるか
 
-i+1番目の料理にjを選ぶ　この時i番目の料理として得られるのはmにないやつ
-mの数は多くない　合計でもm
-使えないものを消す　そのあと戻す
+この値はl, rより外に同じ数字がいくつあるか？のimos
+左と右に分割して集計する
+左端をl回使う　中の数字は何回その左端を使う区間に入れられるか
+1 3 1 3 2 3
+  3   3      1が中に入る += imos1[0][1]
+  3       3  1 3 2が中に入る += (imos1[0][1] + imos1[2][1] + imos[1][1])
+
+連続区間すべての値の集計はimosを使うことができる
+連続するものはまず差分を取ることから考える
+imos[l][r + 1] = imos[l][r] + cがわかれば
+Σ(数字A[i]が区間1であるものの合計)　を求める
+imos[l][r] = (左側の合計) + (右側の合計)　に分割できるので別々に集計する
 """
 
-N = getList()
-D = [getList() for i in range(4)]
-ma = max(N)
-F = [[[] for i in range(ma)] for i in range(3)]
-for i in range(3):
-    n = getN()
-    for _ in range(n):
-        x, y = getNM()
-        F[i][y - 1].append(x - 1)
+T = getN()
+for _ in range(T):
+    N = getN()
+    A = getListGraph()
+    # 左側
+    imos1 = [[0] * N for i in range(N)]
+    # 右側
+    imos2 = [[0] * N for i in range(N)]
+    for i in range(N):
+        if i < N - 1:
+            imos1[A[i]][i + 1] += 1
+        if i > 0:
+            imos2[A[i]][i - 1] += 1
 
-prev = D[0] # 最初の料理
+    # l/rより左側/右側に数字jがいくらあるか
+    for i in range(N):
+        for j in range(1, N):
+            imos1[i][j] += imos1[i][j - 1]
+            imos2[i][-j-1] += imos2[i][-j]
 
-for i in range(3):
-    next = [inf] * len(D[i + 1])
-    # 相手になるi番目の料理　まずは全部追加
-    q = Multiset()
-    for v in prev:
-        q.insert(v)
+    ans = 0
+    # 区間1の左端
+    for i in range(N):
+        acc1, cnt1 = 0, 0
+        acc2, cnt2 = 0, 0
+        # 区間1の右端 or 区間2の右端
+        for j in range(i + 1, N):
+            # 左側集計
+            # 集計 区間1の右端
+            if A[i] == A[j]:
+                cnt1 += acc1
+            # 区間2の右端　中の点が増えた
+            acc1 += imos1[A[j]][i]
 
-    # i+1番目の料理をjにした場合を求める
-    for j in range(len(D[i + 1])):
-        # 選べないものは除外する
-        for k in F[i][j]:
-            q.erase(prev[k])
-        next[j] = min(next[j], q.get_min() + D[i + 1][j])
-        # 戻す
-        for k in F[i][j]:
-            q.insert(prev[k])
+            # 右側集計
+            if A[-i - 1] == A[-j - 1]:
+                cnt2 += acc2
+            acc2 += imos2[A[-j - 1]][-i - 1]
 
-    prev = next
+        ans += (cnt1 + cnt2)
 
-if min(prev) == inf:
-    print(-1)
-else:
-    print(min(prev))
+    print(ans // 2)
