29721bc278339b76aff720cd3657b41293946b4a
customaddone <custom1485@gmail.com>
2021-10-23 11:02:30 +0900

    セグki

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -32,58 +32,399 @@ dx = [1, 0, -1, 0]
 # Main Code #
 #############
 
-def dij(start, edges):
-    dist = [float('inf') for i in range(N)]
-    dist[start] = 0
-    pq = [(0, start)]
+#####segfunc#####
+def segfunc(x, y):
+    return min(x, y)
+#################
+#####ide_ele#####
+ide_ele = float('inf')
+#################
+class SegTree:
+    def __init__(self, init_val, segfunc, ide_ele):
+        n = len(init_val)
+        self.segfunc = segfunc
+        self.ide_ele = ide_ele
+        self.num = 1 << (n - 1).bit_length()
+        self.tree = [ide_ele] * 2 * self.num
+        # 配列の値を葉にセット
+        for i in range(n):
+            self.tree[self.num + i] = init_val[i]
+        # 構築していく
+        for i in range(self.num - 1, 0, -1):
+            self.tree[i] = self.segfunc(self.tree[2 * i], self.tree[2 * i + 1])
+    def update(self, k, x):
+        k += self.num
+        self.tree[k] = x
+        while k > 1:
+            self.tree[k >> 1] = self.segfunc(self.tree[k], self.tree[k ^ 1])
+            k >>= 1
+    def query(self, l, r):
+        res = self.ide_ele
+        l += self.num
+        r += self.num
+        while l < r:
+            if l & 1:
+                res = self.segfunc(res, self.tree[l])
+                l += 1
+            if r & 1:
+                res = self.segfunc(res, self.tree[r - 1])
+            l >>= 1
+            r >>= 1
+        return res
+N, M = getNM()
+seg = SegTree([float('inf')] * (M + 1), segfunc, ide_ele)
+L = [getList() for i in range(N)]
+L.sort()
+seg.update(0, 0)
+# [0, 1, 2, 3, 4, 5]
+# seg.query(0, 2): [0, 1]の最小値
+# seg.query(2, 2 + 1): [2]の最小値
+# seg.update(2, min(vs, opt + c)): 2をmin(vs, opt + c)に更新
+for l, r, c in L:
+    opt = seg.query(l, r)
+    vs = seg.query(r, r + 1)
+    seg.update(r, min(vs, opt + c))
+print(seg.query(M, M + 1))
 
-    # pqの先頭がgoal行きのものなら最短距離を返す
-    while len(pq) > 0:
-        d, now = heappop(pq)
-        if (d > dist[now]):
-            continue
-        for i in edges[now]:
-            if dist[i[0]] > dist[now] + i[1]:
-                dist[i[0]] = dist[now] + i[1]
-                heappush(pq, (dist[i[0]], i[0]))
-    return dist
+#####segfunc#####
+def segfunc(x, y):
+    return x * y
+#################
+#####ide_ele#####
+ide_ele = 1
+#################
+class SegTree:
+    """
+    init(init_val, ide_ele): 配列init_valで初期化 O(N)
+    update(k, x): k番目の値をxに更新 O(logN)
+    query(l, r): 区間[l, r)をsegfuncしたものを返す O(logN)
+    """
+    def __init__(self, init_val, segfunc, ide_ele):
+        """
+        init_val: 配列の初期値
+        segfunc: 区間にしたい操作
+        ide_ele: 単位元
+        n: 要素数
+        num: n以上の最小の2のべき乗
+        tree: セグメント木(1-index)
+        """
+        n = len(init_val)
+        self.segfunc = segfunc
+        self.ide_ele = ide_ele
+        self.num = 1 << (n - 1).bit_length()
+        self.tree = [ide_ele] * 2 * self.num
+        # 配列の値を葉にセット
+        for i in range(n):
+            self.tree[self.num + i] = init_val[i]
+        # 構築していく
+        for i in range(self.num - 1, 0, -1):
+            self.tree[i] = self.segfunc(self.tree[2 * i], self.tree[2 * i + 1])
+    def update(self, k, x):
+        """
+        k番目の値をxに更新
+        k: index(0-index)
+        x: update value
+        """
+        k += self.num
+        self.tree[k] = x
+        while k > 1:
+            self.tree[k >> 1] = self.segfunc(self.tree[k], self.tree[k ^ 1])
+            k >>= 1
+    def query(self, l, r):
+        """
+        [l, r)のsegfuncしたものを得る
+        l: index(0-index)
+        r: index(0-index)
+        """
+        res = self.ide_ele
+        l += self.num
+        r += self.num
+        while l < r:
+            if l & 1:
+                res = self.segfunc(res, self.tree[l])
+                l += 1
+            if r & 1:
+                res = self.segfunc(res, self.tree[r - 1])
+            l >>= 1
+            r >>= 1
+        return res
+
+#ABC157 E - Simple String Queries
+N = 7
+s = 'abcdbbd'
+Q = 6
+query = [
+[2, 3, 6],
+[1, 5, 'z'],
+[2, 1, 1],
+[1, 4, 'a'],
+[1, 7, 'd'],
+[2, 1, 7]
+]
+S = []
+for i in s:
+    # 面倒なので文字を数値化
+	S.append(ord(i) - ord("a"))
+seg = [SegTree([1] * N, segfunc, ide_ele) for _ in range(26)]
+# 入力
+for i in range(N):
+	seg[S[i]].update(i, 0)
+for i in range(Q):
+    a, b, c = query[i]
+    if int(a) == 1:
+        b = int(b) - 1
+        # Sのb番目にある文字をupdate
+        seg[S[b]].update(b, 1)
+        t = ord(c) - ord("a")
+        seg[t].update(b, 0)
+        S[b] = t
+    else:
+        b = int(b) - 1
+        c = int(c)
+        cnt = 0
+        for se in seg:
+            # 1 * 1 * 0 * 1 *...
+            # 区間内に一つでも0があれば0
+            if se.query(b, c) == 0:
+                cnt += 1
+        print(cnt)
+
+
+def segfunc(x, y):
+    return min(x, y)
+
+ide_ele = float('inf')
+
+class SegTree:
+    def __init__(self, init_val, segfunc, ide_ele):
+        n = len(init_val)
+        self.segfunc = segfunc
+        self.ide_ele = ide_ele
+        self.num = 1 << (n - 1).bit_length()
+        self.tree = [ide_ele] * 2 * self.num
+        # 配列の値を葉にセット
+        for i in range(n):
+            self.tree[self.num + i] = init_val[i]
+        # 構築していく
+        for i in range(self.num - 1, 0, -1):
+            self.tree[i] = self.segfunc(self.tree[2 * i], self.tree[2 * i + 1])
+
+    def update(self, k, x):
+        k += self.num
+        self.tree[k] = x
+        while k > 1:
+            self.tree[k >> 1] = self.segfunc(self.tree[k], self.tree[k ^ 1])
+            k >>= 1
+
+    def query(self, l, r):
+        res = self.ide_ele
+
+        l += self.num
+        r += self.num
+        while l < r:
+            if l & 1:
+                res = self.segfunc(res, self.tree[l])
+                l += 1
+            if r & 1:
+                res = self.segfunc(res, self.tree[r - 1])
+            l >>= 1
+            r >>= 1
+        return res
+
+# ABC146 F - Sugoroku
+# 最短手数k回でクリアできるとすると、
+# 1 ~ M　の内１つをk回選んで合計をNにする
+N, M = getNM()
+S = input()
+trap = set()
+for i in range(len(S)):
+    if S[i] == '1':
+        trap.add(i)
+
+# これABC011 123引き算と同じでは
+
+# 案1 dpを使う
+# dp[i]: iマスに止まる時の最短手順
+# dp[i]の時 dp[i + 1] ~ dp[i + M]についてmin(dp[i] + 1, dp[i + j])を見ていく
+# 決まったらdpを前から見ていき最短手順がdp[i] - 1になるものを探す（辞書順）
+# → M <= 10 ** 5より多分無理
+
+# セグ木使えばいける？
+# dp[i] = dp[i - M] ~ dp[i - 1]の最小値 + 1
+# dp[i - M] ~ dp[i - 1]の最小値はlogNで求められるので全体でNlogN
+
+dp = [float('inf')] * (N + 1)
+dp[0] = 0
+seg = SegTree([float('inf')] * (N + 1), segfunc, ide_ele)
+seg.update(0, 0)
+
+# dp[i]をレコード
+for i in range(1, N + 1):
+    # もしドボンマスなら飛ばす（float('inf')のまま）
+    if i in trap:
+        continue
+    # dp[i - M] ~ dp[i - 1]の最小値をサーチ
+    min_t = seg.query(max(0, i - M), i)
+    seg.update(i, min_t + 1)
+    dp[i] = min_t + 1
+
+# goalに到達できないなら
+if dp[-1] == float('inf'):
+    print(-1)
+    exit()
+
+# 何回の試行で到達できるかをグルーピング
+dis = [[] for i in range(dp[-1] + 1)]
+for i in range(len(dp)):
+    if dp[i] == float('inf'):
+        continue
+    dis[dp[i]].append(i)
+
+# ゴールから巻き戻っていく
+now = dp[-1]
+now_index = N
+ans = []
+# 辞書順で1 4 4 < 3 3 3なので
+# 一番前にできるだけ小さい数が来るようにする
+for i in range(now, 0, -1):
+    # dp[i] - 1回で到達できる
+    # 現在地点からMマス以内
+    # で最も現在地点から遠いところが１つ前のマス
+    index = bisect_left(dis[i - 1], now_index - M)
+    # サイコロの目を決める
+    ans.append(now_index - dis[i - 1][index])
+    # 現在地点更新
+    now_index = dis[i - 1][index]
+
+for i in ans[::-1]:
+    print(i)
+
+# ARC026 C - 蛍光灯
+# 範囲全体を照らすのに必要な最小値
+N, M = getNM()
+seg = SegTree([float('inf')] * (M + 1), segfunc, ide_ele)
+L = [getList() for i in range(N)]
+L.sort()
+seg.update(0, 0)
+
+# [0, 1, 2, 3, 4, 5]
+# seg.query(0, 2): [0, 1]の最小値
+# seg.query(2, 2 + 1): [2]の最小値
+# seg.update(2, min(vs, opt + c)): 2をmin(vs, opt + c)に更新
+for l, r, c in L:
+    opt = seg.query(l, r)
+    vs = seg.query(r, r + 1)
+    seg.update(r, min(vs, opt + c))
+print(seg.query(M, M + 1))
+
+# codeforces round731 F. Array Stabilization (GCD version)
+
+#####segfunc#####
+def segfunc(x, y):
+    return math.gcd(x, y)
+#################
+
+# gcdの逆元は0
+#####ide_ele#####
+ide_ele = 0
+#################
+
+# gcdする
+# gcd(a1, a2), gcd(a2, a3)... 輪っかになっている
+
+# だんだんgcdが1とかにならされて行くのでは？
+# 操作回数の最小を求める
+# 連続部分列Kについていずれの列でもgcdが等しくなる
+# gcdはモノイドだぞ セグ木使えば
+
+T = getN()
+for _ in range(T):
+    N = getN()
+    A = getList()
+    A += A
+
+    def f(x):
+        seg = SegTree(A, segfunc, ide_ele) # セグ木立てる
+        res = [seg.query(i, i + x) for i in range(N)]
+        return all([res[i] == res[0] for i in range(N)])
+
+    ok = N + 1
+    ng = 0
+
+    while abs(ok - ng) > 1:
+        mid = (ok + ng) // 2
+        if f(mid):
+            ok = mid
+        else:
+            ng = mid
+
+    print(ok - 1)
+
+# codeforces 736
+# D. Integers Have Friends
+# セグ木 + 尺取り
+
+T = getN()
+for _ in range(T):
+    N = getN()
+    psu = getList()
+    A = [abs(psu[i] - psu[i + 1]) for i in range(N - 1)]
+    seg = SegTree(A, segfunc, ide_ele)
+    if N == 1:
+        print(1)
+        continue
+
+    l, ans = 0, 0
+    for r in range(N - 1):
+        while seg.query(l, r + 1) == 1:
+            l += 1
+        ans = max(ans, r - l + 2)
+    print(ans)
+
+# PG battle2019 みんみんみん
+# xi < x, yi < y, zi < zなる点があるかどうか
 
 """
-どのタイミングで出発すればいいか
-混雑度は線形
-拡張ダイクストラでやる
-何秒か待って出発する
-ダメならtを進めればいい
-時刻Tが一定なので...
-現在の時刻で通過できる場合と出来ない場合を考える
-現在通過出来ない場合は次t = Di + T - Kの時に通過できる
+2つで考えると
+まずはxでソートする
+yの最小値を記録しておく　自身より小さいものがあるか
+前のいくつかについての最小値を求める
+
+２つの場合　最小値しか保持しなくていい　それぞれのyの値についてzの最小値を保持しておけば
+セグ木で区間の最小値を計算できるので...
 """
 
-N, M, T, K = getNM()
-E = [[] for i in range(N)]
-for i in range(M):
-    a, b, c, d = getNM()
-    E[a - 1].append([b - 1, c, d])
-    E[b - 1].append([a - 1, c, d])
+N = getN()
+P = []
+code_y = set()
+code_y.add(-inf)
+for i in range(N):
+    # yだけ圧縮する
+    x, y, z = getNM()
+    code_y.add(y)
+    P.append([x, y, z, i])
 
-def dij(start, edges):
-    dist = [float('inf') for i in range(N)]
-    dist[start] = 0
-    pq = [(0, start)]
+# 座圧
+alt = {}
+code_y = sorted(list(code_y))
+for i in range(len(code_y)):
+    alt[code_y[i]] = i
 
-    # pqの先頭がgoal行きのものなら最短距離を返す
-    while len(pq) > 0:
-        now, u = heappop(pq)
-        if (now > dist[u]):
-            continue
-        for v, c, d in edges[u]:
-            dep = dist[u] # 現在時刻を記録
-            # 現在時刻で通過できない場合は出発時刻をを上書き
-            if d - abs(T - now) >= K:
-                dep = d + T - K
-            if dist[v] > dep + c:
-                dist[v] = dep + c
-                heappush(pq, (dist[v], v))
-    return dist
+P.sort(reverse = True)
+seg = SegTree([ide_ele] * (len(code_y) + 1), segfunc, ide_ele)
 
-print(dij(0, E))
+ans = ['No'] * N
+while P:
+    now = P[-1][0]
+    l = [] # あとで置く
+    while P and P[-1][0] <= now:
+        _, y, z, index = P.pop()
+        if z > seg.query(0, alt[y]): # x未満 y未満の点についてのzの最小値
+            ans[index] = 'Yes'
+        l.append([y, z])
+    for y, z in l:
+        # 更新
+        if seg.query(alt[y], alt[y] + 1) > z:
+            seg.update(alt[y], z)
+
+print(ans)
