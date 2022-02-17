6d9c14d3a88ad655f969b464c638c788c45822ff
customaddone <custom1485@gmail.com>
2022-02-17 22:44:31 +0900

    merge

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -32,52 +32,59 @@ dx = [1, 0, -1, 0]
 # Main Code #
 #############
 
-"""
-刻まれるとしんどい
-時間が来たら頂点iの人を頂点jに動かす
-この操作は2M回しかない
-頂点にいる人をいっぺんに動かせれば
-効率的な付け替え方法
+class Merge():
+    def __init__(self, n):
+        self.n = n # グループの個数
+        self.g = [set() for i in range(n)] # グループ
+        self.g_name = [i for i in range(n)] # グループiの表面上の名前
+        self.rev_na = [i for i in range(n)] # 表面上の名前がjのグループの真の名前は何か
+        self.child = {} # どの人が何処に属するか
 
-マージする
-辺を頂点にする
-"""
+    # 人cがiに加入したい
+    def add(self, c, i):
+        if c in self.child:
+            return
+        # 表面上の名前i→真の名前self.rev_na[i]
+        self.child[c] = self.rev_na[i]
+        self.g[self.rev_na[i]].add(c)
 
-N, M, Q = getNM()
-e_d = {}
-E = [] # イベント
-for i in range(M):
-    a, b, s, t = getNM()
-    e_d[i + N] = (a, b)
-    E.append([s + 0.5, a - 1, i + N])
-    E.append([t + 0.5, i + N, b - 1])
+    # 人cを退場させる
+    def del(self, c):
+        if not c in self.child:
+            return
+        self.g[self.child[c]].remove(c)
+        del self.child[c]
 
-for i in range(Q):
-    x, y, z = getNM()
-    # start
-    E.append([y, -1, x - 1, i])
-    # goal
-    E.append([z, -2, x - 1, i])
+    # uの人全員をvに移動させる
+    def move(self, u, v):
+        if u == v:
+            return
+        t_u, t_v = self.rev_na[u], self.rev_na[v]
+        # 普通に移動させるだけ
+        if len(t_u) <= len(t_v):
+            # 所属変更
+            for c in self.g[t_u]:
+                self.child[c] = t_v
+            # 人の移動
+            self.g[t_v] |= self.g[t_u]
+            # 空にする
+            self.g[t_u] = set()
+        # 付け替えが発生する
+        else:
+            # 所属変更
+            for c in self.g[t_v]:
+                self.child[c] = t_u
+            # 人の移動
+            self.g[t_u] |= self.g[t_v]
+            self.g[t_v] = set()
+            # 表面上の名前の交換
+            self.g_name[t_u], self.g_name[t_v] = self.g_name[t_v], self.g_name[t_u]
+            self.rev_na[u], self.rev_na[v] = self.rev_na[v], self.rev_na[u]
 
-E.sort()
-# グループiの名前は何か
-g_name = [i for i in range(N + M)]
-# グループiには誰が属するか
-g = [set() for i in range(N + M)]
-# 名前jのグループはどこにあるか
-rev_g = [i for i in range(N + M)]
-# どのグループに属するか
-bel = [-1 for i in range(N + M)]
-for e in E:
-    # start グループの名称はe[2], グループはrev_g[e[2]]
-    if e[1] == -1:
-        bel[e[3]] = g_name[rev_g[e[2]]]
-        g[rev_g[e[2]]] .add(i)
-    # goal
-    elif e[1] == -2:
-        bel[e[3]] = -1
-    else:
-        # グループ名称xからグループ名称yへと移動する
-        _, x, y = e
-        # そのまま移動させたらいい
-        if len(x) <= len(y):
+    # 人cは何処にいるか
+    def where(self, c):
+        return self.g_name[self.child[c]]
+
+    # グループiにいる人は誰か
+    def who(self, i):
+        return self.g[self.rev_na[i]]
