a2d99976a12429008b5ba11eba0a0d3e10865c65
customaddone <custom1485@gmail.com>
2022-02-19 13:06:50 +0900

    divide both

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -32,103 +32,64 @@ dx = [1, 0, -1, 0]
 # Main Code #
 #############
 
-class Merge():
-    def __init__(self, n):
-        self.n = n # グループの個数
-        self.g = [set() for i in range(n)] # グループ
-        self.g_name = [i for i in range(n)] # グループiの表面上の名前
-        self.rev_na = [i for i in range(n)] # 表面上の名前がjのグループの真の名前は何か
-        self.child = {} # どの人が何処に属するか
+from functools import lru_cache
 
-    # 人cがiに加入したい
-    def add(self, c, i):
-        if c in self.child:
-            print('hello')
-            return
-        # 表面上の名前i→真の名前self.rev_na[i]
-        self.child[c] = self.rev_na[i]
-        self.g[self.rev_na[i]].add(c)
+@lru_cache(None)
+def f(A, B):
+    """
+    A < x, y <= B であって、gcd(x,y) == 1 となるものを数える。
+    """
 
-    # 人cを退場させる
-    def delete(self, c):
-        if not c in self.child:
-            print('hello')
-            return
-        self.g[self.child[c]].remove(c)
-        del self.child[c]
+    res = (B - A)**2
+    """
+    ここから、 d = gcd(x,y) >= 2 となるものを引いていく。
+    これは、 a = A // d, b = B // d として、f(a, b) 通りである。
+    (a, b) が O(sqrt(B)) 通りしかないことから、f(A, B) はより小さな場合の
+    f(a, b) を O(sqrt(B)) 件使って計算できる。
+    """
+    R = B
+    while R > 1:
+        a, b = A // R, B // R
+        L = max(A // (a + 1), B // (b + 1))
+        res -= (R - L) * f(a, b)
+        R = L
+    return res
 
-    # uの人全員をvに移動させる
-    def move(self, u, v):
-        if u == v:
-            return
-        t_u, t_v = self.rev_na[u], self.rev_na[v]
-        # 普通に移動させるだけ
-        if len(self.g[t_u]) <= len(self.g[t_v]):
-            # 所属変更
-            for c in self.g[t_u]:
-                self.child[c] = t_v
-            # 人の移動
-            self.g[t_v] |= self.g[t_u]
-            # 空にする
-            self.g[t_u] = set()
-        # 付け替えが発生する
-        else:
-            # 所属変更
-            for c in self.g[t_v]:
-                self.child[c] = t_u
-            # 人の移動
-            self.g[t_u] |= self.g[t_v]
-            self.g[t_v] = set()
-            # 表面上の名前の交換
-            self.g_name[t_u], self.g_name[t_v] = self.g_name[t_v], self.g_name[t_u]
-            self.rev_na[u], self.rev_na[v] = self.rev_na[v], self.rev_na[u]
+def g(A, B):
+    """
+    A < x, y <= B であって、x | y となるものを数える。
+    x = d ごとに数える。
+    これは、 a = A // d, b = B // d として、b - a 通りであるから、f と同様に計算できる。
+    """
+    res = 0
+    R = B
+    while R:
+        a, b = A // R, B // R
+        L = max(A // (a + 1), B // (b + 1))
+        cnt = max(0, min(R, B) - max(L, A))
+        res += cnt * (b - a)
+        R = L
+    return res
 
-    # 人cは何処にいるか
-    def where(self, c):
-        return self.g_name[self.child[c]]
+def main(A, B):
+    """
+    半開区間　(A, B] として持つ。
+    """
+    A -= 1
+    """
+    g == 1, g == x, g == y という 3 条件で包除
+    """
+    ___ = (B - A)**2
+    o__ = f(A, B)
+    _o_ = g(A, B)
+    __o = g(A, B)
+    oo_ = B - A if A == 0 else 0
+    o_o = B - A if A == 0 else 0
+    _oo = B - A
+    ooo = 1 if A == 0 else 0
+    return ___ - o__ - _o_ - __o + oo_ + o_o + _oo - ooo
 
-    # グループiにいる人は誰か
-    def who(self, i):
-        return self.g[self.rev_na[i]]
+# main(3, 7), main(4,10), main(1, 10**6)
 
-N, M, Q = getNM()
-e_d = {}
-E = [] # イベント
-for i in range(M):
-    a, b, s, t = getNM()
-    e_d[i + N] = (a, b)
-    E.append([s + 0.5, 1, a - 1, i + N])
-    # こっちが先
-    E.append([t + 0.5, 0, i + N, b - 1])
-
-for i in range(Q):
-    x, y, z = getNM()
-    # start
-    E.append([x, -1, y - 1, i])
-    # goal
-    E.append([z, -2, 0, i])
-
-E.sort(key = lambda i:i[1])
-E.sort(key = lambda i:i[0])
-
-G = Merge(N + M)
-ans = [0] * Q
-for e in E:
-    if e[1] < 0:
-        _, d, x, c = e
-        # add
-        if d == -1:
-            G.add(c, x)
-        else:
-            ans[c] = G.where(c)
-            G.delete(c)
-    else:
-        _, _, x, y = e
-        G.move(x, y)
-    # print(e, G.child, G.g, G.g_name, G.rev_na)
-
-for q in ans:
-    if q < N:
-        print(q + 1)
-    else:
-        print(*e_d[q])
+A, B = map(int, input().split())
+print(main(A, B))
